c240e92b834018b47f4cbef211ff0a3bba11282f
Dimitri <ddiakopoulos@gmail.com>
2015-08-24 18:58:57 -0700

    Support IVCam for depth+color again

--- src/F200/F200.cpp
+++ src/F200/F200.cpp
@@ -11,7 +11,7 @@ namespace f200
     
     F200Camera::F200Camera(uvc_context_t * ctx, uvc_device_t * device) : UVCCamera(ctx, device)
     {
-        
+        subdevices.resize(2);
     }
 
     F200Camera::~F200Camera()
@@ -19,17 +19,7 @@ namespace f200
         
     }
 
-    /*int F200Camera::GetStreamSubdeviceNumber(int stream) const
-    {
-        switch(stream)
-        {
-        case RS_COLOR: return 0;
-        case RS_DEPTH: return 1;
-        default: throw std::runtime_error("invalid stream");
-        }
-    }*/
-/*
-    static ResolutionMode MakeDepthMode(const CameraCalibrationParameters & c, int w, int h)
+    static SubdeviceMode MakeDepthMode(const CameraCalibrationParameters & c, int w, int h)
     {
         rs_intrinsics intrin = {{w,h}};
         intrin.focal_length[0] = c.Kc[0][0]*0.5f * w;
@@ -38,10 +28,10 @@ namespace f200
         intrin.principal_point[1] = (c.Kc[1][2]*0.5f + 0.5f) * h;
         for(int i=0; i<5; ++i) intrin.distortion_coeff[i] = c.Invdistc[i];
         intrin.distortion_model = RS_INVERSE_BROWN_CONRADY_DISTORTION;
-        return {RS_DEPTH, w,h,60,RS_Z16, w,h,60,UVC_FRAME_FORMAT_INVR, intrin};
+        return {1, w, h, UVC_FRAME_FORMAT_INVR, 60, {{RS_DEPTH, intrin, RS_Z16, 60}}, &rs::unpack_strided_image};
     }
 
-    static ResolutionMode MakeColorMode(const CameraCalibrationParameters & c, int w, int h)
+    static SubdeviceMode MakeColorMode(const CameraCalibrationParameters & c, int w, int h)
     {
         rs_intrinsics intrin = {{w,h}};
         intrin.focal_length[0] = c.Kt[0][0]*0.5f;
@@ -59,18 +49,18 @@ namespace f200
         intrin.principal_point[0] *= w;
         intrin.principal_point[1] *= h;
         intrin.distortion_model = RS_NO_DISTORTION;
-        return {RS_COLOR, w,h,60,RS_RGB8, w,h,60,UVC_FRAME_FORMAT_YUYV, intrin};
+        return {0, w, h, UVC_FRAME_FORMAT_YUYV, 60, {{RS_COLOR, intrin, RS_RGB8, 60}}, &rs::unpack_yuyv_to_rgb};
     }
-*/
+
     CalibrationInfo F200Camera::RetrieveCalibration()
     {
         if(!hardware_io) hardware_io.reset(new IVCAMHardwareIO(context));
         const CameraCalibrationParameters & calib = hardware_io->GetParameters();
 
         rs::CalibrationInfo c;
-        /*c.modes.push_back(MakeDepthMode(calib, 640, 480));
+        c.modes.push_back(MakeDepthMode(calib, 640, 480));
         c.modes.push_back(MakeColorMode(calib, 1920, 1080));
-        c.modes.push_back(MakeColorMode(calib, 640, 480));*/
+        c.modes.push_back(MakeColorMode(calib, 640, 480));
         c.stream_poses[RS_DEPTH] = {{{1,0,0},{0,1,0},{0,0,1}}, {0,0,0}};
         c.stream_poses[RS_COLOR] = {transpose((const float3x3 &)calib.Rt), (const float3 &)calib.Tt * 0.001f}; // convert mm to m
         c.depth_scale = (calib.Rmax / 0xFFFF) * 0.001f; // convert mm to m
