1813b10f5e082405727d88d2f47a4585ba897cfe
Sterling Orsten <sterling.g.orsten@intel.com>
2015-09-04 17:06:00 -0700

    gcc doesn't like predeclared enums

--- include/librealsense/rs.h
+++ include/librealsense/rs.h
@@ -7,17 +7,88 @@ extern "C" {
     
 #define RS_API_VERSION 3
 
+/* public enum type definitions */
+typedef enum rs_stream
+{
+    RS_STREAM_DEPTH                         = 0,
+    RS_STREAM_COLOR                         = 1,
+    RS_STREAM_INFRARED                      = 2,
+    RS_STREAM_INFRARED_2                    = 3,
+    RS_STREAM_COUNT                         = 4,
+    RS_STREAM_MAX_ENUM                      = 0x7FFFFFFF
+} rs_stream;
+
+enum
+{
+    RS_STREAM_DEPTH_BIT                     = 1 << RS_STREAM_DEPTH,
+    RS_STREAM_COLOR_BIT                     = 1 << RS_STREAM_COLOR,
+    RS_STREAM_INFRARED_BIT                  = 1 << RS_STREAM_INFRARED,
+    RS_STREAM_INFRARED_2_BIT                = 1 << RS_STREAM_INFRARED_2,
+    RS_ALL_STREAM_BITS                      = (1 << RS_STREAM_COUNT) - 1
+};
+
+typedef enum rs_format
+{
+    RS_FORMAT_ANY                           = 0,
+    RS_FORMAT_Z16                           = 1,
+    RS_FORMAT_YUYV                          = 2,
+    RS_FORMAT_RGB8                          = 3,
+    RS_FORMAT_BGR8                          = 4,
+    RS_FORMAT_RGBA8                         = 5,
+    RS_FORMAT_BGRA8                         = 6,
+    RS_FORMAT_Y8                            = 7,
+    RS_FORMAT_Y16                           = 8,
+    RS_FORMAT_COUNT                         = 9,
+    RS_FORMAT_MAX_ENUM                      = 0x7FFFFFFF
+} rs_format;
+
+typedef enum rs_preset
+{
+    RS_PRESET_BEST_QUALITY                  = 0,
+    RS_PRESET_LARGEST_IMAGE                 = 1,
+    RS_PRESET_HIGHEST_FRAMERATE             = 2,
+    RS_PRESET_COUNT                         = 9,
+    RS_PRESET_MAX_ENUM                      = 0x7FFFFFFF
+} rs_preset;
+
+typedef enum rs_distortion
+{
+    RS_DISTORTION_NONE                      = 0, /* Rectilinear images, no distortion compensation required */
+    RS_DISTORTION_MODIFIED_BROWN_CONRADY    = 1, /* Equivalent to Brown-Conrady distortion, except that tangential distortion is applied to radially distorted points */
+    RS_DISTORTION_INVERSE_BROWN_CONRADY     = 2, /* Equivalent to Brown-Conrady distortion, except undistorts image instead of distorting it */
+    RS_DISTORTION_COUNT                     = 3,
+    RS_DISTORTION_MAX_ENUM                  = 0x7FFFFFFF
+} rs_distortion;
+
+typedef enum rs_option
+{
+    RS_OPTION_F200_LASER_POWER              = 0, /* 0 - 15 */
+    RS_OPTION_F200_ACCURACY                 = 1, /* 0 - 3 */
+    RS_OPTION_F200_MOTION_RANGE             = 2, /* 0 - 100 */
+    RS_OPTION_F200_FILTER_OPTION            = 3, /* 0 - 7 */
+    RS_OPTION_F200_CONFIDENCE_THRESHOLD     = 4, /* 0 - 15 */
+    RS_OPTION_F200_DYNAMIC_FPS              = 5, /* {2, 5, 15, 30, 60} */
+    RS_OPTION_R200_LR_AUTO_EXPOSURE_ENABLED = 6, /* {0, 1} */
+    RS_OPTION_R200_LR_GAIN                  = 7, /* 100 - 1600 (Units of 0.01) */
+    RS_OPTION_R200_LR_EXPOSURE              = 8, /* > 0 (Units of 0.1 ms) */
+    RS_OPTION_R200_EMITTER_ENABLED          = 9, /* {0, 1} */
+    RS_OPTION_R200_DEPTH_CONTROL_PRESET     = 10, /* {0, 5}, 0 is default, 1-5 is low to high outlier rejection */
+    RS_OPTION_R200_DEPTH_UNITS              = 11, /* > 0 */
+    RS_OPTION_R200_DEPTH_CLAMP_MIN          = 12, /* 0 - USHORT_MAX */
+    RS_OPTION_R200_DEPTH_CLAMP_MAX          = 13, /* 0 - USHORT_MAX */
+    RS_OPTION_R200_DISPARITY_MODE_ENABLED   = 14, /* {0, 1} */
+    RS_OPTION_R200_DISPARITY_MULTIPLIER     = 15,
+    RS_OPTION_R200_DISPARITY_SHIFT          = 16,
+    RS_OPTION_COUNT                         = 17,
+    RS_OPTION_MAX_ENUM                      = 0x7FFFFFFF
+} rs_option;
+
 /* public type definitions */
 typedef struct rs_context rs_context;
 typedef struct rs_device rs_device;
 typedef struct rs_error rs_error;
 typedef struct rs_intrinsics rs_intrinsics;
 typedef struct rs_extrinsics rs_extrinsics;
-typedef enum rs_stream rs_stream;
-typedef enum rs_format rs_format;
-typedef enum rs_preset rs_preset;
-typedef enum rs_distortion rs_distortion;
-typedef enum rs_option rs_option;
 
 /* public function declarations */
 rs_context *    rs_create_context           (int api_version, rs_error ** error);
@@ -57,82 +128,6 @@ const char *    rs_format_to_string         (rs_format format);
 const char *    rs_preset_to_string         (rs_preset preset);
 const char *    rs_distortion_to_string     (rs_distortion distortion);
 const char *    rs_option_to_string         (rs_option option);
-
-/* public enum type definitions */
-enum rs_stream
-{
-    RS_STREAM_DEPTH                         = 0,
-    RS_STREAM_COLOR                         = 1,
-    RS_STREAM_INFRARED                      = 2,
-    RS_STREAM_INFRARED_2                    = 3,
-    RS_STREAM_COUNT                         = 4,
-    RS_STREAM_MAX_ENUM                      = 0x7FFFFFFF
-};
-
-enum
-{
-    RS_STREAM_DEPTH_BIT                     = 1 << RS_STREAM_DEPTH,
-    RS_STREAM_COLOR_BIT                     = 1 << RS_STREAM_COLOR,
-    RS_STREAM_INFRARED_BIT                  = 1 << RS_STREAM_INFRARED,
-    RS_STREAM_INFRARED_2_BIT                = 1 << RS_STREAM_INFRARED_2,
-    RS_ALL_STREAM_BITS                      = (1 << RS_STREAM_COUNT) - 1
-};
-
-enum rs_format
-{
-    RS_FORMAT_ANY                           = 0,
-    RS_FORMAT_Z16                           = 1,
-    RS_FORMAT_YUYV                          = 2,
-    RS_FORMAT_RGB8                          = 3,
-    RS_FORMAT_BGR8                          = 4,
-    RS_FORMAT_RGBA8                         = 5,
-    RS_FORMAT_BGRA8                         = 6,
-    RS_FORMAT_Y8                            = 7,
-    RS_FORMAT_Y16                           = 8,
-    RS_FORMAT_COUNT                         = 9,
-    RS_FORMAT_MAX_ENUM                      = 0x7FFFFFFF
-};
-
-enum rs_preset
-{
-    RS_PRESET_BEST_QUALITY                  = 0,
-    RS_PRESET_LARGEST_IMAGE                 = 1,
-    RS_PRESET_HIGHEST_FRAMERATE             = 2,
-    RS_PRESET_COUNT                         = 9,
-    RS_PRESET_MAX_ENUM                      = 0x7FFFFFFF
-};
-
-enum rs_distortion
-{
-    RS_DISTORTION_NONE                      = 0, /* Rectilinear images, no distortion compensation required */
-    RS_DISTORTION_MODIFIED_BROWN_CONRADY    = 1, /* Equivalent to Brown-Conrady distortion, except that tangential distortion is applied to radially distorted points */
-    RS_DISTORTION_INVERSE_BROWN_CONRADY     = 2, /* Equivalent to Brown-Conrady distortion, except undistorts image instead of distorting it */
-    RS_DISTORTION_COUNT                     = 3,
-    RS_DISTORTION_MAX_ENUM                  = 0x7FFFFFFF
-};
-
-enum rs_option
-{
-    RS_OPTION_F200_LASER_POWER              = 0, /* 0 - 15 */
-    RS_OPTION_F200_ACCURACY                 = 1, /* 0 - 3 */
-    RS_OPTION_F200_MOTION_RANGE             = 2, /* 0 - 100 */
-    RS_OPTION_F200_FILTER_OPTION            = 3, /* 0 - 7 */
-    RS_OPTION_F200_CONFIDENCE_THRESHOLD     = 4, /* 0 - 15 */
-    RS_OPTION_F200_DYNAMIC_FPS              = 5, /* {2, 5, 15, 30, 60} */
-    RS_OPTION_R200_LR_AUTO_EXPOSURE_ENABLED = 6, /* {0, 1} */
-    RS_OPTION_R200_LR_GAIN                  = 7, /* 100 - 1600 (Units of 0.01) */
-    RS_OPTION_R200_LR_EXPOSURE              = 8, /* > 0 (Units of 0.1 ms) */
-    RS_OPTION_R200_EMITTER_ENABLED          = 9, /* {0, 1} */
-    RS_OPTION_R200_DEPTH_CONTROL_PRESET     = 10, /* {0, 5}, 0 is default, 1-5 is low to high outlier rejection */
-    RS_OPTION_R200_DEPTH_UNITS              = 11, /* > 0 */
-    RS_OPTION_R200_DEPTH_CLAMP_MIN          = 12, /* 0 - USHORT_MAX */
-    RS_OPTION_R200_DEPTH_CLAMP_MAX          = 13, /* 0 - USHORT_MAX */
-    RS_OPTION_R200_DISPARITY_MODE_ENABLED   = 14, /* {0, 1} */
-    RS_OPTION_R200_DISPARITY_MULTIPLIER     = 15,
-    RS_OPTION_R200_DISPARITY_SHIFT          = 16,
-    RS_OPTION_COUNT                         = 17,
-    RS_OPTION_MAX_ENUM                      = 0x7FFFFFFF
-};
 
 /* public struct type definitions */
 struct rs_intrinsics
