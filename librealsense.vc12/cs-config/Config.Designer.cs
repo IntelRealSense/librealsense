2244afc27fd2d587f9821d7ab5630f810f4d5b13
Sterling Orsten <sterling.g.orsten@intel.com>
2015-09-11 15:27:55 -0700

    Visualize stream output in cs-config.

--- librealsense.vc12/cs-config/Config.Designer.cs
+++ librealsense.vc12/cs-config/Config.Designer.cs
@@ -28,53 +28,27 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.depthConfig = new cs_config.StreamConfig();
-            this.colorConfig = new cs_config.StreamConfig();
-            this.infraredConfig = new cs_config.StreamConfig();
-            this.infrared2Config = new cs_config.StreamConfig();
+            this.components = new System.ComponentModel.Container();
             this.button1 = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.infrared2Config = new cs_config.StreamConfig();
+            this.infraredConfig = new cs_config.StreamConfig();
+            this.colorConfig = new cs_config.StreamConfig();
+            this.depthConfig = new cs_config.StreamConfig();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
             this.SuspendLayout();
-            // 
-            // depthConfig
-            // 
-            this.depthConfig.AutoSize = true;
-            this.depthConfig.Location = new System.Drawing.Point(12, 12);
-            this.depthConfig.Name = "depthConfig";
-            this.depthConfig.Size = new System.Drawing.Size(197, 128);
-            this.depthConfig.TabIndex = 0;
-            this.depthConfig.Stream = RealSense.Stream.Depth;
-            // 
-            // colorConfig
-            // 
-            this.colorConfig.AutoSize = true;
-            this.colorConfig.Location = new System.Drawing.Point(215, 12);
-            this.colorConfig.Name = "colorConfig";
-            this.colorConfig.Size = new System.Drawing.Size(197, 128);
-            this.colorConfig.TabIndex = 1;
-            this.colorConfig.Stream = RealSense.Stream.Color;
-            // 
-            // infraredConfig
-            // 
-            this.infraredConfig.AutoSize = true;
-            this.infraredConfig.Location = new System.Drawing.Point(419, 12);
-            this.infraredConfig.Name = "infraredConfig";
-            this.infraredConfig.Size = new System.Drawing.Size(197, 128);
-            this.infraredConfig.TabIndex = 2;
-            this.infraredConfig.Stream = RealSense.Stream.Infrared;
-            // 
-            // infrared2Config
-            // 
-            this.infrared2Config.AutoSize = true;
-            this.infrared2Config.Location = new System.Drawing.Point(622, 12);
-            this.infrared2Config.Name = "infrared2Config";
-            this.infrared2Config.Size = new System.Drawing.Size(197, 128);
-            this.infrared2Config.TabIndex = 3;
-            this.infrared2Config.Stream = RealSense.Stream.Infrared2;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(12, 147);
+            this.button1.Location = new System.Drawing.Point(842, 23);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(110, 23);
             this.button1.TabIndex = 4;
@@ -85,17 +59,95 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(128, 152);
+            this.label1.Location = new System.Drawing.Point(849, 60);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(72, 13);
             this.label1.TabIndex = 5;
             this.label1.Text = "Not streaming";
             // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 146);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(640, 480);
+            this.pictureBox1.TabIndex = 6;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(672, 146);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(640, 480);
+            this.pictureBox2.TabIndex = 7;
+            this.pictureBox2.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 10;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // infrared2Config
+            // 
+            this.infrared2Config.AutoSize = true;
+            this.infrared2Config.Location = new System.Drawing.Point(622, 12);
+            this.infrared2Config.Name = "infrared2Config";
+            this.infrared2Config.Size = new System.Drawing.Size(197, 128);
+            this.infrared2Config.Stream = RealSense.Stream.Infrared2;
+            this.infrared2Config.TabIndex = 3;
+            // 
+            // infraredConfig
+            // 
+            this.infraredConfig.AutoSize = true;
+            this.infraredConfig.Location = new System.Drawing.Point(419, 12);
+            this.infraredConfig.Name = "infraredConfig";
+            this.infraredConfig.Size = new System.Drawing.Size(197, 128);
+            this.infraredConfig.Stream = RealSense.Stream.Infrared;
+            this.infraredConfig.TabIndex = 2;
+            // 
+            // colorConfig
+            // 
+            this.colorConfig.AutoSize = true;
+            this.colorConfig.Location = new System.Drawing.Point(215, 12);
+            this.colorConfig.Name = "colorConfig";
+            this.colorConfig.Size = new System.Drawing.Size(197, 128);
+            this.colorConfig.Stream = RealSense.Stream.Color;
+            this.colorConfig.TabIndex = 1;
+            // 
+            // depthConfig
+            // 
+            this.depthConfig.AutoSize = true;
+            this.depthConfig.Location = new System.Drawing.Point(12, 12);
+            this.depthConfig.Name = "depthConfig";
+            this.depthConfig.Size = new System.Drawing.Size(197, 128);
+            this.depthConfig.Stream = RealSense.Stream.Depth;
+            this.depthConfig.TabIndex = 0;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Location = new System.Drawing.Point(12, 646);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(640, 480);
+            this.pictureBox3.TabIndex = 8;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Location = new System.Drawing.Point(672, 646);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(640, 480);
+            this.pictureBox4.TabIndex = 9;
+            this.pictureBox4.TabStop = false;
+            // 
             // Config
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(825, 588);
+            this.ClientSize = new System.Drawing.Size(1323, 1137);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.infrared2Config);
@@ -104,6 +156,10 @@
             this.Controls.Add(this.depthConfig);
             this.Name = "Config";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -117,6 +173,11 @@
         private StreamConfig infrared2Config;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
     }
 }
 
--- librealsense.vc12/cs-config/Config.cs
+++ librealsense.vc12/cs-config/Config.cs
@@ -2,6 +2,9 @@
 using System.Linq;
 using System.Collections.Generic;
 using System.Windows.Forms;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Runtime.InteropServices;
 
 namespace cs_config
 {
@@ -52,8 +55,125 @@ namespace cs_config
             foreach (var stream in Streams) stream.Enabled = !device.IsStreaming;
         }
 
+        private void Reserve(ref byte[] buffer, int size)
+        {
+            if (buffer == null || buffer.Length < size) buffer = new byte[size];
+        }
+
+        private void LoadSource(int index, IntPtr data, int size)
+        {
+            Reserve(ref src[index], size);
+            Marshal.Copy(data, src[index], 0, size);
+        }
+
+        private Bitmap GetDest(int index, int width, int height)
+        {
+            GCHandle handle = GCHandle.Alloc(dst[index], GCHandleType.Pinned);
+            var bitmap = new Bitmap(width, height, width * 3, PixelFormat.Format24bppRgb, handle.AddrOfPinnedObject());
+            handle.Free();
+            return bitmap;
+        }
+
+        private Bitmap MakeZ16Bitmap(int index, int width, int height, IntPtr frame)
+        {
+            LoadSource(index, frame, width * height * 2);
+            Reserve(ref dst[index], width * height * 3);
+            for (int i = 0; i < width * height; i++)
+            {
+                int depth = src[index][i * 2] | (src[index][i * 2 + 1] << 8);
+                dst[index][i * 3 + 0] = 0;
+                dst[index][i * 3 + 1] = 0;
+                dst[index][i * 3 + 2] = (byte)(depth >> 2);
+            }
+            return GetDest(index, width, height);
+        }
+
+        private Bitmap MakeYUYVBitmap(int index, int width, int height, IntPtr frame)
+        {
+            LoadSource(index, frame, width * height * 2);
+            Reserve(ref dst[index], width * height * 3);
+            for (int i = 0; i < width * height; i++)
+            {
+                dst[index][i * 3 + 0] = src[index][i * 2];
+                dst[index][i * 3 + 1] = src[index][i * 2];
+                dst[index][i * 3 + 2] = src[index][i * 2];
+            }
+            return GetDest(index, width, height);
+        }
+
+        private Bitmap MakeY8Bitmap(int index, int width, int height, IntPtr frame)
+        {
+            LoadSource(index, frame, width * height);
+            Reserve(ref dst[index], width * height * 3);
+            for (int i = 0; i < width * height; i++)
+            {
+                dst[index][i * 3 + 0] = src[index][i];
+                dst[index][i * 3 + 1] = src[index][i];
+                dst[index][i * 3 + 2] = src[index][i];
+            }
+            return GetDest(index, width, height);
+        }
+
+        private Bitmap MakeY16Bitmap(int index, int width, int height, IntPtr frame)
+        {
+            LoadSource(index, frame, width * height * 2);
+            Reserve(ref dst[index], width * height * 3);
+            for (int i = 0; i < width * height; i++)
+            {
+                dst[index][i * 3 + 0] = src[index][i * 2 + 1];
+                dst[index][i * 3 + 1] = src[index][i * 2 + 1];
+                dst[index][i * 3 + 2] = src[index][i * 2 + 1];
+            }
+            return GetDest(index, width, height);
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (device == null) return;
+            if (!device.IsStreaming) return;
+
+            timer1.Stop();
+            device.WaitForFrames();
+
+            var pictureBoxes = new[] { pictureBox1, pictureBox2, pictureBox3, pictureBox4 };
+            foreach(RealSense.Stream stream in Enum.GetValues(typeof(RealSense.Stream)))
+            {
+                if (device.StreamIsEnabled(stream))
+                {
+                    var intrin = device.GetStreamIntrinsics(stream);
+                    switch (device.GetStreamFormat(stream))
+                    {
+                    case RealSense.Format.Z16:
+                        pictureBoxes[(int)stream].Image = MakeZ16Bitmap((int)stream, intrin.Width, intrin.Height, device.GetFrameData(stream));
+                        break;
+                    case RealSense.Format.YUYV:
+                        pictureBoxes[(int)stream].Image = MakeYUYVBitmap((int)stream, intrin.Width, intrin.Height, device.GetFrameData(stream));
+                        break;
+                    case RealSense.Format.RGB8:
+                    case RealSense.Format.BGR8:
+                        pictureBoxes[(int)stream].Image = new Bitmap(intrin.Width, intrin.Height, intrin.Width * 3, PixelFormat.Format24bppRgb, device.GetFrameData(stream));
+                        break;
+                    case RealSense.Format.RGBA8:
+                    case RealSense.Format.BGRA8:
+                        pictureBoxes[(int)stream].Image = new Bitmap(intrin.Width, intrin.Height, intrin.Width * 4, PixelFormat.Format32bppRgb, device.GetFrameData(stream));
+                        break;
+                    case RealSense.Format.Y8:
+                        pictureBoxes[(int)stream].Image = MakeY8Bitmap((int)stream, intrin.Width, intrin.Height, device.GetFrameData(stream));
+                        break;
+                    case RealSense.Format.Y16:
+                        pictureBoxes[(int)stream].Image = MakeY16Bitmap((int)stream, intrin.Width, intrin.Height, device.GetFrameData(stream));
+                        break;
+                    }
+                }
+                else pictureBoxes[(int)stream].Image = null;
+            }
+
+            timer1.Start();
+        }
+
         private StreamConfig[] Streams { get { return new[] { depthConfig, colorConfig, infraredConfig, infrared2Config }; } }
 
         private RealSense.Device device;
+        private byte[][] src = new byte[4][], dst = new byte[4][];
     }
 }
