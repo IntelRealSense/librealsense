1a72c69ddb7425603f4ce2f15125eefefa1a34f7
Sterling Orsten <sterling.g.orsten@intel.com>
2015-09-14 16:46:34 -0700

    Modify capture sample to show both expected and actual framerate per image.

--- examples/cpp-capture.cpp
+++ examples/cpp-capture.cpp
@@ -1,65 +1,13 @@
 #include <librealsense/rs.hpp>
-#include "example.h"
+#include "example.hpp"
 
 #include <sstream>
 #include <iostream>
 #include <iomanip>
 #include <thread>
 
-GLuint tex;
 font font;
-
-void draw_stream(rs::device & dev, rs::stream stream, int rx, int ry, int rw, int rh)
-{
-    if(!dev.is_stream_enabled(stream)) return;
-
-    const rs::intrinsics intrin = dev.get_stream_intrinsics(stream);     
-    glBindTexture(GL_TEXTURE_2D, tex);
-    static uint8_t rgb_image[640*480*3];
-    switch(dev.get_stream_format(stream))
-    {
-    case rs::format::z16:
-        make_depth_histogram(rgb_image, reinterpret_cast<const uint16_t *>(dev.get_frame_data(stream)), intrin.width(), intrin.height());
-        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, intrin.width(), intrin.height(), 0, GL_RGB, GL_UNSIGNED_BYTE, rgb_image);
-        break;
-    case rs::format::yuyv: // Display YUYV by showing the luminance channel and packing chrominance into ignored alpha channel
-        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, intrin.width(), intrin.height(), 0, GL_LUMINANCE_ALPHA, GL_UNSIGNED_BYTE, dev.get_frame_data(stream)); 
-        break;
-    case rs::format::rgb8: case rs::format::bgr8: // Display both RGB and BGR by interpreting them RGB, to show the flipped byte ordering. Obviously, GL_BGR could be used on OpenGL 1.2+
-        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, intrin.width(), intrin.height(), 0, GL_RGB, GL_UNSIGNED_BYTE, dev.get_frame_data(stream));
-        break;
-    case rs::format::rgba8: case rs::format::bgra8: // Display both RGBA and BGRA by interpreting them RGBA, to show the flipped byte ordering. Obviously, GL_BGRA could be used on OpenGL 1.2+
-        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, intrin.width(), intrin.height(), 0, GL_RGBA, GL_UNSIGNED_BYTE, dev.get_frame_data(stream));
-        break;
-    case rs::format::y8:
-        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, intrin.width(), intrin.height(), 0, GL_LUMINANCE, GL_UNSIGNED_BYTE, dev.get_frame_data(stream));
-        break;
-    case rs::format::y16:
-        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, intrin.width(), intrin.height(), 0, GL_LUMINANCE, GL_UNSIGNED_SHORT, dev.get_frame_data(stream));
-        break;
-    }
-
-    float w = rw, h = rw * intrin.height() / intrin.width();
-    if(h > rh)
-    {
-        float scale = rh/h;
-        w *= scale;
-        h *= scale;
-    }
-    float x0 = rx + (rw - w)/2, x1 = x0 + w, y0 = ry + (rh - h)/2, y1 = y0 + h;
-
-    glEnable(GL_TEXTURE_2D);
-    glDisable(GL_BLEND);
-    glBegin(GL_QUADS);
-    glTexCoord2f(0,0); glVertex2f(x0,y0);
-    glTexCoord2f(1,0); glVertex2f(x1,y0);
-    glTexCoord2f(1,1); glVertex2f(x1,y1);
-    glTexCoord2f(0,1); glVertex2f(x0,y1);
-    glEnd();
-
-    std::ostringstream ss; ss << stream << ": " << intrin.width() << " x " << intrin.height() << " " << dev.get_stream_format(stream) << " (" << dev.get_stream_framerate(stream) << ")";
-    ttf_print(&font, rx+8, ry+16, ss.str().c_str());
-}
+texture_buffer buffers[RS_STREAM_COUNT];
 
 int main(int argc, char * argv[]) try
 {
@@ -68,7 +16,7 @@ int main(int argc, char * argv[]) try
 
     // Configure our device
     rs::device dev = ctx.get_device(0);
-    dev.enable_stream(rs::stream::color, rs::preset::largest_image);
+    dev.enable_stream(rs::stream::color, 640, 480, rs::format::rgb8, 30); //rs::preset::largest_image);
     dev.enable_stream(rs::stream::depth, rs::preset::best_quality);
     dev.enable_stream(rs::stream::infrared, 0, 0, rs::format::any, 0);
     try { dev.enable_stream(rs::stream::infrared2, 0, 0, rs::format::any, 0); } catch(...) {}
@@ -109,13 +57,7 @@ int main(int argc, char * argv[]) try
     std::ostringstream ss; ss << "CPP Capture Example (" << dev.get_name() << ")";
     GLFWwindow * win = glfwCreateWindow(1280, 960, ss.str().c_str(), 0, 0);
     glfwMakeContextCurrent(win);
-
-    glGenTextures(1, &tex);
-    glBindTexture(GL_TEXTURE_2D, tex);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-    glBindTexture(GL_TEXTURE_2D, 0);
-        
+            
     // Load our truetype font
     if (auto f = find_file("examples/assets/Roboto-Bold.ttf", 3))
     {
@@ -129,7 +71,6 @@ int main(int argc, char * argv[]) try
         // Wait for new images
         glfwPollEvents();
         dev.wait_for_frames();
-        std::cout << dev.get_frame_timestamp(rs::stream::depth) << " " << dev.get_frame_timestamp(rs::stream::color) << std::endl;
 
         // Clear the framebuffer
         int w,h;
@@ -141,10 +82,10 @@ int main(int argc, char * argv[]) try
         glPushMatrix();
         glfwGetWindowSize(win, &w, &h);
         glOrtho(0, w, h, 0, -1, +1);
-        draw_stream(dev, rs::stream::color, 0, 0, w/2, h/2);
-        draw_stream(dev, rs::stream::depth, w/2, 0, w-w/2, h/2);
-        draw_stream(dev, rs::stream::infrared, 0, h/2, w/2, h-h/2);
-        draw_stream(dev, rs::stream::infrared2, w/2, h/2, w-w/2, h-h/2);
+        buffers[0].show(dev, rs::stream::color, 0, 0, w/2, h/2, font);
+        buffers[1].show(dev, rs::stream::depth, w/2, 0, w-w/2, h/2, font);
+        buffers[2].show(dev, rs::stream::infrared, 0, h/2, w/2, h-h/2, font);
+        buffers[3].show(dev, rs::stream::infrared2, w/2, h/2, w-w/2, h-h/2, font);
         glPopMatrix();
         glfwSwapBuffers(win);
     }
---
+++ examples/example.hpp
@@ -0,0 +1,90 @@
+#include "example.h"
+
+#include <vector>
+#include <sstream>
+
+class texture_buffer
+{
+    GLuint texture;
+    int last_timestamp;
+    std::vector<uint8_t> rgb;
+
+    int fps, num_frames, next_time;
+public:
+    texture_buffer() : texture(), last_timestamp(-1), fps(), num_frames(), next_time(1000) {}
+
+    void show(rs::device & dev, rs::stream stream, int rx, int ry, int rw, int rh, font & font)
+    {
+        if(!dev.is_stream_enabled(stream)) return;
+
+        const rs::intrinsics intrin = dev.get_stream_intrinsics(stream);     
+        const int timestamp = dev.get_frame_timestamp(stream);
+
+        // If the frame timestamp has changed since the last time show(...) was called, re-upload the texture
+        if(!texture) glGenTextures(1, &texture);
+        glBindTexture(GL_TEXTURE_2D, texture);
+        if(timestamp != last_timestamp)
+        {
+            const void * data = dev.get_frame_data(stream);
+            switch(dev.get_stream_format(stream))
+            {
+            case rs::format::z16:
+                rgb.resize(intrin.width() * intrin.height() * 3);
+                make_depth_histogram(rgb.data(), reinterpret_cast<const uint16_t *>(dev.get_frame_data(stream)), intrin.width(), intrin.height());
+                glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, intrin.width(), intrin.height(), 0, GL_RGB, GL_UNSIGNED_BYTE, rgb.data());
+                break;
+            case rs::format::yuyv: // Display YUYV by showing the luminance channel and packing chrominance into ignored alpha channel
+                glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, intrin.width(), intrin.height(), 0, GL_LUMINANCE_ALPHA, GL_UNSIGNED_BYTE, data); 
+                break;
+            case rs::format::rgb8: case rs::format::bgr8: // Display both RGB and BGR by interpreting them RGB, to show the flipped byte ordering. Obviously, GL_BGR could be used on OpenGL 1.2+
+                glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, intrin.width(), intrin.height(), 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+                break;
+            case rs::format::rgba8: case rs::format::bgra8: // Display both RGBA and BGRA by interpreting them RGBA, to show the flipped byte ordering. Obviously, GL_BGRA could be used on OpenGL 1.2+
+                glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, intrin.width(), intrin.height(), 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
+                break;
+            case rs::format::y8:
+                glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, intrin.width(), intrin.height(), 0, GL_LUMINANCE, GL_UNSIGNED_BYTE, data);
+                break;
+            case rs::format::y16:
+                glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, intrin.width(), intrin.height(), 0, GL_LUMINANCE, GL_UNSIGNED_SHORT, data);
+                break;
+            }
+            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP);
+            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP);
+            last_timestamp = timestamp;
+
+            ++num_frames;
+            if(timestamp >= next_time)
+            {
+                fps = num_frames;
+                num_frames = 0;
+                next_time += 1000;
+            }
+        }
+        
+        float h = rh, w = rh * intrin.width() / intrin.height();
+        if(w > rw)
+        {
+            float scale = rw/w;
+            w *= scale;
+            h *= scale;
+        }
+        float x0 = rx + (rw - w)/2, x1 = x0 + w, y0 = ry + (rh - h)/2, y1 = y0 + h;
+
+        glEnable(GL_TEXTURE_2D);
+        glBegin(GL_QUADS);
+        glTexCoord2f(0,0); glVertex2f(x0,y0);
+        glTexCoord2f(1,0); glVertex2f(x1,y0);
+        glTexCoord2f(1,1); glVertex2f(x1,y1);
+        glTexCoord2f(0,1); glVertex2f(x0,y1);
+        glEnd();    
+        glDisable(GL_TEXTURE_2D);
+
+        glBindTexture(GL_TEXTURE_2D, 0);
+
+        std::ostringstream ss; ss << stream << ": " << intrin.width() << " x " << intrin.height() << " " << dev.get_stream_format(stream) << " (" << fps << "/" << dev.get_stream_framerate(stream) << ")";
+        ttf_print(&font, rx+8, ry+16, ss.str().c_str());
+    }
+};
