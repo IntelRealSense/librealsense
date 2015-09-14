bfe5125222caaed979a50dcf9f20bb28fd83a2c7
Sterling Orsten <sterling.g.orsten@intel.com>
2015-09-14 13:19:43 -0700

    Simplify YUY2 unpacking routines.

--- src/image.cpp
+++ src/image.cpp
@@ -1,7 +1,6 @@
 #include "image.h"
 #include <cstdint>
 #include <cstring>
-#include <algorithm>
 
 #pragma pack(push, 1) // All structs in this file are assumed to be byte-packed
 namespace rsimpl
@@ -30,16 +29,17 @@ namespace rsimpl
     // YUY2 unpacking routines //
     /////////////////////////////
 
-    template<class UNPACK> void unpack_from_yuy2(void * dest[], const void * source, const subdevice_mode & mode, UNPACK unpack)
+    template<class UNPACK> void unpack_from_yuy2(void * dest[], const void * source, const subdevice_mode & mode, rs_format format, UNPACK unpack)
     {
-        assert(mode.fourcc == 'YUY2' && mode.streams.size() == 1 && mode.streams[0].width == mode.width && mode.streams[0].height == mode.height);
+        assert(mode.fourcc == 'YUY2' && mode.streams.size() == 1 && mode.streams[0].width == mode.width && mode.streams[0].height == mode.height && mode.streams[0].format == format);
         auto in = reinterpret_cast<const uint8_t *>(source);
-        auto out = reinterpret_cast<uint8_t *>(dest[0]);        
-        for (int y = 0; y < mode.height; ++y)
+        auto out = reinterpret_cast<decltype(unpack(0,0,0)) *>(dest[0]);
+        for(int y = 0; y < mode.height; ++y)
         {
-            for (int x = 0; x < mode.width; x += 2)
+            for(int x = 0; x < mode.width; x += 2)
             {
-                unpack(out, in[0]-16, in[1]-128, in[2]-16, in[3]-128);
+                *out++ = unpack(in[0]-16, in[1]-128, in[3]-128); // y0, u, v
+                *out++ = unpack(in[2]-16, in[1]-128, in[3]-128); // y1, u, v
                 in += 4;
             }
         }     
@@ -50,69 +50,11 @@ namespace rsimpl
     inline uint8_t yuv_to_g(int y, int u, int v) { return clamp_byte((128 + 298 * y - 100 * u - 208 * v) >> 8); }
     inline uint8_t yuv_to_b(int y, int u, int v) { return clamp_byte((128 + 298 * y + 516 * u          ) >> 8); }
     
-    void unpack_rgb_from_yuy2(void * dest[], const void * source, const subdevice_mode & mode)
-    {
-        assert(mode.streams.size() == 1 && mode.streams[0].format == RS_FORMAT_RGB);
-        unpack_from_yuy2(dest, source, mode, [](uint8_t * & out, int y0, int u, int y1, int v)
-        {
-            *out++ = yuv_to_r(y0, u, v);
-            *out++ = yuv_to_g(y0, u, v);
-            *out++ = yuv_to_b(y0, u, v);
-
-            *out++ = yuv_to_r(y1, u, v);
-            *out++ = yuv_to_g(y1, u, v);
-            *out++ = yuv_to_b(y1, u, v);
-        });  
-    }
-    
-    void unpack_rgba_from_yuy2(void * dest[], const void * source, const subdevice_mode & mode)
-    {
-        assert(mode.streams.size() == 1 && mode.streams[0].format == RS_FORMAT_RGBA);
-        unpack_from_yuy2(dest, source, mode, [](uint8_t * & out, int y0, int u, int y1, int v)
-        {
-            *out++ = yuv_to_r(y0, u, v);
-            *out++ = yuv_to_g(y0, u, v);
-            *out++ = yuv_to_b(y0, u, v);
-            *out++ = 255;
-
-            *out++ = yuv_to_r(y1, u, v);
-            *out++ = yuv_to_g(y1, u, v);
-            *out++ = yuv_to_b(y1, u, v);
-            *out++ = 255;
-        });
-    }
-
-    void unpack_bgr_from_yuy2(void * dest[], const void * source, const subdevice_mode & mode)
-    {
-        assert(mode.streams.size() == 1 && mode.streams[0].format == RS_FORMAT_BGR);
-        unpack_from_yuy2(dest, source, mode, [](uint8_t * & out, int y0, int u, int y1, int v)
-        {
-            *out++ = yuv_to_b(y0, u, v);
-            *out++ = yuv_to_g(y0, u, v);
-            *out++ = yuv_to_r(y0, u, v);
-
-            *out++ = yuv_to_b(y1, u, v);
-            *out++ = yuv_to_g(y1, u, v);
-            *out++ = yuv_to_r(y1, u, v);
-        });    
-    }
-
-    void unpack_bgra_from_yuy2(void * dest[], const void * source, const subdevice_mode & mode)
-    {
-        assert(mode.streams.size() == 1 && mode.streams[0].format == RS_FORMAT_BGRA);
-        unpack_from_yuy2(dest, source, mode, [](uint8_t * & out, int y0, int u, int y1, int v)
-        {
-            *out++ = yuv_to_b(y0, u, v);
-            *out++ = yuv_to_g(y0, u, v);
-            *out++ = yuv_to_r(y0, u, v);
-            *out++ = 255;
-
-            *out++ = yuv_to_b(y1, u, v);
-            *out++ = yuv_to_g(y1, u, v);
-            *out++ = yuv_to_r(y1, u, v);
-            *out++ = 255;
-        });   
-    }
+    struct byte3 { uint8_t a,b,c; }; struct byte4 { uint8_t a,b,c,d; };
+    void unpack_rgb_from_yuy2 (void** d, const void* s, const subdevice_mode& m) { unpack_from_yuy2(d, s, m, RS_FORMAT_RGB8,  [](int y, int u, int v) { return byte3{yuv_to_r(y, u, v), yuv_to_g(y, u, v), yuv_to_b(y, u, v)     }; }); }
+    void unpack_rgba_from_yuy2(void** d, const void* s, const subdevice_mode& m) { unpack_from_yuy2(d, s, m, RS_FORMAT_RGBA8, [](int y, int u, int v) { return byte4{yuv_to_r(y, u, v), yuv_to_g(y, u, v), yuv_to_b(y, u, v), 255}; }); }
+    void unpack_bgr_from_yuy2 (void** d, const void* s, const subdevice_mode& m) { unpack_from_yuy2(d, s, m, RS_FORMAT_BGR8,  [](int y, int u, int v) { return byte3{yuv_to_b(y, u, v), yuv_to_g(y, u, v), yuv_to_r(y, u, v)     }; }); }
+    void unpack_bgra_from_yuy2(void** d, const void* s, const subdevice_mode& m) { unpack_from_yuy2(d, s, m, RS_FORMAT_BGRA8, [](int y, int u, int v) { return byte4{yuv_to_b(y, u, v), yuv_to_g(y, u, v), yuv_to_r(y, u, v), 255}; }); }
 
     //////////////////////////////////////
     // 2-in-1 format splitting routines //
@@ -123,9 +65,9 @@ namespace rsimpl
         assert(mode.streams.size() == 2 && mode.streams[0].width == mode.streams[1].width && mode.streams[0].height == mode.streams[1].height && mode.streams[0].width <= mode.width && mode.streams[0].height <= mode.height);
         auto a = reinterpret_cast<decltype(split_a(SOURCE())) *>(dest[0]);
         auto b = reinterpret_cast<decltype(split_b(SOURCE())) *>(dest[1]);
-        for(int y=0; y<mode.streams[0].height; ++y)
+        for(int y = 0; y < mode.streams[0].height; ++y)
         {
-            for(int x=0; x<mode.streams[0].width; ++x)
+            for(int x = 0; x < mode.streams[0].width; ++x)
             {
                 *a++ = split_a(*source);
                 *b++ = split_b(*source++);
