410196b70dc0b75a7542c2e268647fdb60b74313
Sterling Orsten <sterling.g.orsten@intel.com>
2015-10-08 15:58:28 -0700

    Able to differentiate between F200 and R200

--- src/hack/v4l2-test.cpp
+++ src/hack/v4l2-test.cpp
@@ -3,14 +3,17 @@
 #include <cstdio>
 #include <cstring>
 
+#include <memory>
 #include <vector>
 #include <sstream>
+#include <fstream>
 #include <iostream>
 #include <stdexcept>
 #include <functional>
 
 #include <GLFW/glfw3.h>
 
+#include <dirent.h>
 #include <fcntl.h>
 #include <unistd.h>
 #include <errno.h>
@@ -18,7 +21,6 @@
 #include <sys/mman.h>
 #include <sys/ioctl.h>
 #include <linux/videodev2.h>
-
 
 static int xioctl(int fh, int request, void *arg)
 {
@@ -49,11 +51,12 @@ void warn_error(const char * s)
 class subdevice
 {
     std::string dev_name;
+    int vid, pid;
     int fd;
     std::vector<buffer> buffers;
     std::function<void(const void *, size_t)> callback;
 public:
-    subdevice(const std::string & dev_name) : dev_name(dev_name), fd()
+    subdevice(const std::string & name) : dev_name("/dev/" + name), fd()
     {
         struct stat st;
         if(stat(dev_name.c_str(), &st) < 0)
@@ -62,6 +65,18 @@ public:
             throw std::runtime_error(ss.str());
         }
         if(!S_ISCHR(st.st_mode)) throw std::runtime_error(dev_name + " is no device");
+
+        std::string path = "/sys/class/video4linux/" + name + "/device/input";
+        DIR * dir = opendir(path.c_str());
+        if(!dir) throw std::runtime_error("Could not access " + path);
+        while(dirent * entry = readdir(dir))
+        {
+            std::string inputName = entry->d_name;
+            if(inputName.size() < 5 || inputName.substr(0,5) != "input") continue;
+            if(!(std::ifstream(path + "/" + inputName + "/id/vendor") >> std::hex >> vid)) throw std::runtime_error("Failed to read vendor ID");
+            if(!(std::ifstream(path + "/" + inputName + "/id/product") >> std::hex >> pid)) throw std::runtime_error("Failed to read product ID");
+        }
+        closedir(dir);
 
         fd = open(dev_name.c_str(), O_RDWR | O_NONBLOCK, 0);
         if(fd < 0)
@@ -76,18 +91,18 @@ public:
             if(errno == EINVAL) throw std::runtime_error(dev_name + " is no V4L2 device");
             else throw_error("VIDIOC_QUERYCAP");
         }
-        if (!(cap.capabilities & V4L2_CAP_VIDEO_CAPTURE)) throw std::runtime_error(dev_name + " is no video capture device");
-        if (!(cap.capabilities & V4L2_CAP_STREAMING)) throw std::runtime_error(dev_name + " does not support streaming I/O");
+        if(!(cap.capabilities & V4L2_CAP_VIDEO_CAPTURE)) throw std::runtime_error(dev_name + " is no video capture device");
+        if(!(cap.capabilities & V4L2_CAP_STREAMING)) throw std::runtime_error(dev_name + " does not support streaming I/O");
 
         // Select video input, video standard and tune here.
         v4l2_cropcap cropcap = {};
         cropcap.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
-        if (0 == xioctl(fd, VIDIOC_CROPCAP, &cropcap))
+        if(xioctl(fd, VIDIOC_CROPCAP, &cropcap) == 0)
         {
             v4l2_crop crop = {};
             crop.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
             crop.c = cropcap.defrect; // reset to default
-            if (xioctl(fd, VIDIOC_S_CROP, &crop) < 0)
+            if(xioctl(fd, VIDIOC_S_CROP, &crop) < 0)
             {
                 switch (errno)
                 {
@@ -111,29 +126,19 @@ public:
         if(close(fd) < 0) warn_error("close");
     }
 
-    void set_format(int width, int height, int pixelformat)
+    int get_vid() const { return vid; }
+    int get_pid() const { return pid; }
+
+    void start_capture(int width, int height, int fourcc, std::function<void(const void * data, size_t size)> callback)
     {
         v4l2_format fmt = {};
         fmt.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
         fmt.fmt.pix.width       = width;
         fmt.fmt.pix.height      = height;
-        fmt.fmt.pix.pixelformat = pixelformat;
+        fmt.fmt.pix.pixelformat = fourcc;
         fmt.fmt.pix.field       = V4L2_FIELD_INTERLACED;
         if(xioctl(fd, VIDIOC_S_FMT, &fmt) < 0) throw_error("VIDIOC_S_FMT");
         // Note VIDIOC_S_FMT may change width and height
-    }
-
-    void start_capture(std::function<void(const void * data, size_t size)> callback)
-    {
-        v4l2_format fmt = {};
-        fmt.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
-        if(xioctl(fd, VIDIOC_G_FMT, &fmt) < 0) throw_error("VIDIOC_G_FMT");
-
-        // Buggy driver paranoia.
-        unsigned int min = fmt.fmt.pix.width * 2;
-        if(fmt.fmt.pix.bytesperline < min) fmt.fmt.pix.bytesperline = min;
-        min = fmt.fmt.pix.bytesperline * fmt.fmt.pix.height;
-        if(fmt.fmt.pix.sizeimage < min) fmt.fmt.pix.sizeimage = min;
 
         // Init memory mapped IO
         v4l2_requestbuffers req = {};
@@ -154,17 +159,13 @@ public:
         for(int i=0; i<buffers.size(); ++i)
         {
             v4l2_buffer buf = {};
-            buf.type        = V4L2_BUF_TYPE_VIDEO_CAPTURE;
-            buf.memory      = V4L2_MEMORY_MMAP;
-            buf.index       = i;
+            buf.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
+            buf.memory = V4L2_MEMORY_MMAP;
+            buf.index = i;
             if(xioctl(fd, VIDIOC_QUERYBUF, &buf) < 0) throw_error("VIDIOC_QUERYBUF");
 
             buffers[i].length = buf.length;
-            buffers[i].start = mmap(NULL /* start anywhere */,
-                          buf.length,
-                          PROT_READ | PROT_WRITE /* required */,
-                          MAP_SHARED /* recommended */,
-                          fd, buf.m.offset);
+            buffers[i].start = mmap(NULL, buf.length, PROT_READ | PROT_WRITE, MAP_SHARED, fd, buf.m.offset);
             if(buffers[i].start == MAP_FAILED) throw_error("mmap");
         }
 
@@ -184,7 +185,7 @@ public:
         this->callback = callback;
     }
 
-    static void poll(std::initializer_list<subdevice *> subdevices)
+    static void poll(const std::vector<subdevice *> & subdevices)
     {
         int max_fd = 0;
         fd_set fds;
@@ -226,14 +227,39 @@ public:
 
 int main(int argc, char * argv[])
 {
+    std::vector<std::unique_ptr<subdevice>> subdevices;
+
+    DIR * dir = opendir("/sys/class/video4linux");
+    if(!dir) throw std::runtime_error("Cannot access /sys/class/video4linux");
+    while (dirent * entry = readdir(dir))
+    {
+        std::string name = entry->d_name;
+        if(name == "." || name == "..") continue;
+        std::unique_ptr<subdevice> sub(new subdevice(name));
+        subdevices.push_back(move(sub));
+    }
+    closedir(dir);
+
     uint16_t z[640*480] = {};
     uint8_t yuy2[640*480*2] = {};
-
-    subdevice dev0("/dev/video0"), dev1("/dev/video1");
-    dev0.set_format(640, 480, V4L2_PIX_FMT_YUYV);
-    dev0.start_capture([&](const void * data, size_t size) { if(size == sizeof(yuy2)) memcpy(yuy2, data, size); });
-    dev1.set_format(640, 480, v4l2_fourcc('I','N','V','R'));
-    dev1.start_capture([&](const void * data, size_t size) { if(size == sizeof(z)) memcpy(z, data, size); });
+    std::vector<subdevice *> devs;
+    if(subdevices.size() >= 2 && subdevices[0]->get_vid() == 0x8086 && subdevices[0]->get_pid() == 0xa66)
+    {
+        std::cout << "F200 detected!" << std::endl;
+        subdevices[0]->start_capture(640, 480, V4L2_PIX_FMT_YUYV, [&](const void * data, size_t size) { if(size == sizeof(yuy2)) memcpy(yuy2, data, size); });
+        subdevices[1]->start_capture(640, 480, v4l2_fourcc('I','N','V','R'), [&](const void * data, size_t size) { if(size == sizeof(z)) memcpy(z, data, size); });
+        devs = {subdevices[0].get(), subdevices[1].get()};
+    }
+    else if(subdevices.size() >= 3 && subdevices[0]->get_vid() == 0x8086 && subdevices[0]->get_pid() == 0xa80)
+    {
+        std::cout << "R200 detected!" << std::endl;
+        subdevices[2]->start_capture(640, 480, V4L2_PIX_FMT_YUYV, [&](const void * data, size_t size) { if(size == sizeof(yuy2)) memcpy(yuy2, data, size); });
+        devs = {subdevices[2].get()};
+    }
+    else if(!subdevices.empty())
+    {
+        std::cout << "Unknown V4L2 device detected, vid=0x" << std::hex << subdevices[0]->get_vid() << ", pid=0x" << subdevices[0]->get_pid() << std::endl;
+    }
 
     // Open a GLFW window
     glfwInit();
@@ -245,7 +271,7 @@ int main(int argc, char * argv[])
     {
         glfwPollEvents();
 
-        subdevice::poll({&dev0, &dev1});
+        subdevice::poll(devs);
 
         int w,h;
         glfwGetFramebufferSize(win, &w, &h);
@@ -269,4 +295,3 @@ int main(int argc, char * argv[])
 
     return EXIT_SUCCESS;
 }
-
