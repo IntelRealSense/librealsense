b174e226dfa2eb88377b8721b3b6e96fc2094f71
Sterling Orsten <sterling.g.orsten@intel.com>
2015-10-08 13:18:32 -0700

    Simplify

--- src/hack/v4l2-test.cpp
+++ src/hack/v4l2-test.cpp
@@ -109,109 +109,6 @@ static int read_frame(void)
         return 1;
 }
 
-static void mainloop(void)
-{
-
-    // Open a GLFW window
-    glfwInit();
-    GLFWwindow * win = glfwCreateWindow(640, 480, "V4L2 test", 0, 0);
-    glfwMakeContextCurrent(win);
-
-    while (!glfwWindowShouldClose(win))
-    {
-        // Wait for new images
-        glfwPollEvents();
-
-        int w,h;
-        glfwGetFramebufferSize(win, &w, &h);
-        glViewport(0, 0, w, h);
-        glClear(GL_COLOR_BUFFER_BIT);
-
-        // Draw the images
-        glPushMatrix();
-        glfwGetWindowSize(win, &w, &h);
-        glOrtho(0, w, h, 0, -1, +1);
-
-        for (;;) {
-                fd_set fds;
-                struct timeval tv;
-                int r;
-
-                FD_ZERO(&fds);
-                FD_SET(fd, &fds);
-
-                /* Timeout. */
-                tv.tv_sec = 2;
-                tv.tv_usec = 0;
-
-                r = select(fd + 1, &fds, NULL, NULL, &tv);
-
-                if (-1 == r) {
-                        if (EINTR == errno)
-                                continue;
-                        errno_exit("select");
-                }
-
-                if (0 == r) {
-                        fprintf(stderr, "select timeout\n");
-                        exit(EXIT_FAILURE);
-                }
-
-                if (read_frame())
-                        break;
-                /* EAGAIN - continue select loop. */
-        }
-
-        glPopMatrix();
-        glfwSwapBuffers(win);
-    }
-
-    glfwDestroyWindow(win);
-    glfwTerminate();
-}
-
-static void stop_capturing(void)
-{
-    enum v4l2_buf_type type;
-
-
-    type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
-    if (-1 == xioctl(fd, VIDIOC_STREAMOFF, &type))
-            errno_exit("VIDIOC_STREAMOFF");
-}
-
-static void start_capturing(void)
-{
-    unsigned int i;
-    enum v4l2_buf_type type;
-
-    for (i = 0; i < n_buffers; ++i) {
-            struct v4l2_buffer buf;
-
-            CLEAR(buf);
-            buf.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
-            buf.memory = V4L2_MEMORY_MMAP;
-            buf.index = i;
-
-            if (-1 == xioctl(fd, VIDIOC_QBUF, &buf))
-                    errno_exit("VIDIOC_QBUF");
-    }
-    type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
-    if (-1 == xioctl(fd, VIDIOC_STREAMON, &type))
-            errno_exit("VIDIOC_STREAMON");
-}
-
-static void uninit_device(void)
-{
-    unsigned int i;
-
-    for (i = 0; i < n_buffers; ++i)
-            if (-1 == munmap(buffers[i].start, buffers[i].length))
-                    errno_exit("munmap");
-
-    free(buffers);
-}
-
 static void init_mmap(void)
 {
         struct v4l2_requestbuffers req;
@@ -270,101 +167,6 @@ static void init_mmap(void)
         }
 }
 
-static void init_device(void)
-{
-        struct v4l2_capability cap;
-        struct v4l2_cropcap cropcap;
-        struct v4l2_crop crop;
-        struct v4l2_format fmt;
-        unsigned int min;
-
-        if (-1 == xioctl(fd, VIDIOC_QUERYCAP, &cap)) {
-                if (EINVAL == errno) {
-                        fprintf(stderr, "%s is no V4L2 device\n",
-                                 dev_name);
-                        exit(EXIT_FAILURE);
-                } else {
-                        errno_exit("VIDIOC_QUERYCAP");
-                }
-        }
-
-        if (!(cap.capabilities & V4L2_CAP_VIDEO_CAPTURE)) {
-                fprintf(stderr, "%s is no video capture device\n",
-                         dev_name);
-                exit(EXIT_FAILURE);
-        }
-
-        if (!(cap.capabilities & V4L2_CAP_STREAMING)) {
-                fprintf(stderr, "%s does not support streaming i/o\n",
-                         dev_name);
-                exit(EXIT_FAILURE);
-        }
-
-
-        /* Select video input, video standard and tune here. */
-
-
-        CLEAR(cropcap);
-
-        cropcap.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
-
-        if (0 == xioctl(fd, VIDIOC_CROPCAP, &cropcap)) {
-                crop.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
-                crop.c = cropcap.defrect; /* reset to default */
-
-                if (-1 == xioctl(fd, VIDIOC_S_CROP, &crop)) {
-                        switch (errno) {
-                        case EINVAL:
-                                /* Cropping not supported. */
-                                break;
-                        default:
-                                /* Errors ignored. */
-                                break;
-                        }
-                }
-        } else {
-                /* Errors ignored. */
-        }
-
-
-        CLEAR(fmt);
-
-        fmt.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
-        if (force_format) {
-                fmt.fmt.pix.width       = 640;
-                fmt.fmt.pix.height      = 480;
-                fmt.fmt.pix.pixelformat = V4L2_PIX_FMT_YUYV;
-                fmt.fmt.pix.field       = V4L2_FIELD_INTERLACED;
-
-                if (-1 == xioctl(fd, VIDIOC_S_FMT, &fmt))
-                        errno_exit("VIDIOC_S_FMT");
-
-                /* Note VIDIOC_S_FMT may change width and height. */
-        } else {
-                /* Preserve original settings as set by v4l2-ctl for example */
-                if (-1 == xioctl(fd, VIDIOC_G_FMT, &fmt))
-                        errno_exit("VIDIOC_G_FMT");
-        }
-
-        /* Buggy driver paranoia. */
-        min = fmt.fmt.pix.width * 2;
-        if (fmt.fmt.pix.bytesperline < min)
-                fmt.fmt.pix.bytesperline = min;
-        min = fmt.fmt.pix.bytesperline * fmt.fmt.pix.height;
-        if (fmt.fmt.pix.sizeimage < min)
-                fmt.fmt.pix.sizeimage = min;
-
-        init_mmap();
-}
-
-static void close_device(void)
-{
-        if (-1 == close(fd))
-                errno_exit("close");
-
-        fd = -1;
-}
-
 static void open_device(void)
 {
         struct stat st;
@@ -391,17 +193,158 @@ static void open_device(void)
 
 int main(int argc, char **argv)
 {
-        dev_name = "/dev/video1";
-        out_buf = 1;
+    dev_name = "/dev/video1";
+    out_buf = 1;
 
-        open_device();
-        init_device();
-        start_capturing();
-        mainloop();
-        stop_capturing();
-        uninit_device();
-        close_device();
-        fprintf(stderr, "\n");
-        return 0;
+    open_device();
+
+    v4l2_capability cap = {};
+    if(xioctl(fd, VIDIOC_QUERYCAP, &cap) < 0)
+    {
+        if (EINVAL == errno) {
+                fprintf(stderr, "%s is no V4L2 device\n",
+                         dev_name);
+                exit(EXIT_FAILURE);
+        } else {
+                errno_exit("VIDIOC_QUERYCAP");
+        }
+    }
+
+    if (!(cap.capabilities & V4L2_CAP_VIDEO_CAPTURE)) {
+            fprintf(stderr, "%s is no video capture device\n",
+                     dev_name);
+            exit(EXIT_FAILURE);
+    }
+
+    if (!(cap.capabilities & V4L2_CAP_STREAMING)) {
+            fprintf(stderr, "%s does not support streaming i/o\n",
+                     dev_name);
+            exit(EXIT_FAILURE);
+    }
+
+    // Select video input, video standard and tune here.
+    v4l2_cropcap cropcap = {};
+    cropcap.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
+    if (0 == xioctl(fd, VIDIOC_CROPCAP, &cropcap))
+    {
+        v4l2_crop crop = {};
+        crop.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
+        crop.c = cropcap.defrect; // reset to default
+        if (xioctl(fd, VIDIOC_S_CROP, &crop) < 0)
+        {
+            switch (errno)
+            {
+            case EINVAL: break; // Cropping not supported
+            default: break; // Errors ignored
+            }
+        }
+    } else {} // Errors ignored
+
+    v4l2_format fmt = {};
+    fmt.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
+    if (force_format)
+    {
+        fmt.fmt.pix.width       = 640;
+        fmt.fmt.pix.height      = 480;
+        fmt.fmt.pix.pixelformat = V4L2_PIX_FMT_YUYV;
+        fmt.fmt.pix.field       = V4L2_FIELD_INTERLACED;
+        if(xioctl(fd, VIDIOC_S_FMT, &fmt) < 0) errno_exit("VIDIOC_S_FMT");
+        // Note VIDIOC_S_FMT may change width and height
+    }
+    else
+    {
+        // Preserve original settings as set by v4l2-ctl for example
+        if(xioctl(fd, VIDIOC_G_FMT, &fmt) < 0) errno_exit("VIDIOC_G_FMT");
+    }
+
+    // Buggy driver paranoia.
+    unsigned int min = fmt.fmt.pix.width * 2;
+    if (fmt.fmt.pix.bytesperline < min)
+            fmt.fmt.pix.bytesperline = min;
+    min = fmt.fmt.pix.bytesperline * fmt.fmt.pix.height;
+    if (fmt.fmt.pix.sizeimage < min)
+            fmt.fmt.pix.sizeimage = min;
+
+    init_mmap();
+
+    // Start capturing
+    for(int i = 0; i < n_buffers; ++i)
+    {
+        v4l2_buffer buf = {};
+        buf.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
+        buf.memory = V4L2_MEMORY_MMAP;
+        buf.index = i;
+        if(xioctl(fd, VIDIOC_QBUF, &buf) < 0) errno_exit("VIDIOC_QBUF");
+    }
+
+    v4l2_buf_type type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
+    if(xioctl(fd, VIDIOC_STREAMON, &type) < 0) errno_exit("VIDIOC_STREAMON");
+
+    // Open a GLFW window
+    glfwInit();
+    GLFWwindow * win = glfwCreateWindow(640, 480, "V4L2 test", 0, 0);
+    glfwMakeContextCurrent(win);
+
+    // While window is open
+    while (!glfwWindowShouldClose(win))
+    {
+        glfwPollEvents();
+
+        int w,h;
+        glfwGetFramebufferSize(win, &w, &h);
+        glViewport(0, 0, w, h);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glPushMatrix();
+        glfwGetWindowSize(win, &w, &h);
+        glOrtho(0, w, h, 0, -1, +1);
+        for (;;)
+        {
+            fd_set fds;
+            struct timeval tv;
+            int r;
+
+            FD_ZERO(&fds);
+            FD_SET(fd, &fds);
+
+            /* Timeout. */
+            tv.tv_sec = 2;
+            tv.tv_usec = 0;
+
+            r = select(fd + 1, &fds, NULL, NULL, &tv);
+
+            if (-1 == r) {
+                    if (EINTR == errno)
+                            continue;
+                    errno_exit("select");
+            }
+
+            if (0 == r) {
+                    fprintf(stderr, "select timeout\n");
+                    exit(EXIT_FAILURE);
+            }
+
+            if (read_frame())
+                    break;
+            /* EAGAIN - continue select loop. */
+        }
+
+        glPopMatrix();
+        glfwSwapBuffers(win);
+    }
+    glfwDestroyWindow(win);
+    glfwTerminate();
+
+    if(xioctl(fd, VIDIOC_STREAMOFF, &type) < 0) errno_exit("VIDIOC_STREAMOFF");
+
+    for(int i = 0; i < n_buffers; ++i)
+    {
+        if(munmap(buffers[i].start, buffers[i].length) < 0) errno_exit("munmap");
+    }
+    free(buffers);
+
+    if(close(fd) < 0) errno_exit("close");
+
+    return EXIT_SUCCESS;
 }
 
