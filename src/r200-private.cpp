be8655fc057d2827dbc60176ad6cc45497f45885
Dimitri Diakopoulos <ddiakopoulos@gmail.com>
2015-10-20 10:58:52 -0700

    space normalization

--- src/r200-private.cpp
+++ src/r200-private.cpp
@@ -277,21 +277,21 @@ namespace rsimpl { namespace r200
             ReadCalibrationSector();
         }
 
-		// Format a DSAPI timestamp in a human-readable fashion
-		std::string unix_timestamp_to_human(double secondsSinceEpoch)
-		{
-			time_t time = (time_t)secondsSinceEpoch;
-			char buffer[80];
-			struct tm * pTime = gmtime(&time);
-			if (pTime)
-			{
-				size_t i = strftime(buffer, sizeof(buffer), "%Y-%m-%d %H:%M:%S", gmtime(&time));
-				sprintf(buffer + i, ".%02d UTC", static_cast<int>(fmod(secondsSinceEpoch, 1.0) * 100));
-				return buffer;
-			}
-			else
-				return "";
-		}
+        // Format a DSAPI timestamp in a human-readable fashion
+        std::string unix_timestamp_to_human(double secondsSinceEpoch)
+        {
+            time_t time = (time_t)secondsSinceEpoch;
+            char buffer[80];
+            struct tm * pTime = gmtime(&time);
+            if (pTime)
+            {
+                size_t i = strftime(buffer, sizeof(buffer), "%Y-%m-%d %H:%M:%S", gmtime(&time));
+                sprintf(buffer + i, ".%02d UTC", static_cast<int>(fmod(secondsSinceEpoch, 1.0) * 100));
+                return buffer;
+            }
+            else
+                return "";
+        }
 
     public:
 
@@ -307,17 +307,17 @@ namespace rsimpl { namespace r200
 
         void LogDebugInfo(uvc::device & device, CameraCalibrationParameters & p, CameraHeaderInfo & h)
         {
-			DEBUG_OUT("Model: " << h.modelNumber);
-			DEBUG_OUT("Firmware Version: " << read_firmware_version(device));
+            DEBUG_OUT("Model: " << h.modelNumber);
+            DEBUG_OUT("Firmware Version: " << read_firmware_version(device));
             DEBUG_OUT("Calibration Version: " << p.metadata.versionNumber);
-			DEBUG_OUT("Calibration Date: " << unix_timestamp_to_human(h.calibrationDate));
+            DEBUG_OUT("Calibration Date: " << unix_timestamp_to_human(h.calibrationDate));
             DEBUG_OUT("Serial: " << h.serialNumber);
             DEBUG_OUT("Revision: " << h.revisionNumber);
             DEBUG_OUT("Camera Header Ver: " << h.cameraHeadContentsVersion);
             DEBUG_OUT("Baseline: " << h.nominalBaseline);
             DEBUG_OUT("OEM ID: " << h.OEMID);
-			if (CURRENT_CAMERA_CONTENTS_VERSION_NUMBER != h.cameraHeadContentsVersion)
-				 DEBUG_OUT("(Warning): Device camera header does not match internal struct version: " << CURRENT_CAMERA_CONTENTS_VERSION_NUMBER);
+            if (CURRENT_CAMERA_CONTENTS_VERSION_NUMBER != h.cameraHeadContentsVersion)
+                 DEBUG_OUT("(Warning): Device camera header does not match internal struct version: " << CURRENT_CAMERA_CONTENTS_VERSION_NUMBER);
         }
 
         CameraCalibrationParameters GetCalibration() { return cameraCalibration; }
