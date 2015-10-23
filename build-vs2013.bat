913e4148b45b8f336bf5c79a86022d7e8e7ffb3c
Sterling Orsten <sterling.g.orsten@intel.com>
2015-10-23 16:28:56 -0700

    Copy the release versions of the libs instead of shipping the debug versions.

--- build-vs2013.bat
+++ build-vs2013.bat
@@ -1,7 +1,5 @@
 "C:\Program Files (x86)\MSBuild\12.0\Bin\MSBuild.exe" librealsense.vc12\realsense\realsense.vcxproj /p:Configuration=Release;Platform=x64
 "C:\Program Files (x86)\MSBuild\12.0\Bin\MSBuild.exe" librealsense.vc12\realsense\realsense.vcxproj /p:Configuration=Release;Platform=Win32
-"C:\Program Files (x86)\MSBuild\12.0\Bin\MSBuild.exe" librealsense.vc12\realsense\realsense.vcxproj /p:Configuration=Debug;Platform=x64
-"C:\Program Files (x86)\MSBuild\12.0\Bin\MSBuild.exe" librealsense.vc12\realsense\realsense.vcxproj /p:Configuration=Debug;Platform=Win32
 
 rm /S /Q librealsense-dll.zip
 rmdir /S /Q librealsense-dll
@@ -21,15 +19,19 @@ xcopy librealsense.vc12\* librealsense-dll\librealsense.vc12 /S
 xcopy COPYING librealsense-dll\
 xcopy readme.md librealsense-dll\
 
-rmdir /S /Q librealsense-dll\librealsense.vc12\realsense
+del librealsense-dll\bin\Win32\*.exp
+del librealsense-dll\bin\Win32\*.pdb
+del librealsense-dll\bin\x64\*.exp
+del librealsense-dll\bin\x64\*.pdb
+
+echo F | xcopy librealsense-dll\bin\Win32\realsense.dll librealsense-dll\bin\Win32\realsense-d.dll
+echo F | xcopy librealsense-dll\bin\Win32\realsense.lib librealsense-dll\bin\Win32\realsense-d.lib
+echo F | xcopy librealsense-dll\bin\x64\realsense.dll librealsense-dll\bin\x64\realsense-d.dll
+echo F | xcopy librealsense-dll\bin\x64\realsense.lib librealsense-dll\bin\x64\realsense-d.lib
+
 del librealsense-dll\librealsense.vc12\realsense.sln
 del librealsense-dll\librealsense.vc12\.gitignore
-del librealsense-dll\librealsense.vc12\Win32\*.exp
-del librealsense-dll\librealsense.vc12\Win32\*.ilk
-del librealsense-dll\librealsense.vc12\Win32\*.pdb
-del librealsense-dll\librealsense.vc12\x64\*.exp
-del librealsense-dll\librealsense.vc12\x64\*.ilk
-del librealsense-dll\librealsense.vc12\x64\*.pdb
+rmdir /S /Q librealsense-dll\librealsense.vc12\realsense
 
 powershell.exe -nologo -noprofile -command "& { $theDate = (Get-Date); $theDate.ToShortDateString() | Out-File -FilePath librealsense-dll\release.txt -Append; \"https://github.intel.com/PerCSystemsEngineering/librealsense/\" | Out-File -FilePath librealsense-dll\release.txt -Append;}"
 powershell.exe -nologo -noprofile -command "& { Add-Type -A 'System.IO.Compression.FileSystem'; [IO.Compression.ZipFile]::CreateFromDirectory('librealsense-dll', 'librealsense-dll.zip'); }"
