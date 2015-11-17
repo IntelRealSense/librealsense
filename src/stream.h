c5a8be01e69792d6471cade62aa5209afe75eb5a
Sterling Orsten <sterling.g.orsten@intel.com>
2015-11-17 14:22:55 -0800

    Add override/final specifiers to stream classes.

--- src/stream.h
+++ src/stream.h
@@ -24,7 +24,7 @@ namespace rsimpl
         virtual const byte *                    get_frame_data() const = 0;    
     };
 
-    struct native_stream : public stream_interface
+    struct native_stream final : public stream_interface
     {
         const device_config &                   config;
         const rs_stream                         stream;
@@ -33,23 +33,23 @@ namespace rsimpl
 
                                                 native_stream(device_config & config, rs_stream stream);
 
-        pose                                    get_pose() const { return config.info.stream_poses[stream]; }
-        float                                   get_depth_scale() const { return config.info.depth_scale; }
-        int                                     get_mode_count() const { return (int)modes.size(); }
-        const stream_mode &                     get_mode(int mode) const { throw modes[mode]; }
+        pose                                    get_pose() const override { return config.info.stream_poses[stream]; }
+        float                                   get_depth_scale() const override { return config.info.depth_scale; }
+        int                                     get_mode_count() const override { return (int)modes.size(); }
+        const stream_mode &                     get_mode(int mode) const override { throw modes[mode]; }
 
-        bool                                    is_enabled() const { return static_cast<bool>(buffer); }
+        bool                                    is_enabled() const override { return static_cast<bool>(buffer); }
         stream_mode                             get_mode() const;
-        rs_intrinsics                           get_intrinsics() const { return config.intrinsics.get(get_mode().intrinsics_index); }
-        rs_intrinsics                           get_rectified_intrinsics() const { return config.intrinsics.get_rect(get_mode().intrinsics_index); }
-        rs_format                               get_format() const { return get_mode().format; }
-        int                                     get_framerate() const { return get_mode().fps; }
+        rs_intrinsics                           get_intrinsics() const override { return config.intrinsics.get(get_mode().intrinsics_index); }
+        rs_intrinsics                           get_rectified_intrinsics() const override { return config.intrinsics.get_rect(get_mode().intrinsics_index); }
+        rs_format                               get_format() const override { return get_mode().format; }
+        int                                     get_framerate() const override { return get_mode().fps; }
 
-        int                                     get_frame_number() const { return buffer->get_front_number(); }
-        const byte *                            get_frame_data() const { return buffer->get_front_data(); }
+        int                                     get_frame_number() const override { return buffer->get_front_number(); }
+        const byte *                            get_frame_data() const override { return buffer->get_front_data(); }
     };
 
-    class rectified_stream : public stream_interface
+    class rectified_stream final : public stream_interface
     {
         const stream_interface &                source;
         mutable std::vector<int>                table;
@@ -58,20 +58,20 @@ namespace rsimpl
     public:
                                                 rectified_stream(const stream_interface & source) : source(source), number() {}
 
-        pose                                    get_pose() const { return {{{1,0,0},{0,1,0},{0,0,1}}, source.get_pose().position}; }
-        float                                   get_depth_scale() const { return source.get_depth_scale(); }
+        pose                                    get_pose() const override { return {{{1,0,0},{0,1,0},{0,0,1}}, source.get_pose().position}; }
+        float                                   get_depth_scale() const override { return source.get_depth_scale(); }
 
-        bool                                    is_enabled() const { return source.is_enabled(); }
-        rs_intrinsics                           get_intrinsics() const { return source.get_rectified_intrinsics(); }
-        rs_intrinsics                           get_rectified_intrinsics() const { return source.get_rectified_intrinsics(); }
-        rs_format                               get_format() const { return source.get_format(); }
-        int                                     get_framerate() const { return source.get_framerate(); }
+        bool                                    is_enabled() const override { return source.is_enabled(); }
+        rs_intrinsics                           get_intrinsics() const override { return source.get_rectified_intrinsics(); }
+        rs_intrinsics                           get_rectified_intrinsics() const override { return source.get_rectified_intrinsics(); }
+        rs_format                               get_format() const override { return source.get_format(); }
+        int                                     get_framerate() const override { return source.get_framerate(); }
 
-        int                                     get_frame_number() const { return source.get_frame_number(); }
-        const byte *                            get_frame_data() const;
+        int                                     get_frame_number() const override { return source.get_frame_number(); }
+        const byte *                            get_frame_data() const override;
     };
 
-    class aligned_stream : public stream_interface
+    class aligned_stream final : public stream_interface
     {
         const stream_interface &                from, & to;
         mutable std::vector<byte>               image;
@@ -79,17 +79,17 @@ namespace rsimpl
     public:
                                                 aligned_stream(const stream_interface & from, const stream_interface & to) : from(from), to(to), number() {}
 
-        pose                                    get_pose() const { return to.get_pose(); }
-        float                                   get_depth_scale() const { return to.get_depth_scale(); }
+        pose                                    get_pose() const override { return to.get_pose(); }
+        float                                   get_depth_scale() const override { return to.get_depth_scale(); }
 
-        bool                                    is_enabled() const { return from.is_enabled() && to.is_enabled(); }
-        rs_intrinsics                           get_intrinsics() const { return to.get_intrinsics(); }
-        rs_intrinsics                           get_rectified_intrinsics() const { return to.get_rectified_intrinsics(); }
-        rs_format                               get_format() const { return from.get_format(); }
-        int                                     get_framerate() const { return from.get_framerate(); }
+        bool                                    is_enabled() const override { return from.is_enabled() && to.is_enabled(); }
+        rs_intrinsics                           get_intrinsics() const override { return to.get_intrinsics(); }
+        rs_intrinsics                           get_rectified_intrinsics() const override { return to.get_rectified_intrinsics(); }
+        rs_format                               get_format() const override { return from.get_format(); }
+        int                                     get_framerate() const override { return from.get_framerate(); }
 
-        int                                     get_frame_number() const { return from.get_frame_number(); }
-        const byte *                            get_frame_data() const;
+        int                                     get_frame_number() const override { return from.get_frame_number(); }
+        const byte *                            get_frame_data() const override;
     };
 }
 
