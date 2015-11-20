3dadb777228a4b76df1f44d55b1a3e469d477194
Dimitri Diakopoulos <ddiakopoulos@gmail.com>
2015-11-20 12:45:11 -0800

    normalize header files with boilerplate copyright and license

--- examples/cs-capture.cs
+++ examples/cs-capture.cs
@@ -1,4 +1,12 @@
-﻿using System;
+﻿/*
+    INTEL CORPORATION PROPRIETARY INFORMATION This software is supplied under the
+    terms of a license agreement or nondisclosure agreement with Intel Corporation
+    and may not be copied or disclosed except in accordance with the terms of that
+    agreement.
+    Copyright(c) 2015 Intel Corporation. All Rights Reserved.
+*/
+
+using System;
 using System.Drawing;
 using System.Drawing.Imaging;
 using System.Runtime.InteropServices;
--- src/no-jdk/jni.h
+++ src/no-jdk/jni.h
@@ -1,11 +1,19 @@
 /*
- * This file sits in the /src/no-jdk folder, which is intended to be placed AFTER $(JAVA_HOME)/include in the compiler include path.
- * Therefore the "#include <jni.h>" statement will reach this file when LibRealsense is compiled on a system with no JDK installed.
- * We can still compile LibRealsense and all C/C++/C#/Python examples will work just fine, but we will want to print a message warning
- * the user that JNI entrypoints for Java will not be available. If we are producing packaged binaries for distribution, this should
- * always be done from a machine WITH the JDK installed. The resulting .so/.dll file will have no runtime dependency on Java, but will
- * contain extra entrypoints that allow Java to interact with the library.
- */
+    INTEL CORPORATION PROPRIETARY INFORMATION This software is supplied under the
+    terms of a license agreement or nondisclosure agreement with Intel Corporation
+    and may not be copied or disclosed except in accordance with the terms of that
+    agreement.
+    Copyright(c) 2015 Intel Corporation. All Rights Reserved.
+*/
+
+/*
+	This file sits in the /src/no-jdk folder, which is intended to be placed AFTER $(JAVA_HOME)/include in the compiler include path.
+	Therefore the "#include <jni.h>" statement will reach this file when LibRealsense is compiled on a system with no JDK installed.
+	We can still compile LibRealsense and all C/C++/C#/Python examples will work just fine, but we will want to print a message warning
+	the user that JNI entrypoints for Java will not be available. If we are producing packaged binaries for distribution, this should
+	always be done from a machine WITH the JDK installed. The resulting .so/.dll file will have no runtime dependency on Java, but will
+	contain extra entrypoints that allow Java to interact with the library.
+*/
 
 #pragma message ("***************************************************************************************************************************")
 #pragma message ("* WARNING - No JDK installed or JAVA_HOME not set! JNI entrypoints will not be available in librealsense.so/realsense.dll *")
