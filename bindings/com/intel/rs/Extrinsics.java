2da68c0c67b673e93228351cd64b7d5718e86173
Sterling Orsten <sterling.g.orsten@intel.com>
2016-01-19 16:55:10 -0800

    Remove C#, Java, and Python bindings for now.
    We may reintroduce official bindings as librealsense nears API stability, but these have proven to be too much work to maintain.

--- bindings/com/intel/rs/Extrinsics.java
+++
@@ -1,7 +0,0 @@
-package com.intel.rs;
-
-public class Extrinsics
-{
-    public float[] rotation; // column-major 3x3 rotation matrix
-    public float[] translation; // 3 element translation vector, in meters
-}
