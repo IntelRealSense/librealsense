d04f4f85415a63c1151e02dc051c3a955be4aa53
Dimitri Diakopoulos <ddiakopoulos@gmail.com>
2016-01-19 20:00:05 -0800

    renormalize headers for apache 2.0 license

--- examples/example.h
+++ examples/example.h
@@ -1,10 +1,5 @@
-/*
-    INTEL CORPORATION PROPRIETARY INFORMATION This software is supplied under the
-    terms of a license agreement or nondisclosure agreement with Intel Corporation
-    and may not be copied or disclosed except in accordance with the terms of that
-    agreement.
-    Copyright(c) 2015 Intel Corporation. All Rights Reserved.
-*/
+// License: Apache 2.0. See LICENSE file in root directory.
+// Copyright(c) 2015 Intel Corporation. All Rights Reserved.
 
 #define GLFW_INCLUDE_GLU
 
