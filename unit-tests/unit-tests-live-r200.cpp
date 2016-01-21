f149e1eab4df041b612ddb2895d435dff799fd6d
Sterling Orsten <sterling.g.orsten@intel.com>
2016-01-20 19:10:16 -0800

    Adjust list of tested options for R200

--- unit-tests/unit-tests-live-r200.cpp
+++ unit-tests/unit-tests-live-r200.cpp
@@ -38,16 +38,70 @@ TEST_CASE( "R200 metadata enumerates correctly", "[live] [r200]" )
             REQUIRE(strlen(serial) == 10);
             for(int i=0; i<10; ++i) REQUIRE(isdigit(serial[i]));
         }
+    }
+}
+
+TEST_CASE( "R200 devices support all required options", "[live] [r200]" )
+{
+    // Require at least one device to be plugged in
+    safe_context ctx;
+    const int device_count = rs_get_device_count(ctx, require_no_error());
+    REQUIRE(device_count > 0);
+
+    // For each device
+    for(int i=0; i<device_count; ++i)
+    {
+        rs_device * dev = rs_get_device(ctx, 0, require_no_error());
+        REQUIRE(dev != nullptr);
 
         SECTION( "device supports standard picture options and R200 extension options, and nothing else" )
         {
+            const int supported_options[] = {
+                RS_OPTION_COLOR_BACKLIGHT_COMPENSATION,
+                RS_OPTION_COLOR_BRIGHTNESS,
+                RS_OPTION_COLOR_CONTRAST,
+                RS_OPTION_COLOR_EXPOSURE,
+                RS_OPTION_COLOR_GAIN,
+                RS_OPTION_COLOR_GAMMA,
+                RS_OPTION_COLOR_HUE,
+                RS_OPTION_COLOR_SATURATION,
+                RS_OPTION_COLOR_SHARPNESS,
+                RS_OPTION_COLOR_WHITE_BALANCE,
+                RS_OPTION_COLOR_ENABLE_AUTO_EXPOSURE,
+                RS_OPTION_COLOR_ENABLE_AUTO_WHITE_BALANCE,
+                RS_OPTION_R200_LR_AUTO_EXPOSURE_ENABLED,
+                RS_OPTION_R200_LR_GAIN,
+                RS_OPTION_R200_LR_EXPOSURE,
+                RS_OPTION_R200_EMITTER_ENABLED,
+                RS_OPTION_R200_DEPTH_UNITS,
+                RS_OPTION_R200_DEPTH_CLAMP_MIN,
+                RS_OPTION_R200_DEPTH_CLAMP_MAX,
+                RS_OPTION_R200_DISPARITY_MULTIPLIER,
+                RS_OPTION_R200_DISPARITY_SHIFT,
+                RS_OPTION_R200_AUTO_EXPOSURE_MEAN_INTENSITY_SET_POINT,
+                RS_OPTION_R200_AUTO_EXPOSURE_BRIGHT_RATIO_SET_POINT,
+                RS_OPTION_R200_AUTO_EXPOSURE_KP_GAIN,
+                RS_OPTION_R200_AUTO_EXPOSURE_KP_EXPOSURE,
+                RS_OPTION_R200_AUTO_EXPOSURE_KP_DARK_THRESHOLD,
+                RS_OPTION_R200_AUTO_EXPOSURE_TOP_EDGE,
+                RS_OPTION_R200_AUTO_EXPOSURE_BOTTOM_EDGE,
+                RS_OPTION_R200_AUTO_EXPOSURE_LEFT_EDGE,
+                RS_OPTION_R200_AUTO_EXPOSURE_RIGHT_EDGE,
+                RS_OPTION_R200_DEPTH_CONTROL_ESTIMATE_MEDIAN_DECREMENT,
+                RS_OPTION_R200_DEPTH_CONTROL_ESTIMATE_MEDIAN_INCREMENT,
+                RS_OPTION_R200_DEPTH_CONTROL_MEDIAN_THRESHOLD,
+                RS_OPTION_R200_DEPTH_CONTROL_SCORE_MINIMUM_THRESHOLD,
+                RS_OPTION_R200_DEPTH_CONTROL_SCORE_MAXIMUM_THRESHOLD,
+                RS_OPTION_R200_DEPTH_CONTROL_TEXTURE_COUNT_THRESHOLD,
+                RS_OPTION_R200_DEPTH_CONTROL_TEXTURE_DIFFERENCE_THRESHOLD,
+                RS_OPTION_R200_DEPTH_CONTROL_SECOND_PEAK_THRESHOLD,
+                RS_OPTION_R200_DEPTH_CONTROL_NEIGHBOR_THRESHOLD,
+                RS_OPTION_R200_DEPTH_CONTROL_LR_THRESHOLD
+            };
+
             for(int i=0; i<RS_OPTION_COUNT; ++i)
             {
-                if(i >= RS_OPTION_COLOR_BACKLIGHT_COMPENSATION && i <= RS_OPTION_COLOR_ENABLE_AUTO_WHITE_BALANCE)
-                {
-                    REQUIRE(rs_device_supports_option(dev, (rs_option)i, require_no_error()) == 1);
-                }
-                else if(i >= RS_OPTION_R200_LR_AUTO_EXPOSURE_ENABLED && i <= RS_OPTION_R200_DISPARITY_SHIFT)
+                if(std::find(std::begin(supported_options), std::end(supported_options), i) != std::end(supported_options))
                 {
                     REQUIRE(rs_device_supports_option(dev, (rs_option)i, require_no_error()) == 1);
                 }
