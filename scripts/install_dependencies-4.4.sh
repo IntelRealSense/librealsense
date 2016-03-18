5ea1d3528de2f1269d5d53569df215347faa64fe
archielee <archie_lee7@hotmail.com>
2016-03-18 13:15:44 -0400

    Added proper root permissions when needed

--- scripts/install_dependencies-4.4.sh
+++ scripts/install_dependencies-4.4.sh
@@ -2,13 +2,13 @@
 
 # Install gcc
 echo && echo Installing gcc-4.9 && echo
-add-apt-repository ppa:ubuntu-toolchain-r/test
-apt-get update
-apt-get install gcc-4.9 g++-4.9
-update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-4.9 60 --slave /usr/bin/g++ g++ /usr/bin/g++-4.9
+sudo add-apt-repository ppa:ubuntu-toolchain-r/test
+sudo apt-get update
+sudo apt-get install gcc-4.9 g++-4.9
+sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-4.9 60 --slave /usr/bin/g++ g++ /usr/bin/g++-4.9
 
 # Some dependency on openssl to compile UVC module
-apt-get install libssl-dev
+sudo apt-get install libssl-dev
 
 # Update the kernel to v4.4-wily
 echo && echo Upgrading kernel to v4.4-wily, will reboot upon completion... && echo
@@ -18,9 +18,9 @@ wget http://kernel.ubuntu.com/~kernel-ppa/mainline/v4.4-wily/linux-headers-4.4.0
 wget http://kernel.ubuntu.com/~kernel-ppa/mainline/v4.4-wily/linux-image-4.4.0-040400-generic_4.4.0-040400.201601101930_amd64.deb -P /tmp/
 
 # Install
-dpkg -i /tmp/*.deb
+sudo dpkg -i /tmp/*.deb
 
 # Update grub
-update-grub
+sudo update-grub
 
 echo "Finished! In order to complete the Kernel upgrade you must reboot."
