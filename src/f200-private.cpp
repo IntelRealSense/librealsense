e153376c8ae6a0f7ab6a5f1a8216d7f6aa5dc091
evgeni.raikhel <evgeni.raikhel@intel.com>
2016-03-22 14:08:13 +0200

    white spaces

--- src/f200-private.cpp
+++ src/f200-private.cpp
@@ -60,27 +60,28 @@ namespace rsimpl { namespace f200
 {
     enum class IVCAMMonitorCommand : uint32_t
     {
-        UpdateCalib = 0xBC,
-        GetIRTemp = 0x52,
-        GetMEMSTemp = 0x0A,
-        HWReset = 0x28,
-        GVD = 0x3B,
-        BIST = 0xFF,
-        GoToDFU = 0x80,
-        GetCalibrationTable = 0x3D,
-        DebugFormat = 0x0B,
-        TimeStampEnable = 0x0C,
-        GetPowerGearState = 0xFF,
-        SetDefaultControls = 0xA6,
-        GetDefaultControls = 0xA7,
-        GetFWLastError = 0x0E,
-        CheckI2cConnect = 0x4A,
-        CheckRGBConnect = 0x4B,
-        CheckDPTConnect = 0x4C,
-        SetAutoRange = 0xA6,
-        OnSuspendResume = 0x91,
-        GetWakeReason = 0x93,
-        GetWakeConfidence = 0x92,
+        UpdateCalib                = 0xBC,
+        UpdateCalib                = 0xBC,
+        GetIRTemp                  = 0x52,
+        GetMEMSTemp                = 0x0A,
+        HWReset                    = 0x28,
+        GVD                        = 0x3B,
+        BIST                       = 0xFF,
+        GoToDFU                    = 0x80,
+        GetCalibrationTable        = 0x3D,
+        DebugFormat                = 0x0B,
+        TimeStampEnable            = 0x0C,
+        GetPowerGearState          = 0xFF,
+        SetDefaultControls         = 0xA6,
+        GetDefaultControls         = 0xA7,
+        GetFWLastError             = 0x0E,
+        CheckI2cConnect            = 0x4A,
+        CheckRGBConnect            = 0x4B,
+        CheckDPTConnect            = 0x4C,
+        SetAutoRange               = 0xA6,
+        OnSuspendResume            = 0x91,
+        GetWakeReason              = 0x93,
+        GetWakeConfidence          = 0x92,
         AutoRangeSetParamsforDebug = 0xb3
     };
 
@@ -175,47 +176,47 @@ namespace rsimpl { namespace f200
 
     enum Property
     {
-        IVCAM_PROPERTY_COLOR_EXPOSURE = 1,
-        IVCAM_PROPERTY_COLOR_BRIGHTNESS = 2,
-        IVCAM_PROPERTY_COLOR_CONTRAST = 3,
-        IVCAM_PROPERTY_COLOR_SATURATION = 4,
-        IVCAM_PROPERTY_COLOR_HUE = 5,
-        IVCAM_PROPERTY_COLOR_GAMMA = 6,
-        IVCAM_PROPERTY_COLOR_WHITE_BALANCE = 7,
-        IVCAM_PROPERTY_COLOR_SHARPNESS = 8,
-        IVCAM_PROPERTY_COLOR_BACK_LIGHT_COMPENSATION = 9,
-        IVCAM_PROPERTY_COLOR_GAIN = 10,
-        IVCAM_PROPERTY_COLOR_POWER_LINE_FREQUENCY = 11,
-        IVCAM_PROPERTY_AUDIO_MIX_LEVEL = 12,
-        IVCAM_PROPERTY_APERTURE = 13,
-        IVCAM_PROPERTY_DISTORTION_CORRECTION_I = 202,
-        IVCAM_PROPERTY_DISTORTION_CORRECTION_DPTH = 203,
-        IVCAM_PROPERTY_DEPTH_MIRROR = 204,    //0 - not mirrored, 1 - mirrored
-        IVCAM_PROPERTY_COLOR_MIRROR = 205,
-        IVCAM_PROPERTY_COLOR_FIELD_OF_VIEW = 207,
-        IVCAM_PROPERTY_COLOR_SENSOR_RANGE = 209,
-        IVCAM_PROPERTY_COLOR_FOCAL_LENGTH = 211,
-        IVCAM_PROPERTY_COLOR_PRINCIPAL_POINT = 213,
-        IVCAM_PROPERTY_DEPTH_FIELD_OF_VIEW = 215,
-        IVCAM_PROPERTY_DEPTH_UNDISTORTED_FIELD_OF_VIEW = 223,
-        IVCAM_PROPERTY_DEPTH_SENSOR_RANGE = 217,
-        IVCAM_PROPERTY_DEPTH_FOCAL_LENGTH = 219,
-        IVCAM_PROPERTY_DEPTH_UNDISTORTED_FOCAL_LENGTH = 225,
-        IVCAM_PROPERTY_DEPTH_PRINCIPAL_POINT = 221,
-        IVCAM_PROPERTY_MF_DEPTH_LOW_CONFIDENCE_VALUE = 5000,
-        IVCAM_PROPERTY_MF_DEPTH_UNIT = 5001,   // in micron
-        IVCAM_PROPERTY_MF_CALIBRATION_DATA = 5003,
-        IVCAM_PROPERTY_MF_LASER_POWER = 5004,
-        IVCAM_PROPERTY_MF_ACCURACY = 5005,
-        IVCAM_PROPERTY_MF_INTENSITY_IMAGE_TYPE = 5006,   //0 - (I0 - laser off), 1 - (I1 - Laser on), 2 - (I1-I0), default is I1.
-        IVCAM_PROPERTY_MF_MOTION_VS_RANGE_TRADE = 5007,
-        IVCAM_PROPERTY_MF_POWER_GEAR = 5008,
-        IVCAM_PROPERTY_MF_FILTER_OPTION = 5009,
-        IVCAM_PROPERTY_MF_VERSION = 5010,
-        IVCAM_PROPERTY_MF_DEPTH_CONFIDENCE_THRESHOLD = 5013,
-        IVCAM_PROPERTY_ACCELEROMETER_READING = 3000,   // three values
-        IVCAM_PROPERTY_PROJECTION_SERIALIZABLE = 3003,
-        IVCAM_PROPERTY_CUSTOMIZED = 0x04000000,
+        IVCAM_PROPERTY_COLOR_EXPOSURE                   =   1,
+        IVCAM_PROPERTY_COLOR_BRIGHTNESS                 =   2,
+        IVCAM_PROPERTY_COLOR_CONTRAST                   =   3,
+        IVCAM_PROPERTY_COLOR_SATURATION                 =   4,
+        IVCAM_PROPERTY_COLOR_HUE                        =   5,
+        IVCAM_PROPERTY_COLOR_GAMMA                      =   6,
+        IVCAM_PROPERTY_COLOR_WHITE_BALANCE              =   7,
+        IVCAM_PROPERTY_COLOR_SHARPNESS                  =   8,
+        IVCAM_PROPERTY_COLOR_BACK_LIGHT_COMPENSATION    =   9,
+        IVCAM_PROPERTY_COLOR_GAIN                       =   10,
+        IVCAM_PROPERTY_COLOR_POWER_LINE_FREQUENCY       =   11,
+        IVCAM_PROPERTY_AUDIO_MIX_LEVEL                  =   12,
+        IVCAM_PROPERTY_APERTURE                         =   13,
+        IVCAM_PROPERTY_DISTORTION_CORRECTION_I          =   202,
+        IVCAM_PROPERTY_DISTORTION_CORRECTION_DPTH       =   203,
+        IVCAM_PROPERTY_DEPTH_MIRROR                     =   204,    //0 - not mirrored, 1 - mirrored
+        IVCAM_PROPERTY_COLOR_MIRROR                     =   205,
+        IVCAM_PROPERTY_COLOR_FIELD_OF_VIEW              =   207,
+        IVCAM_PROPERTY_COLOR_SENSOR_RANGE               =   209,
+        IVCAM_PROPERTY_COLOR_FOCAL_LENGTH               =   211,
+        IVCAM_PROPERTY_COLOR_PRINCIPAL_POINT            =   213,
+        IVCAM_PROPERTY_DEPTH_FIELD_OF_VIEW              =   215,
+        IVCAM_PROPERTY_DEPTH_UNDISTORTED_FIELD_OF_VIEW  =   223,
+        IVCAM_PROPERTY_DEPTH_SENSOR_RANGE               =   217,
+        IVCAM_PROPERTY_DEPTH_FOCAL_LENGTH               =   219,
+        IVCAM_PROPERTY_DEPTH_UNDISTORTED_FOCAL_LENGTH   =   225,
+        IVCAM_PROPERTY_DEPTH_PRINCIPAL_POINT            =   221,
+        IVCAM_PROPERTY_MF_DEPTH_LOW_CONFIDENCE_VALUE    =   5000,
+        IVCAM_PROPERTY_MF_DEPTH_UNIT                    =   5001,   // in micron
+        IVCAM_PROPERTY_MF_CALIBRATION_DATA              =   5003,
+        IVCAM_PROPERTY_MF_LASER_POWER                   =   5004,
+        IVCAM_PROPERTY_MF_ACCURACY                      =   5005,
+        IVCAM_PROPERTY_MF_INTENSITY_IMAGE_TYPE          =   5006,   //0 - (I0 - laser off), 1 - (I1 - Laser on), 2 - (I1-I0), default is I1.
+        IVCAM_PROPERTY_MF_MOTION_VS_RANGE_TRADE         =   5007,
+        IVCAM_PROPERTY_MF_POWER_GEAR                    =   5008,
+        IVCAM_PROPERTY_MF_FILTER_OPTION                 =   5009,
+        IVCAM_PROPERTY_MF_VERSION                       =   5010,
+        IVCAM_PROPERTY_MF_DEPTH_CONFIDENCE_THRESHOLD    =   5013,
+        IVCAM_PROPERTY_ACCELEROMETER_READING            =   3000,   // three values
+        IVCAM_PROPERTY_PROJECTION_SERIALIZABLE          =   3003,
+        IVCAM_PROPERTY_CUSTOMIZED                       =   0x04000000,
     };
 
     enum class FirmwareError : int32_t
@@ -268,7 +269,7 @@ namespace rsimpl { namespace f200
     // XU functions //
     //////////////////
 
-    const uvc::extension_unit depth_xu{ 1, 6, 1, { 0xA55751A1, 0xF3C5, 0x4A5E, { 0x8D, 0x5A, 0x68, 0x54, 0xB8, 0xFA, 0x27, 0x16 } } };
+    const uvc::extension_unit depth_xu {1, 6, 1, {0xA55751A1,0xF3C5,0x4A5E,{0x8D,0x5A,0x68,0x54,0xB8,0xFA,0x27,0x16}}};
 
     // N.B. f200 xu_read and xu_write hard code the xu interface to the depth suvdevice. There is only a
     // single *potentially* useful XU on the color device, so let's ignore it for now.
@@ -276,67 +277,54 @@ namespace rsimpl { namespace f200
     {
         uvc::get_control_with_retry(device, depth_xu, static_cast<int>(xu_ctrl), buffer, length);
     }
-
     void xu_write(uvc::device & device, uint8_t xu_ctrl, void * buffer, uint32_t length)
     {
         uvc::set_control_with_retry(device, depth_xu, static_cast<int>(xu_ctrl), buffer, length);
     }
-
     void get_laser_power(const uvc::device & device, uint8_t & laser_power)
     {
         xu_read(device, IVCAM_DEPTH_LASER_POWER, &laser_power, sizeof(laser_power));
     }
-
     void set_laser_power(uvc::device & device, uint8_t laser_power)
     {
         xu_write(device, IVCAM_DEPTH_LASER_POWER, &laser_power, sizeof(laser_power));
     }
-
     void get_accuracy(const uvc::device & device, uint8_t & accuracy)
     {
         xu_read(device, IVCAM_DEPTH_ACCURACY, &accuracy, sizeof(accuracy));
     }
-
     void set_accuracy(uvc::device & device, uint8_t accuracy)
     {
         xu_write(device, IVCAM_DEPTH_ACCURACY, &accuracy, sizeof(accuracy));
     }
-
     void get_motion_range(const uvc::device & device, uint8_t & motion_range)
     {
         xu_read(device, IVCAM_DEPTH_MOTION_RANGE, &motion_range, sizeof(motion_range));
     }
-
     void set_motion_range(uvc::device & device, uint8_t motion_range)
     {
         xu_write(device, IVCAM_DEPTH_MOTION_RANGE, &motion_range, sizeof(motion_range));
     }
-
     void get_filter_option(const uvc::device & device, uint8_t & filter_option)
     {
         xu_read(device, IVCAM_DEPTH_FILTER_OPTION, &filter_option, sizeof(filter_option));
     }
-
     void set_filter_option(uvc::device & device, uint8_t filter_option)
     {
         xu_write(device, IVCAM_DEPTH_FILTER_OPTION, &filter_option, sizeof(filter_option));
     }
-
     void get_confidence_threshold(const uvc::device & device, uint8_t & conf_thresh)
     {
         xu_read(device, IVCAM_DEPTH_CONFIDENCE_THRESH, &conf_thresh, sizeof(conf_thresh));
     }
-
     void set_confidence_threshold(uvc::device & device, uint8_t conf_thresh)
     {
         xu_write(device, IVCAM_DEPTH_CONFIDENCE_THRESH, &conf_thresh, sizeof(conf_thresh));
     }
-
     void get_dynamic_fps(const uvc::device & device, uint8_t & dynamic_fps)
     {
         return xu_read(device, IVCAM_DEPTH_DYNAMIC_FPS, &dynamic_fps, sizeof(dynamic_fps));
     }
-
     void set_dynamic_fps(uvc::device & device, uint8_t dynamic_fps)
     {
         return xu_write(device, IVCAM_DEPTH_DYNAMIC_FPS, &dynamic_fps, sizeof(dynamic_fps));
@@ -348,7 +336,7 @@ namespace rsimpl { namespace f200
 
     void claim_ivcam_interface(uvc::device & device)
     {
-        const uvc::guid IVCAM_WIN_USB_DEVICE_GUID = { 0x175695CD, 0x30D9, 0x4F87, { 0x8B, 0xE3, 0x5A, 0x82, 0x70, 0xF4, 0x9A, 0x31 } };
+        const uvc::guid IVCAM_WIN_USB_DEVICE_GUID = {0x175695CD, 0x30D9, 0x4F87, {0x8B, 0xE3, 0x5A, 0x82, 0x70, 0xF4, 0x9A, 0x31}};
         claim_interface(device, IVCAM_WIN_USB_DEVICE_GUID, IVCAM_MONITOR_INTERFACE);
     }
 
@@ -374,7 +362,7 @@ namespace rsimpl { namespace f200
 
         if (dataLength)
         {
-            memcpy(request + index, data, dataLength);
+            memcpy(request + index , data, dataLength);
             index += dataLength;
         }
 
@@ -398,25 +386,25 @@ namespace rsimpl { namespace f200
         int cur_index = 2;
         *(uint16_t *)(writePtr + cur_index) = preHeaderData;
         cur_index += sizeof(uint16_t);
-        *(int *)(writePtr + cur_index) = opCodeNumber;
+        *(int *)( writePtr + cur_index) = opCodeNumber;
         cur_index += sizeof(uint32_t);
-        *(int *)(writePtr + cur_index) = p1;
+        *(int *)( writePtr + cur_index) = p1;
         cur_index += sizeof(uint32_t);
-        *(int *)(writePtr + cur_index) = p2;
+        *(int *)( writePtr + cur_index) = p2;
         cur_index += sizeof(uint32_t);
-        *(int *)(writePtr + cur_index) = p3;
+        *(int *)( writePtr + cur_index) = p3;
         cur_index += sizeof(uint32_t);
-        *(int *)(writePtr + cur_index) = p4;
+        *(int *)( writePtr + cur_index) = p4;
         cur_index += sizeof(uint32_t);
 
         if (dataLength)
         {
-            memcpy(writePtr + cur_index, data, dataLength);
+            memcpy(writePtr + cur_index , data, dataLength);
             cur_index += dataLength;
         }
 
         length = cur_index;
-        *(uint16_t *)bufferToSend = (uint16_t)(length - header_size); // Length doesn't include header
+        *(uint16_t *) bufferToSend = (uint16_t)(length - header_size); // Length doesn't include header
     }
 
     void execute_usb_command(uvc::device & device, std::timed_mutex & mutex, uint8_t *out, size_t outSize, uint32_t & op, uint8_t * in, size_t & inSize)
@@ -471,21 +459,21 @@ namespace rsimpl { namespace f200
 
     void perform_and_send_monitor_command(uvc::device & device, std::timed_mutex & mutex, IVCAMCommand & newCommand)
     {
-        uint32_t opCodeXmit = (uint32_t)newCommand.cmd;
+        uint32_t opCodeXmit = (uint32_t) newCommand.cmd;
 
         IVCAMCommandDetails details;
         details.oneDirection = newCommand.oneDirection;
         details.TimeOut = newCommand.TimeOut;
 
         fill_usb_buffer(opCodeXmit,
-            newCommand.Param1,
-            newCommand.Param2,
-            newCommand.Param3,
-            newCommand.Param4,
-            newCommand.data,
-            newCommand.sizeOfSendCommandData,
-            details.sendCommandData,
-            details.sizeOfSendCommandData);
+                        newCommand.Param1,
+                        newCommand.Param2,
+                        newCommand.Param3,
+                        newCommand.Param4,
+                        newCommand.data,
+                        newCommand.sizeOfSendCommandData,
+                        details.sendCommandData,
+                        details.sizeOfSendCommandData);
 
         send_hw_monitor_command(device, mutex, details);
 
@@ -494,7 +482,7 @@ namespace rsimpl { namespace f200
             return;
 
         memcpy(newCommand.receivedOpcode, details.receivedOpcode, 4);
-        memcpy(newCommand.receivedCommandData, details.receivedCommandData, details.receivedCommandDataLength);
+        memcpy(newCommand.receivedCommandData, details.receivedCommandData,details.receivedCommandDataLength);
         newCommand.receivedCommandDataLength = details.receivedCommandDataLength;
 
         // endian? 
@@ -511,7 +499,7 @@ namespace rsimpl { namespace f200
     {
         IVCAMCommand cmd(IVCAMMonitorCommand::GVD);
         perform_and_send_monitor_command(device, mutex, cmd);
-        auto minSize = std::min((int)sz, cmd.receivedCommandDataLength);
+        auto minSize = std::min((int) sz, cmd.receivedCommandDataLength);
         memcpy(gvd, cmd.receivedCommandData, minSize);
     }
 
