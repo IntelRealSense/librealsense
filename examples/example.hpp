409eac548d30f2802c9f1ee6ac9208da37cd324f
dorodnic <dorodnic@gmail.com>
2016-08-24 12:17:00 +0300

    fixing manual merge issue

--- examples/example.hpp
+++ examples/example.hpp
@@ -125,7 +125,6 @@ public:
             }
             glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width/2, height/2, 0, GL_RGB, GL_UNSIGNED_BYTE, rgb.data());
             break;
-        default: break;
         }
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
