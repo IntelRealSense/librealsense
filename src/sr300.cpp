526781b8eb1b3f6ad349412a960ad38725e88c86
dorodnic <sergey.dorondicov@intel.com>
2016-09-07 11:54:12 +0300

    w/a for async controls on F200 / SR300

--- src/sr300.cpp
+++ src/sr300.cpp
@@ -212,7 +212,7 @@ namespace rsimpl
 
             if(uvc::is_pu_control(options[i]))
             {
-                values[i] = uvc::get_pu_control(get_device(), 0, options[i]);
+                values[i] = uvc::get_pu_control_with_retry(get_device(), 0, options[i]);
                 continue;
             }
 
