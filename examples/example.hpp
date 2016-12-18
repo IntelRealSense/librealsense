ab3f71944844b1cea6b2d90e2ed17d188e1c83e0
evgeni.raikhel <evgeni.raikhel@intel.com>
2016-12-18 18:31:19 +0200

    Bug fix hard-coded resolution value

--- examples/example.hpp
+++ examples/example.hpp
@@ -144,7 +144,7 @@ public:
         throw std::runtime_error("not a valid format");
         case RS_FORMAT_Z16:
         case RS_FORMAT_DISPARITY16:
-            rgb.resize(640 * 480 * 4);
+            rgb.resize(width * height * 4);
             make_depth_histogram(rgb.data(), reinterpret_cast<const uint16_t *>(data), width, height);
             glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, 640, 480, 0, GL_RGB, GL_UNSIGNED_BYTE, rgb.data());
             
