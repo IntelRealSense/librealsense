bd0716716596b3dc44d1ebb4f599760f1c33705a
dorodnic <dorodnic@gmail.com>
2017-05-04 11:03:48 +0300

    Bump version to 2.5.7

--- include/librealsense/rs2.h
+++ include/librealsense/rs2.h
@@ -16,7 +16,7 @@ extern "C" {
 
 #define RS2_API_MAJOR_VERSION    2
 #define RS2_API_MINOR_VERSION    5
-#define RS2_API_PATCH_VERSION    6
+#define RS2_API_PATCH_VERSION    7
 
 #define STRINGIFY(arg) #arg
 #define VAR_ARG_STRING(arg) STRINGIFY(arg)
