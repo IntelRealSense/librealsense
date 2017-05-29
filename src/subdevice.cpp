ad3b8581fa22e5b4c03b51fb503ac8a909fd72ee
dorodnic <dorodnic@gmail.com>
2017-05-29 17:55:43 +0300

    Fixing Clockwork issues (Tracked On: DSO-3748)

--- src/subdevice.cpp
+++ src/subdevice.cpp
@@ -245,7 +245,7 @@ namespace rsimpl2
         {
             uint32_t device_fourcc = reinterpret_cast<const big_endian<uint32_t>&>(elem);
             char fourcc[sizeof(device_fourcc) + 1];
-            memcpy(fourcc, &device_fourcc, sizeof(device_fourcc));
+            rsimpl2::copy(fourcc, &device_fourcc, sizeof(device_fourcc));
             fourcc[sizeof(device_fourcc)] = 0;
             LOG_WARNING("Unutilized format " << fourcc);
         }
@@ -257,7 +257,7 @@ namespace rsimpl2
             {
                 uint32_t device_fourcc = reinterpret_cast<const big_endian<uint32_t>&>(elem);
                 char fourcc[sizeof(device_fourcc) + 1];
-                memcpy(fourcc, &device_fourcc, sizeof(device_fourcc));
+                rsimpl2::copy(fourcc, &device_fourcc, sizeof(device_fourcc));
                 fourcc[sizeof(device_fourcc)] = 0;
                 ss << fourcc << std::endl;
             }
