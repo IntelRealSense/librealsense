8b74d6be5d9f505452b570505bcb5d2f319aa8f5
Lior Ramati <lior.ramati@intel.com>
2017-08-16 17:12:06 +0300

    Get Multicam example working again

--- examples/example.hpp
+++ examples/example.hpp
@@ -72,6 +72,7 @@ public:
 
         if (!gl_handle)
             glGenTextures(1, &gl_handle);
+        GLenum err = glGetError();
 
         auto format = frame.get_profile().format();
         width = frame.get_width();
@@ -99,7 +100,6 @@ public:
 
     GLuint get_gl_handle() { return gl_handle; }
 
-private:
     void show(const rect& r) const
     {
         if (!gl_handle)
@@ -118,7 +118,7 @@ private:
 
         draw_text(r.x + 15, r.y + 20, rs2_stream_to_string(stream));
     }
-
+private:
     GLuint gl_handle = 0;
     int width = 0;
     int height = 0;
--- examples/multicam/rs-multicam.cpp
+++ examples/multicam/rs-multicam.cpp
@@ -5,109 +5,107 @@
 #include <librealsense/rsutil2.hpp>
 #include "example.hpp"
 
-#include <iostream>
+#include <string>
+#include <map>
 #include <algorithm>
-#include <sstream>
-
-
-using namespace rs2;
-using namespace std;
+#include <mutex>
 
 /***************
 
 ***************/
 
+struct dev_data { rs2::device dev; texture tex; rs2::frame_queue queue; };
+struct state { std::mutex m; std::map<std::string, dev_data> frames; rs2::colorizer depth_to_frame; };
+void add_device(state& app_state, rs2::device& dev);
+void remove_devices(state& app_state, const rs2::event_information& info);
+
 int main(int argc, char * argv[]) try
 {
-    log_to_console(RS2_LOG_SEVERITY_WARN);
+    // Create a simple OpenGL window for rendering:
+    window app(1280, 960, "CPP Multi-Camera Example");
+    state app_state;
 
-    vector<texture> buffers;
+    using namespace rs2;
 
-    context ctx; // Create librealsense context
-    auto devices = ctx.query_devices(); // Query the list of connected RealSense devices
-    if (devices.size() == 0)
+    // Create librealsense context for managing devices
+    context ctx;
+
+    // Register callback for tracking which devices are currently connected
+    ctx.set_devices_changed_callback([&app_state](rs2::event_information& info){
+        remove_devices(app_state, info);
+
+        for (auto&& dev : info.get_new_devices())
+            add_device(app_state, dev);
+    });
+
+    texture t;
+    // Populate the list the first time
+    auto devs = ctx.query_devices();
+    for (auto& dev : devs) // Query the list of connected RealSense devices
+        add_device(app_state, dev);
+
+    std::map<std::string, dev_data> m;
+    m["f"] = dev_data();
+    for (auto&& kvp : m)
+        auto j = kvp.second;
+
+    while (app)
     {
-        throw runtime_error("No device detected. Is it plugged in?");
-    }
+        std::lock_guard<std::mutex> lock(app_state.m);
+        int c = std::ceil(std::sqrt(app_state.frames.size()));
+        int r = std::ceil(app_state.frames.size() / static_cast<float>(c));
+        float w = app.width()/c;
+        float h = app.height()/r;
 
-    vector<frame_queue> syncers;
+        int stream_no = 0;
 
-    // Configure and start our devices
-    for(auto&& dev : devices)
-    {
-        cout << "Starting " << dev.get_info(RS2_CAMERA_INFO_NAME) << "... ";
-        auto sensor = dev.query_sensors().front();
-        auto modes = sensor.get_stream_profiles();
-        sensor.open(modes.back());
-        syncers.emplace_back();
-        sensor.start(syncers.back());
-        cout << "done." << endl;
-    }
-
-    // Depth and color
-    buffers.resize(devices.size());
-
-    // Open a GLFW window
-    glfwInit();
-    ostringstream ss; ss << "CPP Multi-Camera Example";
-    auto win = glfwCreateWindow(1280, 960, ss.str().c_str(), 0, 0);
-    glfwMakeContextCurrent(win);
-
-    int windowWidth, windowHeight;
-    glfwGetWindowSize(win, &windowWidth, &windowHeight);
-
-    // Does not account for correct aspect ratios
-    auto perTextureWidth = 2 * static_cast<float>(windowWidth / devices.size());
-    auto perTextureHeight = 480.0f;
-
-    while (!glfwWindowShouldClose(win))
-    {
-        // Wait for new images
-        glfwPollEvents();
-
-        for(int i = 0; i < syncers.size(); i++)
+        for (auto&& kvp : app_state.frames)
         {
-            frame frame;
-            if (syncers[i].poll_for_frame(&frame))
-            {
-                buffers[i].upload(frame);
-            }
+            rs2::frame f;
+            if (kvp.second.queue.poll_for_frame(&f))
+                kvp.second.tex.upload(f);
+
+            kvp.second.tex.show({ w*(stream_no%c), h*(stream_no / c), w, h });
+            ++stream_no;
         }
-
-        // Draw the images
-        int w,h;
-        glfwGetFramebufferSize(win, &w, &h);
-        glViewport(0, 0, w, h);
-        glClear(GL_COLOR_BUFFER_BIT);
-
-        glfwGetWindowSize(win, &w, &h);
-        glPushMatrix();
-        glOrtho(0, w, h, 0, -1, +1);
-        glPixelZoom(1, -1);
-
-        for(int i = 0; i < syncers.size(); i++)
-        {
-            buffers[i].show({ (i / 2) * perTextureWidth, (i % 2) * perTextureHeight, perTextureWidth, perTextureHeight }, 1);
-        }
-
-        glPopMatrix();
-        glfwSwapBuffers(win);
     }
-
-    glfwDestroyWindow(win);
-    glfwTerminate();
-
-    buffers.clear();
 
     return EXIT_SUCCESS;
 }
-catch(const error & e)
+catch(const rs2::error & e)
 {
-    cerr << "RealSense error calling " << e.get_failed_function() << "(" << e.get_failed_args() << "):\n    " << e.what() << endl;
+    std::cerr << "RealSense error calling " << e.get_failed_function() << "(" << e.get_failed_args() << "):\n    " << e.what() << std::endl;
     return EXIT_FAILURE;
 }
-catch(const exception & e)
+catch(const std::exception & e)
 {
-    cerr << e.what() << endl;
+    std::cerr << e.what() << std::endl;
     return EXIT_FAILURE;
+}
+
+void add_device(state& app_state, rs2::device& dev)
+{
+    rs2::util::config c;
+    if (c.can_enable_stream(dev, RS2_STREAM_DEPTH, rs2::preset::best_quality))
+        c.enable_stream(RS2_STREAM_DEPTH, rs2::preset::best_quality);
+    else
+        c.enable_stream(RS2_STREAM_COLOR, rs2::preset::best_quality);
+    auto s = c.open(dev);
+    std::string sn(dev.get_info(RS2_CAMERA_INFO_SERIAL_NUMBER));
+    std::lock_guard<std::mutex> lock(app_state.m);
+    app_state.frames.emplace(sn, dev_data{ dev, texture(), rs2::frame_queue(1) });
+    s.start([&app_state, sn](rs2::frame& f) {
+        std::lock_guard<std::mutex> lock(app_state.m);
+        app_state.frames[sn].queue(app_state.depth_to_frame(f));
+    });
+}
+void remove_devices(state& app_state, const rs2::event_information& info)
+{
+    std::lock_guard<std::mutex> lock(app_state.m);
+    auto itr = app_state.frames.begin();
+    while (itr != app_state.frames.end())
+        if (info.was_removed(itr->second.dev))
+            app_state.frames.erase(itr++);
+        else
+            ++itr;
 }
