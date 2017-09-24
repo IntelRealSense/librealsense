2e27d821422f8f8b4a1017544e388105639fbd3b
Ziv Shahaf <ziv.shahaf@intel.com>
2017-09-24 13:40:10 +0300

    Changed GUI display for const value options

--- common/model-views.cpp
+++ common/model-views.cpp
@@ -245,7 +245,20 @@ namespace rs2
                         if (read_only)
                         {
                             ImVec2 vec{ 0, 14 };
-                            ImGui::ProgressBar(value / 100.f, vec, std::to_string((int)value).c_str());
+                            std::string text = (value == (int)value) ? std::to_string((int)value) : std::to_string(value);
+                            if (range.min != range.max)
+                            {
+                                ImGui::ProgressBar((value / (range.max - range.min)), vec, text.c_str());
+                            }
+                            else //constant value options
+                            {
+                                auto c = ImGui::ColorConvertU32ToFloat4(ImGui::GetColorU32(ImGuiCol_FrameBg));
+                                ImGui::PushStyleColor(ImGuiCol_FrameBgActive, c);
+                                ImGui::PushStyleColor(ImGuiCol_FrameBgHovered, c);
+                                float dummy = (int)value;
+                                ImGui::DragFloat(id.c_str(), &dummy, 1, 0, 0, text.c_str());
+                                ImGui::PopStyleColor(2);
+                            }
                         }
                         else if (is_all_integers())
                         {
