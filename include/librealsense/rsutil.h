0aef4fd064a48cf95ced5fd81b61400b959a264b
Konrad Malkowski <kmalkowski@6river.com>
2017-10-02 10:24:31 -0400

    Optimize performance of image processing in realsense library

--- include/librealsense/rsutil.h
+++ include/librealsense/rsutil.h
@@ -8,20 +8,25 @@
 #include "assert.h"
 
 /* Given a point in 3D space, compute the corresponding pixel coordinates in an image with no distortion or forward distortion coefficients produced by the same camera */
-static void rs_project_point_to_pixel(float pixel[2], const struct rs_intrinsics * intrin, const float point[3])
+static inline void rs_project_point_to_pixel(float pixel[2], const struct rs_intrinsics * intrin, const float point[3])
 {
     assert(intrin->model != RS_DISTORTION_INVERSE_BROWN_CONRADY); // Cannot project to an inverse-distorted image
     assert(intrin->model != RS_DISTORTION_FTHETA); // Cannot project to an ftheta image
 
-    float x = point[0] / point[2], y = point[1] / point[2];
+    float multiplier = 1 / point[2];
+    float x = point[0] * multiplier, y = point[1] * multiplier;
     if(intrin->model == RS_DISTORTION_MODIFIED_BROWN_CONRADY)
     {
-        float r2  = x*x + y*y;
+        float x2 = x*x;
+        float y2 = y*y;
+        float xy = x * y;
+        float r2  = x2 + y2;
+
         float f = 1 + intrin->coeffs[0]*r2 + intrin->coeffs[1]*r2*r2 + intrin->coeffs[4]*r2*r2*r2;
         x *= f;
         y *= f;
-        float dx = x + 2*intrin->coeffs[2]*x*y + intrin->coeffs[3]*(r2 + 2*x*x);
-        float dy = y + 2*intrin->coeffs[3]*x*y + intrin->coeffs[2]*(r2 + 2*y*y);
+        float dx = x + 2*intrin->coeffs[2]*xy + intrin->coeffs[3]*(r2 + 2*x2);
+        float dy = y + 2*intrin->coeffs[3]*xy + intrin->coeffs[2]*(r2 + 2*y2);
         x = dx;
         y = dy;
     }
@@ -30,19 +35,32 @@ static void rs_project_point_to_pixel(float pixel[2], const struct rs_intrinsics
 }
 
 /* Given pixel coordinates and depth in an image with no distortion or inverse distortion coefficients, compute the corresponding point in 3D space relative to the same camera */
-static void rs_deproject_pixel_to_point(float point[3], const struct rs_intrinsics * intrin, const float pixel[2], float depth)
+static inline void rs_deproject_pixel_to_point(float point[3], const struct rs_intrinsics * intrin, const float pixel[2], float depth, float invfx = 0.0, float invfy = 0.0)
 {
     assert(intrin->model != RS_DISTORTION_MODIFIED_BROWN_CONRADY); // Cannot deproject from a forward-distorted image
     assert(intrin->model != RS_DISTORTION_FTHETA); // Cannot deproject to an ftheta image
 
-    float x = (pixel[0] - intrin->ppx) / intrin->fx;
-    float y = (pixel[1] - intrin->ppy) / intrin->fy;
+    float x = 0;
+    if (invfx == 0.0) {
+        x = (pixel[0] - intrin->ppx) / intrin->fx;
+    } else {
+        x = (pixel[0] - intrin->ppx) * invfx;
+    }
+    float y = 0;
+    if (invfy == 0.0) {
+        y = (pixel[1] - intrin->ppy) / intrin->fy;
+    } else {
+        y = (pixel[1] - intrin->ppy) * invfy;
+    }
     if(intrin->model == RS_DISTORTION_INVERSE_BROWN_CONRADY)
     {
-        float r2  = x*x + y*y;
+        float x2 = x*x;
+        float y2 = y*y;
+        float xy = x * y;
+        float r2  = x2 + y2;
         float f = 1 + intrin->coeffs[0]*r2 + intrin->coeffs[1]*r2*r2 + intrin->coeffs[4]*r2*r2*r2;
-        float ux = x*f + 2*intrin->coeffs[2]*x*y + intrin->coeffs[3]*(r2 + 2*x*x);
-        float uy = y*f + 2*intrin->coeffs[3]*x*y + intrin->coeffs[2]*(r2 + 2*y*y);
+        float ux = x*f + 2*intrin->coeffs[2]*xy + intrin->coeffs[3]*(r2 + 2*x2);
+        float uy = y*f + 2*intrin->coeffs[3]*xy + intrin->coeffs[2]*(r2 + 2*y2);
         x = ux;
         y = uy;
     }
@@ -52,7 +70,7 @@ static void rs_deproject_pixel_to_point(float point[3], const struct rs_intrinsi
 }
 
 /* Transform 3D coordinates relative to one sensor to 3D coordinates relative to another viewpoint */
-static void rs_transform_point_to_point(float to_point[3], const struct rs_extrinsics * extrin, const float from_point[3])
+static inline void rs_transform_point_to_point(float to_point[3], const struct rs_extrinsics * extrin, const float from_point[3])
 {
     to_point[0] = extrin->rotation[0] * from_point[0] + extrin->rotation[3] * from_point[1] + extrin->rotation[6] * from_point[2] + extrin->translation[0];
     to_point[1] = extrin->rotation[1] * from_point[0] + extrin->rotation[4] * from_point[1] + extrin->rotation[7] * from_point[2] + extrin->translation[1];
