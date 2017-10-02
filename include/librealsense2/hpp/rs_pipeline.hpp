2432556452c92658e6be21d5186e9b2ab4f4fecf
Ziv Shahaf <ziv.shahaf@intel.com>
2017-10-02 10:53:27 +0300

    Line limit of 130 to documentation

--- include/librealsense2/hpp/rs_pipeline.hpp
+++ include/librealsense2/hpp/rs_pipeline.hpp
@@ -11,8 +11,9 @@
 namespace rs2
 {
     /**
-    * The pipeline profile includes a device and a selection of active streams, with specific profile. The profile is a selection
-    * of the above under filters and conditions defined by the pipeline. Streams may belong to more than one sensor of the device.
+    * The pipeline profile includes a device and a selection of active streams, with specific profile. 
+    * The profile is a selection of the above under filters and conditions defined by the pipeline. 
+    * Streams may belong to more than one sensor of the device.
     */
     class pipeline_profile
     {
@@ -73,7 +74,7 @@ namespace rs2
         }
 
         /**
-        * Coversion to boolean value to test for the object's validity
+        * Conversion to boolean value to test for the object's validity
         *
         * \return true iff the profile is valid
         */
@@ -116,15 +117,17 @@ namespace rs2
 
         /**
         * Enable a device stream explicitly, with selected stream parameters.
-        * The method allows the application to request a stream with specific configuration. If no stream is explicitly enabled, the pipeline
-        * configures the device and its streams according to the attached computer vision modules and processing blocks requirements, or
-        * default configuration for the first available device.
-        * The application can configure any of the input stream parameters according to its requirement, or set to 0 for don't care value.
-        * The config accumulates the application calls for enable configuration methods, until the configuration is applied. Multiple enable
-        * stream calls for the same stream with conflicting parameters override each other, and the last call is maintained.
-        * Upon calling \c resolve(), the config checks for conflicts between the application configuration requests and the attached computer
-        * vision modules and processing blocks requirements, and fails if conflicts are found. Before \c resolve() is called, no conflict
-        * check is done.
+        * The method allows the application to request a stream with specific configuration. If no stream is explicitly enabled,
+        * the pipeline configures the device and its streams according to the attached computer vision modules and processing 
+        * blocks requirements, or default configuration for the first available device.
+        * The application can configure any of the input stream parameters according to its requirement, or set to 0 for don't 
+        * care value.
+        * The config accumulates the application calls for enable configuration methods, until the configuration is applied. 
+        * Multiple enable stream calls for the same stream with conflicting parameters override each other, and the last call is
+        * maintained.
+        * Upon calling \c resolve(), the config checks for conflicts between the application configuration requests and the 
+        * attached computer vision modules and processing blocks requirements, and fails if conflicts are found. 
+        * Before \c resolve() is called, no conflict check is done.
         *
         * \param[in] stream_type    Stream type to be enabled
         * \param[in] stream_index   Stream index, used for multiple streams of the same type. -1 indicates any.
@@ -167,8 +170,8 @@ namespace rs2
         /**
         * Enable all device streams explicitly.
         * The conditions and behavior of this method are similar to those of \c enable_stream().
-        * This filter enables all raw streams of the selected device. The device is either selected explicitly by the application,
-        * or by the pipeline requirements or default. The list of streams is device dependent.
+        * This filter enables all raw streams of the selected device. The device is either selected explicitly by the 
+        * application, or by the pipeline requirements or default. The list of streams is device dependent.
         */
         void enable_all_streams()
         {
@@ -180,8 +183,8 @@ namespace rs2
         /**
         * Select a specific device explicitly by its serial number, to be used by the pipeline.
         * The conditions and behavior of this method are similar to those of \c enable_stream().
-        * This method is required if the application needs to set device or sensor settings prior to pipeline streaming, to enforce
-        * the pipeline to use the configured device.
+        * This method is required if the application needs to set device or sensor settings prior to pipeline streaming, 
+        * to enforce the pipeline to use the configured device.
         *
         * \param[in] Serial device serial number, as returned by RS2_CAMERA_INFO_SERIAL_NUMBER
         */
@@ -194,8 +197,8 @@ namespace rs2
 
         /**
         * Select a recorded device from a file, to be used by the pipeline through playback.
-        * The device available streams are as recorded to the file, and \c resolve() considers only this device and configuration
-        * as available.
+        * The device available streams are as recorded to the file, and \c resolve() considers only this device and 
+        * configuration as available.
         * This request cannot be used if enable_record_to_file() is called for the current config, and vise versa
         *
         * \param[in] file_name  The playback file of the device
@@ -249,16 +252,18 @@ namespace rs2
 
         /**
         * Resolve the configuration filters, to find a matching device and streams profiles.
-        * The method resolves the user configuration filters for the device and streams, and combines them with the requirements of
-        * the computer vision modules and processing blocks attached to the pipeline. If there are no conflicts of requests, it looks
-        * for an available device, which can satisfy all requests, and selects the first matching streams configuration. In the absence
-        * of any request, the rs2::config selects the first available device and the first color and depth streams configuration.
+        * The method resolves the user configuration filters for the device and streams, and combines them with the requirements
+        * of the computer vision modules and processing blocks attached to the pipeline. If there are no conflicts of requests,
+        * it looks for an available device, which can satisfy all requests, and selects the first matching streams configuration.
+        * In the absence of any request, the rs2::config selects the first available device and the first color and depth 
+        * streams configuration.
         * The pipeline profile selection during \c start() follows the same method. Thus, the selected profile is the same, if no
         * change occurs to the available devices occurs.
-        * Resolving the pipeline configuration provides the application access to the pipeline selected device for advanced control.
-        * The returned configuration is not applied to the device, so the application doesn't own the device sensors. However, the
-        * application can call \c enable_device(), to enforce the device returned by this method is selected by pipeline \c start(),
-        * and configure the device and sensors options or extensions before streaming starts.
+        * Resolving the pipeline configuration provides the application access to the pipeline selected device for advanced 
+        * control.
+        * The returned configuration is not applied to the device, so the application doesn't own the device sensors. However, 
+        * the application can call \c enable_device(), to enforce the device returned by this method is selected by pipeline \c 
+        * start(), and configure the device and sensors options or extensions before streaming starts.
         *
         * \param[in] p  The pipeline for which the selected filters are applied
         * \return       A matching device and streams profile, which satisfies the filters and pipeline requests.
@@ -331,9 +336,10 @@ namespace rs2
 
         /**
         * Start the pipeline streaming with its default configuration.
-        * The pipeline streaming loop captures samples from the device, and delivers them to the attached computer vision modules
-        * and processing blocks, according to each module requirements and threading model.
-        * During the loop execution, the application can access the camera streams by calling \c wait_for_frames() or \c poll_for_frames().
+        * The pipeline streaming loop captures samples from the device, and delivers them to the attached computer vision 
+        * modules and processing blocks, according to each module requirements and threading model.
+        * During the loop execution, the application can access the camera streams by calling \c wait_for_frames() or 
+        * \c poll_for_frames().
         * The streaming loop runs until the pipeline is stopped.
         * Starting the pipeline is possible only when it is not started. If the pipeline was started, an exception is raised.
         *
@@ -354,14 +360,16 @@ namespace rs2
         * Start the pipeline streaming according to the configuraion.
         * The pipeline streaming loop captures samples from the device, and delivers them to the attached computer vision modules
         * and processing blocks, according to each module requirements and threading model.
-        * During the loop execution, the application can access the camera streams by calling \c wait_for_frames() or \c poll_for_frames().
+        * During the loop execution, the application can access the camera streams by calling \c wait_for_frames() or 
+        * \c poll_for_frames().
         * The streaming loop runs until the pipeline is stopped.
         * Starting the pipeline is possible only when it is not started. If the pipeline was started, an exception is raised.
         * The pipeline selects and activates the device upon start, according to configuration or a default configuration.
-        * When the rs2::config is provided to the method, the pipeline tries to activate the config \c resolve() result. If the application
-        * requests are conflicting with pipeline computer vision modules or no matching device is available on the platform, the method fails.
-        * Available configurations and devices may change between config \c resolve() call and pipeline start, in case devices are connected
-        * or disconnected, or another application acquires ownership of a device.
+        * When the rs2::config is provided to the method, the pipeline tries to activate the config \c resolve() result. 
+        * If the application requests are conflicting with pipeline computer vision modules or no matching device is available on
+        * the platform, the method fails.
+        * Available configurations and devices may change between config \c resolve() call and pipeline start, in case devices 
+        * are connected or disconnected, or another application acquires ownership of a device.
         *
         * \param[in] config   A rs2::config with requested filters on the pipeline configuration. By default no filters are applied.
         * \return             The actual pipeline device and streams profile, which was successfully configured to the streaming device.
@@ -380,8 +388,9 @@ namespace rs2
 
         /**
         * Stop the pipeline streaming.
-        * The pipeline stops delivering samples to the attached computer vision modules and processing blocks, stops the device streaming
-        * and releases the device resources used by the pipeline. It is the application's responsibility to release any frame reference it owns.
+        * The pipeline stops delivering samples to the attached computer vision modules and processing blocks, stops the device 
+        * streaming and releases the device resources used by the pipeline. It is the application's responsibility to release any
+        * frame reference it owns.
         * The method takes effect only after \c start() was called, otherwise an exception is raised.
         */
         void stop()
@@ -395,11 +404,11 @@ namespace rs2
         * Wait until a new set of frames becomes available.
         * The frames set includes time-synchronized frames of each enabled stream in the pipeline.
         * The method blocks the calling thread, and fetches the latest unread frames set.
-        * Device frames, which were produced while the function wasn't called, are dropped. To avoid frame drops, this method should be called
-        * as fast as the device frame rate.
-        * The application can maintain the frames handles to defer processing. However, if the application maintains too long history, the device
-        * may lack memory resources to produce new frames, and the following call to this method shall fail to retrieve new frames, until resources
-        * are retained.
+        * Device frames, which were produced while the function wasn't called, are dropped. To avoid frame drops, this method 
+        * should be called as fast as the device frame rate.
+        * The application can maintain the frames handles to defer processing. However, if the application maintains too long 
+        * history, the device may lack memory resources to produce new frames, and the following call to this method shall fail 
+        * to retrieve new frames, until resources are retained.
         *
         * \param[in] timeout_ms   Max time in milliseconds to wait until an exception will be thrown
         * \return                 Set of time synchronized frames, one from each active stream
@@ -416,13 +425,13 @@ namespace rs2
         /**
         * Check if a new set of frames is available and retrieve the latest undelivered set.
         * The frames set includes time-synchronized frames of each enabled stream in the pipeline.
-        * The method returns without blocking the calling thread, with status of new frames available or not. If available, it fetches the
-        * latest frames set.
-        * Device frames, which were produced while the function wasn't called, are dropped. To avoid frame drops, this method should be called
-        * as fast as the device frame rate.
-        * The application can maintain the frames handles to defer processing. However, if the application maintains too long history, the device
-        * may lack memory resources to produce new frames, and the following calls to this method shall return no new frames, until resources are
-        * retained.
+        * The method returns without blocking the calling thread, with status of new frames available or not. 
+        * If available, it fetches the latest frames set.
+        * Device frames, which were produced while the function wasn't called, are dropped. 
+        * To avoid frame drops, this method should be called as fast as the device frame rate.
+        * The application can maintain the frames handles to defer processing. However, if the application maintains too long
+        * history, the device may lack memory resources to produce new frames, and the following calls to this method shall 
+        * return no new frames, until resources are retained.
         *
         * \param[out] f     Frames set handle
         * \return           True if new set of time synchronized frames was stored to f, false if no new frames set is available
@@ -446,7 +455,8 @@ namespace rs2
         * Return the active device and streams profiles, used by the pipeline.
         * The pipeline streams profiles are selected during \c start(). The method returns a valid result only when the pipeline is active -
         * between calls to \c start() and \c stop().
-        * After \c stop() is called, the pipeline doesn't own the device, thus, the pipeline selected device may change in subsequent activations.
+        * After \c stop() is called, the pipeline doesn't own the device, thus, the pipeline selected device may change in 
+        * subsequent activations.
         *
         * \return  The actual pipeline device and streams profile, which was successfully configured to the streaming device on start.
         */
