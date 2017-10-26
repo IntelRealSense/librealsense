e78b7a3d3b764e1cc56d5e8f469f8e1c521d7711
Konrad Malkowski <kmalkowski@6river.com>
2017-10-26 17:22:31 -0400

    Remove unnecessary build system changes

--- cmake_uninstall.cmake
+++ cmake_uninstall.cmake
@@ -1,14 +1,14 @@
-IF(NOT EXISTS "/home/konrad/mfp_workspace/src/librealsense/install_manifest.txt")
-  MESSAGE(WARNING "Cannot find install manifest: \"/home/konrad/mfp_workspace/src/librealsense/install_manifest.txt\"")
+IF(NOT EXISTS "@CMAKE_CURRENT_BINARY_DIR@/install_manifest.txt")
+  MESSAGE(WARNING "Cannot find install manifest: \"@CMAKE_CURRENT_BINARY_DIR@/install_manifest.txt\"")
   MESSAGE(STATUS "Uninstall targets will be skipped")
-ELSE(NOT EXISTS "/home/konrad/mfp_workspace/src/librealsense/install_manifest.txt")
-  FILE(READ "/home/konrad/mfp_workspace/src/librealsense/install_manifest.txt" files)
+ELSE(NOT EXISTS "@CMAKE_CURRENT_BINARY_DIR@/install_manifest.txt")
+  FILE(READ "@CMAKE_CURRENT_BINARY_DIR@/install_manifest.txt" files)
   STRING(REGEX REPLACE "\n" ";" files "${files}")
   FOREACH(file ${files})
     MESSAGE(STATUS "Uninstalling \"$ENV{DESTDIR}${file}\"")
     IF(EXISTS "$ENV{DESTDIR}${file}")
 	  EXEC_PROGRAM(
-	    "/usr/bin/cmake" ARGS "-E remove \"$ENV{DESTDIR}${file}\""
+	    "@CMAKE_COMMAND@" ARGS "-E remove \"$ENV{DESTDIR}${file}\""
 	    OUTPUT_VARIABLE rm_out
 	    RETURN_VALUE rm_retval
 	    )
@@ -19,4 +19,4 @@ ELSE(NOT EXISTS "/home/konrad/mfp_workspace/src/librealsense/install_manifest.tx
 	  MESSAGE(STATUS "File \"$ENV{DESTDIR}${file}\" does not exist.")
     ENDIF(EXISTS "$ENV{DESTDIR}${file}")
   ENDFOREACH(file)
-ENDIF(NOT EXISTS "/home/konrad/mfp_workspace/src/librealsense/install_manifest.txt")
+ENDIF(NOT EXISTS "@CMAKE_CURRENT_BINARY_DIR@/install_manifest.txt")
