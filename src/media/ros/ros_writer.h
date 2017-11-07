c01829b766fc31348d91c65c0bf8a93af13435c8
Ziv Shahaf <ziv.shahaf@intel.com>
2017-11-07 15:15:43 +0200

    Added writing of imu frames to file

--- src/media/ros/ros_writer.h
+++ src/media/ros/ros_writer.h
@@ -16,6 +16,7 @@
 #include "std_msgs/String.h"
 #include "diagnostic_msgs/KeyValue.h"
 #include "sensor_msgs/Image.h"
+#include "sensor_msgs/Imu.h"
 #include "realsense_msgs/StreamInfo.h"
 #include "sensor_msgs/CameraInfo.h"
 #include "ros_file_format.h"
@@ -58,17 +59,17 @@ namespace librealsense
                 return;
             }
 
-           /* if (Is<motion_frame>(data.frame.get()))
+            if (Is<motion_frame>(frame.frame))
             {
-                write_motion(data);
+                write_motion_frame(stream_id, timestamp, std::move(frame));
                 return;
             }
 
-            if (Is<pose_frame>(data.frame.get()))
+            if (Is<pose_frame>(frame.frame))
             {
-                write_pose(data);
+                write_pose_frame(stream_id, timestamp, std::move(frame));
                 return;
-            }*/
+            }
         }
 
         void write_snapshot(uint32_t device_index, const nanoseconds& timestamp, rs2_extension type, const std::shared_ptr<extension_snapshot>& snapshot) override
@@ -94,7 +95,72 @@ namespace librealsense
             write_message(ros_topic::file_version_topic(), get_static_file_info_timestamp(), msg);
         }
 
-        void write_video_frame(stream_identifier stream_id, const nanoseconds& timestamp, const frame_holder& frame)
+
+        void write_frame_metadata(const stream_identifier& stream_id, const nanoseconds& timestamp, frame_interface* frame)
+        {
+            auto metadata_topic = ros_topic::frame_metadata_topic(stream_id);
+            diagnostic_msgs::KeyValue system_time;
+            system_time.key = "system_time";
+            system_time.value = std::to_string(frame->get_frame_system_time());
+            write_message(metadata_topic, timestamp, system_time);
+
+            diagnostic_msgs::KeyValue timestamp_domain;
+            timestamp_domain.key = "timestamp_domain";
+            timestamp_domain.value = to_string() << frame->get_frame_timestamp_domain();
+            write_message(metadata_topic, timestamp, timestamp_domain);
+
+            for (int i = 0; i < static_cast<rs2_frame_metadata_value>(rs2_frame_metadata_value::RS2_FRAME_METADATA_COUNT); i++)
+            {
+                rs2_frame_metadata_value type = static_cast<rs2_frame_metadata_value>(i);
+                if (frame->supports_frame_metadata(type))
+                {
+                    auto md = frame->get_frame_metadata(type);
+                    diagnostic_msgs::KeyValue md_msg;
+                    md_msg.key = to_string() << type;
+                    md_msg.value = std::to_string(md);
+                    write_message(metadata_topic, timestamp, md_msg);
+                }
+            }
+        }
+
+        void write_extrinsics(const stream_identifier& stream_id, const frame_holder& frame)
+        {
+            if (m_extrinsics_msgs.find(stream_id) != m_extrinsics_msgs.end())
+            {
+                return; //already wrote it
+            }
+            auto& dev = frame.frame->get_sensor()->get_device();
+            uint32_t reference_id = 0;
+            rs2_extrinsics ext;
+            std::tie(reference_id, ext) = dev.get_extrinsics(*frame.frame->get_stream());
+            geometry_msgs::Transform tf_msg;
+            convert(ext, tf_msg);
+            write_message(ros_topic::stream_extrinsic_topic(stream_id, reference_id), get_static_file_info_timestamp(), tf_msg);
+            m_extrinsics_msgs[stream_id] = tf_msg;
+        }
+
+        void write_additional_frame_messages(const stream_identifier& stream_id, const nanoseconds& timestamp, frame_interface* frame)
+        {
+            try
+            {
+                write_frame_metadata(stream_id, timestamp, frame);
+            }
+            catch (std::exception const& e)
+            {
+                LOG_WARNING("Failed to write motion metadata for " << stream_id << ". Exception: " << e.what());
+            }
+
+            try
+            {
+                write_extrinsics(stream_id, frame);
+            }
+            catch (std::exception const& e)
+            {
+                LOG_WARNING("Failed to write stream extrinsics for " << stream_id << ". Exception: " << e.what());
+            }
+        }
+
+        void write_video_frame(const stream_identifier& stream_id, const nanoseconds& timestamp, const frame_holder& frame)
         {
             sensor_msgs::Image image;
             auto vid_frame = dynamic_cast<librealsense::video_frame*>(frame.frame);
@@ -115,65 +181,49 @@ namespace librealsense
             image.header.frame_id = TODO_CORRECT_ME;
             auto image_topic = ros_topic::frame_data_topic(stream_id);
             write_message(image_topic, timestamp, image);
-            try
-            {
-                write_image_metadata(stream_id, timestamp, vid_frame);
-            }
-            catch (std::exception const& e)
-            {
-                LOG_WARNING("Failed to write image metadata for " << stream_id.device_index << "/" << stream_id.sensor_index << "/" << stream_id.stream_type << "/" << stream_id.stream_index << " ts: " << timestamp.count() << ". Exception: " << e.what());
-            }
-            try
-            {
-                write_extrinsics(stream_id, frame);
-            }
-            catch (std::exception const& e)
-            {
-                LOG_WARNING("Failed to write stream extrinsics for " << stream_id.device_index << "/" << stream_id.sensor_index << "/" << stream_id.stream_type << "/" << stream_id.stream_index << " ts: " << timestamp.count() << ". Exception: " << e.what());
-            }
+            write_additional_frame_messages(stream_id, timestamp, frame);
         }
 
-        void write_extrinsics(const stream_identifier& stream_id, const frame_holder& frame)
+        void write_motion_frame(const stream_identifier& stream_id, const nanoseconds& timestamp, const frame_holder& frame)
         {
-            if(m_extrinsics_msgs.find(stream_id) != m_extrinsics_msgs.end())
+            sensor_msgs::Imu imu_msg;
+            if (!frame)
             {
-                return; //already wrote it
+                throw io_exception("Null frame passed to write_motion_frame");
             }
-            auto& dev = frame.frame->get_sensor()->get_device();
-            uint32_t reference_id = 0;
-            rs2_extrinsics ext;
-            std::tie(reference_id, ext) = dev.get_extrinsics(*frame.frame->get_stream());
-            geometry_msgs::Transform tf_msg;
-            convert(ext, tf_msg);
-            write_message(ros_topic::stream_extrinsic_topic(stream_id, reference_id), get_static_file_info_timestamp(), tf_msg);
-            m_extrinsics_msgs[stream_id] = tf_msg;
+            
+            imu_msg.header.seq = static_cast<uint32_t>(frame.frame->get_frame_number());
+            std::chrono::duration<double, std::milli> timestamp_ms(frame.frame->get_frame_timestamp());
+            imu_msg.header.stamp = ros::Time(std::chrono::duration<double>(timestamp_ms).count());
+            std::string TODO_CORRECT_ME = "0";
+            imu_msg.header.frame_id = TODO_CORRECT_ME;
+            auto data_ptr = reinterpret_cast<const float*>(frame.frame->get_frame_data());
+            if (stream_id.stream_type == RS2_STREAM_ACCEL)
+            {
+                imu_msg.linear_acceleration.x = data_ptr[0];
+                imu_msg.linear_acceleration.y = data_ptr[1];
+                imu_msg.linear_acceleration.z = data_ptr[2];
+            }
+           
+            else if (stream_id.stream_type == RS2_STREAM_GYRO)
+            {
+                imu_msg.angular_velocity.x = data_ptr[0];
+                imu_msg.angular_velocity.y = data_ptr[1];
+                imu_msg.angular_velocity.z = data_ptr[2]; 
+            }
+            else
+            {
+                throw io_exception("Unsupported stream type for a motion frame");
+            }
+
+            auto topic = ros_topic::frame_data_topic(stream_id);
+            write_message(topic, timestamp, imu_msg);
+            write_additional_frame_messages(stream_id, timestamp, frame);
         }
 
-        void write_image_metadata(const stream_identifier& stream_id, const nanoseconds& timestamp, video_frame* vid_frame)
+        void write_pose_frame(const stream_identifier& stream_id, const nanoseconds& timestamp, const frame_holder& frame)
         {
-            auto metadata_topic = ros_topic::frame_metadata_topic(stream_id);
-            diagnostic_msgs::KeyValue system_time;
-            system_time.key = "system_time";
-            system_time.value = std::to_string(vid_frame->get_frame_system_time());
-            write_message(metadata_topic, timestamp, system_time);
-
-            diagnostic_msgs::KeyValue timestamp_domain;
-            timestamp_domain.key = "timestamp_domain";
-            timestamp_domain.value = to_string() << vid_frame->get_frame_timestamp_domain();
-            write_message(metadata_topic, timestamp, timestamp_domain);
-
-            for (int i = 0; i < static_cast<rs2_frame_metadata_value>(rs2_frame_metadata_value::RS2_FRAME_METADATA_COUNT); i++)
-            {
-                rs2_frame_metadata_value type = static_cast<rs2_frame_metadata_value>(i);
-                if (vid_frame->supports_frame_metadata(type))
-                {
-                    auto md = vid_frame->get_frame_metadata(type);
-                    diagnostic_msgs::KeyValue md_msg;
-                    md_msg.key = to_string() << type;
-                    md_msg.value = std::to_string(md);
-                    write_message(metadata_topic, timestamp, md_msg);
-                }
-            }
+            throw not_implemented_exception("TODO: define pose message and write to file");
         }
 
         void write_streaming_info(nanoseconds timestamp, const sensor_identifier& sensor_id, std::shared_ptr<video_stream_profile_interface> profile)
