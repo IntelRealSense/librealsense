1a5a7a3f496f5d4bc9eb522145919d54633f3504
Bastian Kayser <bastian.kayser@motognosis.com>
2018-01-09 20:57:19 +0100

    fixed no label error in wrappers/csharp/copy_dependencies.bat

--- wrappers/csharp/copy_dependencies.bat
+++ wrappers/csharp/copy_dependencies.bat
@@ -5,11 +5,11 @@ if not exist "%3..\..\build\%2" goto nolocal
 echo Copying from local compilation
 xcopy /Y "%3..\..\build\%2\realsense2.dll" %4
 exit 0
-nolocal:
+:nolocal
 if not exist "C:\Program Files (x86)\Intel RealSense SDK 2.0" goto noinstall
 echo Copying from installed SDK
 xcopy /Y "C:\Program Files (x86)\Intel RealSense SDK 2.0\bin\%1\realsense2.dll" %4
 exit 0
-noinstall:
+:noinstall
 echo "realsense2.dll NOT FOUND! Please build it from source using CMake or install Intel RealSense SDK 2.0 from GitHub releases"
 exit 1
