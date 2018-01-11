9a1e00e4d32ccfb899ea226e6564875f583cd23c
Ziv Shahaf <ziv.shahaf@intel.com>
2018-01-11 14:31:46 +0200

    Adding license to files

--- wrappers/python/pybackend.cpp
+++ wrappers/python/pybackend.cpp
@@ -1,3 +1,6 @@
+/* License: Apache 2.0. See LICENSE file in root directory.
+Copyright(c) 2017 Intel Corporation. All Rights Reserved. */
+
 #include <pybind11/pybind11.h>
 
 // convenience functions
