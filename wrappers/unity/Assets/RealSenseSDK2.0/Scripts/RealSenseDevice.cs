074bb9308338f3f66e82f6682b1ec0d867f6d702
unknown <freeman.lo@intel.com>
2018-04-26 08:47:33 +0800

    modification for win app build

--- wrappers/unity/Assets/RealSenseSDK2.0/Scripts/RealSenseDevice.cs
+++ wrappers/unity/Assets/RealSenseSDK2.0/Scripts/RealSenseDevice.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 using System.Threading;
 using UnityEngine;
 using Intel.RealSense;
-using UnityEditor;
+//using UnityEditor;
 
 /// <summary>
 /// Manages streaming using a RealSense Device
