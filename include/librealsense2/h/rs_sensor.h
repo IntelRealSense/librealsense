977fa1391367c513e8979ebf60a0729cdd18f259
AnnaRomnov <anna.romanov@intel.com>
2018-05-16 15:59:09 +0300

    Swapped recommended_fw_ver and fw_ver enums, changed var names to minimal (instead of latest)

--- include/librealsense2/h/rs_sensor.h
+++ include/librealsense2/h/rs_sensor.h
@@ -22,8 +22,8 @@ extern "C" {
 typedef enum rs2_camera_info {
     RS2_CAMERA_INFO_NAME                           , /**< Friendly name */
     RS2_CAMERA_INFO_SERIAL_NUMBER                  , /**< Device serial number */
-    RS2_CAMERA_INFO_RECOMMENDED_FIRMWARE_VERSION   , /**< Recommended firmware version */
     RS2_CAMERA_INFO_FIRMWARE_VERSION               , /**< Primary firmware version */
+    RS2_CAMERA_INFO_RECOMMENDED_FIRMWARE_VERSION   , /**< Recommended firmware version */
     RS2_CAMERA_INFO_PHYSICAL_PORT                  , /**< Unique identifier of the port the device is connected to (platform specific) */
     RS2_CAMERA_INFO_DEBUG_OP_CODE                  , /**< If device supports firmware logging, this is the command to send to get logs from firmware */
     RS2_CAMERA_INFO_ADVANCED_MODE                  , /**< True iff the device is in advanced mode */
