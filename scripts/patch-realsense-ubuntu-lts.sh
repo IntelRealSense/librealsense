c9395b77ec72920f822ae84484891b051a86e5c4
Alois Klink <alois.klink@gmail.com>
2018-06-06 23:01:04 +0100

    Fix bug in kernel patch script for Ubuntu 18.04
    This script was always overriding the found `ubuntu_codename`
    with `xenial`, even when Ubuntu had a different version.
    This resulted in the error:
    `modprobe: ERROR: could not insert 'videodev': Exec format error`,
    as it was compiling the scripts
    with an older Ubuntu version for Xenial.
    
    This has now been corrected.

--- scripts/patch-realsense-ubuntu-lts.sh
+++ scripts/patch-realsense-ubuntu-lts.sh
@@ -32,7 +32,7 @@ LINUX_BRANCH=$(uname -r)
 kernel_branch=$(choose_kernel_branch $LINUX_BRANCH)
 # Construct branch name from distribution codename {xenial,bionic,..} and kernel version
 ubuntu_codename=`. /etc/os-release; echo ${UBUNTU_CODENAME/*, /}`
-if [ -z "$UBUNTU_CODENAME" ];
+if [ -z "$ubuntu_codename" ];
 then
 	# Trusty Tahr shall use xenial code base
 	ubuntu_codename="xenial"
