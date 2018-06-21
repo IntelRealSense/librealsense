cf3ba8475af220b13fc2fc2a172ccf6b2553112a
Evgeni Raikhel <evgeni.raikhel@intel.com>
2018-06-21 11:01:49 +0300

    Update kernel patches due to Ubuntu promotion of LTS to 4.15 and deprecation of kernel 4.13 Xenial hwe branch is promoted to kernel 4.15 - will use kernel patches originally developed for Bionic release Current user of kernel 4.13 will be redirected to the latest tagged version of this version for back-compatibility
    Change-Id: I5082bb92170f27ea790d35612ba5d612d237f3f8

--- scripts/patch-realsense-ubuntu-lts.sh
+++ scripts/patch-realsense-ubuntu-lts.sh
@@ -29,7 +29,7 @@ retpoline_retrofit=0
 
 LINUX_BRANCH=$(uname -r)
 
-kernel_branch=$(choose_kernel_branch $LINUX_BRANCH)
+
 # Construct branch name from distribution codename {xenial,bionic,..} and kernel version
 ubuntu_codename=`. /etc/os-release; echo ${UBUNTU_CODENAME/*, /}`
 if [ -z "${ubuntu_codename}" ];
@@ -38,7 +38,7 @@ then
 	ubuntu_codename="xenial"
 	retpoline_retrofit=1
 fi
-
+kernel_branch=$(choose_kernel_branch ${LINUX_BRANCH} ${ubuntu_codename})
 kernel_name="ubuntu-${ubuntu_codename}-$kernel_branch"
 
 #Distribution-specific packages
