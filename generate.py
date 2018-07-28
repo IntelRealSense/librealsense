7ef9ec0b3eefad6712f8518c869603a4d4fd2a22
dorodnic <dorodnic@gmail.com>
2018-07-28 18:16:23 +0300

    Adding basic templating mechanism

--- generate.py
+++ generate.py
@@ -15,18 +15,28 @@ if (len(sys.argv) == 3):
     with open(data) as f:
         data_content = json.load(f)
 
+    pages = [];
     body = "";
     index = 0;
     total = len(data_content["items"]);
+
+    items_per_page = 9;
+
+    max_this_page = min([ total, items_per_page]);
+    
     for obj in data_content["items"]:
         if (index % 3 == 0):
             if (index > 0):
                 body = body + "\t\t</div>\n";
-            if (index < total - 1):
+            if (index < max_this_page - 1):
                 body = body + "\t\t<div class=\"row\">\n";
-        else:
-            if (index == total - 1):
-                body = body + "\t\t</div>\n";
+
+        if (index == max_this_page):
+            index = 0;
+            pages.append(body);
+            body = "";
+            total = total - items_per_page;
+            max_this_page = min([ total, items_per_page]);            
                 
         body = body + "\t\t\t<div class=\"col-md-4\">\n";
         body = body + "\t\t\t\t<table class='t-table table borderless' border=1 bordercolor=#3E4D59>\n";
@@ -54,11 +64,68 @@ if (len(sys.argv) == 3):
         
         index = index+1;
 
-    content = content.replace("<!-- Content -->", body);
-
+    pages.append(body);
     output_filename = os.path.basename(template);
-    with open(output_filename, "w") as text_file:
-        text_file.write(content);
+
+    page_id = 1;
+    
+    for b in pages:
+
+        pagination = "";
+        if (len(data_content["items"]) > items_per_page):
+            page_count = len(pages);
+            pagination = "<ul class=\"pagination\">\n";
+            if (page_id > 1):
+                
+                pagination = pagination + "<li class=\"page-item\">\n";
+                pagination = pagination + "<a class=\"page-link\" href=\"" + last_file + "\" aria-label=\"Previous\">\n";
+                pagination = pagination + "<span aria-hidden=\"true\">&laquo;</span>\n";
+                pagination = pagination + "<span class=\"sr-only\">Previous</span>\n";
+                pagination = pagination + "</a></li>\n";
+            else:
+                pagination = pagination + "<li class=\"page-item disabled\">\n";
+                pagination = pagination + "<a class=\"page-link\" href=\"#\" aria-label=\"Previous\">\n";
+                pagination = pagination + "<span aria-hidden=\"true\">&laquo;</span>\n";
+                pagination = pagination + "<span class=\"sr-only\">Previous</span>\n";
+                pagination = pagination + "</a></li>\n";
+
+            for idx in range(1, page_count + 1):
+                if (idx == page_id):
+                    pagination = pagination + "<li class=\"page-item active\">\n";
+                else:
+                    pagination = pagination + "<li class=\"page-item\">\n"
+                pagination = pagination + "<a class=\"page-link\" href=\"";
+                if (idx == 1):
+                    pagination = pagination + os.path.basename(template);
+                else:
+                    pagination = pagination + os.path.basename(template).replace(".html", "-" + str(idx) + ".html");
+                pagination = pagination + "\">" + str(idx) + "</a>\n";
+                pagination = pagination + "</li>"
+
+            if (page_id < page_count):
+                
+                pagination = pagination + "<li class=\"page-item\">\n";
+                pagination = pagination + "<a class=\"page-link\" href=\"" + os.path.basename(template).replace(".html", "-" + str(page_id + 1) + ".html") + "\" aria-label=\"Previous\">\n";
+                pagination = pagination + "<span aria-hidden=\"true\">&raquo;</span>\n";
+                pagination = pagination + "<span class=\"sr-only\">Next</span>\n";
+                pagination = pagination + "</a></li>\n";
+            else:
+                pagination = pagination + "<li class=\"page-item disabled\">\n";
+                pagination = pagination + "<a class=\"page-link\" href=\"#\" aria-label=\"Previous\">\n";
+                pagination = pagination + "<span aria-hidden=\"true\">&raquo;</span>\n";
+                pagination = pagination + "<span class=\"sr-only\">Next</span>\n";
+                pagination = pagination + "</a></li>\n";
+        
+        new_c = content.replace("<!-- Content -->", b);
+        new_c = new_c.replace("<!-- Pagination -->", pagination);
+
+        with open(output_filename, "w") as text_file:
+            text_file.write(new_c);
+
+        page_id = page_id + 1;
+        last_file = output_filename;
+        output_filename = os.path.basename(template).replace(".html", "-" + str(page_id) + ".html");
+        
 
     print("Done!");
 
