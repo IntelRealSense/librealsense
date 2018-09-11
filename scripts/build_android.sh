a7bb199461f45ea8299cc066e111918a649bb190
Weiguo Zhou <zhouwg2000@gmail.com>
2018-09-11 22:16:41 +0800

    add log mechanism for Android port

--- scripts/build_android.sh
+++ scripts/build_android.sh
@@ -1,3 +1,3 @@
-cmake .. -DANDROID_ABI=armeabi-v7a -DCMAKE_TOOLCHAIN_FILE=/opt/android-ndk-r16b/build/cmake/android.toolchain.cmake -DFORCE_LIBUVC=false -DBUILD_SHARED_LIBS=true
+cmake .. -DANDROID_ABI=armeabi-v7a -DCMAKE_TOOLCHAIN_FILE=/opt/android-ndk-r16b/build/cmake/android.toolchain.cmake -DFORCE_LIBUVC=false -DBUILD_SHARED_LIBS=false
 
 make 
