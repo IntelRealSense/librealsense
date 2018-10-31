047ec6e065f43390574e7621fc47fc46275063d6
Maximilian Krüger <Maximilian.Krueger@airsphere.de>
2018-10-31 13:55:23 +0100

    Add Legacy codeblocks for unity compatibility
    For Unity versions lower than 2018.1 preprocessor instructions (NET 3.5) have been added. These should only be used for Unity compatibility.

--- wrappers/csharp/Intel.RealSense/Frames/FramePool.cs
+++ wrappers/csharp/Intel.RealSense/Frames/FramePool.cs
@@ -1,5 +1,13 @@
 ﻿using System;
+#if NET35
+/*
+ * ---------------------------------------------------------------------
+ * This code block is only for legacy Unity (version < 2018.1) support.
+ * ---------------------------------------------------------------------
+ */
+#else
 using System.Collections.Concurrent;
+#endif
 using System.Collections.Generic;
 using System.Runtime.InteropServices;
 using System.Text;
@@ -8,17 +16,79 @@ namespace Intel.RealSense.Frames
 {
     internal class FramePool<T> where T : Frame
     {
+#if NET35
+        /*
+         * ---------------------------------------------------------------------
+         * This code block is only for legacy Unity (version < 2018.1) support.
+         * ---------------------------------------------------------------------
+         */
+        private readonly Stack<T> stack;
+        private readonly object stackLock;
+#else
         private readonly ConcurrentStack<T> stack;
+#endif
         private readonly Func<IntPtr, T> factory;
 
         public FramePool(Func<IntPtr, T> factory)
         {
+#if NET35
+            /*
+             * ---------------------------------------------------------------------
+             * This code block is only for legacy Unity (version < 2018.1) support.
+             * ---------------------------------------------------------------------
+             */
+            stack = new Stack<T>();
+            stackLock = new object();
+#else
             stack = new ConcurrentStack<T>();
+#endif
 
             this.factory = factory;
         }
 
+#if NET35
+        /*
+         * ---------------------------------------------------------------------
+         * This code block is only for legacy Unity (version < 2018.1) support.
+         * ---------------------------------------------------------------------
+         */
         public T Get(IntPtr ptr)
+        {
+            int count;
+
+            lock (stackLock)
+            {
+                count = stack.Count;
+            }
+
+            if (count > 0)
+            {
+                T frame;
+                lock (stackLock)
+                {
+                    frame = stack.Pop();
+                }
+
+                frame.Instance = new HandleRef(frame, ptr);
+                frame.disposedValue = false;
+                return frame;
+            }
+            else
+            {
+                return factory(ptr);
+            }
+
+        }
+
+        public void Release(T t)
+        {
+            lock (stackLock)
+            {
+                stack.Push(t);
+            }
+        }
+#else
+         public T Get(IntPtr ptr)
         {
             if (stack.TryPop(out T frame))
             {
@@ -35,5 +105,7 @@ namespace Intel.RealSense.Frames
 
         public void Release(T t)
             => stack.Push(t);
+#endif
+
     }
 }
--- wrappers/csharp/Intel.RealSense/Profiles/ProfilePool.cs
+++ wrappers/csharp/Intel.RealSense/Profiles/ProfilePool.cs
@@ -1,6 +1,14 @@
 ﻿using Intel.RealSense.Types;
 using System;
-using System.Collections.Concurrent;
+#if NET35
+/*
+ * ---------------------------------------------------------------------
+ * This code block is only for legacy Unity (version < 2018.1) support.
+ * ---------------------------------------------------------------------
+ */
+#else
+    using System.Collections.Concurrent;
+#endif
 using System.Collections.Generic;
 using System.Runtime.InteropServices;
 using System.Text;
@@ -9,13 +17,85 @@ namespace Intel.RealSense.Profiles
 {
     internal class ProfilePool<T> where T : StreamProfile
     {
+#if NET35
+        /*
+         * ---------------------------------------------------------------------
+         * This code block is only for legacy Unity (version < 2018.1) support.
+         * ---------------------------------------------------------------------
+         */
+        private readonly Stack<T> stack;
+        private readonly object stackLock;
+#else
         private readonly ConcurrentStack<T> stack;
+#endif
 
         public ProfilePool()
         {
+#if NET35
+            /*
+             * ---------------------------------------------------------------------
+             * This code block is only for legacy Unity (version < 2018.1) support.
+             * ---------------------------------------------------------------------
+             */
+            stack = new Stack<T>();
+            stackLock = new object();
+#else
             stack = new ConcurrentStack<T>();
+#endif
+        }
+#if NET35
+        /*
+         * ---------------------------------------------------------------------
+         * This code block is only for legacy Unity (version < 2018.1) support.
+         * ---------------------------------------------------------------------
+         */
+        public T Get(IntPtr ptr)
+        {
+            bool isVideo = NativeMethods.rs2_stream_profile_is(ptr, Extension.VideoProfile, out var error) > 0;
+
+            int count;
+
+            lock (stackLock)
+            {
+                count = stack.Count;
+            }
+
+            T profile;
+
+            if (count > 0)
+            {
+                lock (stackLock)
+                {
+                    profile = stack.Pop();
+                }
+            }
+            else
+            {
+                profile = (isVideo ? new VideoStreamProfile(ptr) : new StreamProfile(ptr)) as T;
+            }
+            profile.Instance = new HandleRef(profile, ptr);
+            profile.disposedValue = false;
+
+            NativeMethods.rs2_get_stream_profile_data(ptr, out profile.stream, out profile.format, out profile.index, out profile.uniqueID, out profile.framerate, out error);
+
+            if (isVideo)
+            {
+                var v = profile as VideoStreamProfile;
+                NativeMethods.rs2_get_video_stream_resolution(ptr, out v.width, out v.height, out error);
+                return v as T;
+            }
+
+            return profile;
         }
 
+        public void Release(T t)
+        {
+            lock (stackLock)
+            {
+                stack.Push(t);
+            }
+        }
+#else
         public T Get(IntPtr ptr)
         {
             bool isVideo = NativeMethods.rs2_stream_profile_is(ptr, Extension.VideoProfile, out var error) > 0;
@@ -43,5 +123,6 @@ namespace Intel.RealSense.Profiles
         {
             stack.Push(t);
         }
+#endif
     }
 }
