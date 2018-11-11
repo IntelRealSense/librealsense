097e4772eecaecaf76d6b793afe5df5981258e80
Katz, Matan <matan.katz@intel.com>
2018-11-11 18:01:18 +0200

    move processing-blocks-factory implementation to cpp file

--- src/proc/processing-blocks-factory.h
+++ src/proc/processing-blocks-factory.h
@@ -4,28 +4,9 @@
 #pragma once
 
 #include "align.h"
-#include "sse/sse-align.h"
-#include "cuda/cuda-align.h"
  
 namespace librealsense
 {
-#ifdef RS2_USE_CUDA  
-    std::shared_ptr<librealsense::align> create_align(rs2_stream align_to)                                                               
-    { 
-        return std::make_shared<librealsense::align_cuda>(align_to);
-    } 
-#else
-#ifdef __SSSE3__
-    std::shared_ptr<librealsense::align> create_align(rs2_stream align_to)
-    {
-        return std::make_shared<librealsense::align_sse>(align_to);
-    }
-#else // No optimizations
-    std::shared_ptr<librealsense::align> create_align(rs2_stream align_to)
-    {
-        return std::make_shared<librealsense::align>(align_to);
-    }
-#endif // __SSSE3__
-#endif // RS2_USE_CUDA   
+    std::shared_ptr<librealsense::align> create_align(rs2_stream align_to);
 }
  
