15312efb04376dd624a5fd67dd12651360dadcf8
Katz, Matan <matan.katz@intel.com>
2018-11-11 20:28:18 +0200

    remove tabs

--- src/cuda/rscuda_utils.cuh
+++ src/cuda/rscuda_utils.cuh
@@ -26,15 +26,15 @@ namespace rscuda
         return std::shared_ptr<T>(d_data, [](T* p) { cudaFree(p); });
     }
 
-	template<typename  T>
-	std::shared_ptr<T> make_device_copy(T obj)
-	{
-		T* d_data;
-		auto res = cudaMalloc(&d_data, sizeof(T));
+    template<typename  T>
+    std::shared_ptr<T> make_device_copy(T obj)
+    {
+        T* d_data;
+        auto res = cudaMalloc(&d_data, sizeof(T));
         if (res != cudaSuccess)
             throw std::runtime_error("cudaMalloc failed status: " + res);
-		cudaMemcpy(d_data, &obj, sizeof(T), cudaMemcpyHostToDevice);
-		return std::shared_ptr<T>(d_data, [](T* data) { cudaFree(data); });
-	}
+        cudaMemcpy(d_data, &obj, sizeof(T), cudaMemcpyHostToDevice);
+        return std::shared_ptr<T>(d_data, [](T* data) { cudaFree(data); });
+    }
 }
 #endif //RS2_USE_CUDA
