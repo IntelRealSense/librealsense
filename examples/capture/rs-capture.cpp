1f16f1e900d4cd3ceda66893e8035da2f942b7b5
tsonoda <tetsuri.sonoda@intel.com>
2019-03-15 22:09:42 -0700

    Added disparity format colorization. But only equalized. Fixed crop is not ready yet.

--- examples/capture/rs-capture.cpp
+++ examples/capture/rs-capture.cpp
@@ -14,6 +14,9 @@ int main(int argc, char * argv[]) try
 
     // Declare depth colorizer for pretty visualization of depth data
     rs2::colorizer color_map;
+
+	rs2::disparity_transform depth_to_disparity(true);
+	
     // Declare rates printer for showing streaming rates of the enabled streams.
     rs2::rates_printer printer;
 
@@ -29,7 +32,8 @@ int main(int argc, char * argv[]) try
     {
         rs2::frameset data = pipe.wait_for_frames().    // Wait for next set of frames from the camera
                              apply_filter(printer).     // Print each enabled stream frame rate
-                             apply_filter(color_map);   // Find and colorize the depth data
+//							 apply_filter(depth_to_disparity).
+							apply_filter(color_map);   // Find and colorize the depth data
 
         // The show method, when applied on frameset, break it to frames and upload each frame into a gl textures
         // Each texture is displayed on different viewport according to it's stream unique id
--- src/proc/colorizer.cpp
+++ src/proc/colorizer.cpp
@@ -133,6 +133,13 @@ namespace librealsense
         { 0, 0, 0 },
         } };
 
+	void colorizer::update_disparity_histogram(int* hist, const float* depth_data, int w, int h)
+	{
+		memset(hist, 0, MAX_DEPTH * sizeof(int));
+		for (auto i = 0; i < w*h; ++i) ++hist[(int)depth_data[i]];
+		for (auto i = 2; i < MAX_DEPTH; ++i) hist[i] += hist[i - 1]; // Build a cumulative histogram for the indices in [1,0xFFFF]
+	}
+
     void colorizer::update_histogram(int* hist, const uint16_t* depth_data, int w, int h)
     {
         memset(hist, 0, MAX_DEPTH * sizeof(int));
@@ -143,12 +150,18 @@ namespace librealsense
     colorizer::colorizer()
         : stream_filter_processing_block("Depth Visualization"),
          _min(0.f), _max(6.f), _equalize(true), 
-         _target_stream_profile(), _histogram()
+         _target_stream_profile(), _histogram(),
+		_stereoscopic_depth(false),
+		_focal_lenght_mm(0.f),
+		_stereo_baseline_meter(0.f),
+		_depth_units(0.f),
+		_d2d_convert_factor(0.f)
     {
         _histogram = std::vector<int>(MAX_DEPTH, 0);
         _hist_data = _histogram.data();
         _stream_filter.stream = RS2_STREAM_DEPTH;
         _stream_filter.format = RS2_FORMAT_Z16;
+//		_stream_filter.format = RS2_FORMAT_DISPARITY32;
 
         _maps = { &jet, &hue, &classic, &grayscale, &inv_grayscale, &biomes, &cold, &warm, &quantized, &pattern };
 
@@ -216,13 +229,69 @@ namespace librealsense
         register_option(RS2_OPTION_HISTOGRAM_EQUALIZATION_ENABLED, hist_opt);
     }
 
+	bool colorizer::should_process(const rs2::frame& frame)
+	{
+		if (!frame || frame.is<rs2::frameset>())
+			return false;
+
+		if (frame.get_profile().stream_type() != RS2_STREAM_DEPTH)
+			return false;
+
+		return true;
+	}
+
     rs2::frame colorizer::process_frame(const rs2::frame_source& source, const rs2::frame& f)
     {
         if (f.get_profile().get() != _source_stream_profile.get())
         {
             _source_stream_profile = f.get_profile();
             _target_stream_profile = f.get_profile().clone(RS2_STREAM_DEPTH, 0, RS2_FORMAT_RGB8);
+
+			// set params for handling disparity
+			if (_source_stream_profile.format() == RS2_FORMAT_DISPARITY32)
+			{
+				const uint8_t fractional_bits = 5;
+				const uint8_t fractions = 1 << fractional_bits;
+				_d2d_convert_factor = (_stereo_baseline_meter * _focal_lenght_mm * fractions) / _depth_units;
+
+				// Check if the new frame originated from stereo-based depth sensor
+				// and retrieve the stereo baseline parameter that will be used in transformations
+				auto snr = ((frame_interface*)f.get())->get_sensor().get();
+				librealsense::depth_stereo_sensor* dss;
+
+				// Playback sensor
+				if (auto a = As<librealsense::extendable_interface>(snr))
+				{
+					librealsense::depth_stereo_sensor* ptr;
+					if (_stereoscopic_depth = a->extend_to(TypeToExtension<librealsense::depth_stereo_sensor>::value, (void**)&ptr))
+					{
+						dss = ptr;
+						_depth_units = dss->get_depth_scale();
+						_stereo_baseline_meter = dss->get_stereo_baseline_mm()*0.001f;
+					}
+				}
+				else // Live sensor
+				{
+					_stereoscopic_depth = Is<librealsense::depth_stereo_sensor>(snr);
+					if (_stereoscopic_depth)
+					{
+						dss = As<librealsense::depth_stereo_sensor>(snr);
+						_depth_units = dss->get_depth_scale();
+						_stereo_baseline_meter = dss->get_stereo_baseline_mm()* 0.001f;
+					}
+				}
+
+				if (_stereoscopic_depth)
+				{
+					auto vp = _source_stream_profile.as<rs2::video_stream_profile>();
+					_focal_lenght_mm = vp.get_intrinsics().fx;
+					const uint8_t fractional_bits = 5;
+					const uint8_t fractions = 1 << fractional_bits;
+					_d2d_convert_factor = (_stereo_baseline_meter * _focal_lenght_mm * fractions) / _depth_units;
+				}
+			}
         }
+
         auto make_equalized_histogram = [this](const rs2::video_frame& depth, rs2::video_frame rgb)
         {
             const auto w = depth.get_width(), h = depth.get_height();
@@ -286,16 +355,55 @@ namespace librealsense
                 }
             }
         };
+
+		auto make_disparity_equalized_histogram = [this](const rs2::video_frame& depth, rs2::video_frame rgb)
+		{
+			const auto w = depth.get_width(), h = depth.get_height();
+			const auto depth_data = reinterpret_cast<const float*>(depth.get_data());
+			auto rgb_data = reinterpret_cast<uint8_t*>(const_cast<void *>(rgb.get_data()));
+
+			update_disparity_histogram(_hist_data, depth_data, w, h);
+
+			auto cm = _maps[_map_index];
+			for (auto i = 0; i < w*h; ++i)
+			{
+				auto d = (int)depth_data[i];
+
+				if (d)
+				{
+					auto f = _hist_data[d] / (float)_hist_data[MAX_DEPTH - 1]; // 0-255 based on histogram location
+
+					auto c = cm->get(f);
+					rgb_data[i * 3 + 0] = (uint8_t)c.x;
+					rgb_data[i * 3 + 1] = (uint8_t)c.y;
+					rgb_data[i * 3 + 2] = (uint8_t)c.z;
+				}
+				else
+				{
+					rgb_data[i * 3 + 0] = 0;
+					rgb_data[i * 3 + 1] = 0;
+					rgb_data[i * 3 + 2] = 0;
+				}
+			}
+		};
+
         rs2::frame ret;
 
         auto vf = f.as<rs2::video_frame>();
         //rs2_extension ext = f.is<rs2::disparity_frame>() ? RS2_EXTENSION_DISPARITY_FRAME : RS2_EXTENSION_DEPTH_FRAME;
         ret = source.allocate_video_frame(_target_stream_profile, f, 3, vf.get_width(), vf.get_height(), vf.get_width() * 3, RS2_EXTENSION_VIDEO_FRAME);
 
-        if (_equalize)
-            make_equalized_histogram(f, ret);
-        else
-            make_value_cropped_frame(f, ret);
+		if (_source_stream_profile.format() == RS2_FORMAT_DISPARITY32)
+		{
+			make_disparity_equalized_histogram(f, ret);
+		}
+		else
+		{ 
+			if (_equalize)
+				make_equalized_histogram(f, ret);
+			else
+				make_value_cropped_frame(f, ret);
+		}
 
         return ret;
     }
