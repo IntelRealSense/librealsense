d5dcc8db0faea11a588c3db77818e304cef643a4
Brian Fulkerson <brian.fulkerson@intel.com>
2019-03-18 15:52:27 +0100

    T265: Log connection errors to the console

--- third-party/libtm/libtm/src/Manager.cpp
+++ third-party/libtm/libtm/src/Manager.cpp
@@ -79,8 +79,8 @@ Manager::Manager(Listener* lis, void* param) : mDispatcher(new Dispatcher()), mL
     }
 
 
-    TrackingData::LogControl logControl(LogVerbosityLevel::None,
-        LogOutputMode::LogOutputModeBuffer,
+    TrackingData::LogControl logControl(LogVerbosityLevel::Error,
+        LogOutputMode::LogOutputModeScreen,
         true);
 
     setHostLogControl(logControl);
