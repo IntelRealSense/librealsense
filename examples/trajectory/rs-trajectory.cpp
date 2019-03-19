68b374a2d9c3501104ddebed9a6d99a6d4cb6556
AnnaRomnov <anna.romanov@intel.com>
2019-03-19 09:43:55 +0200

    Added copyright note

--- examples/trajectory/rs-trajectory.cpp
+++ examples/trajectory/rs-trajectory.cpp
@@ -1,3 +1,6 @@
+// License: Apache 2.0. See LICENSE file in root directory.
+// Copyright(c) 2019 Intel Corporation. All Rights Reserved.
+
 #include <librealsense2/rs.hpp>
 #include "example.hpp"          // Include short list of convenience functions for rendering
 #include <cstring>
