33651ca511af3b3b7ea3aa108119cf276c47ee04
doronhi <doron_hirshberg@hotmail.com>
2019-05-08 08:36:53 +0300

    fixed system time reading method.

--- src/global_timestamp_reader.cpp
+++ src/global_timestamp_reader.cpp
@@ -127,9 +127,9 @@ namespace librealsense
             LOG_DEBUG("time_diff_keeper::update_diff_time - in");
             std::lock_guard<std::recursive_mutex> lock(_mtx);
             LOG_DEBUG("time_diff_keeper::update_diff_time - lock");
-            double system_time_start = static_cast<double>(duration_cast<nanoseconds>(system_clock::now().time_since_epoch()).count()) * TIMESTAMP_USEC_TO_MSEC * TIMESTAMP_USEC_TO_MSEC;
+            double system_time_start = duration<double, std::milli>(system_clock::now().time_since_epoch()).count();
             double sample_hw_time = _device->get_device_time();
-            double system_time_finish = static_cast<double>(duration_cast<nanoseconds>(system_clock::now().time_since_epoch()).count()) * TIMESTAMP_USEC_TO_MSEC * TIMESTAMP_USEC_TO_MSEC;
+            double system_time_finish = duration<double, std::milli>(system_clock::now().time_since_epoch()).count();
             double system_time((system_time_finish + system_time_start) / 2);
             if (sample_hw_time < _last_sample_hw_time)
             {
