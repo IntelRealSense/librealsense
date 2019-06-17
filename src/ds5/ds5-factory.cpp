318e0ada1dd9c9496f630f1bfb206bb93714589b
Katz, Matan <matan.katz@intel.com>
2019-06-17 15:44:24 +0300

    fix f416 rgb matcher

--- src/ds5/ds5-factory.cpp
+++ src/ds5/ds5-factory.cpp
@@ -1018,10 +1018,10 @@ namespace librealsense
 
     std::shared_ptr<matcher> rs416_rgb_device::create_matcher(const frame_holder& frame) const
     {
-        std::vector<stream_interface*> streams = { _depth_stream.get() , _left_ir_stream.get() , _right_ir_stream.get() };
+        std::vector<stream_interface*> streams = { _depth_stream.get() , _left_ir_stream.get() , _right_ir_stream.get(), _color_stream.get() };
         if (frame.frame->supports_frame_metadata(RS2_FRAME_METADATA_FRAME_COUNTER))
         {
-            return matcher_factory::create(RS2_MATCHER_DLR, streams);
+            return matcher_factory::create(RS2_MATCHER_DLR_C, streams);
         }
         return matcher_factory::create(RS2_MATCHER_DEFAULT, streams);
     }
