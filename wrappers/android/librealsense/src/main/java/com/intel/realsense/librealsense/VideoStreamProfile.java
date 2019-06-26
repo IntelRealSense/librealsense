78e366012d84abd9ce2e005b6122207d1301e774
Katz, Matan <matan.katz@intel.com>
2019-06-26 16:07:29 +0300

    Revert "Added camera intrinsic information to jni/stream_profile.cpp"
    This reverts commit 0748e5f64b6b79c565045eb0205594d8ea6cb9fa.

--- wrappers/android/librealsense/src/main/java/com/intel/realsense/librealsense/VideoStreamProfile.java
+++ wrappers/android/librealsense/src/main/java/com/intel/realsense/librealsense/VideoStreamProfile.java
@@ -1,45 +1,26 @@
 package com.intel.realsense.librealsense;
 
 public class VideoStreamProfile extends StreamProfile {
-    IntrinsicsParams mIntrinsicsParams;
+    ResolutionParams mResolutionParams;
 
-    private class IntrinsicsParams {
+    private class ResolutionParams {
         public int width;
         public int height;
-        public float fx;
-        public float fy;
-        public float ppx;
-        public float ppy;
     }
 
     VideoStreamProfile(long handle) {
         super(handle);
-        mIntrinsicsParams = new IntrinsicsParams();
-        nGetIntrinsics(mHandle, mIntrinsicsParams);
+        mResolutionParams = new ResolutionParams();
+        nGetResolution(mHandle, mResolutionParams);
     }
 
     public int getWidth() {
-        return mIntrinsicsParams.width;
+        return mResolutionParams.width;
     }
 
     public int getHeight() {
-        return mIntrinsicsParams.height;
+        return mResolutionParams.height;
     }
 
-    public float getFx() {
-        return mIntrinsicsParams.fx;
-    }
-
-    public float getFy() {
-        return mIntrinsicsParams.fy;
-    }
-
-    public float getPpx() {
-        return mIntrinsicsParams.ppx;
-    }
-
-    public float getPpy() {return mIntrinsicsParams.ppy;  }
-
-
-    private static native void nGetIntrinsics(long handle, IntrinsicsParams params);
+    private static native void nGetResolution(long handle, ResolutionParams params);
 }
