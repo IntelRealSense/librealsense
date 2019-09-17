21ec0a73035e7e8da3c417d3778e5512c604a6bb
Phillip Schmidt <phillip.schmidt@intel.com>
2019-09-17 16:49:41 -0700

    wheel odometry sample/test

--- wrappers/python/examples/t265_wheel_odometry.py
+++ wrappers/python/examples/t265_wheel_odometry.py
@@ -1,39 +1,44 @@
-#!/usr/bin/python
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 ## License: Apache 2.0. See LICENSE file in root directory.
 ## Copyright(c) 2019 Intel Corporation. All Rights Reserved.
 
-#####################################################
-##           librealsense T265 example             ##
-#####################################################
-
-# First import the library
+####################################################################
+##           librealsense T265 wheel odometry example             ##
+####################################################################
 import pyrealsense2 as rs
 
-# Declare RealSense pipeline, encapsulating the actual device and sensors
+# before start: get device by cfg.resolve(pipe)
 pipe = rs.pipeline()
-
-# Build config object and request pose data
 cfg = rs.config()
-cfg.enable_stream(rs.stream.pose)
+profile = cfg.resolve(pipe)
+dev = profile.get_device()
+pose_sensor = dev.first_pose_sensor()
+wheel_odometer = pose_sensor.as_wheel_odometer()
 
-# Start streaming with requested config
-pipe.start(cfg)
+# read calibration, to list of uint8
+f = open("calibration_odometry.json")
+chars = []
+for line in f:
+   for c in line:
+       chars.append(ord(c))  # char to uint8
 
-try:
-    for _ in range(50):
-        # Wait for the next set of frames from the camera
-        frames = pipe.wait_for_frames()
+wheel_odometer.load_wheel_odometery_config(chars)
 
-        # Fetch pose frame
-        pose = frames.get_pose_frame()
-        if pose:
-            # Print some of the pose data to the terminal
-            data = pose.get_pose_data()
-            print("Frame #{}".format(pose.frame_number))
-            print("Position: {}".format(data.translation))
-            print("Velocity: {}".format(data.velocity))
-            print("Acceleration: {}\n".format(data.acceleration))
+# start
+pipe.start()
 
-finally:
-    pipe.stop()
+for _ in range(50):
+    frames = pipe.wait_for_frames()
+    pose = frames.get_pose_frame()
+    if pose:
+        data = pose.get_pose_data()
+        print("Frame #{}".format(pose.frame_number))
+        print("Position: {}".format(data.translation))
+
+        # send wheel odometry measurement
+        v = rs.vector()
+        v.x = 10
+        wheel_odometer.send_wheel_odometry(0,0,v)
+
+pipe.stop()
