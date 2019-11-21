e0bebd2ffd6216f77e5eda4c94eaa2c6417605c7
Ariel Lowenstein <ariel.lowenstein@intel.com>
2019-11-21 14:14:56 +0200

    add options to matlab

--- wrappers/matlab/option.m
+++ wrappers/matlab/option.m
@@ -59,6 +59,9 @@ classdef option < int64
         enable_relocalization           (56)
         enable_pose_jumping             (57)
         enable_dynamic_calibration      (58)
-        count                           (59)
+        depth_offset                    (59)
+        led_power                       (60)
+        zero_order_enabled              (61)
+        count                           (62)
     end
 end
