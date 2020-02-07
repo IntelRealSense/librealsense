b1c5bfa7499d19d9702f2ee444937f5fe43759a9
Daniel Piro <daniel.piro@intel.com>
2020-02-07 15:09:57 -0500

    Auto calibration android sample app added
    Signed-off-by: Daniel Piro <daniel.piro@intel.com>

---
+++ wrappers/android/examples/auto_calibration/app/src/main/java/com/intel/realsense/auto_calibration/SettingsActivity.java
@@ -0,0 +1,31 @@
+package com.intel.realsense.auto_calibration;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.preference.PreferenceFragmentCompat;
+
+public class SettingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.settings_activity);
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.settings, new SettingsFragment())
+                .commit();
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+    }
+
+    public static class SettingsFragment extends PreferenceFragmentCompat {
+        @Override
+        public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+            setPreferencesFromResource(R.xml.root_preferences, rootKey);
+        }
+    }
+}
