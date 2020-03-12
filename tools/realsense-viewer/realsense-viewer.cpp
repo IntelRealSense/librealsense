530f84b18adf807f61678e338c99dbfaac6a877f
Eran <librealsense.eran@gmail.com>
2020-03-12 12:38:16 +0200

    separated dl_vino_model into own cmake file; viewer now uses it to download models from AWS

--- tools/realsense-viewer/realsense-viewer.cpp
+++ tools/realsense-viewer/realsense-viewer.cpp
@@ -29,7 +29,12 @@
 #endif // INTERNAL_FW
 
 #include <easylogging++.h>
+#ifdef BUILD_SHARED_LIBS
+// With static linkage, ELPP is initialized by librealsense, so doing it here will
+// create errors. When we're using the shared .so/.dll, the two are separate and we have
+// to initialize ours if we want to use the APIs!
 INITIALIZE_EASYLOGGINGPP
+#endif
 
 using namespace rs2;
 using namespace rs400;
