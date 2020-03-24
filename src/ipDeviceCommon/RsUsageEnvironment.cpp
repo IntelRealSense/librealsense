ead293c9297daae3a1d4f731cd607e56d8ea92ce
apuzhevi <alexey.puzhevich@intel.com>
2020-03-24 14:21:50 +0200

    Static binaries support. (#186)

--- src/ipDeviceCommon/RsUsageEnvironment.cpp
+++ src/ipDeviceCommon/RsUsageEnvironment.cpp
@@ -3,11 +3,11 @@
 
 #include "RsUsageEnvironment.h"
 
-// #ifdef BUILD_EASYLOGGINGCPP
-// #ifdef BUILD_SHARED_LIBS
+#ifdef BUILD_EASYLOGGINGCPP
+#ifdef BUILD_SHARED_LIBS
 INITIALIZE_EASYLOGGINGPP
-// #endif
-// #endif
+#endif
+#endif
 
 RSUsageEnvironment::RSUsageEnvironment(TaskScheduler& taskScheduler)
     : BasicUsageEnvironment(taskScheduler)
