9426f24a439935d03bdd376186930d60b43ed0f3
Alexey Puzhevich <alexey.puzhevich@intel.com>
2020-03-26 12:37:51 +0200

    zlib leftovers

--- src/compression/RvlCompression.cpp
+++ src/compression/RvlCompression.cpp
@@ -6,7 +6,6 @@
 #include <cstring>
 #include <iostream>
 #include <ipDeviceCommon/Statistic.h>
-#include <zlib.h>
 
 RvlCompression::RvlCompression(int t_width, int t_height, rs2_format t_format, int t_bpp)
     :ICompression(t_width, t_height, t_format, t_bpp)
