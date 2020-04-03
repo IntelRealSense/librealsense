249dec6a1c5e795dbed26048af377937f924a9c5
Eran <librealsense.eran@gmail.com>
2020-04-03 23:51:44 +0300

    fix

--- src/rs.cpp
+++ src/rs.cpp
@@ -1357,6 +1357,7 @@ int rs2_is_device_extendable_to(const rs2_device* dev, rs2_extension extension, 
         case RS2_EXTENSION_UPDATE_DEVICE         : return VALIDATE_INTERFACE_NO_THROW(dev->device, librealsense::update_device_interface)     != nullptr;
         case RS2_EXTENSION_GLOBAL_TIMER          : return VALIDATE_INTERFACE_NO_THROW(dev->device, librealsense::global_time_interface)       != nullptr;
         case RS2_EXTENSION_AUTO_CALIBRATED_DEVICE: return VALIDATE_INTERFACE_NO_THROW(dev->device, librealsense::auto_calibrated_interface) != nullptr;
+        case RS2_EXTENSION_DEPTH_TO_RGB_CALIBRATION_DEVICE: return VALIDATE_INTERFACE_NO_THROW(dev->device, librealsense::depth_to_rgb_calibration_device) != nullptr;
         case RS2_EXTENSION_SERIALIZABLE          : return VALIDATE_INTERFACE_NO_THROW(dev->device, librealsense::serializable_interface) != nullptr;
 
         default:
