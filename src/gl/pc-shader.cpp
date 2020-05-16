bde8e5dfefd7711a2bd4480b783c8a7e29ffc3b7
dorodnic <dorodnic@gmail.com>
2020-05-16 12:25:45 +0300

    WIP on frustum decomposition

--- src/gl/pc-shader.cpp
+++ src/gl/pc-shader.cpp
@@ -179,6 +179,75 @@ static const char* blit_frag_shader_text =
 
 using namespace rs2;
 
+struct Matrix4x4
+{
+ // The elements of the 4x4 matrix are stored in
+ // column-major order (see "OpenGL Programming Guide",
+ // 3rd edition, pp 106, glLoadMatrix).
+ float _11, _21, _31, _41;
+ float _12, _22, _32, _42;
+ float _13, _23, _33, _43;
+ float _14, _24, _34, _44;
+};
+struct Plane { float a, b, c, d; };
+
+void NormalizePlane(Plane & plane)
+{
+ float mag;
+ mag = sqrt(plane.a * plane.a + plane.b * plane.b + plane.c * plane.c);
+ plane.a = plane.a / mag;
+ plane.b = plane.b / mag;
+ plane.c = plane.c / mag;
+ plane.d = plane.d / mag;
+}
+
+void ExtractPlanesGL(
+ Plane * p_planes,
+ const Matrix4x4 & comboMatrix,
+ bool normalize)
+{
+ // Left clipping plane
+ p_planes[0].a = comboMatrix._41 + comboMatrix._11;
+ p_planes[0].b = comboMatrix._42 + comboMatrix._12;
+ p_planes[0].c = comboMatrix._43 + comboMatrix._13;
+ p_planes[0].d = comboMatrix._44 + comboMatrix._14;
+ // Right clipping plane
+ p_planes[1].a = comboMatrix._41 - comboMatrix._11;
+ p_planes[1].b = comboMatrix._42 - comboMatrix._12;
+ p_planes[1].c = comboMatrix._43 - comboMatrix._13;
+ p_planes[1].d = comboMatrix._44 - comboMatrix._14;
+ // Top clipping plane
+ p_planes[2].a = comboMatrix._41 - comboMatrix._21;
+ p_planes[2].b = comboMatrix._42 - comboMatrix._22;
+ p_planes[2].c = comboMatrix._43 - comboMatrix._23;
+ p_planes[2].d = comboMatrix._44 - comboMatrix._24;
+ // Bottom clipping plane
+ p_planes[3].a = comboMatrix._41 + comboMatrix._21;
+ p_planes[3].b = comboMatrix._42 + comboMatrix._22;
+ p_planes[3].c = comboMatrix._43 + comboMatrix._23;
+ p_planes[3].d = comboMatrix._44 + comboMatrix._24;
+ // Near clipping plane
+ p_planes[4].a = comboMatrix._41 + comboMatrix._31;
+ p_planes[4].b = comboMatrix._42 + comboMatrix._32;
+ p_planes[4].c = comboMatrix._43 + comboMatrix._33;
+ p_planes[4].d = comboMatrix._44 + comboMatrix._34;
+ // Far clipping plane
+ p_planes[5].a = comboMatrix._41 - comboMatrix._31;
+ p_planes[5].b = comboMatrix._42 - comboMatrix._32;
+ p_planes[5].c = comboMatrix._43 - comboMatrix._33;
+ p_planes[5].d = comboMatrix._44 - comboMatrix._34;
+ // Normalize the plane equations, if requested
+ if (normalize == true)
+ {
+ NormalizePlane(p_planes[0]);
+ NormalizePlane(p_planes[1]);
+ NormalizePlane(p_planes[2]);
+ NormalizePlane(p_planes[3]);
+ NormalizePlane(p_planes[4]);
+ NormalizePlane(p_planes[5]);
+ }
+}
+
 namespace librealsense
 {
     namespace gl
@@ -439,6 +508,12 @@ namespace librealsense
                     auto vf_profile = f.get_profile().as<video_stream_profile>();
                     int width = vf_profile.width();
                     int height = vf_profile.height();
+
+                    rs2::matrix4 p = get_matrix(RS2_GL_MATRIX_PROJECTION);
+                    rs2::matrix4 v = get_matrix(RS2_GL_MATRIX_CAMERA);
+                    rs2::matrix4 f = get_matrix(RS2_GL_MATRIX_TRANSFORMATION);
+
+                    
                     
                     if (glsl_enabled())
                     {
