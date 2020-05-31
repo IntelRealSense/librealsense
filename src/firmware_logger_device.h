2f6235a85c773f963817758ff5d8cd0224fd0783
remibettan <remi.bettan@intel.com>
2020-05-31 16:08:25 +0300

    Flash logs work - work to do in fw logger device - see comment

--- src/firmware_logger_device.h
+++ src/firmware_logger_device.h
@@ -14,6 +14,7 @@ namespace librealsense
 	{
 	public:
 		virtual bool get_fw_log(fw_logs::fw_logs_binary_data& binary_data) = 0;
+		virtual bool get_flash_log(fw_logs::fw_logs_binary_data& binary_data) = 0;
 		virtual ~firmware_logger_extensions() = default;
 	};
 	MAP_EXTENSION(RS2_EXTENSION_FW_LOGGER, librealsense::firmware_logger_extensions);
@@ -26,13 +27,30 @@ namespace librealsense
 			std::string camera_op_code);
 
 		bool get_fw_log(fw_logs::fw_logs_binary_data& binary_data) override;
+		bool get_flash_log(fw_logs::fw_logs_binary_data& binary_data) override;
+
+
 
 	private:
-		std::vector<uint8_t> _input_code;
+		enum log_type
+		{
+			LOG_TYPE_FW_LOG = 0,
+			LOG_TYPE_FLASH_LOG = 1,
+			LOG_TYPE_NUM_OF_TYPES = 2
+		};
+
+		bool get_log(fw_logs::fw_logs_binary_data& binary_data, log_type type);
+		void get_logs_from_hw_monitor(log_type type);
+
+		std::vector<uint8_t> _input_code_for_fw_logs;
+		std::vector<uint8_t> _input_code_for_flash_logs;
 		std::shared_ptr<hw_monitor> _hw_monitor;
 
-		std::queue<fw_logs::fw_logs_binary_data> _message_queue;
-		void get_fw_logs_from_hw_monitor();
+		std::queue<fw_logs::fw_logs_binary_data> _fw_logs_queue;
+		std::queue<fw_logs::fw_logs_binary_data> _flash_logs_queue;
+
+
+
 	};
 
 }
