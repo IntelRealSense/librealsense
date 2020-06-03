4d2f7430b9dc98737ce7c6473babce0e17295d30
remibettan <remi.bettan@intel.com>
2020-06-03 09:01:51 +0300

    rs-fw-logger tool using new API

--- tools/fw-logger/rs-fw-logger.cpp
+++ tools/fw-logger/rs-fw-logger.cpp
@@ -5,12 +5,10 @@
 #include <fstream>
 #include <thread>
 #include "tclap/CmdLine.h"
-#include "fw-logs-parser.h"
 
 
 using namespace std;
 using namespace TCLAP;
-using namespace fw_logger;
 using namespace rs2;
 
 string hexify(unsigned char n)
@@ -52,20 +50,9 @@ int main(int argc, char* argv[])
     cmd.parse(argc, argv);
 
     log_to_file(RS2_LOG_SEVERITY_WARN, "librealsense.log");
-    // Obtain a list of devices currently present on the system
 
-    unique_ptr<fw_logs_parser> fw_log_parser;
     auto use_xml_file = false;
     auto xml_full_file_path = xml_arg.getValue();
-    if (!xml_full_file_path.empty())
-    {
-        ifstream f(xml_full_file_path);
-        if (f.good())
-        {
-            fw_log_parser = unique_ptr<fw_logs_parser>(new fw_logs_parser(xml_full_file_path));
-            use_xml_file = true;
-        }
-    }
 
     context ctx;
     device_hub hub(ctx);
@@ -78,47 +65,55 @@ int main(int argc, char* argv[])
             auto dev = hub.wait_for_device();
             cout << "RealSense device was connected...\n";
 
-            vector<uint8_t> input;
-            auto str_op_code = dev.get_info(RS2_CAMERA_INFO_DEBUG_OP_CODE);
-            auto op_code = static_cast<uint8_t>(stoi(str_op_code));
-            input = {0x14, 0x00, 0xab, 0xcd, op_code, 0x00, 0x00, 0x00,
-                     0xf4, 0x01, 0x00, 0x00, 0x00,    0x00, 0x00, 0x00,
-                     0x00, 0x00, 0x00, 0x00, 0x00,    0x00, 0x00, 0x00};
-
-            cout << "Device Name: " << dev.get_info(RS2_CAMERA_INFO_NAME) << endl <<
-                    "Device Location: " << dev.get_info(RS2_CAMERA_INFO_PHYSICAL_PORT) << endl << endl;
-
             setvbuf(stdout, NULL, _IONBF, 0); // unbuffering stdout
+
+            auto fw_log_device = dev.as<rs2::firmware_logger>();
+
+            bool using_parser = false;
+            if (!xml_full_file_path.empty())
+            {
+                ifstream f(xml_full_file_path);
+                if (f.good())
+                {
+                    bool parser_initialized = fw_log_device.init_parser(xml_full_file_path);
+                    if (parser_initialized)
+                        using_parser = true;
+                }
+            }
 
             while (hub.is_connected(dev))
             {
-                this_thread::sleep_for(chrono::milliseconds(100));
-
-                auto raw_data = dev.as<debug_protocol>().send_and_receive_raw_data(input);
-                vector<string> fw_log_lines = {""};
-                if (raw_data.size() <= 4)
-                    continue;
-
-                if (use_xml_file)
+                auto fw_log_message = fw_log_device.create_message();
+                bool result = fw_log_device.get_firmware_log(fw_log_message);
+                if (result)
                 {
-                    fw_logs_binary_data fw_logs_binary_data = {raw_data};
-                    fw_logs_binary_data.logs_buffer.erase(fw_logs_binary_data.logs_buffer.begin(),fw_logs_binary_data.logs_buffer.begin()+4);
-                    fw_log_lines = fw_log_parser->get_fw_log_lines(fw_logs_binary_data);
-                    for (auto& elem : fw_log_lines)
-                        elem = datetime_string() + "  " + elem;
-                }
-                else
-                {
-                    stringstream sstr;
-                    sstr << datetime_string() << "  FW_Log_Data:";
-                    for (size_t i = 0; i < raw_data.size(); ++i)
-                        sstr << hexify(raw_data[i]) << " ";
+                    std::vector<string> fw_log_lines;
+                    if (using_parser)
+                    {
+                        rs2::firmware_log_parsed_message parsed_log = fw_log_device.parse_log(fw_log_message);
+                        stringstream sstr;
+                        sstr << parsed_log.timestamp() << " " << parsed_log.severity() << " " << parsed_log.message()
+                            << " " << parsed_log.thread_name() << " " << parsed_log.file_name()
+                            << " " << parsed_log.line();
 
-                    fw_log_lines.push_back(sstr.str());
+                        fw_log_lines.push_back(sstr.str());
+                    }
+                    else
+                    {
+                        stringstream sstr;
+                        sstr << fw_log_message.get_timestamp();
+                        sstr << " " << fw_log_message.get_severity_str();
+                        sstr << "  FW_Log_Data:";
+                        std::vector<uint8_t> msg_data = fw_log_message.data();
+                        for (int i = 0; i < msg_data.size(); ++i)
+                        {
+                            sstr << hexify(msg_data[i]) << " ";
+                        }
+                        fw_log_lines.push_back(sstr.str());
+                    }
+                    for (auto& line : fw_log_lines)
+                        cout << line << endl;
                 }
-
-                for (auto& line : fw_log_lines)
-                    cout << line << endl;
             }
         }
         catch (const error & e)
