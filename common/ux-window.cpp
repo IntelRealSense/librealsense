0a8ad126f6b423ec469e0a5140fb3c5e61769ae9
Evgeni Raikhel <evgeni.raikhel@intel.com>
2020-06-29 22:21:18 +0300

    Reduce font oversample for MacOS to avoid out of font memory

--- common/ux-window.cpp
+++ common/ux-window.cpp
@@ -75,7 +75,8 @@ namespace rs2
         config_file::instance().set_default(configurations::viewer::hwlogger_xml, "./HWLoggerEvents.xml");
 
 #ifdef __APPLE__
-        config_file::instance().set_default(configurations::performance::font_oversample, 8);
+
+        config_file::instance().set_default(configurations::performance::font_oversample, 2);
         config_file::instance().set_default(configurations::performance::enable_msaa, true);
         config_file::instance().set_default(configurations::performance::msaa_samples, 4);
         // On Mac-OS, mixing OpenGL 2 with OpenGL 3 is not supported by the driver
