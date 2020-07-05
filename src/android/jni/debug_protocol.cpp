aec703983f8475fdcd3b9066a7a752b1cf50d08a
remibettan <remi.bettan@intel.com>
2020-07-05 13:35:59 +0300

    using C++ in jni files, but only C API

--- src/android/jni/debug_protocol.cpp
+++ src/android/jni/debug_protocol.cpp
@@ -17,46 +17,21 @@ Java_com_intel_realsense_librealsense_DebugProtocol_nSendAndReceiveRawData(JNIEn
     jsize length = env->GetArrayLength(buffer_);
 
     rs2_error* e = NULL;
-    const rs2_raw_data_buffer* response_bytes =
-            rs2_send_and_receive_raw_data(reinterpret_cast<rs2_device*>(handle), (void*)buffer, (int)length, &e);
+    std::shared_ptr<const rs2_raw_data_buffer> response_bytes(
+            rs2_send_and_receive_raw_data(reinterpret_cast<rs2_device*>(handle), (void*)buffer, (int)length, &e),
+            rs2_delete_raw_data);
+
+    int response_size = rs2_get_raw_data_size(response_bytes.get(), &e);
     handle_error(env, e);
 
-    int response_size = rs2_get_raw_data_size(response_bytes, &e);
-    handle_error(env, e);
-
-    const unsigned char* response_start = rs2_get_raw_data(response_bytes, &e);
+    const unsigned char* response_start = rs2_get_raw_data(response_bytes.get(), &e);
     handle_error(env, e);
 
     env->ReleaseByteArrayElements(buffer_, buffer, 0);
     jbyteArray rv = env->NewByteArray(response_size);
     env->SetByteArrayRegion (rv, 0, response_size, reinterpret_cast<const jbyte *>(response_start));
 
-    rs2_delete_raw_data(response_bytes);
     return rv;
-}
-
-char* get_file_content_from_file_path(const char* file_path)
-{
-    FILE* fp;
-    long lSize;
-    char* xml_content;
-
-    fp = fopen(file_path, "rb");
-    if (!fp) perror(file_path), exit(1);
-
-    fseek(fp, 0L, SEEK_END);
-    lSize = ftell(fp);
-    rewind(fp);
-
-    xml_content = (char*)calloc(1, lSize + 1);
-    if (!xml_content) fclose(fp), fputs("memory alloc fails", stderr), exit(1);
-
-    if (1 != fread(xml_content, lSize, 1, fp))
-        fclose(fp), free(xml_content), fputs("entire read fails", stderr), exit(1);
-
-    fclose(fp);
-
-    return xml_content;
 }
 
 extern "C"
@@ -68,48 +43,52 @@ Java_com_intel_realsense_librealsense_DebugProtocol_nSendAndReceiveData(JNIEnv *
     const char *file_path = env->GetStringUTFChars(filePath, 0);
     const char *line = env->GetStringUTFChars(command, 0);
 
-    char* xml_content = get_file_content_from_file_path(file_path);
+    std::ifstream f(file_path);
+    std::string xml_content((std::istreambuf_iterator<char>(f)), std::istreambuf_iterator<char>());
 
     rs2_error* e = NULL;
 
     //creating terminal parser
-    rs2_terminal_parser* terminal_parser = rs2_create_terminal_parser(xml_content, &e);
+    std::shared_ptr<rs2_terminal_parser> terminal_parser = std::shared_ptr<rs2_terminal_parser>(
+            rs2_create_terminal_parser(xml_content.c_str(), &e),
+            rs2_delete_terminal_parser);
     handle_error(env, e);
-
-    free(xml_content);
 
     //using parser to get bytes command from the string command
     int line_length = strlen(line);
-    const rs2_raw_data_buffer* command_buffer =
-            rs2_terminal_parse_command(terminal_parser, line, line_length, &e);
+    std::shared_ptr<const rs2_raw_data_buffer> command_buffer(
+            rs2_terminal_parse_command(terminal_parser.get(), line, line_length, &e),
+            rs2_delete_raw_data);
     handle_error(env, e);
 
-    int command_size = rs2_get_raw_data_size(command_buffer, &e);
+    int command_size = rs2_get_raw_data_size(command_buffer.get(), &e);
     handle_error(env, e);
 
-    const unsigned char* command_start = rs2_get_raw_data(command_buffer, &e);
+    const unsigned char* command_start = rs2_get_raw_data(command_buffer.get(), &e);
     handle_error(env, e);
 
     //calling send_receive to get the bytes response
-    const rs2_raw_data_buffer* response_bytes =
-            rs2_send_and_receive_raw_data(reinterpret_cast<rs2_device*>(handle), (void*)command_start, command_size, &e);
+    std::shared_ptr<const rs2_raw_data_buffer> response_bytes(
+            rs2_send_and_receive_raw_data(reinterpret_cast<rs2_device*>(handle), (void*)command_start, command_size, &e),
+            rs2_delete_raw_data);
 
-    int response_size = rs2_get_raw_data_size(response_bytes, &e);
+    int response_size = rs2_get_raw_data_size(response_bytes.get(), &e);
     handle_error(env, e);
 
-    const unsigned char* response_start = rs2_get_raw_data(response_bytes, &e);
+    const unsigned char* response_start = rs2_get_raw_data(response_bytes.get(), &e);
     handle_error(env, e);
 
     //using parser to get response string from response bytes and string command
-    const rs2_raw_data_buffer* response_buffer =
-            rs2_terminal_parse_response(terminal_parser, line, line_length,
-                                        (void*)response_start, response_size, &e);
+    std::shared_ptr<const rs2_raw_data_buffer> response_buffer(
+            rs2_terminal_parse_response(terminal_parser.get(), line, line_length,
+                                       (void*)response_start, response_size, &e),
+            rs2_delete_raw_data);
     handle_error(env, e);
 
-    int response_buffer_size = rs2_get_raw_data_size(response_buffer, &e);
+    int response_buffer_size = rs2_get_raw_data_size(response_buffer.get(), &e);
     handle_error(env, e);
 
-    const unsigned char* response_buffer_start = rs2_get_raw_data(response_buffer, &e);
+    const unsigned char* response_buffer_start = rs2_get_raw_data(response_buffer.get(), &e);
     handle_error(env, e);
 
     //releasing jni resources
@@ -120,12 +99,6 @@ Java_com_intel_realsense_librealsense_DebugProtocol_nSendAndReceiveData(JNIEnv *
     jbyteArray rv = env->NewByteArray(response_buffer_size);
     env->SetByteArrayRegion(rv, 0, response_buffer_size,
                             reinterpret_cast<const jbyte *>(response_buffer_start));
-
-    //releasing c resources
-    rs2_delete_raw_data(response_buffer);
-    rs2_delete_raw_data(response_bytes);
-    rs2_delete_raw_data(command_buffer);
-    rs2_delete_terminal_parser(terminal_parser);
 
     return rv;
 }
