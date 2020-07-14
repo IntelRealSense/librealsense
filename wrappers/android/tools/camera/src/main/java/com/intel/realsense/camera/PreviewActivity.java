92a1134c59299b7a9cb9e1e4de2ebe88cc02719c
gwen2018 <guoping.wen@intel.com>
2020-07-14 15:43:44 -0700

    fix camera app performance degradation and activity out of sync issues after repeated device unplug and plug

--- wrappers/android/tools/camera/src/main/java/com/intel/realsense/camera/PreviewActivity.java
+++ wrappers/android/tools/camera/src/main/java/com/intel/realsense/camera/PreviewActivity.java
@@ -55,6 +55,8 @@ public class PreviewActivity extends AppCompatActivity {
     private boolean statsToggle = false;
     private boolean mShow3D = false;
 
+    boolean keepalive = true;
+
     public synchronized void toggleStats(){
         statsToggle = !statsToggle;
         if(statsToggle){
@@ -73,6 +75,15 @@ public class PreviewActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_preview);
+
+        keepalive = true;
+
+        Intent intent = this.getIntent();
+
+        if (intent != null && intent.getExtras() != null ) {
+            keepalive = intent.getExtras().getBoolean("keepalive");
+        }
+
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
 
         mGLSurfaceView = findViewById(R.id.glSurfaceView);
@@ -143,6 +154,7 @@ public class PreviewActivity extends AppCompatActivity {
     @Override
     protected void onDestroy() {
         super.onDestroy();
+
         mGLSurfaceView.close();
     }
 
@@ -204,6 +216,11 @@ public class PreviewActivity extends AppCompatActivity {
     @Override
     protected void onResume() {
         super.onResume();
+
+        if(keepalive == false)
+        {
+            return;
+        }
 
         mStreamingStats  = new StreamingStats();
         mStreamer = new Streamer(this, true, new Streamer.Listener() {
@@ -319,5 +336,21 @@ public class PreviewActivity extends AppCompatActivity {
             s.setValue(option, val);
         else
             Toast.makeText(this, "This control is not supported by this device", Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    protected void onNewIntent(Intent intent)
+    {
+        super.onNewIntent(intent);
+
+        if (intent != null && intent.getExtras() != null ) {
+            keepalive = intent.getExtras().getBoolean("keepalive");
+        }
+
+        // destroy activity if requested
+        if(keepalive == false)
+        {
+            PreviewActivity.this.finish();
+        }
     }
 }
