c701aee7f43b2ea7174474e6d1cb931472ebe73b
Noha Yassin <37175819+nohayassin@users.noreply.github.com>
2020-07-26 12:33:41 +0300

    FW logger bug fix

--- tools/fw-logger/rs-fw-logger.cpp
+++ tools/fw-logger/rs-fw-logger.cpp
@@ -127,10 +127,12 @@ int main(int argc, char* argv[])
                     else
                     {
                         stringstream sstr;
-                        sstr << log_message.get_timestamp();
-                        sstr << " " << log_message.get_severity_str();
-                        sstr << "  FW_Log_Data:";
+                        sstr << datetime_string() << "  FW_Log_Data:";
+                        //sstr << log_message.get_timestamp();
+                        //sstr << " " << log_message.get_severity_str();
+                        //sstr << "  FW_Log_Data:";
                         std::vector<uint8_t> msg_data = log_message.data();
+                        size_t sizeMsg = msg_data.size();
                         for (int i = 0; i < msg_data.size(); ++i)
                         {
                         sstr << char2hex(msg_data[i]) << " ";
