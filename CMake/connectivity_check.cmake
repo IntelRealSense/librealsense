f7cacde37236582670b9f83b4cc62c70dee82128
libdavid <55893291+libdavid@users.noreply.github.com>
2020-08-18 18:31:31 -0700

    Uncomment "message" call

--- CMake/connectivity_check.cmake
+++ CMake/connectivity_check.cmake
@@ -1,5 +1,6 @@
 # License: Apache 2.0. See LICENSE file in root directory.
-# Copyright(c) 2019 Intel Corporation. All Rights Reserved.message(STATUS "Checking internet connection...")
+# Copyright(c) 2019 Intel Corporation. All Rights Reserved.
+message(STATUS "Checking internet connection...")
 file(DOWNLOAD "http://realsense-hw-public.s3-eu-west-1.amazonaws.com/Releases/connectivity_check" "${CMAKE_CURRENT_SOURCE_DIR}/connectivity_check" SHOW_PROGRESS TIMEOUT 5 STATUS status)
 list (FIND status "\"No error\"" _index)
 if (${_index} EQUAL -1)
