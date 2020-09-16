b62f925d6186f0c4a006183f98ce6b5935c53b31
remibettan <remi.bettan@intel.com>
2020-09-16 11:16:59 +0300

    HDR Merge - changing ir saturation threasholds and handling Y8, Y16 ir formats

--- src/proc/hdr-merge.h
+++ src/proc/hdr-merge.h
@@ -20,16 +20,23 @@ namespace librealsense
 
 
     private:
-        const int IR_UNDER_SATURATED_VALUE = 0x10; // 16
-        const int IR_OVER_SATURATED_VALUE = 0xf0; // 240
-        bool is_infrared_valid(uint8_t ir_value) const;
-        void discard_depth_merged_frame_if_needed(const rs2::frame& f);
-        bool check_frames_mergeability(const rs2::frameset first_fs, const rs2::frameset second_fs, bool& use_ir) const;
-        rs2::frame merging_algorithm(const rs2::frame_source& source, const rs2::frameset first_fs, 
-            const rs2::frameset second_fs, const bool use_ir);
+        const int IR_UNDER_SATURATED_VALUE = 0x05; // 5
+        const int IR_OVER_SATURATED_VALUE_Y8 = 0xfa; // 250
+        const int IR_OVER_SATURATED_VALUE_Y16 = 0xfffa; // 65530
 
+        void discard_depth_merged_frame_if_needed(const rs2::frame& f);
+        
+        bool check_frames_mergeability(const rs2::frameset first_fs, const rs2::frameset second_fs, bool& use_ir) const;
         bool should_ir_be_used_for_merging(const rs2::depth_frame& first_depth, const rs2::video_frame& first_ir,
             const rs2::depth_frame& second_depth, const rs2::video_frame& second_ir) const;
+        rs2::frame merging_algorithm(const rs2::frame_source& source, const rs2::frameset first_fs, 
+            const rs2::frameset second_fs, const bool use_ir) const;
+        template <typename T>
+        bool is_infrared_valid(T ir_value, rs2_format ir_format) const;
+        template <typename T>
+        void merge_frames_using_ir(uint16_t* new_data, uint16_t* d0, uint16_t* d1, 
+            const rs2::video_frame& first_ir, const rs2::video_frame& second_ir, int width_height_prod) const;
+        void merge_frames_using_only_depth(uint16_t* new_data, uint16_t* d0, uint16_t* d1, int width_height_prod) const;
 
         std::map<int, rs2::frameset> _framesets;
         rs2::frame _depth_merged_frame;
