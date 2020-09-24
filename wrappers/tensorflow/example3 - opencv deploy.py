6f196622b615fb508a13307a92c73227a35f4f6e
Noha Yassin <37175819+nohayassin@users.noreply.github.com>
2020-09-24 13:10:53 +0300

    example 1,2,3 - code

--- wrappers/tensorflow/example3 - opencv deploy.py
+++ wrappers/tensorflow/example3 - opencv deploy.py
@@ -19,6 +19,7 @@ pipeline.start(config)
 a = rs.align(rs.stream.color) # align between color and depth
 pc = rs.pointcloud()
 
+print("[INFO] loading model...")
 net = cv2.dnn.readNetFromTensorflow(r"C:\work\git\tensorflow\model\frozen_inference_graph.pb", r"C:\work\git\tensorflow\model\faster_rcnn_inception_v2_coco_2018_01_28.pbtxt.txt")
 while True:
     frames = pipeline.wait_for_frames()
@@ -35,15 +36,21 @@ while True:
 
     color_image = np.asanyarray(color_frame.get_data())
     scaled_size = (int(W), int(H))
-    color_image_small = cv2.resize(color_image, scaled_size) # needed to improve performance
+    #color_image_small = cv2.resize(color_image, scaled_size) # needed to improve performance
     net.setInput(cv2.dnn.blobFromImage(color_image, size=scaled_size, swapRB=True, crop=False))
     detections = net.forward()
 
+    colors_list = []
+    for i in range(61):
+        colors_list.append(tuple(np.random.choice(range(256), size=3)))
+
+    print("[INFO] drawing bounding box on detected objects...")
+    #print("[INFO] each detected object has a unique color") for ex1
     for detection in detections[0,0]:
         score = float(detection[2])
         idx = int(detection[1])
 
-        if score > 0.4 :# and (idx == 0 or idx == 61): for ex2/3 idx=0, for ex1 idx = 0-61
+        if score > 0.4 and idx == 0:# and (idx == 0 or idx == 61): for ex2/3 idx=0, for ex1 idx = 0-61
             left = detection[3] * W
             top = detection[4] * H
             right = detection[5] * W
@@ -56,7 +63,9 @@ while True:
             p1 = (int(bbox[0]), int(bbox[1]))
             p2 = (int(bbox[0] + bbox[2]), int(bbox[1] + bbox[3]))
             # draw box
-            cv2.rectangle(color_image, p1, p2, (255,0,0), 2, 1) # red.. pick different color for each idx (define array of colors.. use modolu)
+            #r, g, b = colors_list[idx%61] for ex1 we need different color for each object
+            #cv2.rectangle(color_image, p1, p2, (int(r), int(g), int(b)), 2, 1) # for ex1
+            cv2.rectangle(color_image, p1, p2, (255, 0, 0), 2, 1) # red.. pick different color for each idx (define array of colors.. use modolu)
 
             # x,y,z of bounding box
             obj_points = verts[int(bbox[1]):int(bbox[1] + bbox[3]), int(bbox[0]):int(bbox[0] + bbox[2])].reshape(-1, 3)
@@ -73,13 +82,18 @@ while True:
             my = np.amin(ys, initial=1)
             My = np.amax(ys, initial=-1)
 
+            print("[INFO] calculating height of object ", idx,"...")
             height = My - my # add next to rectangle print of height using cv library
 
     # Show images
-    cv2.namedWindow("RealSense", cv2.WND_PROP_FULLSCREEN)
-    cv2.setWindowProperty("RealSense", cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
+    cv2.namedWindow('RealSense', cv2.WINDOW_AUTOSIZE)
     cv2.imshow('RealSense', color_image)
     cv2.waitKey(1)
+
+    #cv2.namedWindow("RealSense", cv2.WND_PROP_FULLSCREEN)
+    #cv2.setWindowProperty("RealSense", cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
+    #cv2.imshow('RealSense', color_image)
+    #cv2.waitKey(1)
 
 
 # Stop streaming
