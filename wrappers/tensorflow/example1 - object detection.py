5229aaf96bd1d4929909aec0d67f1ee5bf23ae9c
Noha Yassin <37175819+nohayassin@users.noreply.github.com>
2020-09-24 15:36:36 +0300

    examples 1 with tensorflow

--- wrappers/tensorflow/example1 - object detection.py
+++ wrappers/tensorflow/example1 - object detection.py
@@ -1,6 +1,10 @@
 import pyrealsense2 as rs
 import numpy as np
 import cv2
+import tensorflow as tf
+from tensorflow import keras
+from skimage import img_as_uint
+import time, sys
 
 W = 848
 H = 480
@@ -12,21 +16,22 @@ config.enable_stream(rs.stream.depth, W, H, rs.format.z16, 30)
 config.enable_stream(rs.stream.color, W, H, rs.format.bgr8, 30)
 
 
-# Start streaming
-# add prints ..
+print("[INFO] start streaming...")
 pipeline.start(config)
 
-a = rs.align(rs.stream.color) # align between color and depth
-pc = rs.pointcloud()
+aligned_stream = rs.align(rs.stream.color)  #align between color and depth
+point_cloud = rs.pointcloud()
 
 print("[INFO] loading model...")
-net = cv2.dnn.readNetFromTensorflow(r"C:\work\git\tensorflow\model\frozen_inference_graph.pb", r"C:\work\git\tensorflow\model\faster_rcnn_inception_v2_coco_2018_01_28.pbtxt.txt")
+PB_PATH = r"C:\Users\nyassin\Downloads\faster_rcnn_inception_v2_coco_2018_01_28.tar\faster_rcnn_inception_v2_coco_2018_01_28\saved_model"
+model = keras.models.load_model(PB_PATH)
+
 while True:
     frames = pipeline.wait_for_frames()
-    frames = a.process(frames)
+    frames = aligned_stream.process(frames)
     depth_frame = frames.get_depth_frame()
     color_frame = frames.get_color_frame()
-    points = pc.calculate(depth_frame)
+    points = point_cloud.calculate(depth_frame)
     verts = np.asanyarray(points.get_vertices()).view(np.float32).reshape(-1, W, 3)  # xyz
 
     # Convert images to numpy arrays
@@ -36,8 +41,9 @@ while True:
 
     color_image = np.asanyarray(color_frame.get_data())
     scaled_size = (int(W), int(H))
-    net.setInput(cv2.dnn.blobFromImage(color_image, size=scaled_size, swapRB=True, crop=False))
-    detections = net.forward()
+    #net.setInput(cv2.dnn.blobFromImage(color_image, size=scaled_size, swapRB=True, crop=False))
+    #detections = net.forward()
+    detections = model.predict(color_image)
 
     colors_list = []
     for i in range(61):
@@ -63,14 +69,13 @@ while True:
             p2 = (int(bbox[0] + bbox[2]), int(bbox[1] + bbox[3]))
             # draw box
             r, g, b = colors_list[idx%61]
-            cv2.rectangle(color_image, p1, p2, (int(r), int(g), int(b)), 2, 1) # for ex1
+            cv2.rectangle(color_image, p1, p2, (int(r), int(g), int(b)), 2, 1)
 
 
-    # Show images
+    print("[INFO] displaying image ...")
     cv2.namedWindow('RealSense', cv2.WINDOW_AUTOSIZE)
     cv2.imshow('RealSense', color_image)
     cv2.waitKey(1)
 
-# Stop streamin
 print("[INFO] stop streaming ...")
 pipeline.stop()
