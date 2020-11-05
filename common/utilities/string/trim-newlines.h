8796da908b7133bf7469e73bb7dc187c6a87a4ac
Eran <librealsense.eran@gmail.com>
2020-11-05 12:49:15 +0200

    more docs :)

--- common/utilities/string/trim-newlines.h
+++ common/utilities/string/trim-newlines.h
@@ -26,23 +26,32 @@ namespace string {
 //     \n
 //     third line:\n
 //         fourth\n
-inline std::string trim_newlines( std::string s )
+inline std::string trim_newlines( std::string text )
 {
-    char const * const base = s.c_str();
-    char * dest = (char *)base;
-    char const * src = dest;
+    char const * const base = text.c_str();  // first character in the text
+    char * dest = (char *)base;              // running pointer to next destination character
+    char const * src = dest;                 // running pointer to current source character
+    //      "abc  \nline"
+    //      dest-^  ^-src
     while( *src )
     {
+        // Copy everything until we hit a newline
         if( *src != '\n' )
         {
             *dest++ = *src++;
             continue;
         }
+        // Go back and remove spaces
         while( dest > base && dest[-1] == ' ' )
             --dest;
+        // We're going to insert something (either space or newline); move src forward by one
         ++src;
+        // If we're at the end of the string -- don't touch
+        // If we're at another newline -- don't touch
+        // If we're at the start of the text (dest==base) -- don't touch
         if( *src && *src != '\n' && dest > base )
         {
+            // If previous line does not end with ':' and is not empty, insert a space
             if( dest[-1] != ':' && dest[-1] != '\n' )
             {
                 *dest++ = ' ';
@@ -51,8 +60,8 @@ inline std::string trim_newlines( std::string s )
         }
         *dest++ = '\n';
     }
-    s.resize( dest - base );
-    return s;
+    text.resize( dest - base );
+    return text;
 }
 
 
