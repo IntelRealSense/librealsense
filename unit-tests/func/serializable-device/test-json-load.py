044084f6887acd95d2e44acca35aa273c5091505
MMirbach <m.mirbach153@gmail.com>
2020-11-30 09:38:02 +0200

    tested

--- unit-tests/func/serializable-device/test-json-load.py
+++ unit-tests/func/serializable-device/test-json-load.py
@@ -3,7 +3,7 @@ sys.path.insert(0,"C://Users//mmirbach//git//librealsense//build//RelWithDebInfo
 sys.path.insert(0,"C://Users//mmirbach//git//librealsense//unit-tests//py")
 
 from time import sleep
-import pyrealsense2 as rs, test, ac
+import pyrealsense2 as rs, test
 
 devices = test.find_devices_by_product_line_or_exit(rs.product_line.L500)
 device = devices[0]
@@ -35,11 +35,14 @@ presets = [rs.l500_visual_preset.custom,
            rs.l500_visual_preset.max_range,
            rs.l500_visual_preset.short_range]
 
-for i in presets:
+for preset in presets:
     try:
-        depth_sensor.set_option(rs.option.visual_preset, int(i))
+        depth_sensor.set_option(rs.option.visual_preset, int(preset))
         serialized_json = sd.serialize_json()
         sd.load_json(serialized_json)
+        visual_preset_number = depth_sensor.get_option(rs.option.visual_preset)
+        visual_preset_name = rs.l500_visual_preset(int(visual_preset_number))
+        test.check_equal(visual_preset_name, preset)
     except:
         test.unexpected_exception()
 test.finish()
