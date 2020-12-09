fd1bac2fcc18ded17b384298b441e71da17c000f
daversintel <dave.tong@intel.com>
2020-12-08 17:14:44 -0800

    1. Added three time auto retry. 2. Restricted only one OCC calibration floating around.

--- common/model-views.cpp
+++ common/model-views.cpp
@@ -5215,6 +5215,8 @@ namespace rs2
                             for (auto&& n : related_notifications)
                                 if (dynamic_cast<autocalib_notification_model*>(n.get()))
                                     n->dismiss(false);
+
+                            related_notifications.push_back(n);
                         }
                         catch (const error& e)
                         {
