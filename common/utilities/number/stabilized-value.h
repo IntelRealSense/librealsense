38810488fdf1befc754386aef05e005213f5eead
Eran <librealsense.eran@gmail.com>
2020-12-10 09:24:29 +0200

    Some fixes and docs in stabilized_value

--- common/utilities/number/stabilized-value.h
+++ common/utilities/number/stabilized-value.h
@@ -10,31 +10,37 @@
 namespace utilities {
 namespace number {
 
-// The stabilized value implement a history based stable value.
-// It supply a way to define and get values with a required stabilization.
-// When the user adds a value, the value is inserted into the history
-// When the user call "get" the last stable value is returned
-// The get will return one of 3 values, depend on the case:
-//      case 1 : the highest occurrences value in the history meet the require percent stability - return it
-//      case 2 : if not , if the last stable value is still exist in the history, return it.
-//      case 3 : if the last stable value is not in the history, return the highest occurrences value
-// Inputs: Template <value_type>
-//         required history size
-//         required stabilization percentage as a fraction , range [0-1]
+// Given a rapidly changing history of a value, we do not always want to show every minor
+// change right away. Rather, a "stable" value can be deduced from the history and changed
+// only when the history clearly reflects a new "stabilization percentage".
+//
+// The stabilized value is, in order of importance:
+//   - the highest-occurring value in history that's over the required percentage
+//   - the previous stabilized value, if it's still in history
+//   - the value which occurs the most in history
+//
+// If the history is empty, no stable value exists and an exception will be thrown.
+//
+// Inputs: Type of value <T>
+//         Required history size in the ctor
+//
 // The stabilized_value class is thread safe.
-template < typename T > class stabilized_value
+//
+template < typename T >
+class stabilized_value
 {
 public:
     stabilized_value( size_t history_size )
         : _history_size( history_size )
         , _last_stable_value( 0 )
-        , _last_stable_value_set( false )
         , _recalc_stable_val( false )
     {
+        if( ! history_size )
+            throw std::runtime_error( "history size must be > 0" );
     }
 
     stabilized_value() = delete;
-    stabilized_value(const stabilized_value &) = delete;
+    stabilized_value( const stabilized_value & ) = delete;
 
 
     void add( T val )
@@ -42,42 +48,40 @@ public:
         const std::lock_guard< std::mutex > lock( _mutex );
         if( _values.empty() )
         {
-            _values.push_back( val );
             _last_stable_value = val;
-            _recalc_stable_val = false;
-            return;
+            _last_stable_percentage = 100.f;
+            _values.push_back( val );
         }
-
-        _recalc_stable_val = true;
-
-        _values.push_back( val );
-        if( _values.size() > _history_size )
-            _values.pop_front();
+        else
+        {
+            if( _values.size() >= _history_size )
+                _values.pop_front();
+            _last_stable_percentage = 0.f;
+            _values.push_back( val );
+        }
     }
 
-    T get( float stabilization_percent = 0.75f) const
+    T get( float stabilization_percent = 0.75f ) const
     {
-        if ((stabilization_percent <= 0.0f) || (stabilization_percent > 1.0f))
-            throw std::runtime_error("Illegal value for stabilize_percentage: "
-                + std::to_string(stabilization_percent));
+        if( stabilization_percent <= 0.f  ||  stabilization_percent > 1.f )
+            throw std::runtime_error( "illegal stabilization percentage "
+                + std::to_string( stabilization_percent ));
 
         std::lock_guard< std::mutex > lock( _mutex );
 
         if( _values.empty() )
-        {
             throw std::runtime_error( "history is empty; no stable value" );
-        }
 
-        if (_recalc_stable_val || (_stabilize_percentage != stabilization_percent))
+        if( _last_stable_percentage != stabilization_percent )
         {
-            _stabilize_percentage = stabilization_percent;
+            _last_stable_percentage = stabilization_percent;
             std::unordered_map< T, int > values_count_map;
             std::pair< T, int > most_stable_value = { 0, 0 };
             for( T val : _values )
             {
                 auto current_val = ++values_count_map[val];
 
-                if( most_stable_value.second < current_val)
+                if( most_stable_value.second < current_val )
                 {
                     most_stable_value.first = val;
                     most_stable_value.second = current_val;
@@ -87,24 +91,16 @@ public:
             auto new_value_percentage
                 = most_stable_value.second / static_cast< float >( _values.size() );
             
-            // if the highest occurrences value meet the stabilized percentage request return it.
-            // If not, if the last stable value exist in the history, return it , if not return the best we got. (highest occurrences value)
-            if (!_last_stable_value_set || (new_value_percentage >= _stabilize_percentage))
+            // The stabilized value is, in order of importance:
+            //   - the highest-occurring value in history that's over the required percentage
+            //   - the previous stabilized value, if it's still in history
+            //   - the value which occurs the most in history
+            if( new_value_percentage >= _last_stable_percentage
+                ||  values_count_map.find( _last_stable_value ) == values_count_map.end() )
             {
                 _last_stable_value = most_stable_value.first;
             }
-            else
-            {
-                if (values_count_map.find(_last_stable_value) == values_count_map.end())
-                {
-                    _last_stable_value = most_stable_value.first;
-                }
-            }
-
-            _recalc_stable_val = false;
         }
-
-        _last_stable_value_set = true;
 
         return _last_stable_value;
     }
@@ -113,7 +109,6 @@ public:
     {
         const std::lock_guard< std::mutex > lock( _mutex );
         _values.clear();
-        _last_stable_value_set = false;
     }
 
     bool empty() const
@@ -126,9 +121,7 @@ private:
     std::deque< T > _values;
     const size_t _history_size;
     mutable T _last_stable_value;
-    mutable bool _last_stable_value_set;
-    mutable float _stabilize_percentage;
-    mutable bool _recalc_stable_val;
+    mutable float _last_stable_percentage;
     mutable std::mutex _mutex;
 };
 
