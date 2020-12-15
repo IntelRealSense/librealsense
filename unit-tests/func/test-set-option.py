73505f286e649548acaaf47d8e7475728d5c2309
aseelegbaria <aseel.egbaria@intel.com>
2020-12-15 13:17:13 +0200

    CR fixes

--- unit-tests/func/test-set-option.py
+++ unit-tests/func/test-set-option.py
@@ -1,3 +1,4 @@
+import platform
 import pyrealsense2 as rs, test
 import time
 
@@ -11,22 +12,30 @@ previous_depth_frame_number = -1
 previous_color_frame_number = -1
 after_set_option = 0
 
-def collect_frames_after_set(): 
+
+def get_allowed_drops(): 
+    global after_set_option
+    if platform.system() == 'Linux' and after_set_option == 1:
+        return 4 #the number of allowed drops after set_option in linux is 4  
+    return 0
+
+def set_new_value(sensor, option, value): 
     global after_set_option
     after_set_option = 1
+    sensor.set_option(option, value)
     time.sleep(0.5) #collect frames for 0.5 seconds
     after_set_option = 0
 
 def check_depth_frame_drops(frame):
     global previous_depth_frame_number
-    global after_set_option
-    test.check_frame_drops(frame, previous_depth_frame_number, after_set_option)
+    allowed_drops = get_allowed_drops()
+    test.check_frame_drops(frame, previous_depth_frame_number, allowed_drops)
     previous_depth_frame_number = frame.get_frame_number()
 
 def check_color_frame_drops(frame):
     global previous_color_frame_number
-    global after_set_option
-    test.check_frame_drops(frame, previous_color_frame_number, after_set_option)
+    allowed_drops = get_allowed_drops()
+    test.check_frame_drops(frame, previous_color_frame_number, allowed_drops)
     previous_color_frame_number = frame.get_frame_number()
 
 depth_profile = next(p for p in
@@ -57,8 +66,9 @@ current_laser_control = 10
 test.start("Checking for frame drops when setting laser power several times")
 
 for i in range(1,5): 
-    depth_sensor.set_option(laser_power, current_laser_control + 10*i)
-    collect_frames_after_set()
+    new_value = current_laser_control + 10*i
+    set_new_value(depth_sensor, laser_power, new_value)
+
 
 test.finish()
 #############################################################################################
@@ -73,15 +83,13 @@ for option in depth_options:
     if (depth_sensor.is_option_read_only(option) == 1): 
         continue
     new_value = depth_sensor.get_option_range(option).min
-    depth_sensor.set_option(option,new_value)
-    collect_frames_after_set()
+    set_new_value(depth_sensor, option, new_value)
 
 for option in color_options:
     if (color_sensor.is_option_read_only(option) == 1): 
         continue
     new_value = color_sensor.get_option_range(option).min
-    color_sensor.set_option(option,new_value)
-    collect_frames_after_set()
+    set_new_value(color_sensor, option, new_value)
 
 test.finish()
 #############################################################################################
