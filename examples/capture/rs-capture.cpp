580e44ed46debd5a69637c6e61b37ff28380c6f5
Noha Yassin <37175819+nohayassin@users.noreply.github.com>
2021-01-14 10:31:24 +0200

    Time to first frame delay issue - Solution

--- examples/capture/rs-capture.cpp
+++ examples/capture/rs-capture.cpp
@@ -6,7 +6,7 @@
 
 // Capture Example demonstrates how to
 // capture depth and color video streams and render them to the screen
-int main(int argc, char * argv[]) try
+int main(int argc, char* argv[]) try
 {
     rs2::log_to_console(RS2_LOG_SEVERITY_ERROR);
     // Create a simple OpenGL window for rendering:
