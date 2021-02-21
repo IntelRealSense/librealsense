c6d605ab961fa0b3b25e76246e5da8d02d5d0def
NirAz <nir.azkiel@intel.com>
2021-02-21 13:14:44 +0200

    update python package descriptors for python 3.8 + 3.9

--- wrappers/python/setup.py
+++ wrappers/python/setup.py
@@ -63,6 +63,8 @@ setup(
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
         'Topic :: Multimedia :: Video',
         'Topic :: Scientific/Engineering',
         'Topic :: Scientific/Engineering :: Human Machine Interfaces',
