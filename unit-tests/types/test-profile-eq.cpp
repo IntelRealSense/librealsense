bedec8bebc3a6e1025f0cf589876377e0547d81b
doronhi <doron.hirshberg@intel.com>
2021-03-08 10:06:23 +0200

    Add test for equal profiles.
    Clean.

--- unit-tests/types/test-profile-eq.cpp
+++ unit-tests/types/test-profile-eq.cpp
@@ -9,16 +9,18 @@ using namespace rs2;
 
 TEST_CASE("test video_stream_profile operator==", "[live]")
 {
+    // Test that for 2 video_stream_profile objects, if width and height are different
+    // then, video_stream_profile.operator== returns false.
+
     auto devices = find_devices_by_product_line_or_exit(RS2_PRODUCT_LINE_ANY_INTEL);
     auto dev = devices[0];
 
     auto depth_sens = dev.first< rs2::depth_sensor >();
 
-    REQUIRE_NOTHROW(depth_sens.set_option(RS2_OPTION_EMITTER_ON_OFF, 1));
-
     stream_profile profile0, profile1;
     std::vector< stream_profile > stream_profiles;
     REQUIRE_NOTHROW(stream_profiles = depth_sens.get_stream_profiles());
+    REQUIRE_NOTHROW(stream_profiles.size() > 1);
     profile0 = stream_profiles[0];
     video_stream_profile vprofile0 = profile0.as<video_stream_profile>();
 
@@ -27,11 +29,13 @@ TEST_CASE("test video_stream_profile operator==", "[live]")
         if (profile == profile0)
         {
             video_stream_profile vprofile = profile.as<video_stream_profile>();
-            if (vprofile0.width() != vprofile.width() ||
-                vprofile0.height() != vprofile.height())
+            if (vprofile0.width() == vprofile.width() &&
+                vprofile0.height() == vprofile.height())
             {
-                //std::cout << "compare: " << vprofile0.stream_type() << "(" << vprofile0.stream_index() << "):" << vprofile0.width() << "x" << vprofile0.height() << " with:" <<
-                //    vprofile.stream_type() << "(" << vprofile.stream_index() << "):" << vprofile.width() << "x" << vprofile.height() << std::endl;
+                REQUIRE(vprofile0 == vprofile);
+            }
+            else
+            {
                 REQUIRE(!(vprofile0 == vprofile));
             }
         }
