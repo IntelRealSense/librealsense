ab06e1b8bf122ded5d5bbf619c19c107a48ac8a0
Eran <librealsense.eran@gmail.com>
2021-03-08 11:56:08 +0200

    modify rs-hello-realsense to show mrd/mwd

--- examples/hello-realsense/rs-hello-realsense.cpp
+++ examples/hello-realsense/rs-hello-realsense.cpp
@@ -4,34 +4,131 @@
 #include <librealsense2/rs.hpp> // Include RealSense Cross Platform API
 #include <iostream>             // for cout
 
-// Hello RealSense example demonstrates the basics of connecting to a RealSense device
-// and taking advantage of depth data
+#include <../src/hw-monitor.h>
+using librealsense::to_string;
+
+
+rs2::device find_first_l515( rs2::context const& ctx )
+{
+    auto devices = ctx.query_devices();
+    for( auto&& dev : devices )
+    {
+        bool dev_is_l515 = dev.supports( RS2_CAMERA_INFO_NAME ) ?
+            std::string( dev.get_info( RS2_CAMERA_INFO_NAME ) ).find( "L515" ) != std::string::npos : false;
+        if( dev_is_l515 )
+            return dev;
+    }
+    return {};
+}
+
+
+std::ostream&
+dump( std::ostream& os, std::vector< byte > const& bytes, std::string const& prefix = "..." )
+{
+    auto it = bytes.begin();
+    while( it < bytes.end() )
+    {
+        os << std::string( to_string() << std::setfill( '0' ) << std::setw( 6 ) << std::hex
+            << (it - bytes.begin()) );
+        if( !prefix.empty() )
+            os << ' ' << prefix;
+        for( size_t offset = 0; offset < 16 && it < bytes.end(); ++offset, ++it )
+        {
+            os << ' ';
+            os << (to_string()
+                << std::setfill( '0' ) << std::setw( 2 ) << std::hex << +*it)
+                .
+                operator std::string();
+        }
+        os << std::endl;
+    }
+    return os;
+}
+
+void check( std::vector< byte >& bytes, uint32_t op_code_sent )
+{
+    if( bytes.size() < 4 )
+        throw std::runtime_error( "unexpected size returned" );
+
+    auto op_code_returned = librealsense::pack( bytes[3], bytes[2], bytes[1], bytes[0] );
+    if( op_code_returned != op_code_sent )
+    {
+        auto response = static_cast<librealsense::hwmon_response>(op_code_returned);
+
+        auto str = librealsense::hwmon_error2str( response );
+        throw std::runtime_error( to_string()
+            << "hwmon command 0x" << std::hex << unsigned( op_code_sent )
+            << " failed (response " << response << "= "
+            << (str.empty() ? "unknown" : str) << ")" );
+    }
+
+    bytes.erase( bytes.begin(), bytes.begin() + sizeof( op_code_returned ) );
+    //std::cout << "Success" << std::endl;
+}
+
+
+std::vector< byte > mrd( rs2::debug_protocol& fw,
+    unsigned start,
+    unsigned end )  // not inclusive!
+{
+    /*
+      <Command Name="MRD" CmdPipe="WinUsb" CmdInterface="UvcAndI2c" Opcode="0x01" I2cRegOffset="0x3008" I2cCmdType="ReadAfterWrite" CmdPermission="Public" ReadFormat="Doubles" IsReadCommand="true" Description="Read Tensilica memory ( 32bit ). Output : 32bit dump">
+        <Parameter1 Name="Offset" />
+        <Parameter2 Name="End Address" />
+      </Command>
+    */
+    std::cout << "\nMRD:\n";
+    std::array< byte, librealsense::HW_MONITOR_BUFFER_SIZE > cmd;
+    int cb;
+    librealsense::hw_monitor::fill_usb_buffer( 0x01, start, end, 0, 0, nullptr, 0, cmd.data(), cb );
+    auto bytes = fw.send_and_receive_raw_data( { cmd.begin(), cmd.end() } );
+    check( bytes, 0x01 );
+    dump( std::cout, bytes );
+    return bytes;
+}
+
+void mwd( rs2::debug_protocol& fw,
+    unsigned start,
+    unsigned end,  // not inclusive!
+    std::vector< byte > const& bytes )
+{
+    /*
+      <Command Name="MWD" CmdPipe="WinUsb" CmdInterface="UvcAndI2c" Opcode="0x02" I2cRegOffset="0x3014" I2cCmdType="WriteOnly" CmdPermission="Private" Description="Write Tensilica memory ( 32bit )">
+        <Parameter1 Name="Start Address" />
+        <Parameter2 Name="End Address" />
+        <Data Name="Data" IsReverseBytes="true" FormatLength="8" />
+      </Command>
+    */
+    std::cout << "\nMWD\n";
+    dump( std::cout, bytes );
+    std::array< byte, librealsense::HW_MONITOR_BUFFER_SIZE > cmd;
+    int cb;
+    librealsense::hw_monitor::fill_usb_buffer( 0x02, start, end, 0, 0, bytes.data(), (int)bytes.size(), cmd.data(), cb );
+    auto rbytes = fw.send_and_receive_raw_data( { cmd.begin(), cmd.end() } );
+    check( rbytes, 0x02 );
+    dump( std::cout, rbytes );
+}
+
+
 int main(int argc, char * argv[]) try
 {
-    // Create a Pipeline - this serves as a top-level API for streaming and processing frames
-    rs2::pipeline p;
-
-    // Configure and start the pipeline
-    p.start();
-
-    while (true)
+    rs2::context ctx;
+    auto device = find_first_l515( ctx );
+    if( ! device )
     {
-        // Block program until frames arrive
-        rs2::frameset frames = p.wait_for_frames();
-
-        // Try to get a frame of a depth image
-        rs2::depth_frame depth = frames.get_depth_frame();
-
-        // Get the depth frame's dimensions
-        auto width = depth.get_width();
-        auto height = depth.get_height();
-
-        // Query the distance from the camera to the object in the center of the image
-        float dist_to_center = depth.get_distance(width / 2, height / 2);
-
-        // Print the distance
-        std::cout << "The camera is facing an object " << dist_to_center << " meters away \r";
+        std::cerr << "No L515 device found!" << std::endl;
+        return 1;
     }
+    auto fw = device.as< rs2::debug_protocol >();
+
+    std::vector< byte > bytes = mrd( fw, 0x90000040, 0x90000044 );
+
+    bytes[1] += 1;
+    mwd( fw, 0x90000040, 0x90000044, bytes );
+    // This specific memory location will get changed only while streaming color!
+    // So, if you see the same value in the next MRD, that's why...
+
+    mrd( fw, 0x90000040, 0x90000044 );
 
     return EXIT_SUCCESS;
 }
