944b223204cac2bedcc1b98ea0b293dd79855638
MMirbach <m.mirbach153@gmail.com>
2021-03-11 15:03:22 +0200

    runs and removes dir

--- unit-tests/func/rec-play/test-rec-play.py
+++ unit-tests/func/rec-play/test-rec-play.py
@@ -4,13 +4,11 @@
 #test:device L500*
 #test:device D400*
 
-import pyrealsense2 as rs, os, time, tempfile
+import pyrealsense2 as rs, os, time, tempfile, sys
 from rspy import devices, log, test
 
-current_dir = os.path.dirname( os.path.abspath( __file__ ))
-
-f = tempfile.NamedTemporaryFile( mode='w+', suffix='.bag', prefix='rnp_', dir=current_dir)
-file_name = f.name
+temp_dir = tempfile.TemporaryDirectory(prefix='recordings')
+file_name = temp_dir.name + os.sep + 'rec.bag'
 print(file_name)
 pipeline = rs.pipeline()
 cfg = rs.config()
@@ -21,142 +19,161 @@ pipeline.stop()
 pipeline = rs.pipeline()
 cfg = rs.config()
 cfg.enable_device_from_file( file_name )
-cfg.enable_stream(rs.stream.depth, rs.format.z16, 30)
 pipeline.start( cfg )
-device = pipeline.get_active_profile().get_device()
-playback = rs.playback( device )
-playback.resume()
-pipeline.wait_for_frames() # fails, no frames arrive
+pipeline.wait_for_frames()
 print("waited")
 pipeline.stop()
-#
-#
-# dev = test.find_first_device_or_exit()
-# recorder = rs.recorder( "recordings.bag", dev )
-# depth_sensor = dev.first_depth_sensor()
-# color_sensor = dev.first_color_sensor()
-#
-# cp = next(p for p in color_sensor.profiles if p.fps() == 30
-#                 and p.stream_type() == rs.stream.color
-#                 and p.format() == rs.format.yuyv
-#                 and p.as_video_stream_profile().width() == 1280
-#                 and p.as_video_stream_profile().height() == 720)
-#
-# dp = next(p for p in
-#                 depth_sensor.profiles if p.fps() == 30
-#                 and p.stream_type() == rs.stream.depth
-#                 and p.format() == rs.format.z16
-#                 and p.as_video_stream_profile().width() == 1024
-#                 and p.as_video_stream_profile().height() == 768)
-#
-# depth_sensor.open( dp )
-# depth_sensor.start( lambda f: None )
-# color_sensor.open( cp )
-# color_sensor.start( lambda f: None )
-#
-# time.sleep(9)
-# recorder.pause()
-# color_sensor.stop()
-# color_sensor.close()
-# depth_sensor.stop()
-# depth_sensor.close()
-#
-# recorder = None
-# ctx = rs.context()
-# playback = ctx.load_device( "recordings.bag" )
-#
-# frames = False
-# def got_frames(frame):
-#     global frames
-#     frames = True
-#
-# depth_sensor = playback.first_depth_sensor()
-# color_sensor = playback.first_color_sensor()
-# cp = next(p for p in color_sensor.profiles if p.fps() == 30
-#                 and p.stream_type() == rs.stream.color
-#                 and p.format() == rs.format.yuyv
-#                 and p.as_video_stream_profile().width() == 1280
-#                 and p.as_video_stream_profile().height() == 720)
-#
-# dp = next(p for p in
-#                 depth_sensor.profiles if p.fps() == 30
-#                 and p.stream_type() == rs.stream.depth
-#                 and p.format() == rs.format.z16
-#                 and p.as_video_stream_profile().width() == 1024
-#                 and p.as_video_stream_profile().height() == 768)
-#
-# depth_sensor.open( dp )
-# depth_sensor.start( got_frames )
-#
-# color_sensor.open( cp )
-# color_sensor.start( got_frames )
-#
-# time.sleep(3)
-# print("res:", frames)
-#
-#
-#
-#
-#
-#
-# sync = rs.syncer()
-# dev = test.find_first_device_or_exit()
-# recorder = rs.recorder( "recordings.bag", dev )
-# depth_sensor = dev.first_depth_sensor()
-# color_sensor = dev.first_color_sensor()
-#
-# cp = next(p for p in color_sensor.profiles if p.fps() == 30
-#                 and p.stream_type() == rs.stream.color
-#                 and p.format() == rs.format.yuyv
-#                 and p.as_video_stream_profile().width() == 1280
-#                 and p.as_video_stream_profile().height() == 720)
-#
-# dp = next(p for p in
-#                 depth_sensor.profiles if p.fps() == 30
-#                 and p.stream_type() == rs.stream.depth
-#                 and p.format() == rs.format.z16
-#                 and p.as_video_stream_profile().width() == 1024
-#                 and p.as_video_stream_profile().height() == 768)
-#
-# depth_sensor.open( dp )
-# depth_sensor.start( sync )
-# color_sensor.open( cp )
-# color_sensor.start( sync )
-#
-# time.sleep(9)
-# recorder.pause()
-# color_sensor.stop()
-# color_sensor.close()
-# depth_sensor.stop()
-# depth_sensor.close()
-#
-# recorder = None
-# ctx = rs.context()
-# playback = ctx.load_device( "recordings.bag" )
-#
-# depth_sensor = playback.first_depth_sensor()
-# color_sensor = playback.first_color_sensor()
-# cp = next(p for p in color_sensor.profiles if p.fps() == 30
-#                 and p.stream_type() == rs.stream.color
-#                 and p.format() == rs.format.yuyv
-#                 and p.as_video_stream_profile().width() == 1280
-#                 and p.as_video_stream_profile().height() == 720)
-#
-# dp = next(p for p in
-#                 depth_sensor.profiles if p.fps() == 30
-#                 and p.stream_type() == rs.stream.depth
-#                 and p.format() == rs.format.z16
-#                 and p.as_video_stream_profile().width() == 1024
-#                 and p.as_video_stream_profile().height() == 768)
-#
-# # sync = rs.syncer()
-#
-# depth_sensor.open( dp )
-# depth_sensor.start( sync )
-#
-# color_sensor.open( cp )
-# color_sensor.start( sync )
-# sync.wait_for_frames()
-# print("waited")
-#
-#
+
+del cfg # otherwise file cant be deleted
+
+################################################################################################
+
+dev = test.find_first_device_or_exit()
+recorder = rs.recorder( file_name, dev )
+depth_sensor = dev.first_depth_sensor()
+color_sensor = dev.first_color_sensor()
+
+cp = next(p for p in color_sensor.profiles if p.fps() == 30
+                and p.stream_type() == rs.stream.color
+                and p.format() == rs.format.yuyv
+                and p.as_video_stream_profile().width() == 1280
+                and p.as_video_stream_profile().height() == 720)
+
+dp = next(p for p in
+                depth_sensor.profiles if p.fps() == 30
+                and p.stream_type() == rs.stream.depth
+                and p.format() == rs.format.z16
+                and p.as_video_stream_profile().width() == 1024
+                and p.as_video_stream_profile().height() == 768)
+
+depth_sensor.open( dp )
+depth_sensor.start( lambda f: None )
+color_sensor.open( cp )
+color_sensor.start( lambda f: None )
+
+time.sleep(9)
+
+recorder.pause()
+del recorder
+color_sensor.stop()
+color_sensor.close()
+depth_sensor.stop()
+depth_sensor.close()
+
+ctx = rs.context()
+playback = ctx.load_device( file_name )
+
+frames = False
+def got_frames(frame):
+    global frames
+    frames = True
+
+depth_sensor = playback.first_depth_sensor()
+color_sensor = playback.first_color_sensor()
+
+cp = next(p for p in color_sensor.profiles if p.fps() == 30
+                and p.stream_type() == rs.stream.color
+                and p.format() == rs.format.yuyv
+                and p.as_video_stream_profile().width() == 1280
+                and p.as_video_stream_profile().height() == 720)
+
+dp = next(p for p in
+                depth_sensor.profiles if p.fps() == 30
+                and p.stream_type() == rs.stream.depth
+                and p.format() == rs.format.z16
+                and p.as_video_stream_profile().width() == 1024
+                and p.as_video_stream_profile().height() == 768)
+
+depth_sensor.open( dp )
+depth_sensor.start( got_frames )
+color_sensor.open( cp )
+color_sensor.start( got_frames )
+
+time.sleep(3)
+
+color_sensor.stop()
+color_sensor.close()
+depth_sensor.stop()
+depth_sensor.close()
+playback.pause()
+
+print("res:", frames)
+time.sleep(3)
+
+del playback
+del depth_sensor
+del color_sensor
+
+#####################################################################################################
+
+sync = rs.syncer()
+dev = test.find_first_device_or_exit()
+recorder = rs.recorder( file_name, dev )
+depth_sensor = dev.first_depth_sensor()
+color_sensor = dev.first_color_sensor()
+
+cp = next(p for p in color_sensor.profiles if p.fps() == 30
+                and p.stream_type() == rs.stream.color
+                and p.format() == rs.format.yuyv
+                and p.as_video_stream_profile().width() == 1280
+                and p.as_video_stream_profile().height() == 720)
+
+dp = next(p for p in
+                depth_sensor.profiles if p.fps() == 30
+                and p.stream_type() == rs.stream.depth
+                and p.format() == rs.format.z16
+                and p.as_video_stream_profile().width() == 1024
+                and p.as_video_stream_profile().height() == 768)
+
+depth_sensor.open( dp )
+depth_sensor.start( sync )
+color_sensor.open( cp )
+color_sensor.start( sync )
+
+time.sleep(9)
+
+recorder.pause()
+del recorder
+color_sensor.stop()
+color_sensor.close()
+depth_sensor.stop()
+depth_sensor.close()
+
+ctx = rs.context()
+playback = ctx.load_device( file_name )
+
+depth_sensor = playback.first_depth_sensor()
+color_sensor = playback.first_color_sensor()
+cp = next(p for p in color_sensor.profiles if p.fps() == 30
+                and p.stream_type() == rs.stream.color
+                and p.format() == rs.format.yuyv
+                and p.as_video_stream_profile().width() == 1280
+                and p.as_video_stream_profile().height() == 720)
+
+dp = next(p for p in
+                depth_sensor.profiles if p.fps() == 30
+                and p.stream_type() == rs.stream.depth
+                and p.format() == rs.format.z16
+                and p.as_video_stream_profile().width() == 1024
+                and p.as_video_stream_profile().height() == 768)
+
+depth_sensor.open( dp )
+depth_sensor.start( sync )
+color_sensor.open( cp )
+color_sensor.start( sync )
+
+sync.wait_for_frames()
+
+color_sensor.stop()
+color_sensor.close()
+depth_sensor.stop()
+depth_sensor.close()
+playback.pause()
+
+print("waited")
+print("at end")
+time.sleep(3)
+
+del playback
+del depth_sensor
+del color_sensor
