3c051b69ac500fd91a24bdecad93d77a5f8fd781
Eran <librealsense.eran@gmail.com>
2021-03-17 17:52:16 +0200

    Added print_results_and_exit

--- unit-tests/func/frames/test_pipeline_first_frame_delay.py
+++ unit-tests/func/frames/test_pipeline_first_frame_delay.py
@@ -17,23 +17,11 @@ import platform
 # Note - Using Windows Media Foundation to handle power management between USB actions take time (~27 ms)
 
 
-def time_to_first_frame(config):
-    pipe = rs.pipeline()
-    start_call_stopwatch = Stopwatch()
-    pipe.start(config)
-    pipe.wait_for_frames()
-    delay = start_call_stopwatch.get_elapsed()
-    pipe.stop()
-    return delay
-
-
-time.sleep(3)  # The device starts at D0 (Operational) state, allow time for it to get into idle state
-
-max_delay_for_depth_frame = 5
-max_delay_for_color_frame = 5
-
 # Set maximum delay for first frame according to product line
 dev = test.find_first_device_or_exit()
+
+# The device starts at D0 (Operational) state, allow time for it to get into idle state
+time.sleep( 3 )
 
 product_line = dev.get_info(rs.camera_info.product_line)
 if product_line == "D400":
@@ -45,6 +33,18 @@ elif product_line == "L500":
 else:
     log.f("This test support only D400 + L515 devices")
 
+
+def time_to_first_frame(config):
+    pipe = rs.pipeline()
+    start_call_stopwatch = Stopwatch()
+    pipe.start(config)
+    pipe.wait_for_frames()
+    delay = start_call_stopwatch.get_elapsed()
+    pipe.stop()
+    return delay
+
+
+################################################################################################
 test.start("Testing pipeline first depth frame delay on " + product_line + " device - " + platform.system() + " OS")
 depth_cfg = rs.config()
 depth_cfg.enable_stream(rs.stream.depth, rs.format.z16, 30)
@@ -53,6 +53,8 @@ print("Delay from pipeline.start() until first depth frame is: {:.3f} [sec] max 
 test.check(frame_delay < max_delay_for_depth_frame)
 test.finish()
 
+
+################################################################################################
 test.start("Testing pipeline first color frame delay on " + product_line + " device - " + platform.system() + " OS")
 color_cfg = rs.config()
 color_cfg.enable_stream(rs.stream.color, rs.format.rgb8, 30)
@@ -60,3 +62,7 @@ frame_delay = time_to_first_frame(color_cfg)
 print("Delay from pipeline.start() until first color frame is: {:.3f} [sec] max allowed is: {:.1f} [sec] ".format(frame_delay, max_delay_for_color_frame))
 test.check(frame_delay < max_delay_for_color_frame)
 test.finish()
+
+
+################################################################################################
+test.print_results_and_exit()
