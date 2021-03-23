728c0d6e0c99574ee40be2d3cff699d7ea4861f2
Andrey Uzbekov <manson.andrew@live.ru>
2021-03-23 17:26:30 +0300

    +manual setting and reading option to/from sensor

--- wrappers/csharp/cs-tutorial-5-load-json/ADP_TestLoadSettingsJson.cs
+++ wrappers/csharp/cs-tutorial-5-load-json/ADP_TestLoadSettingsJson.cs
@@ -3,17 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.IO;
+using System.Text.RegularExpressions;
 
 namespace Intel.RealSense
 {
     class ADP_TestLoadSettingsJson
     {
+
         public bool LoadSettingsJson(Device device)
         {
             var deviceName = device.Info[CameraInfo.Name];
             var usbType = device.Info[CameraInfo.UsbTypeDescriptor];
             string[] deviceNameParts = deviceName.Split(" ".ToCharArray());
             var model = deviceNameParts.Last().Trim();
+
+
+            AdvancedDevice adv=null;
+            SerializableDevice ser = null;
+
             Console.WriteLine($"Device model: '{model}'");
             
             var jsonFileName = $"ADP_{model}_TEST_JSON_USB{usbType}.json";
@@ -29,7 +36,7 @@ namespace Intel.RealSense
             if (model == "L515")
             {
                 Console.WriteLine($"Loading by using of SerialiazableDevice...");
-                var ser = SerializableDevice.FromDevice(device);
+                ser = SerializableDevice.FromDevice(device);
                 try
                 {
                     ser.JsonConfiguration = File.ReadAllText(jsonPath);
@@ -45,7 +52,7 @@ namespace Intel.RealSense
             else
             {
                 Console.WriteLine($"Loading by using of AdvancedDevice...");
-                var adv = AdvancedDevice.FromDevice(device);
+                adv = AdvancedDevice.FromDevice(device);
                 try
                 {
                     adv.JsonConfiguration = File.ReadAllText(jsonPath);
@@ -58,7 +65,73 @@ namespace Intel.RealSense
                     return false;
                 }
             }
+
+            // check for manual options setting
+            var sensors = device.QuerySensors();
+            var depthSensor = sensors[0];
+            var colorSensor = sensors[1];
+            Console.WriteLine($"\nManulally read options:");
+            Console.WriteLine($"--------------------------------------------------");            
+            Console.WriteLine($"DEPTH OPTIONS:");
+            Console.WriteLine($"================");
+            Console.WriteLine($"Total count: {depthSensor.Options.Count()}\n");
+            for (int i = 0; i < depthSensor.Options.Count(); i++)
+            {
+                // there may be some exceptions when trying to get some options, even when going through by index. So using this relatively safe method to iterate all the options, even unacceptible
+                try
+                {
+                    var opt = depthSensor.Options.Skip(i).Take(1).FirstOrDefault();
+                    if (opt != null)
+                    {
+                        Console.WriteLine($"{opt.Key}: {opt.Value}");
+                        
+                    }
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine($"Error getting option №{i}");
+                }                
+            }
+            Console.WriteLine($"\nCOLOR OPTIONS:");
+            Console.WriteLine($"================");
+            Console.WriteLine($"Total count: {colorSensor.Options.Count()}\n");
+            for (int i = 0; i < colorSensor.Options.Count(); i++)
+            {
+                // there may be some exceptions when trying to get some options, even when going through by index. So using this relatively safe method to iterate all the options, even unacceptible
+                try
+                {
+                    var opt = colorSensor.Options.Skip(i).Take(1).FirstOrDefault();
+                    if (opt != null)
+                    {
+                        Console.WriteLine($"{opt.Key}: {opt.Value}");
+                        if (opt.Key.ToString() == "Brightness") // check if we can set value to option. We take brightness for example
+                        {
+                            var newValue = opt.Value + 1;
+                            Console.Write($"\nCheck if we can set option manually: {opt.Key}: from {opt.Value} to {newValue}...");                            
+                            opt.Value = newValue;
+                            
+                            var optionsJson = adv !=  null ? Regex.Replace(adv.JsonConfiguration, @" ", ""): Regex.Replace(ser.JsonConfiguration, @" ", "");
+                            // if we can read brightness from json settings (not all cameras returns all params), use it
+                            if (optionsJson.IndexOf("controls-color-brightness") > 0)
+                            {
+                                var shouldBe = $"\"controls-color-brightness\":\"{newValue}\"";
+                                Console.Write($"new value set: {optionsJson.IndexOf(shouldBe) > 0}\n\n");
+                            }
+                            else // else just reread value from option - it's getter will get it from device
+                            {
+                                Console.Write($"new value set: {opt.Value == newValue}\n\n"); // opt.Value getter will read value from device
+                            }
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Error read option №{i}: {ex.Message}");
+                }
+            }
+
             return true;
         }
     }
+
 }
