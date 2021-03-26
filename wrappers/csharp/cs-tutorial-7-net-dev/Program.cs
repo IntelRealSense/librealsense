e8bf11e5cf841a21f75eaf9beed1cef4bf9a002f
Andrey Uzbekov <manson.andrew@live.ru>
2021-03-26 16:08:21 +0300

    +NetDevice object; +cs-tutorial-7-net-dev

---
+++ wrappers/csharp/cs-tutorial-7-net-dev/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+
+namespace Intel.RealSense
+{
+    class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                Console.WriteLine($"Please, specify network server IP address as a parameter");
+                return;
+            }
+            var w = new CaptureWindow(args[0]);
+            w.ShowDialog();
+        }
+    }
+}
