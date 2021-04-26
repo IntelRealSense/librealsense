80b2234bd8b17cc52e65297ce1a04fa6322f6024
daversintel <dave.tong@intel.com>
2021-04-26 14:06:04 -0700

    Fixed stop problem.

--- common/on-chip-calib.cpp
+++ common/on-chip-calib.cpp
@@ -161,8 +161,7 @@ namespace rs2
                 }
                 std::this_thread::sleep_for(std::chrono::milliseconds(10));
             }
-
-            std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+            _viewer.streams.clear();
         }
         catch (...) {}
     }
