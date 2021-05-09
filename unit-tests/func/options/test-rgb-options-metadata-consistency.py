9c6daa7ff5587dfd8c5a8480ff13ed26485daf25
Remi Bettan <remibettan@gmail.com>
2021-05-09 22:23:50 +0300

    changes as requested within code review comments

--- unit-tests/func/options/test-rgb-options-metadata-consistency.py
+++ unit-tests/func/options/test-rgb-options-metadata-consistency.py
@@ -1,5 +1,5 @@
 # License: Apache 2.0. See LICENSE file in root directory.
-# Copyright(c) 2020 Intel Corporation. All Rights Reserved.
+# Copyright(c) 2021 Intel Corporation. All Rights Reserved.
 
 #test:device L500*
 #test:device D400*
@@ -25,7 +25,7 @@ color_metadata = [rs.frame_metadata_value.backlight_compensation,
                      rs.frame_metadata_value.saturation,
                      rs.frame_metadata_value.sharpness,
                      rs.frame_metadata_value.auto_white_balance_temperature,
-                     rs.frame_metadata_value.white_balance]
+                     rs.frame_metadata_value.manual_white_balance]
 
 def check_option_and_metadata_values(option, metadata, value_to_set, frame):
     changed = color_sensor.get_option(option)
@@ -45,6 +45,9 @@ def check_option_and_metadata_values(option, metadata, value_to_set, frame):
 ctx = rs.context()
 dev = ctx.query_devices()[0]
 color_sensor = dev.first_color_sensor()
+if not color_sensor:
+    rs.log.f("The device found has no color sensor")
+
 
 # Using a profile common to both L500 and D400
 color_profile = next(p for p in color_sensor.profiles if p.fps() == 30
@@ -60,11 +63,14 @@ color_sensor.start(lrs_queue)
 #############################################################################################
 test.start("checking color options")
 # test scenario:
-# for each option, set value, wait 5 frames, check value with get_option and get_frame_metadata
+# for each option, set value, wait 10 frames, check value with get_option and get_frame_metadata
 # values set for each option are min, max, and default values
 iteration = 0
 option_index = -1 #running over options
 value_index = -1  # 0, 1, 2 for min, max, default
+# number of frames to wait between set_option and checking metadata
+# is set as 10 - the expected delay is ~120ms for Win and ~80-90ms for Linux
+num_of_frames_to_wait = 10
 while True:
     try:
         lrs_frame = lrs_queue.wait_for_frame(5000)
@@ -78,15 +84,15 @@ while True:
             option_range = color_sensor.get_option_range(option)
             metadata = color_metadata[option_index]
             value_to_set = option_range.min
-        elif iteration == 6:
+        elif iteration == (num_of_frames_to_wait + 1):
             value_to_set = option_range.max
-        elif iteration == 12:
+        elif iteration == 2 * (num_of_frames_to_wait + 1):
             value_to_set = option_range.default
-        if iteration % 6 == 0: # for iterations 0, 6, 12
+        if iteration % (num_of_frames_to_wait + 1) == 0: # for iterations 0, 11, 22
             color_sensor.set_option(option, value_to_set)
-        if (iteration + 1) % 6 == 0: # for iterations 5, 11, 17
+        if (iteration + 1) % (num_of_frames_to_wait + 1) == 0: # for iterations 10, 21, 32
             check_option_and_metadata_values(option, metadata, value_to_set, lrs_frame)
-        iteration = (iteration + 1 ) % 18
+        iteration = (iteration + 1 ) % (3 * (num_of_frames_to_wait + 1))
 
     except:
         test.unexpected_exception()
