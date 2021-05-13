14f5670707c689ee0df41acbaf735ee3563070ed
doronhi <doron.hirshberg@intel.com>
2021-05-13 14:33:44 +0300

    Add REQUIRE to test-sensor-option

--- unit-tests/utilities/memory/test-sensor-option.cpp
+++ unit-tests/utilities/memory/test-sensor-option.cpp
@@ -6,7 +6,7 @@
 TEST_CASE( "sensor get_option memory leak", "[live]" )
 {
     // running this test with the following command:
-    // `valgrind --leak-check=yes --show-leak-kinds=all --track-origins=yes ./unit-tests/build/utilities/memory/test-sensor-option/test-utilities-memory-sensor-option 2>&1 | grep depends`
+    // `valgrind --leak-check=yes --show-leak-kinds=all --track-origins=yes ./unit-tests/build/utilities/memory/test-sensor-option/test-utilities-memory-sensor-option 2>&1 | grep 'depends\|No device'`
     // should print nothing!
 
     auto devices = find_devices_by_product_line_or_exit(RS2_PRODUCT_LINE_DEPTH);
@@ -17,5 +17,6 @@ TEST_CASE( "sensor get_option memory leak", "[live]" )
     // float option_value(depth_sens.get_option(RS2_OPTION_EXPOSURE));
     float option_value(depth_sens.get_option(RS2_OPTION_ENABLE_AUTO_EXPOSURE));
 
-    std::cout << "Declare: BOOL::" << option_value << std::endl;
+    REQUIRE( (option_value) ); // Using the value of option_value, if memory error exist, causes valgrind to output: "Conditional jump or move depends on uninitialised value(s)"
 }
+
