756b386229d95bdb753a5d9c23b1a10d98a25b63
daversintel <dave.tong@intel.com>
2021-05-21 10:22:06 -0700

    Updated version to 2.45.33.3.

--- include/librealsense2/rs.h
+++ include/librealsense2/rs.h
@@ -25,7 +25,7 @@ extern "C" {
 #define RS2_API_MAJOR_VERSION    2
 #define RS2_API_MINOR_VERSION    45
 #define RS2_API_PATCH_VERSION    33
-#define RS2_API_BUILD_VERSION    2
+#define RS2_API_BUILD_VERSION    3
 
 #ifndef STRINGIFY
 #define STRINGIFY(arg) #arg
