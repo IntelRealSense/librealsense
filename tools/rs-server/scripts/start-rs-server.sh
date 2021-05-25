443c454ca298ff65c9f96b592b75630e1eb191b4
mchanan <miri.chanan@intel.com>
2021-05-25 09:20:59 +0300

    OTG networking fix.

--- tools/rs-server/scripts/start-rs-server.sh
+++ tools/rs-server/scripts/start-rs-server.sh
@@ -12,18 +12,24 @@ do
   if [ x${IFACE_DEFAULT} != x ]; then
     IFACE=${IFACE_DEFAULT}
   else
-    IFACE=`ip link show up | grep BROADCAST | cut -f 2 -d: | head -n 1`
+    IFACE=`ifconfig | grep BROADCAST | grep RUNNING | cut -f 1 -d: | grep -v usb | head -n 1`
   fi
 
-  IFACE_ADDR=`ip addr show dev ${IFACE} | grep "inet " | cut -d\  -f 6 | cut -d/ -f 1`
-  if  [ x${IFACE_ADDR} != x ]; then
-    echo "Binding on ${IFACE} with IP address ${IFACE_ADDR}."
-    /usr/bin/nice -n -20 /usr/bin/rs-server -i ${IFACE_ADDR} 1> /dev/null 2> /dev/null
+  if [ x${IFACE} != x ]; then
+    IFACE_ADDR=`ifconfig ${IFACE} | grep "inet " | cut -d\  -f10`
+    if  [ x${IFACE_ADDR} != x ]; then
+      echo "Binding on ${IFACE} with IP address ${IFACE_ADDR}."
+      /usr/bin/nice -n -20 /usr/bin/rs-server -i ${IFACE_ADDR} 1> /dev/null 2> /dev/null
+    else
+      echo "Waiting for the interface ${IFACE}."
+    fi
   else
-    echo "Waiting for the interface."
+    echo "Waiting for suitable network interface."
   fi
 
   echo "Restarting in 5 sec."
-  sleep 1
+  sleep 5
 done
+
+
 
