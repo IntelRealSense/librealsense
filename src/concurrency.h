7008767db9307fae56956da97cbfc845fc9434bc
Eran <librealsense.eran@gmail.com>
2021-06-02 13:33:30 +0300

    revise concurrency.h

--- src/concurrency.h
+++ src/concurrency.h
@@ -17,123 +17,173 @@ class single_consumer_queue
     std::deque<T> _queue;
     mutable std::mutex _mutex;
     std::condition_variable _deq_cv; // not empty signal
-    std::condition_variable _enq_cv; // not empty signal
+    std::condition_variable _enq_cv; // not full signal
 
-    unsigned int _cap;
+    unsigned int const _cap;
     bool _accepting;
 
-    // flush mechanism is required to abort wait on cv
-    // when need to stop
-    std::atomic<bool> _need_to_flush;
-    std::atomic<bool> _was_flushed;
-    std::function<void(T const &)> _on_drop_callback;
-public:
-    explicit single_consumer_queue<T>(unsigned int cap = QUEUE_MAX_SIZE, std::function<void(T const &)> on_drop_callback = nullptr)
-        : _queue(), _mutex(), _deq_cv(), _enq_cv(), _cap(cap), _accepting(true), _need_to_flush(false), _was_flushed(false), _on_drop_callback(on_drop_callback)
-    {}
+    std::function<void(T const &)> const _on_drop_callback;
 
+public:
+    explicit single_consumer_queue< T >( unsigned int cap = QUEUE_MAX_SIZE,
+                                         std::function< void( T const & ) > on_drop_callback = nullptr )
+        : _queue()
+        , _mutex()
+        , _deq_cv()
+        , _enq_cv()
+        , _cap( cap )
+        , _accepting( true )
+        , _on_drop_callback( on_drop_callback )
+    {
+    }
+
+    // Enqueue an item onto the queue.
+    // If the queue grows beyond capacity, the front will be removed, losing whatever was there!
     void enqueue(T&& item)
     {
         std::unique_lock<std::mutex> lock(_mutex);
-        if (_accepting)
+        if( ! _accepting )
         {
-            _queue.push_back(std::move(item));
-            if (_queue.size() > _cap)
+            if( _on_drop_callback )
+                _on_drop_callback( item );
+            return;
+        }
+
+        _queue.push_back(std::move(item));
+
+        if( _queue.size() > _cap )
+        {
+            if( _on_drop_callback )
+                _on_drop_callback( _queue.front() );
+            _queue.pop_front();
+        }
+
+        lock.unlock();
+
+        // We pushed something -- let others know there's something to dequeue
+        _deq_cv.notify_one();
+    }
+
+
+    // Enqueue an item, but wait for room if there isn't any
+    // Returns true if the enqueue succeeded
+    bool blocking_enqueue(T&& item)
+    {
+        std::unique_lock<std::mutex> lock(_mutex);
+        if( ! _accepting )
+        {
+            if( _on_drop_callback )
+                _on_drop_callback( item );
+            return false;
+        }
+
+        if( _queue.size() >= _cap )
+        {
+            _enq_cv.wait( lock );
+            if( ! _accepting )
             {
-                if (_on_drop_callback)
-                {
-                    _on_drop_callback(_queue.front());
-                }
-                _queue.pop_front();
+                // We shouldn't be adding anything to the queue when we're stopping
+                if( _on_drop_callback )
+                    _on_drop_callback( item );
+                return false;
             }
         }
+
+        _queue.push_back(std::move(item));
         lock.unlock();
+
+        // We pushed something -- let another know there's something to dequeue
         _deq_cv.notify_one();
-    }
 
-    void blocking_enqueue(T&& item)
-    {
-        auto pred = [this]()->bool { return _queue.size() < _cap || _need_to_flush; };
-
-        std::unique_lock<std::mutex> lock(_mutex);
-        if (_accepting)
-        {
-            _enq_cv.wait(lock, pred);
-            _queue.push_back(std::move(item));
-        }
-        lock.unlock();
-        _deq_cv.notify_one();
-    }
-
-
-    bool dequeue(T* item ,unsigned int timeout_ms)
-    {
-        std::unique_lock<std::mutex> lock(_mutex);
-        _accepting = true;
-        _was_flushed = false;
-        const auto ready = [this]() { return (_queue.size() > 0) || _need_to_flush; };
-        if (!ready() && !_deq_cv.wait_for(lock, std::chrono::milliseconds(timeout_ms), ready))
-        {
-            return false;
-        }
-
-        if (_queue.size() <= 0)
-        {
-            return false;
-        }
-        *item = std::move(_queue.front());
-        _queue.pop_front();
-        _enq_cv.notify_one();
         return true;
     }
 
+
+    // Remove one item; if unavailable, wait for it
+    // Return true if an item was removed -- otherwise, false
+    bool dequeue( T * item, unsigned int timeout_ms )
+    {
+        std::unique_lock<std::mutex> lock(_mutex);
+
+        if( _queue.empty() )
+        {
+            _deq_cv.wait_for( lock, std::chrono::milliseconds( timeout_ms ) );
+            if( _queue.empty() )
+                return false;
+        }
+
+        *item = std::move(_queue.front());
+        _queue.pop_front();
+
+        // We've made room -- let whoever is waiting for room know about it
+        _enq_cv.notify_one();
+
+        return true;
+    }
+
+    // Remove one item if available; do not wait for one
+    // Return true if an item was removed -- otherwise, false
     bool try_dequeue(T* item)
     {
         std::lock_guard< std::mutex > lock( _mutex );
-        _accepting = true;
-        if (_queue.size() > 0)
-        {
-            auto val = std::move(_queue.front());
-            _queue.pop_front();
-            *item = std::move(val);
-            _enq_cv.notify_one();
-            return true;
-        }
-        return false;
+        if( _queue.empty() )
+            return false;
+
+        *item = std::move(_queue.front());
+        _queue.pop_front();
+
+        // We've made room -- let whoever is waiting for room know about it
+        _enq_cv.notify_one();
+
+        return true;
     }
 
     bool peek(T** item)
     {
         std::lock_guard< std::mutex > lock( _mutex );
 
-        if (_queue.size() <= 0)
-        {
+        if (_queue.empty())
             return false;
-        }
+
         *item = &_queue.front();
         return true;
+    }
+
+    void stop()
+    {
+        std::lock_guard< std::mutex > lock( _mutex );
+
+        // We no longer accept any more items!
+        _accepting = false;
+
+        _clear();
     }
 
     void clear()
     {
         std::lock_guard< std::mutex > lock( _mutex );
+        _clear();
+    }
 
-        _accepting = false;
-        _need_to_flush = true;
-
-        _enq_cv.notify_all();
-        while (_queue.size() > 0)
+protected:
+	void _clear()
+	{
+        // Empty out the queue (TODO: why no just _queue.clear()?)
+        while( _queue.size() > 0 )
         {
-            auto item = std::move(_queue.front());
+            auto item = std::move( _queue.front() );
             _queue.pop_front();
         }
+
+        // Wake up anyone who is waiting for room to enqueue, or waiting for something to dequeue -- there's nothing now
+        _enq_cv.notify_all();
         _deq_cv.notify_all();
     }
 
+public:
     void start()
     {
         std::lock_guard< std::mutex > lock( _mutex );
-        _need_to_flush = false;
         _accepting = true;
     }
 
@@ -180,6 +230,11 @@ public:
     void clear()
     {
         _queue.clear();
+    }
+
+    void stop()
+    {
+        _queue.stop();
     }
 
     void start()
@@ -309,7 +364,7 @@ public:
             _was_stopped_cv.notify_all();
         }
 
-        _queue.clear();
+        _queue.stop();
 
         {
             std::unique_lock<std::mutex> lock(_was_flushed_mutex);
@@ -325,11 +380,10 @@ public:
     ~dispatcher()
     {
         stop();
-        _queue.clear();
         _is_alive = false;
 
-        if (_thread.joinable())
-        _thread.join();
+        if( _thread.joinable() )
+            _thread.join();
     }
 
     bool flush()
