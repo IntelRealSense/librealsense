da0be4a7c67bf14a642d2d5c1f4d6495b947b093
Tomer Ashuach <tomer.ashuach@gmail.com>
2021-06-06 12:18:21 +0300

    implemented PR comments

--- unit-tests/unit-tests-rsutil.cpp
+++ unit-tests/unit-tests-rsutil.cpp
@@ -5,22 +5,27 @@
 #include <librealsense2/rsutil.h>
 
 // C++ test to check that rsutil function 
-TEST_CASE( "rsutil", "[string]" )
+TEST_CASE( "rsutil-api", "[live][rsutil-api]" )
 {
-    float pixel[2] = { 0.5 };
-    float point[3] = { 0.5 };
-    rs2_intrinsics intrinsic = { 1, 1, 0.5, 0.5, 0.5 , 0.5, RS2_DISTORTION_NONE, { 0.0, 0.0, 0.0, 0.0, 0.0 } };
-    float rotation[9] = { 0.0 };
-    float translation[3] = { 0.0 };
-    rs2_extrinsics extrinsic = { *rotation ,*translation };
+    rs2::context ctx;
 
-    rs2_project_point_to_pixel(pixel, &intrinsic, point);
+    if (make_context(SECTION_FROM_TEST_NAME, &ctx))
+    {
+        float pixel[2] = { 0.5 };
+        float point[3] = { 0.5 };
+        rs2_intrinsics intrinsic = { 1, 1, 0.5, 0.5, 0.5 , 0.5, RS2_DISTORTION_NONE, { 0.0, 0.0, 0.0, 0.0, 0.0 } };
+        float rotation[9] = { 0.0 };
+        float translation[3] = { 0.0 };
+        rs2_extrinsics extrinsic = { *rotation ,*translation };
 
-    rs2_deproject_pixel_to_point(point, &intrinsic, pixel, 0.0);
+        CHECK_NOTHROW(rs2_project_point_to_pixel(pixel, &intrinsic, point));
 
-    rs2_transform_point_to_point(point, &extrinsic, point);
+        CHECK_NOTHROW(rs2_deproject_pixel_to_point(point, &intrinsic, pixel, 0.0));
 
-    rs2_fov(&intrinsic, pixel);
+        CHECK_NOTHROW(rs2_transform_point_to_point(point, &extrinsic, point));
 
-    rs2_project_color_pixel_to_depth_pixel(pixel, 0, 0.5, 0.0, 1.0, &intrinsic, &intrinsic, &extrinsic, &extrinsic, pixel);
+        CHECK_NOTHROW(rs2_fov(&intrinsic, pixel));
+
+        CHECK_NOTHROW(rs2_project_color_pixel_to_depth_pixel(pixel, 0, 0.5, 0.0, 1.0, &intrinsic, &intrinsic, &extrinsic, &extrinsic, pixel));
+    }
 }
