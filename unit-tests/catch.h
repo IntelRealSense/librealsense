1dc75763f77edef979a139dce17ff96a75819f47
Eran <librealsense.eran@gmail.com>
2021-06-07 14:35:42 +0300

    Updated error for CATCH_CONFIG_MAIN

--- unit-tests/catch.h
+++ unit-tests/catch.h
@@ -9,9 +9,9 @@
 // Otherwise, don't let it define its own:
 #define ELPP_NO_CHECK_MACROS
 
-// We always set our own main, CATCH_CONFIG_MAIN will cause errors in build due to having our main and catch2 main
+// We set our own main by default, so CATCH_CONFIG_MAIN will cause linker errors!
 #if defined(CATCH_CONFIG_MAIN)
-#error CATCH_CONFIG_MAIN defined
+#error CATCH_CONFIG_MAIN must not be defined: to run your own main(), use CATCH_CONFIG_RUNNER with #cmake:custom-main
 #endif
 
 #include "catch/catch.hpp"
