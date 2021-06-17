8b88077a29dd38019f47a1b42329f68888bc3579
Alexey Puzhevich <alexey.puzhevich@intel.com>
2021-06-17 09:52:02 +0300

    pr_check

--- tools/rs-server/scripts/start-rs-server.sh
+++ tools/rs-server/scripts/start-rs-server.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+# License: Apache 2.0. See LICENSE file in root directory.
+# Copyright(c) 2020 Intel Corporation. All Rights Reserved.
+
 IFACE_DEFAULT=""
 
 echo "Delaying start for 5 sec."
