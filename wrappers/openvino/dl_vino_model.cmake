f36eba1017fc6c2dd41187e59936cf7a7380deac
gwen2018 <guoping.wen@intel.com>
2021-06-18 12:05:10 -0700

    update newer openvino model files and download path

--- wrappers/openvino/dl_vino_model.cmake
+++ wrappers/openvino/dl_vino_model.cmake
@@ -1,7 +1,12 @@
 
 # OpenVINO model files are not included in our distribution. Define a function for easy
 # downloading at CMake time:
+if(OPENVINO2019)
 set(OPENVINO_MODEL_SRC_URL "https://librealsense.intel.com/rs-tests/OpenVINO_data")
+else()
+set(OPENVINO_MODEL_SRC_URL "https://librealsense.intel.com/rs-tests/OpenVINO_data/2021.3.394")
+endif()
+
 function(dl_vino_model filename sha1)
     set(path "${CMAKE_CURRENT_BINARY_DIR}/${filename}")
     if(NOT EXISTS "${path}")
