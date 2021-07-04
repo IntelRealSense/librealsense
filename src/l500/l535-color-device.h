9557833bd541ac4fe1a301835ce2f8595a7723dc
Eran <librealsense.eran@gmail.com>
2021-07-04 12:35:11 +0300

    Update l535-color-device.h

--- src/l500/l535-color-device.h
+++ src/l500/l535-color-device.h
@@ -8,11 +8,15 @@
 namespace librealsense {
 namespace ivcam2 {
 namespace l535 {
+
+
 class color_device : public l500_color
 {
 public:
     color_device( std::shared_ptr< context > ctx, const platform::backend_device_group & group );
 };
-};  // namespace l535
-};  // namespace ivcam2
-};  // namespace librealsense
+
+
+}  // namespace l535
+}  // namespace ivcam2
+}  // namespace librealsense
