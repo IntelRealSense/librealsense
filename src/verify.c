619a85f901bbbe9c10827f99e6be3cc876070c14
removedporn <86824510+removedporn@users.noreply.github.com>
2021-07-15 12:44:46 +0800

    Delete src directory

--- src/verify.c
+++
@@ -1,6 +0,0 @@
-/* License: Apache 2.0. See LICENSE file in root directory.
-   Copyright(c) 2015 Intel Corporation. All Rights Reserved. */
-
-/* This file simply verifies that rs.h and rs2_advanced_mode.h can be correctly included by a C compiler */
-#include "../include/librealsense2/rs.h"
-#include "../include/librealsense2/rs_advanced_mode.h"
