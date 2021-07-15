c962fa4ea56d566399983d4a3d94dbdb52fed4a2
Eran <librealsense.eran@gmail.com>
2021-07-15 09:29:31 +0300

    Update y4112rgb-gl.cpp

--- src/gl/y4112rgb-gl.cpp
+++ src/gl/y4112rgb-gl.cpp
@@ -1,9 +1,9 @@
 // License: Apache 2.0. See LICENSE file in root directory.
-// Copyright(c) 2017 Intel Corporation. All Rights Reserved.
+// Copyright(c) 2021 Intel Corporation. All Rights Reserved.
 
-#include <../include/librealsense2/hpp/rs_sensor.hpp>
-#include <../include/librealsense2/hpp/rs_processing.hpp>
-#include <../include/librealsense2-gl/rs_processing_gl.hpp>
+#include <librealsense2/hpp/rs_sensor.hpp>
+#include <librealsense2/hpp/rs_processing.hpp>
+#include <librealsense2-gl/rs_processing_gl.hpp>
 
 #include "../proc/synthetic-stream.h"
 #include "y4112rgb-gl.h"
