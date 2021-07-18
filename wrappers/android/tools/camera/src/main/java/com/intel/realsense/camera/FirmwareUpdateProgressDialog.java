c9d5e90a1d80b08ee86d24ec4dfad7b0a1fe8086
Eran <librealsense.eran@gmail.com>
2021-07-18 09:04:25 +0300

    Android: L5xx -> L51x & L53x

--- wrappers/android/tools/camera/src/main/java/com/intel/realsense/camera/FirmwareUpdateProgressDialog.java
+++ wrappers/android/tools/camera/src/main/java/com/intel/realsense/camera/FirmwareUpdateProgressDialog.java
@@ -61,7 +61,11 @@ public class FirmwareUpdateProgressDialog extends DialogFragment {
             switch (pl){
                 case D400: return R.raw.fw_d4xx;
                 case SR300: return R.raw.fw_sr3xx;
-                case L500: return  R.raw.fw_l5xx;
+                case L500:
+                    if( device.getInfo(CameraInfo.PRODUCT_ID) == "0B68" )
+                        return R.raw.fw_l53x;
+                    else
+                        return  R.raw.fw_l51x;
             }
         }
         throw new RuntimeException("FW update is not supported for the connected device");
