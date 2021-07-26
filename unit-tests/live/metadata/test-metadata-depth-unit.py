e2e8ac896a3b10e38ac5e956e8dbdaf718b31c27
Nir Azkiel <nir.azkiel@intel.com>
2021-07-26 14:47:52 +0300

    fix depth units on SR300 devices

--- unit-tests/live/metadata/test-metadata-depth-unit.py
+++ unit-tests/live/metadata/test-metadata-depth-unit.py
@@ -9,6 +9,7 @@ import pyrealsense2 as rs
 from rspy import test
 
 #############################################################################################
+# get metadata depth units value and make sure it's non zero and equal to the depth sensor matching option value
 test.start("checking depth units on metadata")
 
 dev = test.find_first_device_or_exit()
@@ -18,12 +19,19 @@ try:
     cfg = pipeline = None
     pipeline = rs.pipeline()
     cfg = rs.config()
-    pipeline.start(cfg)
+    pipeline_profile = pipeline.start(cfg)
 
+    # Check that depth units on meta data is non zero
     frame_set = pipeline.wait_for_frames()
     depth_frame = frame_set.get_depth_frame()
-    test.check(depth_frame != None, abort_if_failed=True)
-    test.check(depth_frame.get_units() != 0)
+    depth_units_from_metadata = depth_frame.get_units()
+    test.check(depth_units_from_metadata > 0)
+
+    # Check metadata depth unit value match option value
+    dev = pipeline_profile.get_device()
+    ds = dev.first_depth_sensor()
+    test.check(ds.supports(rs.option.depth_units))
+    test.check_equal(ds.get_option(rs.option.depth_units), depth_units_from_metadata)
 
     pipeline.stop()
 
