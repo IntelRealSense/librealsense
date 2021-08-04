8efea0d767c608766a749972f0b3e4af5047b18e
Nir Azkiel <nir.azkiel@intel.com>
2021-08-04 14:52:21 +0300

    remove support for L535 FW update to allow recovery update

--- common/fw-update-helper.cpp
+++ common/fw-update-helper.cpp
@@ -45,10 +45,10 @@ namespace rs2
         RS2_FWU_STATE_FAILED = 3,
     };
 
-    bool is_recommended_fw_available(const std::string& product_line, const std::string& PID)
+    bool is_recommended_fw_available(const std::string& product_line)
     {
         auto pl = parse_product_line(product_line);
-        auto fv = get_available_firmware_version(pl, PID);
+        auto fv = get_available_firmware_version(pl);
         return !(fv == "");
     }
 
@@ -60,27 +60,27 @@ namespace rs2
         else return -1;
     }
 
-    std::string get_available_firmware_version(int product_line, const std::string& PID)
+    std::string get_available_firmware_version(int product_line)
     {
         if (product_line == RS2_PRODUCT_LINE_D400) return FW_D4XX_FW_IMAGE_VERSION;
         //else if (product_line == RS2_PRODUCT_LINE_SR300) return FW_SR3XX_FW_IMAGE_VERSION;
-        else if (product_line == RS2_PRODUCT_LINE_L500 && PID == "0B64") return FW_L51X_FW_IMAGE_VERSION;
-        else if (product_line == RS2_PRODUCT_LINE_L500 && PID == "0B68") return FW_L53X_FW_IMAGE_VERSION;
+        //else if (product_line == RS2_PRODUCT_LINE_L500 && PID == "0B68") return FW_L53X_FW_IMAGE_VERSION;
+        else if (product_line == RS2_PRODUCT_LINE_L500 ) return FW_L51X_FW_IMAGE_VERSION;
         else return "";
     }
 
-    std::map<std::pair<int, std::string>, std::vector<uint8_t>> create_default_fw_table()
+    std::map<int, std::vector<uint8_t>> create_default_fw_table()
     {
         bool allow_rc_firmware = config_file::instance().get_or_default(configurations::update::allow_rc_firmware, false);
 
-        std::map<std::pair<int, std::string>, std::vector<uint8_t>> rv;
+        std::map<int, std::vector<uint8_t>> rv;
 
         if (strlen(FW_D4XX_FW_IMAGE_VERSION) && !allow_rc_firmware)
         {
             int size = 0;
             auto hex = fw_get_D4XX_FW_Image(size);
             auto vec = std::vector<uint8_t>(hex, hex + size);
-            rv[{RS2_PRODUCT_LINE_D400, ""}] = vec;
+            rv[{RS2_PRODUCT_LINE_D400}] = vec;
         }
 
         if (strlen(FW_SR3XX_FW_IMAGE_VERSION))
@@ -88,7 +88,7 @@ namespace rs2
             int size = 0;
             auto hex = fw_get_SR3XX_FW_Image(size);
             auto vec = std::vector<uint8_t>(hex, hex + size);
-            rv[{RS2_PRODUCT_LINE_SR300, ""}] = vec;
+            rv[{RS2_PRODUCT_LINE_SR300}] = vec;
         }
 
         if (strlen(FW_L51X_FW_IMAGE_VERSION))
@@ -96,15 +96,15 @@ namespace rs2
             int size = 0;
             auto hex = fw_get_L51X_FW_Image(size);
             auto vec = std::vector<uint8_t>(hex, hex + size);
-            rv[{RS2_PRODUCT_LINE_L500, "0B64"}] = vec;
+            rv[{RS2_PRODUCT_LINE_L500}] = vec;
         }
-        if (strlen(FW_L53X_FW_IMAGE_VERSION))
-        {
-            int size = 0;
-            auto hex = fw_get_L53X_FW_Image(size);
-            auto vec = std::vector<uint8_t>(hex, hex + size);
-            rv[{RS2_PRODUCT_LINE_L500, "0B68"}] = vec;
-        }
+        //if (strlen(FW_L53X_FW_IMAGE_VERSION))
+        //{
+        //    int size = 0;
+        //    auto hex = fw_get_L53X_FW_Image(size);
+        //    auto vec = std::vector<uint8_t>(hex, hex + size);
+        //    rv[{RS2_PRODUCT_LINE_L500, "0B68"}] = vec;
+        //}
 
         return rv;
     }
--- common/fw-update-helper.h
+++ common/fw-update-helper.h
@@ -11,14 +11,12 @@ namespace rs2
     class viewer_model;
 
     int parse_product_line(const std::string& id);
-    std::string get_available_firmware_version(int product_line, const std::string& PID);
+    std::string get_available_firmware_version(int product_line);
 
-    // product line + PID to FW image data
-    // if PID doesn't make any difference (ds5, sr300) its will be ""
-    std::map<std::pair<int, std::string>, std::vector<uint8_t>> create_default_fw_table();
+    std::map<int, std::vector<uint8_t>> create_default_fw_table();
     std::vector<int> parse_fw_version(const std::string& fw);
     bool is_upgradeable(const std::string& curr, const std::string& available);
-    bool is_recommended_fw_available(const std::string& product_line, const std::string& PID);
+    bool is_recommended_fw_available(const std::string& product_line);
 
     class firmware_update_manager : public process_manager
     {
