b9ef75831fc99454e53db013db7cc279a6320f25
Eran <librealsense.eran@gmail.com>
2021-08-04 11:17:59 +0300

    move test into string utilities; add some tests; fix bug in version

--- unit-tests/utilities/version/common.h
+++
@@ -1,7 +0,0 @@
-// License: Apache 2.0. See LICENSE file in root directory.
-// Copyright(c) 2021 Intel Corporation. All Rights Reserved.
-
-#include <unit-tests/test.h>
-
-#include <easylogging++.h>
-
