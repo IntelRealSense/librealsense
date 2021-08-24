543fc905b1a548713ec7e66c2ebc97c470621dda
nohayassin <noha.yassin@gmail.com>
2021-08-24 11:55:16 +0300

    removed D455 from 2 libci tests

--- unit-tests/live/memory/test-extrinsics.cpp
+++ unit-tests/live/memory/test-extrinsics.cpp
@@ -2,7 +2,8 @@
 // Copyright(c) 2021 Intel Corporation. All Rights Reserved.
 
 //#cmake: static!
-//#test:device D400*
+//#test:device D465
+//#test:device D435
 //#test:donotrun:!nightly
 //#test:timeout 480
 
