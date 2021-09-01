c1e489b1d17858786dfa18bb86b4103becec8ce2
Eran <librealsense.eran@gmail.com>
2021-09-01 09:13:20 +0300

    misc minor changes

--- src/proc/synthetic-stream.cpp
+++ src/proc/synthetic-stream.cpp
@@ -45,11 +45,11 @@ namespace librealsense
         }
         catch (std::exception const & e)
         {
-            LOG_ERROR("Exception was thrown during user processing callback: " + std::string(e.what()));
+            LOG_ERROR( "Exception was thrown during callback: " << e.what() );
         }
         catch (...)
         {
-            LOG_ERROR("Exception was thrown during user processing callback!");
+            LOG_ERROR( "Exception was thrown during callback!" );
         }
     }
 
