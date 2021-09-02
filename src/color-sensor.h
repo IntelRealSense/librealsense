20586ca0dbf7d210e04c4a726861ff46e53ac10c
Nir Azkiel <nir.azkiel@intel.com>
2021-09-02 09:08:53 +0300

    CR updates

--- src/color-sensor.h
+++ src/color-sensor.h
@@ -5,35 +5,36 @@
 
 #include "core/extension.h"
 
-namespace librealsense
+namespace librealsense {
+class color_sensor : public recordable< color_sensor >
 {
-    class color_sensor : public recordable<color_sensor>
+public:
+    virtual ~color_sensor() = default;
+
+    void create_snapshot( std::shared_ptr< color_sensor > & snapshot ) const override;
+    void
+    enable_recording( std::function< void( const color_sensor & ) > recording_function ) override{};
+};
+
+MAP_EXTENSION( RS2_EXTENSION_COLOR_SENSOR, librealsense::color_sensor );
+
+class color_sensor_snapshot
+    : public virtual color_sensor
+    , public extension_snapshot
+{
+public:
+    color_sensor_snapshot() {}
+
+    void update( std::shared_ptr< extension_snapshot > ext ) override {}
+
+    void create_snapshot( std::shared_ptr< color_sensor > & snapshot ) const override
     {
-    public:
-        virtual ~color_sensor() = default;
-
-        void create_snapshot(std::shared_ptr<color_sensor>& snapshot) const override;
-        void enable_recording(std::function<void(const color_sensor&)> recording_function) override {};
-    };
-
-    MAP_EXTENSION(RS2_EXTENSION_COLOR_SENSOR, librealsense::color_sensor);
-
-    class color_sensor_snapshot : public virtual color_sensor, public extension_snapshot
+        snapshot = std::make_shared< color_sensor_snapshot >( *this );
+    }
+    void
+    enable_recording( std::function< void( const color_sensor & ) > recording_function ) override
     {
-    public:
-        color_sensor_snapshot() {}
-
-        void update(std::shared_ptr<extension_snapshot> ext) override
-        {
-        }
-
-        void create_snapshot(std::shared_ptr<color_sensor>& snapshot) const  override
-        {
-            snapshot = std::make_shared<color_sensor_snapshot>(*this);
-        }
-        void enable_recording(std::function<void(const color_sensor&)> recording_function) override
-        {
-            //empty
-        }
-    };
-}
+        // empty
+    }
+};
+}  // namespace librealsense
