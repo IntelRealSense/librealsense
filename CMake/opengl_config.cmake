43890b180deac821636f4eeeba7854955c156d29
Yu Meng <yu1.meng@intel.com>
2021-10-27 12:37:30 +0800

    disable warnings about OpenGL API on macOS OpenGL API is deprecated from macOS 10.14

--- CMake/opengl_config.cmake
+++ CMake/opengl_config.cmake
@@ -1,2 +1,6 @@
 find_package(OpenGL REQUIRED)
 set(DEPENDENCIES realsense2 glfw ${OPENGL_LIBRARIES})
+
+if (${CMAKE_SYSTEM_NAME} MATCHES "Darwin")
+    add_definitions(-DGL_SILENCE_DEPRECATION)
+endif()
