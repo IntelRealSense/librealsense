ecb2246f3f6132b7d68aa2d9265ef1872def0343
Nir Azkiel <nir.azkiel@intel.com>
2022-02-17 10:44:46 +0200

    handle_subdirectory variables set

--- CMake/external_fastdds.cmake
+++ CMake/external_fastdds.cmake
@@ -1,6 +1,6 @@
 cmake_minimum_required(VERSION 3.15) 
 include(FetchContent)
-set(FETCHCONTENT_BASE_DIR ${CMAKE_BINARY_DIR}/fastdds/fastdds_install)
+
 # For debugging purpose
 #set(FETCHCONTENT_QUIET OFF)
 
@@ -16,22 +16,35 @@ FetchContent_Declare(
   SOURCE_DIR ${CMAKE_BINARY_DIR}/third-party/fastdds
 )
 
-# Set internal variables for FastDDS
-set(THIRDPARTY_Asio FORCE)
-set(THIRDPARTY_fastcdr FORCE)
-set(THIRDPARTY_TinyXML2 FORCE)
-set(BUILD_SHARED_LIBS OFF)
-set(COMPILE_TOOLS OFF)
-set(BUILD_TESTING OFF)
-set(SQLITE3_SUPPORT OFF) 
+# Set FastDDS internal variables for FastDDS
+# We use cached variables so the default parameter inside the sub directory will not override the required values
+# We add "FORCE" so that is a previous cached value is set our assignment will override it.
+set(THIRDPARTY_Asio FORCE CACHE INTERNAL "" FORCE)
+set(THIRDPARTY_fastcdr FORCE CACHE INTERNAL "" FORCE)
+set(THIRDPARTY_TinyXML2 FORCE CACHE INTERNAL "" FORCE)
+set(COMPILE_TOOLS OFF CACHE INTERNAL "" FORCE)
+set(BUILD_TESTING OFF CACHE INTERNAL "" FORCE)
+set(SQLITE3_SUPPORT OFF CACHE INTERNAL "" FORCE)
 
+
+# save previous common variables - TBD consider adding a generic mechanism for it
+# We need special values for the subdirectory but want to return to the parent values after the subdirectory is configured
+# (FetchContent_MakeAvailable calls add_subdirectory internally)
+set(CACHED_BUILD_SHARED_LIBS ${BUILD_SHARED_LIBS})
+set(CACHED_CMAKE_INSTALL_PREFIX ${CMAKE_INSTALL_PREFIX}) 
+set(CACHED_CMAKE_PREFIX_PATH ${CMAKE_PREFIX_PATH})  
+
+# Set new values for subdirecory
+set(BUILD_SHARED_LIBS OFF)
 set(CMAKE_INSTALL_PREFIX ${CMAKE_BINARY_DIR}/fastdds/fastdds_install) 
 set(CMAKE_PREFIX_PATH ${CMAKE_BINARY_DIR}/fastdds/fastdds_install)  
 
-
-
 FetchContent_MakeAvailable(fastdds)
 
+# reset common variables to previous values
+set(BUILD_SHARED_LIBS ${CACHED_BUILD_SHARED_LIBS})
+set(CMAKE_INSTALL_PREFIX ${CACHED_CMAKE_INSTALL_PREFIX}) 
+set(CMAKE_PREFIX_PATH ${CACHED_CMAKE_PREFIX_PATH})  
 
 list(POP_BACK CMAKE_MESSAGE_INDENT) # Unindent outputs
 message(CHECK_PASS "fastdds fetched")
