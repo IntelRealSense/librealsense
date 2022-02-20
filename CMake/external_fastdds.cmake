8b40328a04831c9bb09ec38674f9cbcb181263ac
Nir Azkiel <nir.azkiel@intel.com>
2022-02-20 17:19:31 +0200

    use scopped variables for building FastDDS

--- CMake/external_fastdds.cmake
+++ CMake/external_fastdds.cmake
@@ -1,85 +1,79 @@
 cmake_minimum_required(VERSION 3.5) 
 include(FetchContent)
 
-mark_as_advanced(FETCHCONTENT_QUIET)
-mark_as_advanced(FETCHCONTENT_BASE_DIR)
-mark_as_advanced(FETCHCONTENT_FULLY_DISCONNECTED)
-mark_as_advanced(FETCHCONTENT_UPDATES_DISCONNECTED)
+# We use a function to enforce a scoped variables creation only for FastDDS build (i.e turn off BUILD_SHARED_LIBS)
+function(get_fastdds)
 
-# For debugging purpose
-#set(FETCHCONTENT_QUIET OFF)
+    mark_as_advanced(FETCHCONTENT_QUIET)
+    mark_as_advanced(FETCHCONTENT_BASE_DIR)
+    mark_as_advanced(FETCHCONTENT_FULLY_DISCONNECTED)
+    mark_as_advanced(FETCHCONTENT_UPDATES_DISCONNECTED)
 
-message(CHECK_START  "Fetching fastdds...")
-list(APPEND CMAKE_MESSAGE_INDENT "  ")  # Indent outputs
+    # For debugging purpose
+    #set(FETCHCONTENT_QUIET OFF)
 
-FetchContent_Declare(
-  fastdds
-  GIT_REPOSITORY https://github.com/eProsima/Fast-DDS.git
-  GIT_TAG        ecb9711cf2b9bcc608de7d45fc36d3a653d3bf05 # Git tag "v2.5.0"
-  GIT_SUBMODULES ""
-  GIT_SHALLOW ON
-  SOURCE_DIR ${CMAKE_BINARY_DIR}/third-party/fastdds
-)
+    message(CHECK_START  "Fetching fastdds...")
+    list(APPEND CMAKE_MESSAGE_INDENT "  ")  # Indent outputs
 
-# Set FastDDS internal variables for FastDDS
-# We use cached variables so the default parameter inside the sub directory will not override the required values
-# We add "FORCE" so that is a previous cached value is set our assignment will override it.
-set(THIRDPARTY_Asio FORCE CACHE INTERNAL "" FORCE)
-set(THIRDPARTY_fastcdr FORCE CACHE INTERNAL "" FORCE)
-set(THIRDPARTY_TinyXML2 FORCE CACHE INTERNAL "" FORCE)
-set(COMPILE_TOOLS OFF CACHE INTERNAL "" FORCE)
-set(BUILD_TESTING OFF CACHE INTERNAL "" FORCE)
-set(SQLITE3_SUPPORT OFF CACHE INTERNAL "" FORCE)
+    FetchContent_Declare(
+      fastdds
+      GIT_REPOSITORY https://github.com/eProsima/Fast-DDS.git
+      GIT_TAG        ecb9711cf2b9bcc608de7d45fc36d3a653d3bf05 # Git tag "v2.5.0"
+      GIT_SUBMODULES ""
+      GIT_SHALLOW ON
+      SOURCE_DIR ${CMAKE_BINARY_DIR}/third-party/fastdds
+    )
 
+    # Set FastDDS internal variables
+    # We use cached variables so the default parameter inside the sub directory will not override the required values
+    # We add "FORCE" so that is a previous cached value is set our assignment will override it.
+    set(THIRDPARTY_Asio FORCE CACHE INTERNAL "" FORCE)
+    set(THIRDPARTY_fastcdr FORCE CACHE INTERNAL "" FORCE)
+    set(THIRDPARTY_TinyXML2 FORCE CACHE INTERNAL "" FORCE)
+    set(COMPILE_TOOLS OFF CACHE INTERNAL "" FORCE)
+    set(BUILD_TESTING OFF CACHE INTERNAL "" FORCE)
+    set(SQLITE3_SUPPORT OFF CACHE INTERNAL "" FORCE)
 
-# save previous common variables - TBD consider adding a generic mechanism for it
-# We need special values for the subdirectory but want to return to the parent values after the subdirectory is configured
-# (FetchContent_MakeAvailable calls add_subdirectory internally)
-set(CACHED_BUILD_SHARED_LIBS ${BUILD_SHARED_LIBS})
-set(CACHED_CMAKE_INSTALL_PREFIX ${CMAKE_INSTALL_PREFIX}) 
-set(CACHED_CMAKE_PREFIX_PATH ${CMAKE_PREFIX_PATH})  
+    # Set new values for subdirecory
+    set(BUILD_SHARED_LIBS OFF)
+    set(CMAKE_INSTALL_PREFIX ${CMAKE_BINARY_DIR}/fastdds/fastdds_install) 
+    set(CMAKE_PREFIX_PATH ${CMAKE_BINARY_DIR}/fastdds/fastdds_install)  
 
-# Set new values for subdirecory
-set(BUILD_SHARED_LIBS OFF)
-set(CMAKE_INSTALL_PREFIX ${CMAKE_BINARY_DIR}/fastdds/fastdds_install) 
-set(CMAKE_PREFIX_PATH ${CMAKE_BINARY_DIR}/fastdds/fastdds_install)  
+    # FastDDS does not support UNICODE see https://github.com/eProsima/Fast-DDS/issues/2501
+    # Should be removed when fetching FastDDS new release that will contain PR https://github.com/eProsima/Fast-DDS/pull/2510
+    if (MSVC)
+        remove_definitions(-D_UNICODE -DUNICODE)
+    endif()
 
+    # Get fastdds
+    FetchContent_MakeAvailable(fastdds)
 
-# FastDDS does not support UNICODE see https://github.com/eProsima/Fast-DDS/issues/2501
-# Should be removed when fetching FastDDS new release that will contain PR https://github.com/eProsima/Fast-DDS/pull/2510
-if (MSVC)
-    remove_definitions(-D_UNICODE -DUNICODE)
-endif()
+    # Move new options from FetchContent to advanced section
+    mark_as_advanced(FETCHCONTENT_SOURCE_DIR_FASTDDS)
+    mark_as_advanced(FETCHCONTENT_UPDATES_DISCONNECTED_FASTDDS)
 
-# Get fastdds
-FetchContent_MakeAvailable(fastdds)
+    # place FastDDS project with other 3rd-party projects
+    set_target_properties(fastcdr fastrtps PROPERTIES
+                          FOLDER "ExternalProjectTargets/fastdds")
 
-# Move new options from FetchContent to advanced section
-mark_as_advanced(FETCHCONTENT_SOURCE_DIR_FASTDDS)
-mark_as_advanced(FETCHCONTENT_UPDATES_DISCONNECTED_FASTDDS)
+    # Add back the UNICODE definitions (Should be removed with the above remove_definitions once conditions mentioned on it are met)
+    if (MSVC)
+        # Restore UNICODE
+        add_definitions(-D_UNICODE -DUNICODE)
+    endif()
 
-# place FastDDS project with other 3rd-party projects
-set_target_properties(fastcdr fastrtps PROPERTIES
-                      FOLDER "ExternalProjectTargets/fastdds")
+    list(POP_BACK CMAKE_MESSAGE_INDENT) # Unindent outputs
+    message(CHECK_PASS "fastdds fetched")
 
-# Add back the UNICODE definitions (Should be removed with the above remove_definitions once conditions mentioned on it are met)
-if (MSVC)
-    # Restore UNICODE
-    add_definitions(-D_UNICODE -DUNICODE)
-endif()
+    add_library(dds INTERFACE)
+    target_link_libraries(dds INTERFACE  fastcdr fastrtps)
 
-# reset common variables to previous values
-set(BUILD_SHARED_LIBS ${CACHED_BUILD_SHARED_LIBS})
-set(CMAKE_INSTALL_PREFIX ${CACHED_CMAKE_INSTALL_PREFIX}) 
-set(CMAKE_PREFIX_PATH ${CACHED_CMAKE_PREFIX_PATH})  
+    add_definitions(-DBUILD_WITH_DDS)
 
-list(POP_BACK CMAKE_MESSAGE_INDENT) # Unindent outputs
-message(CHECK_PASS "fastdds fetched")
+    install(TARGETS dds EXPORT realsense2Targets)
+endfunction()
 
-add_library(dds INTERFACE)
-target_link_libraries(dds INTERFACE  fastcdr fastrtps)
+# Trigger the FastDDS build
+get_fastdds()
 
-add_definitions(-DBUILD_WITH_DDS)
-
-install(TARGETS dds EXPORT realsense2Targets)
 
