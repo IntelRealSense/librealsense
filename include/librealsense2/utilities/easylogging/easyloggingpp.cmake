bb022aae1359f1ad521919d398515400d12f7587
Nir-Az <nir.azkiel@intel.com>
2022-04-11 17:16:39 +0300

    use a REPO_ROOT var

--- include/librealsense2/utilities/easylogging/easyloggingpp.cmake
+++ include/librealsense2/utilities/easylogging/easyloggingpp.cmake
@@ -1,10 +1,9 @@
 # License: Apache 2.0. See LICENSE file in root directory.
 # Copyright(c) 2022 Intel Corporation. All Rights Reserved.
-
-set( ELPP_INCLUDES ${CMAKE_CURRENT_LIST_DIR}/../../../../third-party/easyloggingpp/src )
+set( ELPP_INCLUDES ${REPO_ROOT}/third-party/easyloggingpp/src )
 set( ELPP_SOURCES
-    ${CMAKE_CURRENT_LIST_DIR}/../../../../third-party/easyloggingpp/src/easylogging++.cc
-    ${CMAKE_CURRENT_LIST_DIR}/../../../../third-party/easyloggingpp/src/easylogging++.h
+    ${REPO_ROOT}/third-party/easyloggingpp/src/easylogging++.cc
+    ${REPO_ROOT}/third-party/easyloggingpp/src/easylogging++.h
     ${CMAKE_CURRENT_LIST_DIR}/shared-init.cpp
 )
 
