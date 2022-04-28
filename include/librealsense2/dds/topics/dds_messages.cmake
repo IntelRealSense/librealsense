031772b75d963da9e2dde1dd10fff63dfc44f5f5
Nir-Az <nir.azkiel@intel.com>
2022-04-28 10:34:01 +0300

    change cmake structure for reuse

---
+++ include/librealsense2/dds/topics/dds_messages.cmake
@@ -0,0 +1,7 @@
+# License: Apache 2.0. See LICENSE file in root directory.
+# Copyright(c) 2022 Intel Corporation. All Rights Reserved.
+
+set(DDS_TOPICS_FILES 
+    ${DEVICE_INFO_TOPIC_FILES}
+    "${CMAKE_CURRENT_LIST_DIR}/dds-messages.h"
+)
---
+++ include/librealsense2/dds/topics/device-info/device_info.cmake
@@ -0,0 +1,10 @@
+# License: Apache 2.0. See LICENSE file in root directory.
+# Copyright(c) 2022 Intel Corporation. All Rights Reserved.
+
+set(DEVICE_INFO_TOPIC_FILES
+   "${CMAKE_CURRENT_LIST_DIR}/deviceInfoPubSubTypes.h"
+   "${CMAKE_CURRENT_LIST_DIR}/deviceInfoPubSubTypes.cpp"
+   "${CMAKE_CURRENT_LIST_DIR}/deviceInfo.h"
+   "${CMAKE_CURRENT_LIST_DIR}/deviceInfo.cpp"
+   "${CMAKE_CURRENT_LIST_DIR}/device-info-msg.h"
+)
