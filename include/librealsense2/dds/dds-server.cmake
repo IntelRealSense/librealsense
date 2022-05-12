ac2a4cd5dcd02b0570443a3fed3330b1d970f35b
Nir-Az <nir.azkiel@intel.com>
2022-05-12 15:21:23 +0300

    rename cmake file

--- include/librealsense2/dds/dds-server.cmake
+++ include/librealsense2/dds/dds-server.cmake
@@ -1,7 +1,7 @@
 # License: Apache 2.0. See LICENSE file in root directory.
 # Copyright(c) 2022 Intel Corporation. All Rights Reserved.
 
-set(DDS_UTILITIES_FILES 
+set(DDS_SERVER_FILES 
     "${CMAKE_CURRENT_LIST_DIR}/dds-participant.h"
     "${CMAKE_CURRENT_LIST_DIR}/dds-participant.cpp"
     "${CMAKE_CURRENT_LIST_DIR}/dds-device-broadcaster.h"
