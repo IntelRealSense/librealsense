162ce03f87e71818979b9485bc7b32c1f1c978f5
Remi Bettan <remibettan@gmail.com>
2022-09-15 11:15:00 +0300

    nv12 format instead of yuyv

--- src/ds5/ds5-color.cpp
+++ src/ds5/ds5-color.cpp
@@ -249,16 +249,18 @@ namespace librealsense
         color_ep.register_metadata(RS2_FRAME_METADATA_LOW_LIGHT_COMPENSATION, make_attribute_parser(&md_rgb_control::low_light_comp, md_rgb_control_attributes::low_light_comp_attribute, md_prop_offset));
 
 
-        color_ep.register_processing_block(processing_block_factory::create_pbf_vector<yuy2_converter>(RS2_FORMAT_YUYV, map_supported_color_formats(RS2_FORMAT_YUYV), RS2_STREAM_COLOR));
         color_ep.register_processing_block(processing_block_factory::create_id_pbf(RS2_FORMAT_RAW16, RS2_STREAM_COLOR));
 
         if (_pid == ds::RS465_PID)
         {
-            color_ep.register_processing_block({ {RS2_FORMAT_MJPEG} }, { {RS2_FORMAT_RGB8, RS2_STREAM_COLOR} }, []() { return std::make_shared<mjpeg_converter>(RS2_FORMAT_RGB8); });
+            // TODO REMI - to be restored after PID is updated
+            //color_ep.register_processing_block({ {RS2_FORMAT_MJPEG} }, { {RS2_FORMAT_RGB8, RS2_STREAM_COLOR} }, []() { return std::make_shared<mjpeg_converter>(RS2_FORMAT_RGB8); });
             color_ep.register_processing_block(processing_block_factory::create_id_pbf(RS2_FORMAT_MJPEG, RS2_STREAM_COLOR));
 
             color_ep.register_processing_block(processing_block_factory::create_pbf_vector<nv12_converter>(RS2_FORMAT_NV12, map_supported_color_formats(RS2_FORMAT_NV12), RS2_STREAM_COLOR));
         }
+        else
+            color_ep.register_processing_block(processing_block_factory::create_pbf_vector<yuy2_converter>(RS2_FORMAT_YUYV, map_supported_color_formats(RS2_FORMAT_YUYV), RS2_STREAM_COLOR));
     }
 
     rs2_intrinsics ds5_color_sensor::get_intrinsics(const stream_profile& profile) const
