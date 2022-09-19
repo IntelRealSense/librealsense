1bce1cea1500f57dcf4dbb0a8b1d7b0e3d14dff8
Eran <librealsense.eran@gmail.com>
2022-09-19 08:56:00 +0300

    moved print(dds_guid) to own .cpp; using dds_participant::name_from_guid()

--- include/librealsense2/dds/dds-client.cmake
+++ include/librealsense2/dds/dds-client.cmake
@@ -4,6 +4,7 @@
 set(DDS_CLIENT_FILES 
     "${CMAKE_CURRENT_LIST_DIR}/dds-defines.h"
     "${CMAKE_CURRENT_LIST_DIR}/dds-guid.h"
+    "${CMAKE_CURRENT_LIST_DIR}/dds-guid.cpp"
     "${CMAKE_CURRENT_LIST_DIR}/dds-participant.h"
     "${CMAKE_CURRENT_LIST_DIR}/dds-participant.cpp"
     "${CMAKE_CURRENT_LIST_DIR}/dds-utilities.h"
--- include/librealsense2/dds/dds-guid.h
+++ include/librealsense2/dds/dds-guid.h
@@ -5,7 +5,7 @@
 
 #include "dds-defines.h"
 #include <fastdds/rtps/common/Guid.h>
-#include <sstream>
+
 
 namespace librealsense {
 namespace dds {
@@ -15,39 +15,8 @@ namespace dds {
 // If a base_prefix is provided, will try to minimize a common denominator (vendor, host, etc.) -- you can use your
 // participant's guid if you want to shorten
 //
-inline std::string print( dds_guid const & guid,
-                          dds_guid_prefix const & base_prefix = eprosima::fastrtps::rtps::c_Guid_Unknown.guidPrefix )
-{
-    std::ostringstream output;
-    if( guid != eprosima::fastrtps::rtps::c_Guid_Unknown )
-    {
-        output << std::hex;
-        char old_fill = output.fill( '0' );
-        size_t i = 0;
-        if( base_prefix != eprosima::fastrtps::rtps::c_Guid_Unknown.guidPrefix )
-        {
-            while( i < base_prefix.size && guid.guidPrefix.value[i] == base_prefix.value[i] )
-                ++i;
-        }
-        if( i < guid.guidPrefix.size )
-        {
-            for( ; i < guid.guidPrefix.size; ++i )
-                output << std::setw( 2 ) << +guid.guidPrefix.value[i];  // << ".";
-
-            output << '|';  // between prefix and entity
-        }
-        for( uint8_t i = 0; i < guid.entityId.size; ++i )
-            output << std::setw( 2 ) << +guid.entityId.value[i];
-
-        output.fill( old_fill );
-        output << std::dec;
-    }
-    else
-    {
-        output << "|GUID UNKNOWN|";
-    }
-    return output.str();
-}
+std::string print( dds_guid const & guid,
+                   dds_guid_prefix const & base_prefix = eprosima::fastrtps::rtps::c_Guid_Unknown.guidPrefix );
 
 // Same as above, with a guid base for flexibility
 inline std::string print( dds_guid const & guid, dds_guid const & base_guid )
