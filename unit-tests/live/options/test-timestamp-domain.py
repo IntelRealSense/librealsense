ae281dc3b8b850f7affa6dfb05d81370c4883ea6
Tamir Yurovskiy <tamir.yurovskiy@intel.com>
2022-09-22 15:37:28 +0300

    new version with queue

--- unit-tests/live/options/test-timestamp-domain.py
+++ unit-tests/live/options/test-timestamp-domain.py
@@ -1,90 +1,120 @@
 # License: Apache 2.0. See LICENSE file in root directory.
 # Copyright(c) 2022 Intel Corporation. All Rights Reserved.
 
-# test: device D400*
+# test:device D400*
 
 import time
 import pyrealsense2 as rs
+
 from rspy import test
 
-# This test checks Timestamp domain toggle option for color and depth sensor
+
+def get_connected_device():
+    """
+    Get first connected device
+    :return: connected device
+    """
+    dev = None
+    try:
+        context = rs.context()
+        dev = context.devices[0]
+    except:
+        print('Exception: Failed to find connected device')
+    return dev
 
 
-def frame_callback(frame):
-    global expected_time_stamp_domain
-    test.check_equal(int(frame.get_frame_timestamp_domain()), expected_time_stamp_domain)
+def close_resources(sensor):
+    """
+    Stop and Close sensor.
+    :param: sensor of device
+    """
+    if len(sensor.get_active_streams()) > 0:
+        sensor.stop()
+        sensor.close()
 
 
-################################################################################################
-test.start("Start test")
+def start_sensor_test(sensor, global_time_enabled: int):
+    """
+    Perform depth sensor test according given global time
+    :global_time_enabled int: 1 - time is enabled, 0 - time disabled
+    """
 
-ENABLE = 1
-DISABLE = 0
+    if global_time_enabled not in [0, 1]:
+        raise ValueError(f'Invalid parameter in start depth test: {global_time_enabled}. Only 1 or 0 are valid.')
 
-devices = test.find_first_device_or_exit()
+    try:
+        sensor.set_option(rs.option.global_time_enabled, global_time_enabled)
+        time.sleep(2)
 
-time.sleep(3)  # The device starts at D0 (Operational) state, allow time for it to get into idle state
-depth_sensor = devices.first_depth_sensor()
-color_sensor = devices.first_color_sensor()
+        frame = frame_queue.wait_for_frame()
 
-depth_sensor.get_option(rs.option.global_time_enabled)
-color_sensor.get_option(rs.option.global_time_enabled)
+        if global_time_enabled == 0 and sensor.get_option(rs.option.global_time_enabled) == 0:
 
-depth_profile = next(p for p in depth_sensor.profiles if p.fps() == 30
-                     and p.stream_type() == rs.stream.depth
-                     and p.format() == rs.format.z16
-                     and p.as_video_stream_profile().width() == 640
-                     and p.as_video_stream_profile().height() == 480)
+            test.check_equal(frame.get_frame_timestamp_domain(), rs.timestamp_domain.hardware_clock)
+            print('frame_timestamp_domain: ', int(frame.get_frame_timestamp_domain()))  # remove it
 
-color_profile = next(p for p in color_sensor.profiles if p.fps() == 30
-                     and p.stream_type() == rs.stream.color
-                     and p.format() == rs.format.yuyv
-                     and p.as_video_stream_profile().width() == 640
-                     and p.as_video_stream_profile().height() == 480)
+        elif global_time_enabled == 1 and sensor.get_option(rs.option.global_time_enabled) == 1:
 
-depth_sensor.open(depth_profile)
-depth_sensor.start(frame_callback)
+            test.check_equal(frame.get_frame_timestamp_domain(),
+                             rs.timestamp_domain.global_time)
+            print('frame_timestamp_domain: ', int(frame.get_frame_timestamp_domain()))  # remove it
 
-#############################################################################################
-# Test #1
-expected_time_stamp_domain = 0
-depth_sensor.set_option(rs.option.global_time_enabled, DISABLE)
-test.check_equal(DISABLE, depth_sensor.get_option(rs.option.global_time_enabled))
-time.sleep(0.2)
+    except:
+        print('Exception: start_depth_sensor_test throw exception with value: ', global_time_enabled)
 
-#############################################################################################
-# Test #2
-expected_time_stamp_domain = 2
-depth_sensor.set_option(rs.option.global_time_enabled, ENABLE)
-test.check_equal(ENABLE, depth_sensor.get_option(rs.option.global_time_enabled))
-time.sleep(0.2)
 
-#############################################################################################
+depth_sensor = None
+color_sensor = None
 
-depth_sensor.stop()
-depth_sensor.close()
+try:
+    device = get_connected_device()
+    depth_sensor = device.first_depth_sensor()
+    color_sensor = device.first_color_sensor()
 
-color_sensor.open(color_profile)
-color_sensor.start(frame_callback)
+    depth_profile = next(p for p in depth_sensor.profiles if p.fps() == 30
+                         and p.stream_type() == rs.stream.depth
+                         and p.format() == rs.format.z16
+                         and p.as_video_stream_profile().width() == 640
+                         and p.as_video_stream_profile().height() == 480)
 
-#############################################################################################
-# Test #3
-expected_time_stamp_domain = 0
-color_sensor.set_option(rs.option.global_time_enabled, DISABLE)
-test.check_equal(DISABLE, color_sensor.get_option(rs.option.global_time_enabled))
-time.sleep(0.2)
+    color_profile = next(p for p in color_sensor.profiles if p.fps() == 30
+                         and p.stream_type() == rs.stream.color
+                         and p.format() == rs.format.yuyv
+                         and p.as_video_stream_profile().width() == 640
+                         and p.as_video_stream_profile().height() == 480)
 
-#############################################################################################
-# Test #4
-expected_time_stamp_domain = 2
-color_sensor.set_option(rs.option.global_time_enabled, ENABLE)
-test.check_equal(ENABLE, color_sensor.get_option(rs.option.global_time_enabled))
-time.sleep(0.2)
+    depth_sensor.open(depth_profile)
+    color_sensor.open(color_profile)
+    frame_queue = rs.frame_queue(capacity=10, keep_frames=False)
 
-#############################################################################################
+    depth_sensor.start(frame_queue)
 
-color_sensor.stop()
-color_sensor.close()
+    # Test #1
+    test.start('Start depth sensor test: global time disabled')
+    start_sensor_test(depth_sensor, 0)
+    test.finish()
 
-test.finish()
-test.print_results_and_exit()
+    # Test #2
+    test.start('Start depth sensor test: global time enabled')
+    start_sensor_test(depth_sensor, 1)
+    test.finish()
+
+    close_resources(depth_sensor)
+    color_sensor.start(frame_queue)
+
+    # Test #3
+    test.start('Start color sensor test: global time disabled')
+    start_sensor_test(color_sensor, 0)
+    test.finish()
+
+    # Test #4
+    test.start('Start color sensor test: global time enabled')
+    start_sensor_test(color_sensor, 1)
+    test.finish()
+
+except Exception as e:
+    print(str(e))
+    print("The device found has no depth sensor")
+finally:
+    # close_resources(depth_sensor)
+    close_resources(color_sensor)
