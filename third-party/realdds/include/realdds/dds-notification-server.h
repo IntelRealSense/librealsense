e7e1701bffde2f073b3c6a49a086c5276daebea1
Eran <librealsense.eran@gmail.com>
2022-10-06 11:13:10 +0300

    split dds-notification-server and use dds objects

---
+++ third-party/realdds/include/realdds/dds-notification-server.h
@@ -0,0 +1,64 @@
+// License: Apache 2.0. See LICENSE file in root directory.
+// Copyright(c) 2022 Intel Corporation. All Rights Reserved.
+
+#pragma once
+
+
+#include <realdds/topics/notification/notificationPubSubTypes.h>  // raw::device::notification
+#include <librealsense2/utilities/concurrency/concurrency.h>
+
+#include <memory>
+#include <string>
+
+
+namespace realdds {
+
+
+namespace topics {
+namespace raw {
+namespace device {
+class notification;
+}
+}  // namespace raw
+}  // namespace topics
+
+
+class dds_publisher;
+class dds_topic_writer;
+
+
+// A notification is a generic message, with data, in a specific direction: the server (this class) sends and a client
+// received and interprets.
+// 
+// Notifications are either on-demand (i.e., send this message right now) or on-discovery (send these messages to any
+// client that connects). We optionally store and automatically send such messages.
+//
+class dds_notification_server
+{
+public:
+    dds_notification_server( std::shared_ptr< dds_publisher > const & publisher, const std::string & topic_name );
+    ~dds_notification_server();
+
+    // On-demand notification: these happen sequentially and from another thread
+    void send_notification( topics::raw::device::notification && notification );
+
+    // On-discovery notification: when a new client is detected
+    void add_init_notification( topics::raw::device::notification && notification );
+
+private:
+    void send_init_msgs();
+
+    std::shared_ptr< dds_publisher > _publisher;
+    std::shared_ptr< dds_topic_writer > _writer;
+    active_object<> _notifications_loop;
+    single_consumer_queue< topics::raw::device::notification > _instant_notifications;
+    std::vector< topics::raw::device::notification > _init_notifications;
+    std::mutex _notification_send_mutex;
+    std::condition_variable _send_notification_cv;
+    std::atomic_bool _send_init_msgs = { false };
+    std::atomic_bool _new_instant_notification = { false };
+    std::atomic_bool _active = { false };
+};
+
+
+}
---
+++ third-party/realdds/src/dds-notification-server.cpp
@@ -0,0 +1,138 @@
+// License: Apache 2.0. See LICENSE file in root directory.
+// Copyright(c) 2022 Intel Corporation. All Rights Reserved.
+
+#include <realdds/dds-notification-server.h>
+
+#include <realdds/dds-participant.h>
+#include <realdds/dds-publisher.h>
+#include <realdds/dds-stream-server.h>
+#include <realdds/dds-utilities.h>
+#include <realdds/topics/dds-topics.h>
+#include <realdds/dds-topic.h>
+#include <realdds/dds-topic-writer.h>
+
+#include <fastdds/dds/topic/Topic.hpp>
+
+#include <fastdds/dds/domain/DomainParticipant.hpp>
+#include <fastdds/dds/publisher/Publisher.hpp>
+#include <fastdds/dds/publisher/DataWriter.hpp>
+#include <fastdds/dds/publisher/DataWriterListener.hpp>
+#include <fastdds/dds/publisher/qos/PublisherQos.hpp>
+#include <librealsense2/utilities/concurrency/concurrency.h>
+
+
+using namespace eprosima::fastdds::dds;
+
+namespace realdds {
+
+
+dds_notification_server::dds_notification_server( std::shared_ptr< dds_publisher > const & publisher, const std::string & topic_name )
+    : _publisher( publisher )
+    , _notifications_loop( [this]( dispatcher::cancellable_timer timer ) {
+        if( _active )
+        {
+            // We can send 2 types of notifications:
+            // * Initialize notifications that should be sent for every new reader (like sensors & profiles data)
+            // * Instant notifications that can be sent upon an event and is not required to be
+            //   resent to new readers
+            std::unique_lock< std::mutex > lock( _notification_send_mutex );
+            _send_notification_cv.wait( lock, [this]() {
+                return ! _active || _send_init_msgs || _new_instant_notification;
+            } );
+
+            if( _active && _send_init_msgs )
+            {
+                send_init_msgs();
+                _send_init_msgs = false;
+            }
+
+            if( _active && _new_instant_notification )
+            {
+                // Send all instant notifications
+                topics::raw::device::notification notification;
+                while( _instant_notifications.dequeue( &notification, 1000 ) )
+                {
+                    DDS_API_CALL( _writer->get()->write( &notification ) );
+                }
+                _new_instant_notification = false;
+            }
+        }
+    } )
+{
+    auto topic = topics::device::notification::create_topic( publisher->get_participant(), topic_name.c_str() );
+    _writer = std::make_shared< dds_topic_writer >( topic, publisher );
+
+    _writer->on_publication_matched( [this]( PublicationMatchedStatus const & info ) {
+        if( info.current_count_change == 1 )
+        {
+            LOG_DEBUG( "DataReader " << _writer->topic()->get_participant()->print(
+                           (const dds_guid &)info.last_subscription_handle )
+                                     << " for '" << _writer->topic()->get()->get_name() << "' discovered" );
+            {
+                std::lock_guard< std::mutex > lock( _notification_send_mutex );
+                _send_init_msgs = true;
+            }
+            _send_notification_cv.notify_all();
+        }
+        else if( info.current_count_change == -1 )
+        {
+            LOG_DEBUG( "DataReader " << _writer->topic()->get_participant()->print(
+                           (const dds_guid &)info.last_subscription_handle )
+                                     << " for '" << _writer->topic()->get()->get_name() << "' disappeared" );
+        }
+        else
+        {
+            LOG_ERROR( std::to_string( info.current_count_change )
+                       << " is not a valid value for on_publication_matched" );
+        }
+    } );
+
+    dds_topic_writer::writer_qos wqos( RELIABLE_RELIABILITY_QOS );
+    wqos.history().depth = 10;                                                                      // default is 1
+    wqos.endpoint().history_memory_policy = eprosima::fastrtps::rtps::DYNAMIC_RESERVE_MEMORY_MODE;  // TODO: why?
+    _writer->run( wqos );
+
+    _notifications_loop.start();
+    _active = true;
+}
+
+
+dds_notification_server::~dds_notification_server()
+{
+    // Mark us inactive and wake up the active object so we can properly stop it
+    _active = false;
+    _send_notification_cv.notify_all();
+    _notifications_loop.stop();
+}
+
+
+void dds_notification_server::send_notification( topics::raw::device::notification && notification )
+{
+    std::unique_lock< std::mutex > lock( _notification_send_mutex );
+    if( ! _instant_notifications.enqueue( std::move( notification ) ) )
+    {
+        LOG_ERROR( "error while trying to enqueue a message id:" << notification.id()
+                                                                 << " to instant notifications queue" );
+    }
+};
+
+
+void dds_notification_server::add_init_notification( topics::raw::device::notification && msg )
+{
+    std::unique_lock< std::mutex > lock( _notification_send_mutex );
+    topics::raw::device::notification msg_to_move( msg );
+    _init_notifications.push_back( std::move( msg_to_move ) );
+};
+
+
+void dds_notification_server::send_init_msgs()
+{
+    // Send all initialization notifications
+    for( auto notification : _init_notifications )
+    {
+        DDS_API_CALL( _writer->get()->write( &notification ) );
+    }
+}
+
+
+}  // namespace realdds
