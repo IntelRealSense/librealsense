e40f6cf5f2cd0e083e842e8b6ad090fa8722797d
Eran <librealsense.eran@gmail.com>
2022-10-19 12:49:19 +0300

    make dds-participant's _next_entry_id atomic, per CR

--- third-party/realdds/include/realdds/dds-participant.h
+++ third-party/realdds/include/realdds/dds-participant.h
@@ -9,6 +9,7 @@
 #include <functional>
 #include <string>
 #include <list>
+#include <atomic>
 
 
 namespace realdds {
@@ -149,7 +150,7 @@ public:
 private:
     std::list< std::weak_ptr< listener > > _listeners;
     std::shared_ptr< listener_impl > _domain_listener;
-    uint32_t _next_entity_id = 0;  // for create_guid()
+    std::atomic< uint32_t > _next_entity_id = 0;  // for create_guid()
 
     void on_writer_added( dds_guid, char const * topic_name );
     void on_writer_removed( dds_guid, char const * topic_name );
