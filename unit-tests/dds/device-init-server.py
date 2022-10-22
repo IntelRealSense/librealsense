98e7fd7e0059a04a1565f0cebb3c33b9d4840196
Eran <librealsense.eran@gmail.com>
2022-10-22 12:17:37 +0300

    added more device tests

--- unit-tests/dds/device-init-server.py
+++ unit-tests/dds/device-init-server.py
@@ -10,14 +10,35 @@ dds.debug( True, log.nested )
 participant = dds.participant()
 participant.init( 123, "device-init-server" )
 
-def test_one():
-    log.d( 'test-one running...' )
+
+def test_one_stream():
+    log.d( 'test-one-stream running...' )
     s1p1 = dds.video_stream_profile( dds.stream_uid(23,45), dds.stream_format("RGB8"), 9, 10, 10, 123 )
     s1profiles = [s1p1]
     s1 = dds.video_stream_server( "s1", s1profiles )
     global server
     server = dds.device_server( participant, "realdds/device/topic-root" )
     server.init( [s1] )
+
+def test_no_profiles():
+    log.d( 'test-no-profiles running...' )
+    s1profiles = []
+    s1 = dds.video_stream_server( "s1", s1profiles )
+    global server
+    server = dds.device_server( participant, "realdds/device/topic-root" )
+    server.init( [s1] )
+
+def test_no_streams():
+    log.d( 'test-no-streams running...' )
+    global server
+    server = dds.device_server( participant, "realdds/device/topic-root" )
+    server.init( [] )
+
+
+def close_server():
+    global server
+    server = None
+
 
 # From here down, we're in "interactive" mode (see test-device-init.py)
 # ...
--- unit-tests/dds/test-device-init.py
+++ unit-tests/dds/test-device-init.py
@@ -7,8 +7,8 @@ import pyrealdds as dds
 from rspy import log, test
 
 
-dds.debug( True, 'cli' )
-log.nested = 'cli'
+dds.debug( True, 'C  ' )
+log.nested = 'C  '
 
 
 participant = dds.participant()
@@ -21,19 +21,77 @@ info.topic_root = "realdds/device/topic-root"
 
 import os.path
 cwd = os.path.dirname(os.path.realpath(__file__))
-dir( test )
-with test.remote( os.path.join( cwd, 'device-init-server.py' )) as remote:
+remote_script = os.path.join( cwd, 'device-init-server.py' )
+second_client_script = os.path.join( cwd, 'device-2nd-client.py' )
+with test.remote( remote_script, nested_indent="  S" ) as remote:
     remote.wait_until_ready()
     #
     #############################################################################################
     #
-    test.start( "Test 1..." )
+    test.start( "Test 1 stream..." )
     try:
-        remote.run( 'test_one()', timeout=5 )
+        remote.run( 'test_one_stream()', timeout=5 )
         device = dds.device( participant, participant.create_guid(), info )
         device.run()  # If no device is available in 30 seconds, this will throw
         test.check( device.is_running() )
-        test.check_equal( device.num_of_sensors(), 1 )
+        test.check_equal( device.n_streams(), 1 )
+        remote.run( 'close_server()', timeout=5 )
+    except:
+        test.unexpected_exception()
+    device = None
+    test.finish()
+    #
+    #############################################################################################
+    #
+    test.start( "Test no streams..." )
+    try:
+        remote.run( 'test_no_streams()', timeout=5 )
+        device = dds.device( participant, participant.create_guid(), info )
+        device.run()  # If no device is available in 30 seconds, this will throw
+        test.check( device.is_running() )
+        test.check_equal( device.n_streams(), 0 )
+        remote.run( 'close_server()', timeout=5 )
+    except:
+        test.unexpected_exception()
+    device = None
+    test.finish()
+    #
+    #############################################################################################
+    #
+    test.start( "Test no profiles... (should fail on server side)" )
+    try:
+        remote.run( 'test_no_profiles()', timeout=5 )
+        device = dds.device( participant, participant.create_guid(), info )
+        device.run()  # If no device is available in 30 seconds, this will throw
+        test.check( device.is_running() )
+        test.check_equal( device.n_streams(), 1 )
+        remote.run( 'close_server()', timeout=5 )
+    except test.remote.Error as e:
+        # this fails because streams require at least one profile
+        test.check_exception( e, test.remote.Error, "RuntimeError: at least one profile is required in stream 's1'" )
+    except:
+        test.unexpected_exception()
+    device = None
+    test.finish()
+    #
+    #############################################################################################
+    #
+    test.start( "Test discovery of another client device..." )
+    try:
+        remote.run( 'test_one_stream()', timeout=5 )
+        device = dds.device( participant, participant.create_guid(), info )
+        device.run()  # If no device is available in 30 seconds, this will throw
+        test.check( device.is_running() )
+        test.check_equal( device.n_streams(), 1 )
+        # now start another server, and have it access the device
+        # NOTE: I had trouble nesting another 'with' statement here...
+        with test.remote( second_client_script, name="remote2", nested_indent="  c" ) as remote2:
+            remote2.wait_until_ready()
+            remote2.run( 'test_second_device()', timeout=5 )
+            # the notifications for the second client should be sent again from the server -- we
+            # should ignore them...
+            remote2.run( 'close_device()', timeout=5 )
+        remote.run( 'close_server()', timeout=5 )
     except:
         test.unexpected_exception()
     device = None
