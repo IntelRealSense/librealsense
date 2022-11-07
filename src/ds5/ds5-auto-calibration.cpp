4035ee73591ba54c61c1a198aff744969d2b0097
Evgeni Raikhel <evgeni.raikhel@intel.com>
2022-11-07 10:00:37 +0200

    Dev comment for maintenance

--- src/ds5/ds5-auto-calibration.cpp
+++ src/ds5/ds5-auto-calibration.cpp
@@ -529,7 +529,7 @@ namespace librealsense
                         catch(...)
                         {
                             LOG_OCC_WARN("occ Save Statistics result failed");
-                            std::this_thread::sleep_for(std::chrono::milliseconds(100));
+                            std::this_thread::sleep_for(std::chrono::milliseconds(100)); // For the FW to recuperate
                         };
                     }
                     while(( ++iter < 3) && (!success));
