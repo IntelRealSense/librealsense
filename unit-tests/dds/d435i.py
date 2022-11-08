2fd9227003a0cd20f2759d005b7d37eb2729131a
Eran <librealsense.eran@gmail.com>
2022-11-08 11:37:15 +0200

    add librs test for d435i

--- unit-tests/dds/d435i.py
+++ unit-tests/dds/d435i.py
@@ -12,29 +12,50 @@ device_info.product_line = "D400"
 device_info.topic_root = "realdds/D435I/" + device_info.serial
 
 
-def accel_stream():
-    stream = dds.accel_stream_server( "Accel", "Motion Module" )
-    profiles = [
+def build( participant ):
+    """
+    Build a D435i device server for use in DDS
+    """
+    accel = accel_stream()
+    gyro = gyro_stream()
+    depth = depth_stream()
+    ir1 = ir_stream( 1 )
+    ir2 = ir_stream( 2 )
+    color = color_stream()
+    #
+    d435i = dds.device_server( participant, device_info.topic_root )
+    d435i.init( [accel, color, depth, gyro, ir1, ir2] )
+    return d435i
+
+
+def accel_stream_profiles():
+    return [
         dds.motion_stream_profile( 200, dds.stream_format("MXYZ") ),
         dds.motion_stream_profile( 100, dds.stream_format("MXYZ") )
         ]
-    stream.init_profiles( profiles, 0 )
+
+
+def accel_stream():
+    stream = dds.accel_stream_server( "Accel", "Motion Module" )
+    stream.init_profiles( accel_stream_profiles(), 0 )
     return stream
+
+
+def gyro_stream_profiles():
+    return [
+        dds.motion_stream_profile( 400, dds.stream_format("MXYZ") ),
+        dds.motion_stream_profile( 200, dds.stream_format("MXYZ") )
+        ]
 
 
 def gyro_stream():
     stream = dds.gyro_stream_server( "Gyro", "Motion Module" )
-    profiles = [
-        dds.motion_stream_profile( 400, dds.stream_format("MXYZ") ),
-        dds.motion_stream_profile( 200, dds.stream_format("MXYZ") )
-        ]
-    stream.init_profiles( profiles, 0 )
+    stream.init_profiles( gyro_stream_profiles(), 0 )
     return stream
 
 
-def depth_stream():
-    stream = dds.depth_stream_server( "Depth", "Stereo Module" )
-    profiles = [
+def depth_stream_profiles():
+    return [
         dds.video_stream_profile( 30, dds.stream_format("Z16"), 1280,720 ),
         dds.video_stream_profile( 15, dds.stream_format("Z16"), 1280,720 ),
         dds.video_stream_profile( 6, dds.stream_format("Z16"), 1280,720 ),
@@ -68,13 +89,16 @@ def depth_stream():
         dds.video_stream_profile( 300, dds.stream_format("Z16"), 256,144 ),
         dds.video_stream_profile( 90, dds.stream_format("Z16"), 256,144 )
         ]
-    stream.init_profiles( profiles, 0 )
+
+
+def depth_stream():
+    stream = dds.depth_stream_server( "Depth", "Stereo Module" )
+    stream.init_profiles( depth_stream_profiles(), 0 )
     return stream
 
 
-def ir_stream( number ):
-    stream = dds.ir_stream_server( "Infrared " + str(number), "Stereo Module" )
-    profiles = [
+def ir_stream_profiles():
+    return [
         dds.video_stream_profile( 30, dds.stream_format("GREY"), 1280,800 ),
         dds.video_stream_profile( 25, dds.stream_format("Y16"), 1280,800 ),
         dds.video_stream_profile( 15, dds.stream_format("Y16"), 1280,800 ),
@@ -112,13 +136,16 @@ def ir_stream( number ):
         dds.video_stream_profile( 15, dds.stream_format("GREY"), 424,240 ),
         dds.video_stream_profile( 6, dds.stream_format("GREY"), 424,240 )
         ]
-    stream.init_profiles( profiles, 0 )
+
+
+def ir_stream( number ):
+    stream = dds.ir_stream_server( "Infrared " + str(number), "Stereo Module" )
+    stream.init_profiles( ir_stream_profiles(), 0 )
     return stream
 
 
-def color_stream():
-    stream = dds.color_stream_server( "Color",  "RGB Camera" )
-    profiles = [
+def color_stream_profiles():
+    return [
         dds.video_stream_profile( 30, dds.stream_format("RGB8"), 1920,1080 ),
         dds.video_stream_profile( 30, dds.stream_format("BYR2"), 1920,1080 ),
         dds.video_stream_profile( 30, dds.stream_format("Y16"), 1920,1080 ),
@@ -313,21 +340,9 @@ def color_stream():
         dds.video_stream_profile( 6, dds.stream_format("RGB2"), 320,180 ),
         dds.video_stream_profile( 6, dds.stream_format("YUYV"), 320,180 )
         ]
-    stream.init_profiles( profiles, 0 )
+
+
+def color_stream():
+    stream = dds.color_stream_server( "Color",  "RGB Camera" )
+    stream.init_profiles( color_stream_profiles(), 0 )
     return stream
-
-
-def build( participant ):
-    """
-    Build a D435i device server for use in DDS
-    """
-    accel = accel_stream()
-    gyro = gyro_stream()
-    depth = depth_stream()
-    ir1 = ir_stream( 1 )
-    ir2 = ir_stream( 2 )
-    color = color_stream()
-    #
-    d435i = dds.device_server( participant, device_info.topic_root )
-    d435i.init( [accel, color, depth, gyro, ir1, ir2] )
-    return d435i
