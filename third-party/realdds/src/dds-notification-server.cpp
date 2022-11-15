3985402eae027788edb0967f1e97e6a48b523cb0
Eran <github@elibrach.fastmail.fm>
2022-11-15 07:53:52 +0200

    Update dds-notification-server.cpp

--- third-party/realdds/src/dds-notification-server.cpp
+++ third-party/realdds/src/dds-notification-server.cpp
@@ -87,7 +87,7 @@ dds_notification_server::dds_notification_server( std::shared_ptr< dds_publisher
     // Writer can finish reader-writer handshake and start transmitting the discovery messages before the reader have
     // finished the handshake and can receivethe messages.
     // If history is too small writer will not be able to re-transmit needed samples.
-    // Setting history to cover known streams in Safety Camera plus some spare
+    // Setting history to cover known use-cases plus some spare
     wqos.history().depth = 16;
 
     _writer->run( wqos );
