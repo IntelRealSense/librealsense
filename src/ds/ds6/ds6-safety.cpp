e0609c05415db3c05bfa0edf8f5182f3fbb1a7da
remibettan <remibettan@gmail.com>
2022-11-30 12:52:47 +0200

    adjustments after refact

--- src/ds/ds6/ds6-safety.cpp
+++ src/ds/ds6/ds6-safety.cpp
@@ -1,7 +1,7 @@
 // License: Apache 2.0. See LICENSE file in root directory.
 // Copyright(c) 2022 Intel Corporation. All Rights Reserved.
 
-#include "ds5-safety.h"
+#include "ds6-safety.h"
 
 #include <mutex>
 #include <chrono>
@@ -10,8 +10,8 @@
 #include <iterator>
 #include <cstddef>
 
-#include "ds5-timestamp.h"
-#include "ds5-options.h"
+#include "ds/ds-timestamp.h"
+#include "ds/ds-options.h"
 #include "stream.h"
 
 namespace librealsense
@@ -23,14 +23,14 @@ namespace librealsense
         {rs_fourcc('R','A','W','8'), RS2_STREAM_SAFETY}
     };
 
-    ds5_safety::ds5_safety(std::shared_ptr<context> ctx,
+    ds6_safety::ds6_safety(std::shared_ptr<context> ctx,
         const platform::backend_device_group& group)
-        : device(ctx, group), ds5_device(ctx, group),
+        : device(ctx, group), ds6_device(ctx, group),
         _safety_stream(new stream(RS2_STREAM_SAFETY))
     {
         using namespace ds;
-
-        auto safety_devs_info = filter_by_mi(group.uvc_devices, 8); // TODO REMI - assumption EP 8
+        const uint32_t safety_stream_end_point = 9;
+        auto safety_devs_info = filter_by_mi(group.uvc_devices, safety_stream_end_point);
         if (safety_devs_info.size() != 1)
             throw invalid_value_exception(to_string() << "RS4XX with Safety models are expected to include a single safety device! - "
                 << safety_devs_info.size() << " found");
@@ -38,14 +38,14 @@ namespace librealsense
         auto safety_ep = create_safety_device(ctx, safety_devs_info);
     }
 
-    std::shared_ptr<synthetic_sensor> ds5_safety::create_safety_device(std::shared_ptr<context> ctx,
+    std::shared_ptr<synthetic_sensor> ds6_safety::create_safety_device(std::shared_ptr<context> ctx,
         const std::vector<platform::uvc_device_info>& safety_devices_info)
     {
         using namespace ds;
         auto&& backend = ctx->get_backend();
 
-        std::unique_ptr<frame_timestamp_reader> ds5_timestamp_reader_backup(new ds5_timestamp_reader(backend.create_time_service()));
-        std::unique_ptr<frame_timestamp_reader> ds5_timestamp_reader_metadata(new ds5_timestamp_reader_from_metadata(std::move(ds5_timestamp_reader_backup)));
+        std::unique_ptr<frame_timestamp_reader> ds5_timestamp_reader_backup(new ds_timestamp_reader(backend.create_time_service()));
+        std::unique_ptr<frame_timestamp_reader> ds5_timestamp_reader_metadata(new ds_timestamp_reader_from_metadata(std::move(ds5_timestamp_reader_backup)));
 
         auto enable_global_time_option = std::shared_ptr<global_time_option>(new global_time_option());
         auto raw_safety_ep = std::make_shared<uvc_sensor>("Raw Safety Device",
@@ -53,7 +53,7 @@ namespace librealsense
             std::unique_ptr<frame_timestamp_reader>(new global_timestamp_reader(std::move(ds5_timestamp_reader_metadata), _tf_keeper, enable_global_time_option)),
             this);
 
-        auto safety_ep = std::make_shared<ds5_safety_sensor>(this,
+        auto safety_ep = std::make_shared<ds6_safety_sensor>(this,
             raw_safety_ep,
             safety_fourcc_to_rs2_format,
             safety_fourcc_to_rs2_stream);
@@ -75,12 +75,12 @@ namespace librealsense
     }
 
 
-    void ds5_safety::register_options(std::shared_ptr<ds5_safety_sensor> safety_ep)
+    void ds6_safety::register_options(std::shared_ptr<ds6_safety_sensor> safety_ep)
     {
         // empty
     }
 
-    void ds5_safety::register_metadata(std::shared_ptr<uvc_sensor> raw_safety_ep)
+    void ds6_safety::register_metadata(std::shared_ptr<uvc_sensor> raw_safety_ep)
     {
         raw_safety_ep->register_metadata(RS2_FRAME_METADATA_FRAME_TIMESTAMP, 
             make_uvc_header_parser(&platform::uvc_header::timestamp));
@@ -161,13 +161,13 @@ namespace librealsense
                 }));
     }
 
-    void ds5_safety::register_processing_blocks(std::shared_ptr<ds5_safety_sensor> safety_ep)
+    void ds6_safety::register_processing_blocks(std::shared_ptr<ds6_safety_sensor> safety_ep)
     {
         safety_ep->register_processing_block(processing_block_factory::create_id_pbf(RS2_FORMAT_RAW16, RS2_STREAM_SAFETY));
     }
 
 
-    stream_profiles ds5_safety_sensor::init_stream_profiles()
+    stream_profiles ds6_safety_sensor::init_stream_profiles()
     {
         auto lock = environment::get_instance().get_extrinsics_graph().lock();
         auto results = synthetic_sensor::init_stream_profiles();
--- src/ds/ds6/ds6-safety.h
+++ src/ds/ds6/ds6-safety.h
@@ -3,36 +3,36 @@
 
 #pragma once
 
-#include "ds5-device.h"
+#include "ds6-device.h"
 
 namespace librealsense
 {
-    class ds5_safety : public virtual ds5_device
+    class ds6_safety : public virtual ds6_device
     {
     public:
         std::shared_ptr<synthetic_sensor> create_safety_device(std::shared_ptr<context> ctx,
             const std::vector<platform::uvc_device_info>& safety_devices_info);
 
-        ds5_safety(std::shared_ptr<context> ctx,
+        ds6_safety(std::shared_ptr<context> ctx,
                    const platform::backend_device_group& group);
 
     private:
 
-        friend class ds5_safety_sensor;
+        friend class ds6_safety_sensor;
 
-        uint16_t _pid;    // product PID
-        void register_options(std::shared_ptr<ds5_safety_sensor> safety_ep);
+        void register_options(std::shared_ptr<ds6_safety_sensor> safety_ep);
         void register_metadata(std::shared_ptr<uvc_sensor> safety_ep);
-        void register_processing_blocks(std::shared_ptr<ds5_safety_sensor> safety_ep);
+        void register_processing_blocks(std::shared_ptr<ds6_safety_sensor> safety_ep);
 
     protected:
         std::shared_ptr<stream_interface> _safety_stream;
     };
 
-    class ds5_safety_sensor : public synthetic_sensor
+    class ds6_safety_sensor : public synthetic_sensor,
+                              public safety_sensor
     {
     public:
-        explicit ds5_safety_sensor(ds5_safety* owner,
+        explicit ds6_safety_sensor(ds6_safety* owner,
             std::shared_ptr<uvc_sensor> uvc_sensor,
             std::map<uint32_t, rs2_format> safety_fourcc_to_rs2_format,
             std::map<uint32_t, rs2_stream> safety_fourcc_to_rs2_stream)
@@ -45,6 +45,6 @@ namespace librealsense
         stream_profiles init_stream_profiles() override;
 
     protected:
-        const ds5_safety* _owner;
+        const ds6_safety* _owner;
     };
 }
