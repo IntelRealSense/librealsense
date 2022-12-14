a9071c50b93a06bae34848d3488866428b185d6f
Eran <librealsense.eran@gmail.com>
2022-12-14 13:22:58 +0200

    add stabilized_value< double > to pyrsutils

--- third-party/rsutils/py/pyrsutils.cpp
+++ third-party/rsutils/py/pyrsutils.cpp
@@ -5,6 +5,8 @@
 #include <rsutils/easylogging/easyloggingpp.h>
 #include <rsutils/string/split.h>
 #include <rsutils/version.h>
+#include <rsutils/number/stabilized-value.h>
+#include <rsutils/string/from.h>
 
 #define NAME pyrsutils
 #define SNAME "pyrsutils"
@@ -37,8 +39,7 @@ PYBIND11_MODULE(NAME, m) {
               py::arg( "build" ) = 0 )
         .def_static( "from_number", []( version::number_type n ) { return version( n ); } )
         .def( "is_valid", &version::is_valid )
-        .def( "__nonzero__", &version::is_valid )  // Called to implement truth value testing in Python 2
-        .def( "__bool__", &version::is_valid )     // Called to implement truth value testing in Python 3
+        .def( "__bool__", &version::is_valid )
         .def( "major", &version::get_major )
         .def( "minor", &version::get_minor )
         .def( "patch", &version::get_patch )
@@ -63,4 +64,23 @@ PYBIND11_MODULE(NAME, m) {
         .def( py::self >= py::self )
         .def( py::self > py::self )
         .def( "is_between", &version::is_between );
+
+    using stabilized_value = rsutils::number::stabilized_value< double >;
+    auto not_empty = []( stabilized_value const & self ) {
+        return ! self.empty();
+    };
+    auto to_string = []( stabilized_value const & self ) -> std::string {
+        if( self.empty() )
+            return "EMPTY";
+        return rsutils::string::from( self.get() );
+    };
+    py::class_< stabilized_value >( m, "stabilized_value" )
+        .def( py::init< size_t >() )
+        .def( "empty", &stabilized_value::empty )
+        .def( "__bool__", not_empty )
+        .def( "add", &stabilized_value::add )
+        .def( "get", &stabilized_value::get, py::arg( "stabilization-percent" ) = 0.75 )
+        .def( "clear", &stabilized_value::clear )
+        .def( "to_string", to_string )
+        .def( "__str__", to_string );
 }
