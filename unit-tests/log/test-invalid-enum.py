7c7baf09dd61211c9f017fcfdcfaf1b092d8ccb3
Eran <librealsense.eran@gmail.com>
2022-12-27 07:52:53 +0200

    remove python3.6-incompatible enum from test-invalid-enum

--- unit-tests/log/test-invalid-enum.py
+++ unit-tests/log/test-invalid-enum.py
@@ -15,8 +15,13 @@ try:
     test.check_equal( common.n_messages, 0 )
     # Following will throw a recoverable_exception, which will issue a log by itself!
     test.check_throws( lambda: rs.log( rs.log_severity( 10 ), "10" ), RuntimeError, 'invalid enum value for argument "severity"' )
-    test.check_throws( lambda: rs.log( rs.log_severity( -1 ), "-1" ), RuntimeError, 'invalid enum value for argument "severity"' )
-    test.check_equal( common.n_messages, 2 )
+    # Following does not work in Python 3.6:
+    #     TypeError: __init__(): incompatible constructor arguments. The following argument types are supported:
+    #         1. pyrealsense2.pyrealsense2.log_severity(value: int)
+    #     Invoked with: -1
+    # I.e., instead of getting a RuntimeError from the C++, we get a TypeError from Python...
+    #test.check_throws( lambda: rs.log( rs.log_severity( -1 ), "-1" ), RuntimeError, 'invalid enum value for argument "severity"' )
+    test.check_equal( common.n_messages, 1 )
 except:
     test.unexpected_exception()
 
