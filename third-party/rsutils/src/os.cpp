45d1c2f63567f2a4ce3ec1d362d3aceb7618f681
Remi Bettan <remibettan@gmail.com>
2023-01-08 15:07:08 +0200

    opening the tegra file only once

--- third-party/rsutils/src/os.cpp
+++ third-party/rsutils/src/os.cpp
@@ -13,15 +13,22 @@ namespace rsutils {
 namespace os {
     bool is_platform_jetson()
     {
+        static bool is_jetson = false;
+        static bool is_first_time = true;
+        if (is_first_time)
+        {
+            is_first_time = false;
 #ifdef __linux__
-        // RAII to handle exceptions
-        std::unique_ptr<int, std::function<void(int*)> > fd(
-            new int(open("/etc/nv_tegra_release", O_RDONLY)),
-            [](int* d) { if (d && (*d)) { ::close(*d); } delete d; });
-        return (fd >= 0);
+            // RAII to handle exceptions
+            std::unique_ptr<int, std::function<void(int*)> > fd(
+                new int(open("/etc/nv_tegra_release", O_RDONLY)),
+                [](int* d) { if (d && (*d)) { ::close(*d); } delete d; });
+            is_jetson = (fd >= 0);
 #else
-        return false;
+            is_jetson = false;
 #endif
+        }
+        return is_jetson;
     }
 }  // namespace os
 }  // namespace rsutils
