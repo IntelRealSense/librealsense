565d93183b9cc38c6da549325f8ac4a23db162f3
Eran <librealsense.eran@gmail.com>
2023-02-20 09:17:07 +0200

    add test-stream-sensor-bridge

---
+++ unit-tests/dds/test-stream-sensor-bridge.py
@@ -0,0 +1,237 @@
+# License: Apache 2.0. See LICENSE file in root directory.
+# Copyright(c) 2023 Intel Corporation. All Rights Reserved.
+
+#test:donotrun:!dds
+
+import pyrealdds as dds
+from rspy import log, test
+dds.debug( True )
+from time import sleep
+
+participant = dds.participant()
+participant.init( 123, "test-stream-sensor-bridge" )
+
+# set up a server device with a bridge
+import d435i
+device_server = dds.device_server( participant, d435i.device_info.topic_root )
+servers = {}
+for server in d435i.build_streams():
+    servers[server.name()] = server
+bridge = dds.stream_sensor_bridge()
+server_list = list( servers.values() )
+bridge.init( server_list )
+device_server.init( server_list, d435i.build_options(), d435i.get_extrinsics() )
+active_sensors = dict()  # sensors that are open; name -> active profiles
+def on_start_sensor( sensor_name, profiles ):
+    log.d( f'starting {sensor_name} with {profiles}' )
+    global active_sensors
+    test.check( sensor_name not in active_sensors )
+    active_sensors[sensor_name] = profiles
+def on_stop_sensor( sensor_name ):
+    log.d( f'stopping {sensor_name}' )
+    global active_sensors
+    test.check( sensor_name in active_sensors )
+    del active_sensors[sensor_name]
+bridge.on_start_sensor( on_start_sensor )
+bridge.on_stop_sensor( on_stop_sensor )
+last_error = None
+def on_error( error_string ):
+    log.d( f'error on bridge: {error_string}' )  # not an error because it may be expected...
+    global last_error
+    last_error = error_string
+bridge.on_error( on_error )
+class bridge_error_expected:
+    def __init__( self, expected_msg ):
+        self._msg = expected_msg
+    def __enter__( self ):
+        global last_error
+        last_error = None
+    def __exit__( self, type, value, traceback ):
+        test.check_equal( last_error, self._msg )
+
+# It can take a while for servers to get the message that a reader is available... we need to wait for it
+import threading
+readers_changed = threading.Event()
+def on_readers_changed( server, n_readers ):
+    readers_changed.set()
+def detect_change():
+    readers_changed.clear()
+def wait_for_change( timeout = 1 ):  # seconds
+    if not readers_changed.wait( timeout ):
+        raise TimeoutError( 'timeout waiting for change' )
+bridge.on_readers_changed( on_readers_changed )
+class change_expected:
+    def __enter__( self ):
+        detect_change()
+    def __exit__( self, type, value, traceback ):
+        wait_for_change()
+
+# set up the client device and keep all its streams
+device = dds.device( participant, participant.create_guid(), d435i.device_info )
+device.run( 1000 )  # this will throw if something's wrong
+test.check( device.is_running() )
+streams = {}
+for stream in device.streams():
+    streams[stream.name()] = stream
+#def on_frame( f ):
+#    pass
+subscriber = dds.subscriber( participant )
+def start_stream( stream_name ):
+    log.i( 'starting', stream_name )
+    stream = streams[stream_name]
+    topic_name = 'rt/' + d435i.device_info.topic_root + '_' + stream_name
+    with change_expected():
+        stream.open( topic_name, subscriber )
+        #stream.start_streaming( on_frame )
+def stop_stream( stream_name ):
+    stream = streams[stream_name]
+    if stream.is_open():
+        log.i( 'stopping', stream_name )
+        #stream.stop_streaming()  # will throw if not streaming
+        with change_expected():
+            stream.close()
+def reset():
+    for stream in streams:
+        stop_stream( stream )
+    bridge.reset()
+    test.check_equal( len(active_sensors), 0 )
+
+
+# profile utilities
+def find_active_profile( stream_name ):
+    for active_profiles in active_sensors.values():
+        for profile in active_profiles:
+            if profile.stream().name() == stream_name:
+                return profile
+    raise KeyError( f"can't find '{sensor_name}' profile for stream '{stream_name}'" )
+def find_server_profile( stream_name, profile_string ):
+    by_string = f"<'{stream_name}' {profile_string}>"
+    for profile in servers[stream_name].profiles():
+        if profile.to_string() == by_string:
+            return profile
+    raise KeyError( f"can't find '{stream_name}' profile '{profile_string}'" )
+
+
+#############################################################################################
+#
+test.start( "sanity" )
+test.check_equal( len(active_sensors), 0 )
+test.finish()
+#
+#############################################################################################
+#
+test.start( "single stream, not streaming" )
+try:
+    bridge.open( servers['Color'].default_profile() )  # 1920x1080 RGB8 @ 30 Hz
+    test.check_equal( len(active_sensors), 0 )
+    bridge.commit()
+    test.check_equal( len(active_sensors), 0 )  # nothing streaming; no need to start a sensor
+except Exception:
+    test.unexpected_exception()
+finally:
+    reset()
+test.finish()
+#
+#############################################################################################
+#
+test.start( "single stream streaming default profile" )
+try:
+    start_stream( 'Color' )
+    test.check_equal( len(active_sensors), 1 )
+    stop_stream( 'Color' )
+    test.check_equal( len(active_sensors), 0 )
+except Exception:
+    test.unexpected_exception()
+finally:
+    reset()
+test.finish()
+#
+#############################################################################################
+#
+test.start( "multiple streams per sensor, all compatible" )
+try:
+    bridge.open( servers['Depth'].default_profile() )  # 1280x720 Z16 @ 30 Hz
+    bridge.commit()
+    test.check_equal( len(active_sensors), 0 )  # not streaming yet
+    start_stream( 'Depth' )
+    ( test.check_equal( len(active_sensors), 1 )
+        and test.check_equal( next(iter(active_sensors)), 'Stereo Module' )
+        and test.check_equal( len(active_sensors['Stereo Module']), 3 )  # Depth, IR1, IR2
+        )
+    # We're streaming Depth, but IR1 and IR2 are also open; if we try to open a profile on the server
+    # that's incompatible ...
+    test.check_throws( lambda: bridge.open( servers['Infrared 1'].default_profile() ),
+                       RuntimeError, "profile <'Infrared 1' 1280x800 GREY @ 30 Hz> is incompatible with already-open <'Depth' 1280x720 Z16 @ 30 Hz>" )
+    # ... but if the profile is compatible with what's already streaming:
+    bridge.open( find_active_profile( 'Infrared 1' ))
+    # We gave a stream streaming; reset shouldn't touch it
+    bridge.reset()
+    test.check_equal( len(active_sensors), 1 )
+    # Start another sensor while Depth is streaming
+    # NOTE we didn't open any profile so it should pick the default
+    start_stream( 'Color' )
+    test.check_equal( len(active_sensors), 2 )
+    test.check_equal( find_active_profile( 'Color' ).to_string(), servers['Color'].default_profile().to_string() )
+except Exception:
+    test.unexpected_exception()
+finally:
+    reset()
+test.finish()
+#
+#############################################################################################
+#
+test.start( "incompatible streams" )
+try:
+    bridge.open( servers['Infrared 1'].default_profile() )  # 1280x800 GREY @ 30 Hz
+    bridge.commit()
+    test.check_equal( len(active_sensors), 0 )  # not streaming yet
+    with bridge_error_expected( "failure trying to start/stop 'Depth': profile <'Depth' 1280x720 Z16 @ 30 Hz> is incompatible with already-open <'Infrared 1' 1280x800 GREY @ 30 Hz>" ):
+        start_stream( 'Depth' )  # no depth at 1280x800, so no stream!
+    test.check_equal( len(active_sensors), 0 )
+    test.check_throws( lambda: 
+        bridge.open( servers['Depth'].default_profile() ),
+        RuntimeError, "profile <'Depth' 1280x720 Z16 @ 30 Hz> is incompatible with already-open <'Infrared 1' 1280x800 GREY @ 30 Hz>" )
+    test.check_throws( lambda: 
+        bridge.open( find_server_profile( 'Infrared 2', '848x480 GREY @ 30 Hz' )),
+        RuntimeError, "profile <'Infrared 2' 848x480 GREY @ 30 Hz> is incompatible with already-open <'Infrared 1' 1280x800 GREY @ 30 Hz>" )
+    test.check_throws( lambda: 
+        bridge.open( find_server_profile( 'Infrared 2', '1280x800 GREY @ 15 Hz' )),
+        RuntimeError, "profile <'Infrared 2' 1280x800 GREY @ 15 Hz> is incompatible with already-open <'Infrared 1' 1280x800 GREY @ 30 Hz>" )
+    start_stream( 'Infrared 2' )
+    ( test.check_equal( len(active_sensors), 1 )
+        and test.check_equal( next(iter(active_sensors)), 'Stereo Module' )
+        and test.check_equal( len(active_sensors['Stereo Module']), 2 )  # IR1, IR2
+        )
+    test.check_throws( lambda: 
+        bridge.open( servers['Depth'].default_profile() ),
+        RuntimeError, "stream 'Depth' was not opened by the sensor" )
+except Exception:
+    test.unexpected_exception()
+finally:
+    reset()
+test.finish()
+#
+#############################################################################################
+#
+test.start( "open and close" )
+try:
+    bridge.open( servers['Infrared 1'].default_profile() )  # 1280x800 GREY @ 30 Hz
+    bridge.open( servers['Infrared 1'].default_profile() )  # "compatible"
+    bridge.close( servers['Infrared 1'] )
+    bridge.open( find_server_profile( 'Infrared 1', '1280x800 GREY @ 30 Hz' ))
+    bridge.close( servers['Infrared 1'] )
+    bridge.close( servers['Infrared 1'] )
+    bridge.open( find_server_profile( 'Infrared 1', '1280x800 Y16 @ 25 Hz' ))
+    bridge.reset()
+    bridge.open( find_server_profile( 'Infrared 1', '1280x800 Y16 @ 15 Hz' ))
+    test.check_throws( lambda:
+        bridge.open( find_server_profile( 'Infrared 1', '1280x800 Y16 @ 25 Hz' )),
+        RuntimeError, "profile <'Infrared 1' 1280x800 Y16 @ 25 Hz> is incompatible with already-open <'Infrared 1' 1280x800 Y16 @ 15 Hz>" )
+except Exception:
+    test.unexpected_exception()
+finally:
+    reset()
+test.finish()
+#
+#############################################################################################
+test.print_results_and_exit()
