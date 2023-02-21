925954845c5a86f16f4f2a8b88827943b07d6f3f
Tamir <1hotlev@gmail.com>
2023-02-21 09:45:17 +0200

    the policy moved to opengl_config.cmake

--- CMake/opengl_config.cmake
+++ CMake/opengl_config.cmake
@@ -1,2 +1,7 @@
+# Comment why we are doing this
+if (POLICY CMP0072)
+    cmake_policy(SET CMP0072 NEW)
+endif()
+
 find_package(OpenGL REQUIRED)
 set(DEPENDENCIES realsense2 glfw ${OPENGL_LIBRARIES})
