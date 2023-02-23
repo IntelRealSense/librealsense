ecd14ab794d90b68d0eb8fc1643b95b6305c6044
Eran <librealsense.eran@gmail.com>
2023-02-23 08:59:19 +0200

    add motion tests; improve error-handling

--- unit-tests/dds/test-stream-sensor-bridge.py
+++ unit-tests/dds/test-stream-sensor-bridge.py
@@ -22,6 +22,7 @@ server_list = list( servers.values() )
 bridge.init( server_list )
 device_server.init( server_list, d435i.build_options(), d435i.get_extrinsics() )
 active_sensors = dict()  # sensors that are open; name -> active profiles
+
 def on_start_sensor( sensor_name, profiles ):
     log.d( f'starting {sensor_name} with {profiles}' )
     global active_sensors
@@ -34,14 +35,17 @@ def on_stop_sensor( sensor_name ):
     del active_sensors[sensor_name]
 bridge.on_start_sensor( on_start_sensor )
 bridge.on_stop_sensor( on_stop_sensor )
+
 last_error = None
 _bridge_error_expected = False
+
 def on_error( error_string ):
-    log.d( f'error on bridge: {error_string}' )  # not an error because it may be expected...
     global last_error
     last_error = error_string
-    test.check( _bridge_error_expected )
+    if test.check( _bridge_error_expected, description = f'Unexpected error on bridge: {error_string}' ):
+        log.d( f'error on bridge: {error_string}' )  # not an error because it may be expected...
 bridge.on_error( on_error )
+
 class bridge_error_expected:
     def __init__( self, expected_msg ):
         self._msg = expected_msg
@@ -51,30 +55,35 @@ class bridge_error_expected:
         global last_error
         last_error = None
     def __exit__( self, type, value, traceback ):
-        if not last_error:
-            log.e( 'Expected an error on bridge but got none' )
-            test.check_failed()
-        else:
-            test.check_equal( last_error, self._msg )
+        if type is None:  # If an exception is thrown, don't check anything
+            if test.check( last_error is not None, description = 'Expected an error on bridge but got none' ):
+                test.check_equal( last_error, self._msg )
         global _bridge_error_expected
         _bridge_error_expected = False
 
 # It can take a while for servers to get the message that a reader is available... we need to wait for it
 import threading
 readers_changed = threading.Event()
+
 def on_readers_changed( server, n_readers ):
     readers_changed.set()
+
 def detect_change():
     readers_changed.clear()
+
 def wait_for_change( timeout = 1 ):  # seconds
     if not readers_changed.wait( timeout ):
         raise TimeoutError( 'timeout waiting for change' )
+
 bridge.on_readers_changed( on_readers_changed )
+
 class change_expected:
     def __enter__( self ):
         detect_change()
     def __exit__( self, type, value, traceback ):
-        wait_for_change()
+        log.d( f'{self=}, {type=}, {value=}, {traceback=}' )
+        if type is None:  # If an exception is thrown, don't do anything
+            wait_for_change()
 
 # set up the client device and keep all its streams
 device = dds.device( participant, participant.create_guid(), d435i.device_info )
@@ -83,9 +92,8 @@ test.check( device.is_running() )
 streams = {}
 for stream in device.streams():
     streams[stream.name()] = stream
-#def on_frame( f ):
-#    pass
 subscriber = dds.subscriber( participant )
+
 def start_stream( stream_name ):
     log.i( 'starting', stream_name )
     stream = streams[stream_name]
@@ -93,6 +101,7 @@ def start_stream( stream_name ):
     with change_expected():
         stream.open( topic_name, subscriber )
         #stream.start_streaming( on_frame )
+
 def stop_stream( stream_name ):
     stream = streams[stream_name]
     if stream.is_open():
@@ -100,6 +109,7 @@ def stop_stream( stream_name ):
         #stream.stop_streaming()  # will throw if not streaming
         with change_expected():
             stream.close()
+
 def reset():
     for stream in streams:
         stop_stream( stream )
@@ -108,12 +118,14 @@ def reset():
 
 
 # profile utilities
+
 def find_active_profile( stream_name ):
     for active_profiles in active_sensors.values():
         for profile in active_profiles:
             if profile.stream().name() == stream_name:
                 return profile
     raise KeyError( f"can't find '{sensor_name}' profile for stream '{stream_name}'" )
+
 def find_server_profile( stream_name, profile_string ):
     by_string = f"<'{stream_name}' {profile_string}>"
     for profile in servers[stream_name].profiles():
@@ -276,6 +288,53 @@ try:
     start_stream( 'Color' )
     test.check_equal( len(active_sensors), 2 )
     test.check_equal( find_active_profile( 'Color' ).to_string(), servers['Color'].default_profile().to_string() )
+except Exception:
+    test.unexpected_exception()
+finally:
+    reset()
+test.finish()
+#
+#############################################################################################
+#
+test.start( "motion module" )
+try:
+    start_stream( 'Accel' )  # MXYZ @ 200 Hz
+    test.check_equal( len(active_sensors), 1 )
+    start_stream( 'Gyro' )
+    test.check_equal( len(active_sensors), 1 )  # same sensor
+    stop_stream( 'Accel' )
+    test.check_equal( len(active_sensors), 1 )
+    stop_stream( 'Gyro' )
+    test.check_equal( len(active_sensors), 0 )
+
+    bridge.open( servers['Gyro'].default_profile() )  # MXYZ @ 400 Hz
+    # Gyro has no 400 Hz
+    with bridge_error_expected( "failure trying to start/stop 'Accel': profile <'Accel' MXYZ @ 200 Hz> is incompatible with already-open <'Gyro' MXYZ @ 400 Hz>" ):
+        start_stream( 'Accel' )
+    # Note that while the stream shouldn't be streaming because the _SERVER_ failed, the stream still
+    # has the reader open and therefore we still think is streaming...! This requires handling on
+    # the client-side (device needs some kind of on-error callback)
+    test.check_throws( lambda:
+        start_stream( 'Accel' ),
+        RuntimeError, "stream 'Accel' is already open" )
+except Exception:
+    test.unexpected_exception()
+finally:
+    reset()
+test.finish()
+#
+#############################################################################################
+#
+test.start( "motion + color" )
+try:
+    start_stream( 'Gyro' )  # MXYZ @ 400 Hz
+    test.check_equal( len(active_sensors), 1 )
+    start_stream( 'Color' )
+    test.check_equal( len(active_sensors), 2 )
+    stop_stream( 'Gyro' )
+    test.check_equal( len(active_sensors), 1 )
+    stop_stream( 'Color' )
+    test.check_equal( len(active_sensors), 0 )
 except Exception:
     test.unexpected_exception()
 finally:
