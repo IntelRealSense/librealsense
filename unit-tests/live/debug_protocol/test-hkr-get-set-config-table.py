a4fd35f1cf74694257c0cdc716411567e42d3ac6
Remi Bettan <remibettan@gmail.com>
2023-03-19 11:45:31 +0200

    test updated

--- unit-tests/live/debug_protocol/test-hkr-get-set-config-table.py
+++ unit-tests/live/debug_protocol/test-hkr-get-set-config-table.py
@@ -7,13 +7,14 @@
 import pyrealsense2 as rs
 from rspy import test
 
+ctx = rs.context()
+dev = test.find_first_device_or_exit()
+deb = dev.as_debug_protocol()
+
+
 #############################################################################################
 
 test.start("Get ds5 standard buffer")
-
-ctx = rs.context()
-dev = ctx.query_devices()[0]
-deb = dev.as_debug_protocol()
 
 # getting gvd
 gvd_opcode = 0x10
@@ -30,10 +31,6 @@ test.finish()
 
 test.start("Get buffer less than 1 KB")
 
-ctx = rs.context()
-dev = ctx.query_devices()[0]
-deb = dev.as_debug_protocol()
-
 # getting depth_calibration_table - size is 512
 depth_calib_table_id = 0xb4
 depth_calib_table_size = 512
@@ -48,11 +45,7 @@ test.finish()
 
 #############################################################################################
 
-test.start("Get buffer more than 1 KB")
-
-ctx = rs.context()
-dev = ctx.query_devices()[0]
-deb = dev.as_debug_protocol()
+test.start("Get buffer more than 1 KB - getting the whole table at once")
 
 # getting rgb_lens_shading table - size is 1088
 rgb_lens_shading_table_id = 0xb2
@@ -65,6 +58,29 @@ test.check_equal(len(ans), rgb_lens_shading_table_size + 4)
 
 test.finish()
 
+#############################################################################################
+
+test.start("Get buffer more than 1 KB - getting the table chunk by chunk")
+
+# getting rgb_lens_shading table - size is 1088
+rgb_lens_shading_table_id = 0xb2
+rgb_lens_shading_table_size = 1088
+get_hkr_config_table_opcode = 0xa7
+first_chunk_from_two_param = 0x10000
+cmd1 = deb.build_command(opcode=get_hkr_config_table_opcode, param1=0, param2=rgb_lens_shading_table_id, param3=0, param4=first_chunk_from_two_param)
+ans1 = deb.send_and_receive_raw_data(cmd1)
+test.check_equal(ans1[0], get_hkr_config_table_opcode)
+
+second_chunk_from_two_param = 0x10001
+cmd2 = deb.build_command(opcode=get_hkr_config_table_opcode, param1=0, param2=rgb_lens_shading_table_id, param3=0, param4=second_chunk_from_two_param)
+ans2 = deb.send_and_receive_raw_data(cmd2)
+test.check_equal(ans2[0], get_hkr_config_table_opcode)
+
+ans = ans1 + ans2
+twice_opcode_length = 8
+test.check_equal(len(ans), rgb_lens_shading_table_size + twice_opcode_length)
+
+test.finish()
 #############################################################################################
 
 test.print_results_and_exit()
