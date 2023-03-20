933b082bea14673be4fd90ecb9cf30e0e602d5d5
Remi Bettan <remibettan@gmail.com>
2023-03-20 12:08:01 +0200

    adding depth_mapping streams

--- src/ds/d500/d500-factory.cpp
+++ src/ds/d500/d500-factory.cpp
@@ -248,7 +248,8 @@ namespace librealsense
 
     std::shared_ptr<matcher> rs_d585s_device::create_matcher(const frame_holder& frame) const
     {
-        std::vector<stream_interface*> streams = { _depth_stream.get() , _left_ir_stream.get() , _right_ir_stream.get(), _color_stream.get(), _safety_stream.get()};
+        std::vector<stream_interface*> streams = { _depth_stream.get() , _left_ir_stream.get() , _right_ir_stream.get(), _color_stream.get(), 
+            _safety_stream.get(), _occupancy_stream.get(), _point_cloud_stream.get()};
         std::vector<stream_interface*> mm_streams = { _ds_motion_common->get_accel_stream().get(),
                                                       _ds_motion_common->get_gyro_stream().get() };
         streams.insert(streams.end(), mm_streams.begin(), mm_streams.end());
