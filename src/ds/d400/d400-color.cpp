a11548438c441b6176f5c36dc06f3873d1891fa2
Remi Bettan <remibettan@gmail.com>
2023-03-20 16:36:54 +0200

    correction to avoid failure with d457

--- src/ds/d400/d400-color.cpp
+++ src/ds/d400/d400-color.cpp
@@ -142,9 +142,8 @@ namespace librealsense
         if (!val_in_range(_pid, { ds::RS457_PID }))
         {
             _ds_color_common->register_color_options();
+            color_ep.register_pu(RS2_OPTION_GAMMA);
         }
-
-        color_ep.register_pu(RS2_OPTION_GAMMA);
 
         if (_separate_color)
         {
