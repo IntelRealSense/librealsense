631ee83ca0f03cc8018dfdbb27cbb120daba2460
Eran <librealsense.eran@gmail.com>
2023-04-08 18:59:57 +0300

    revised unit-tests/dds/test-watcher.py

--- unit-tests/dds/watcher-server.py
+++ unit-tests/dds/watcher-server.py
@@ -1,46 +1,36 @@
 # License: Apache 2.0. See LICENSE file in root directory.
 # Copyright(c) 2022 Intel Corporation. All Rights Reserved.
 
-import pyrealdds as server
+import pyrealdds as dds
 from rspy import log, test
 
 
-server.debug( True, log.nested )
+dds.debug( True, log.nested )
 
 
-#############################################################################################
-#
-test.start( "broadcast two devices" )
-
-participant = server.participant()
-participant.init( 123, "test-watcher-server" )
+participant = dds.participant()
+participant.init( 123, "server" )
 test.check( participant.is_valid() )
 
-publisher = server.publisher( participant )
+publisher = dds.publisher( participant )
 
-di = server.device_info()
-di.name = 'my device'
-di.serial = '12345'
-di.product_line = 'PL'
-di.topic_root = 'testing/my/device/sn'
-broadcaster1 = server.device_broadcaster( publisher, di )
+broadcasters = []
 
-from time import sleep
-sleep(1)  # give the client some time with it
 
-di = server.device_info()
-di.name = 'another device'
-di.serial = '67890'
-di.product_line = 'PL'
-di.topic_root = 'testing/my/device2/sn'
-broadcaster2 = server.device_broadcaster( publisher, di )
+def broadcast( props ):
+    global broadcasters, publisher
+    di = dds.device_info()
+    di.serial = props.get( 'serial', str(len(broadcasters)) )
+    di.name = props.get( 'name', f'device{di.serial}' )
+    di.product_line = props.get( 'product_line', '' )
+    di.topic_root = props.get( 'topic_root', f'path/to/{di.name}' )
+    broadcasters.append( dds.device_broadcaster( publisher, di ) )
 
-from time import sleep
-sleep(1)  # give the client some time with it
 
-broadcaster1 = broadcaster2 = None
-participant = None
-test.finish()
-#
-#############################################################################################
-test.print_results_and_exit()
+def unbroadcast_all():
+    global broadcasters
+    broadcasters = []
+
+
+# From here down, we're in "interactive" mode (see test-watcher.py)
+# ...
