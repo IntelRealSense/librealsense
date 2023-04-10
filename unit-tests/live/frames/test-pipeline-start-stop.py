76ec09f56013104d5b933a5ee73ef7989da8ce36
Nir Azkiel <nir.azkiel@intel.com>
2023-04-10 16:41:46 +0300

    comment update

--- unit-tests/live/frames/test-pipeline-start-stop.py
+++ unit-tests/live/frames/test-pipeline-start-stop.py
@@ -6,23 +6,16 @@
 import pyrealsense2 as rs
 from rspy.stopwatch import Stopwatch
 from rspy import test, log
-import time
-import platform
 
+# Run multiple start stop of all streams and verify we get a frame for each once
 
-# Start depth + color streams and measure the time from stream opened until first frame arrived using pipeline API.
-# Verify that the time does not exceed the maximum time allowed
-# Note - Using Windows Media Foundation to handle power management between USB actions take time (~27 ms)
-
-
-# Set maximum delay for first frame according to product line
 dev = test.find_first_device_or_exit()
 
 def verify_frame_received(config):
     pipe = rs.pipeline()
     start_call_stopwatch = Stopwatch()
     pipe.start(config)
-    # wait_for_frames will through if no frames received so no assert is needed
+    # wait_for_frames will throw if no frames received so no assert is needed
     f = pipe.wait_for_frames()
     delay = start_call_stopwatch.get_elapsed()
     log.out("After ", delay, " [sec] got first frame of ", f)
