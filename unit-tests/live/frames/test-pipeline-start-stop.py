faf8bbba8e3c8167518142fa9704bc23d15cf78a
Nir Azkiel <nir.azkiel@intel.com>
2023-04-13 08:36:37 +0300

    exclude D457 from start stop stress test

--- unit-tests/live/frames/test-pipeline-start-stop.py
+++ unit-tests/live/frames/test-pipeline-start-stop.py
@@ -1,7 +1,8 @@
 # License: Apache 2.0. See LICENSE file in root directory.
 # Copyright(c) 2023 Intel Corporation. All Rights Reserved.
 
-# test:device each(D400*)
+# Currently, we exclude D457 as it's failing
+# test:device each(D400*) !D457
 
 import pyrealsense2 as rs
 from rspy.stopwatch import Stopwatch
