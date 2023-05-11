19d0d725093b11f32147404f561b84a4d481cb55
Tamir <1hotlev@gmail.com>
2023-05-11 14:12:08 +0300

    comment changed

--- unit-tests/unit-test-config.py
+++ unit-tests/unit-test-config.py
@@ -85,7 +85,7 @@ def add_slash_before_spaces(links):
         return links.replace(' ', '\ ')
     if links and type(links) is list:
         # Build list comprehension of strings with backslash before spaces in case the link.
-        # In case we have Cmake variable we are not perform changes.
+        # We can get a ${var} so, when we get this we return as is
         return [link.replace(' ', '\ ') if link[0] != '$' else link for link in links]
     else:
         raise TypeError
