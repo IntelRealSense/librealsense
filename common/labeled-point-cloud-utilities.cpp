8869507812a9090c24e966b8228c81300c755daf
Remi Bettan <remibettan@gmail.com>
2023-05-23 15:53:48 +0300

    adding label - cropped -

--- common/labeled-point-cloud-utilities.cpp
+++ common/labeled-point-cloud-utilities.cpp
@@ -14,6 +14,7 @@ namespace rs2
 {
     std::map<rs2_point_cloud_label, float3> labeled_point_cloud_utilities::get_label_to_color3f()
     {
+        static const float3 ORANGE_COL = { 1.0f, 0.65f, 0.2f };
         static const float3 DARK_PURPLE_COL = { 0.2f, 0.1f, 0.2f };
         static const float3 BROWN_COL = { 0.6f, 0.3f, 0.0f };
         static const float3 GREEN_COL = { 0.0f, 1.0f, 0.0f };
@@ -22,9 +23,10 @@ namespace rs2
         static const float3 BLUE_COL = { 0.0f, 0.0f, 1.0f };
         static const float3 PINK_COL = { 1.0f, 0.75f, 0.8f };
         static const float3 GREY_COL = { 0.5f, 0.5f, 0.5f };
+        static const float3 YELLOW_COL = { 1.0f, 1.0f, 0.0f };
         std::map<rs2_point_cloud_label, float3> label_to_color3f;
 
-        label_to_color3f[RS2_POINT_CLOUD_LABEL_UNKNOWN] = DARK_PURPLE_COL;
+        label_to_color3f[RS2_POINT_CLOUD_LABEL_UNKNOWN] = ORANGE_COL;
         label_to_color3f[RS2_POINT_CLOUD_LABEL_UNDEFINED] = DARK_PURPLE_COL;
         label_to_color3f[RS2_POINT_CLOUD_LABEL_INVALID] = DARK_PURPLE_COL;
         label_to_color3f[RS2_POINT_CLOUD_LABEL_GROUND] = BROWN_COL;
@@ -34,6 +36,7 @@ namespace rs2
         label_to_color3f[RS2_POINT_CLOUD_LABEL_ABOVE_CEILING_HEIGHT] = BLUE_COL;
         label_to_color3f[RS2_POINT_CLOUD_LABEL_GAP] = PINK_COL;
         label_to_color3f[RS2_POINT_CLOUD_LABEL_MASKED] = GREY_COL;
+        label_to_color3f[RS2_POINT_CLOUD_LABEL_CROPPED] = YELLOW_COL;
 
         return label_to_color3f;
     }
