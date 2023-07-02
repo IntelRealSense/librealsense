65b8dc8e3c7476c1c6738b91c540af851372ec3d
Remi Bettan <remibettan@gmail.com>
2023-07-02 08:51:32 +0300

    real required gvd size updated

--- unit-tests/live/d500/test-get-set-config-table.py
+++ unit-tests/live/d500/test-get-set-config-table.py
@@ -18,8 +18,7 @@ test.start("Get ds5 standard buffer")
 
 # getting gvd
 gvd_opcode = 0x10
-# size should be 602 - to be checked
-gvd_size = 606
+gvd_size = 602
 cmd = dp_device.build_command(opcode=gvd_opcode)
 ans = dp_device.send_and_receive_raw_data(cmd)
 
