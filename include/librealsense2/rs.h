9ce7fded26d9ce8a51887dc9d5c30f06f4e713a5
Eran <librealsense.eran@gmail.com>
2023-07-27 10:16:54 +0300

    bump to 2.54.2

--- include/librealsense2/rs.h
+++ include/librealsense2/rs.h
@@ -25,7 +25,7 @@ extern "C" {
 
 #define RS2_API_MAJOR_VERSION    2
 #define RS2_API_MINOR_VERSION    54
-#define RS2_API_PATCH_VERSION    1
+#define RS2_API_PATCH_VERSION    2
 #define RS2_API_BUILD_VERSION    0
 
 #ifndef STRINGIFY
