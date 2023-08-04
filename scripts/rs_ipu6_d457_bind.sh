46125395eabb06cffb596ef55932f3207f9001b7
Dmitry Perchanov <dmitry.perchanov@intel.com>
2023-08-04 09:42:16 +0300

    scripts: ipu6 aggregated camera support.
    Signed-off-by: Dmitry Perchanov <dmitry.perchanov@intel.com>

--- scripts/rs_ipu6_d457_bind.sh
+++ scripts/rs_ipu6_d457_bind.sh
@@ -8,6 +8,7 @@ if [ -z ${v4l2_util} ]; then
 fi
 
 # d457_bind.sh
+metadata_enabled=1
 while [[ $# -gt 0 ]]; do
   case $1 in
     -q|--quiet)
@@ -19,6 +20,10 @@ while [[ $# -gt 0 ]]; do
       mux_param=$1
       shift
     ;;
+    -n|--no-metadata)
+      metadata_enabled=0
+      shift
+    ;;
     *)
       quiet=0
       shift
@@ -26,11 +31,12 @@ while [[ $# -gt 0 ]]; do
     esac
 done
 
-declare -A media_mux_capture_link=( [a]='' [b]='1 ' [c]='2 ' [d]='3 ' )
-declare -A media_mux_csi2_link=( [a]=0 [b]=1 [c]=2 [d]=3 )
+declare -A media_mux_capture_link=( [a]='' [b]='1 ' [c]='2 ' [d]='3 ' [e]='' [f]='1 ' [g]='2 ' [h]='3 ')
+declare -A media_mux_csi2_link=( [a]=0 [b]=1 [c]=2 [d]=3 [e]=0 [f]=1 [g]=2 [h]=3)
 
-declare -A serdes_mux_i2c_bus=( [a]=2 [b]=2 [c]=4 [d]=4 )
-mux_list=${mux_param:-'a b c d'}
+declare -A media_mux_capture_pad=( [a]=0 [b]=1 [c]=2 [d]=3 [e]=0 [f]=1 [g]=2 [h]=3)
+
+mux_list=${mux_param:-'a b c d e f g h'}
 
 mdev=$(${v4l2_util} --list-devices | grep -A1 ipu6 | grep media)
 [[ -z "${mdev}" ]] && exit 0
@@ -52,27 +58,35 @@ csi2="CSI-2 ${media_mux_csi2_link[${mux}]}"
 
 be_soc_cap="BE SOC ${media_mux_capture_link[${mux}]}capture"
 
+cap_pad=0
 $media_ctl_cmd -v -l "\"Intel IPU6 ${csi2}\":1 -> \"Intel IPU6 ${csi2_be_soc}\":0[1]" 1>/dev/null
 $media_ctl_cmd -v -l "\"DS5 mux ${mux}\":0 -> \"Intel IPU6 ${csi2}\":0[1]" 1>/dev/null
 
-$media_ctl_cmd -v -l "\"D4XX depth ${mux}\":0 -> \"DS5 mux ${mux}\":1[1]" 1>/dev/null
-# DEPTH video streaming node
-$media_ctl_cmd -v -l "\"Intel IPU6 ${csi2_be_soc}\":1 -> \"Intel IPU6 ${be_soc_cap} 0\":0[5]" 1>/dev/null
-# DEPTH metadata node
-$media_ctl_cmd -v -l "\"Intel IPU6 ${csi2_be_soc}\":2 -> \"Intel IPU6 ${be_soc_cap} 1\":0[5]" 1>/dev/null
 
-$media_ctl_cmd -v -l "\"D4XX rgb ${mux}\":0 -> \"DS5 mux ${mux}\":2[1]" 1>/dev/null
+cap_pad=0
+if [ "${mux}" \> "d" ]; then
+	cap_pad=6
+fi
+
+# DEPTH video streaming node
+$media_ctl_cmd -v -l "\"Intel IPU6 ${csi2_be_soc}\":$((${cap_pad}+1)) -> \"Intel IPU6 ${be_soc_cap} $((${cap_pad}+0))\":0[5]" 1>/dev/null
+# DEPTH metadata node
+if [[ $metadata_enabled -eq 1 ]]; then
+  $media_ctl_cmd -v -l "\"Intel IPU6 ${csi2_be_soc}\":$((${cap_pad}+2)) -> \"Intel IPU6 ${be_soc_cap} $((${cap_pad}+1))\":0[5]" 1>/dev/null
+fi
+
 # RGB link
-$media_ctl_cmd -v -l "\"Intel IPU6 ${csi2_be_soc}\":3 -> \"Intel IPU6 ${be_soc_cap} 2\":0[5]" 1>/dev/null
+$media_ctl_cmd -v -l "\"Intel IPU6 ${csi2_be_soc}\":$((${cap_pad}+3)) -> \"Intel IPU6 ${be_soc_cap} $((${cap_pad}+2))\":0[5]" 1>/dev/null
 # RGB metadata node
-$media_ctl_cmd -v -l "\"Intel IPU6 ${csi2_be_soc}\":4 -> \"Intel IPU6 ${be_soc_cap} 3\":0[5]" 1>/dev/null
+if [[ $metadata_enabled -eq 1 ]]; then
+  $media_ctl_cmd -v -l "\"Intel IPU6 ${csi2_be_soc}\":$((${cap_pad}+4)) -> \"Intel IPU6 ${be_soc_cap} $((${cap_pad}+3))\":0[5]" 1>/dev/null
+fi
 
 # IR link
-$media_ctl_cmd -v -l "\"D4XX motion detection ${mux}\":0 -> \"DS5 mux ${mux}\":3[1]" 1>/dev/null
-$media_ctl_cmd -v -l "\"Intel IPU6 ${csi2_be_soc}\":5 -> \"Intel IPU6 ${be_soc_cap} 4\":0[5]" 1>/dev/null
+$media_ctl_cmd -v -l "\"Intel IPU6 ${csi2_be_soc}\":$((${cap_pad}+5)) -> \"Intel IPU6 ${be_soc_cap} $((${cap_pad}+4))\":0[5]" 1>/dev/null
 
 # IMU link
-$media_ctl_cmd -v -l "\"D4XX imu ${mux}\":0 -> \"DS5 mux ${mux}\":4[1]" 1>/dev/null
-$media_ctl_cmd -v -l "\"Intel IPU6 ${csi2_be_soc}\":6 -> \"Intel IPU6 ${be_soc_cap} 5\":0[5]" 1>/dev/null
+$media_ctl_cmd -v -l "\"Intel IPU6 ${csi2_be_soc}\":$((${cap_pad}+6)) -> \"Intel IPU6 ${be_soc_cap} $((${cap_pad}+5))\":0[5]" 1>/dev/null
+
 [[ $quiet -eq 0 ]] && echo done
 done
