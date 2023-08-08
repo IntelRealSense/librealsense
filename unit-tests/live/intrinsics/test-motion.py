32c498fa3b108047ab63a36bdb33bd3844d154f6
Tamir <1hotlev@gmail.com>
2023-08-08 08:57:06 +0300

    small fixes

--- unit-tests/live/intrinsics/test-motion.py
+++ unit-tests/live/intrinsics/test-motion.py
@@ -7,42 +7,34 @@
 import pyrealsense2 as rs
 from rspy import test, log
 
+# This test check existence motion intrinsic data in accel and gyro profiles.
+
 device = test.find_first_device_or_exit()
 motion_sensor = device.first_motion_sensor()
 
-# Test #1
-test.start('Checking for the existence of motion sensor:')
+test.start('Check intrinsics in motion sensor:')
+
 test.check(motion_sensor)
-test.finish()
 
 if motion_sensor:
 
     motion_profile_accel = next(p for p in motion_sensor.profiles if p.stream_type() == rs.stream.accel)
     motion_profile_gyro = next(p for p in motion_sensor.profiles if p.stream_type() == rs.stream.gyro)
 
-    # Test #2
-    test.start('Checking for the existence of motion profiles - ACCEL and GYRO:')
     test.check(motion_profile_accel and motion_profile_gyro)
-    test.finish()
 
     if motion_profile_accel:
         motion_profile_accel = motion_profile_accel.as_motion_stream_profile()
         intrinsics_accel = motion_profile_accel.get_motion_intrinsics()
 
-        # Test #3
-        test.start('Check intrinsics in motion sensor - ACCEL profile:')
-        test.check(len(str(intrinsics_accel)) > 0)  # Checking if intrinsics has data
         log.d(str(intrinsics_accel))
-        test.finish(str(intrinsics_accel))
+        test.check(len(str(intrinsics_accel)) > 0)  # Checking if intrinsics has data
 
     if motion_profile_gyro:
         motion_profile_gyro = motion_profile_gyro.as_motion_stream_profile()
         intrinsics_gyro = motion_profile_gyro.get_motion_intrinsics()
 
-        # Test #4
-        test.start('Check intrinsics in motion sensor - GYRO profile:')
-        test.check(len(str(intrinsics_gyro)) > 0)  # Checking if intrinsics has data
         log.d(intrinsics_gyro)
-        test.finish()
+        test.check(len(str(intrinsics_gyro)) > 0)  # Checking if intrinsics has data
 
     test.print_results_and_exit()
