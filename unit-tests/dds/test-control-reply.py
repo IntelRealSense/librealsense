85b03c2797bd474d3fd9baf7b1174d073046b203
Eran <librealsense.eran@gmail.com>
2023-09-07 09:50:12 +0300

    add test-control-reply

--- unit-tests/dds/test-control-reply.py
+++ unit-tests/dds/test-control-reply.py
@@ -2,7 +2,7 @@
 # Copyright(c) 2023 Intel Corporation. All Rights Reserved.
 
 #test:donotrun:!dds
-#test:retries 2
+#xxtest:retries 2
 
 from rspy import log, test
 import pyrealdds as dds
@@ -37,10 +37,7 @@ with test.remote.fork( nested_indent=None ) as remote:
                 return True  # otherwise the control will be flagged as error
             server.on_control( _on_control )
 
-        with test.closure( 'Broadcast the device', on_fail=test.ABORT ):
-            server.broadcast( device_info )
-
-        raise StopIteration()  # quit the remote
+        raise StopIteration()  # exit the 'with' statement
 
 
     ###############################################################################################################
@@ -56,28 +53,84 @@ with test.remote.fork( nested_indent=None ) as remote:
         info = dds.message.device_info()
         info.name = 'Server Device'
         info.topic_root = 'control-reply/device'
-        device = dds.device( participant, participant.create_guid(), info )
-        device.wait_until_ready()  # If unavailable before timeout, this throws
+        device = dds.device( participant, info )
+        device.wait_until_ready()
 
     with test.closure( 'Set up a notification handler' ):
         n_replies = 0
+        notification_count = dict()
+        reply_count = dict()
+        notification_count[device.guid()] = 0
+        reply_count[device.guid()] = 0
+        import threading
+        have_reply = threading.Event()
         def _on_notification( device, id, notification ):
+            global notification_count, have_reply
+            have_reply.set()
+            notification_count[device.guid()] += 1
+            sample = notification.get( 'sample' )
+            if sample is None:
+                log.d( f'notification to {device}: {notification}' )
+            elif sample[0] == str(device.guid()):
+                log.d( f'reply to {device}' )
+                reply_count[device.guid()] += 1
+            else:
+                log.d( f'notification to {device}' )
             return True  # otherwise the notification will be flagged as unhandled
         device.on_notification( _on_notification )
 
+    with test.closure( 'Send a notification that is not a reply' ):
+        dev1_notifications = notification_count[device.guid()]
+        dev1_replies = reply_count[device.guid()]
+        have_reply.clear()
+        remote.run( 'server.publish_notification( { "id": "something" } )' )
+        have_reply.wait( 3 )
+        test.check_equal( notification_count[device.guid()], dev1_notifications + 1 )  # notification
+        test.check_equal( reply_count[device.guid()], dev1_replies )                   # not a reply
+
     wait_for_reply = True
-    sequence = 0
-    def send( json ):
-        global sequence
-        sequence += 1
+    server_sequence = 0
+    def control( device, json ):
+        global server_sequence
+        server_sequence += 1
         reply = device.send_control( json, wait_for_reply )
         test.check_equal( reply['id'], json['id'] )
-        test.check_equal( reply['sequence'], sequence )
+        if test.check( reply.get('control') is not None ):
+            test.check_equal( reply['control']['id'], json['id'] )
+        test.check_equal( reply['sequence'], server_sequence )
+        test.check_equal( reply['sample'][0], str(device.guid()) )
         return reply
 
     with test.closure( 'Send some controls' ):
-        send( { 'id': 'control' } )
-        send( { 'id': 'control-2' } )
+        control( device, { 'id': 'control' } )
+        control( device, { 'id': 'control-2' } )
+
+    with test.closure( 'Add a second device!' ):
+        device2 = dds.device( participant, info )
+        notification_count[device2.guid()] = 0
+        reply_count[device2.guid()] = 0
+        device2.on_notification( _on_notification )
+        device2.wait_until_ready()
+
+    with test.closure( 'Controls generate notifications to all devices' ):
+        dev1_notifications = notification_count[device.guid()]
+        dev2_notifications = notification_count[device2.guid()]
+        control( device, { 'id': 'dev1' } )
+        test.check_equal( notification_count[device.guid()], dev1_notifications + 1 )
+        test.check_equal( notification_count[device2.guid()], dev2_notifications + 1 )  # both get notifications
+        control( device2, { 'id': 'dev2' } )
+        test.check_equal( notification_count[device.guid()], dev1_notifications + 2 )
+        test.check_equal( notification_count[device2.guid()], dev2_notifications + 2 )
+
+    with test.closure( 'But only one gets a reply' ):
+        dev1_replies = reply_count[device.guid()]
+        dev2_replies = reply_count[device2.guid()]
+        control( device, { 'id': 'dev1' } )
+        test.check_equal( reply_count[device.guid()], dev1_replies + 1 )
+        test.check_equal( reply_count[device2.guid()], dev2_replies )
+        control( device2, { 'id': 'dev2' } )
+        test.check_equal( reply_count[device.guid()], dev1_replies + 1 )
+        test.check_equal( reply_count[device2.guid()], dev2_replies + 1 )
 
     device = None
 
