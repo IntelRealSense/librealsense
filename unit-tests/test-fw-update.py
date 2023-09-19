774f11914e2de90952e596f04cf217358259e745
ohadmeir <ohad1.meir@intel.com>
2023-09-19 10:19:19 +0300

    Don't run FW update on GitHub Actions

--- unit-tests/test-fw-update.py
+++ unit-tests/test-fw-update.py
@@ -3,6 +3,7 @@
 
 # we want this test to run first so that all tests run with updated FW versions, so we give it priority 0
 #test:priority 0
+#test:donotrun:gha
 #test:device each(D400*)
 
 import sys
