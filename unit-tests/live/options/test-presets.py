6d2d918ca415923efc4b99071bf31b0dd1f76ba4
ohadmeir <ohad1.meir@intel.com>
2023-10-09 16:48:46 +0300

    Do not run live presets test on GHA

--- unit-tests/live/options/test-presets.py
+++ unit-tests/live/options/test-presets.py
@@ -1,6 +1,7 @@
 # License: Apache 2.0. See LICENSE file in root directory.
 # Copyright(c) 2023 Intel Corporation. All Rights Reserved.
 
+# test:donotrun:gha
 
 import pyrealsense2 as rs
 from rspy import test
