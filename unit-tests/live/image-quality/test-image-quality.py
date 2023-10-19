2b5b504012c33a22928997b2f45cae318c4b359c
Avia Avraham <145359432+AviaAv@users.noreply.github.com>
2023-10-19 16:55:20 +0300

    Issues addressed, added debug mode and detail level options

--- unit-tests/live/image-quality/test-image-quality.py
+++ unit-tests/live/image-quality/test-image-quality.py
@@ -1,24 +1,31 @@
 # License: Apache 2.0. See LICENSE file in root directory.
-# Copyright(c) 2022 Intel Corporation. All Rights Reserved.
+# Copyright(c) 2023 Intel Corporation. All Rights Reserved.
 
 # test:device D400*
 
 import pyrealsense2 as rs
-from rspy.stopwatch import Stopwatch
 from rspy import test, log
-import time
-import sys
 import os
+import math
 
 # Start depth + color streams and go through the frame to make sure it is showing a depth image
 # Color stream is only used to display the way the camera is facing
 # Verify that the frame does indeed have variance - therefore it is showing a depth image
 
+# In debug mode, an image with the frames found will be displayed and saved
+# Make sure you have the required packages installed
+DEBUG_MODE = False
+
+# Defines how far in cm do pixels have to be, to be considered in a different distance
+# for example, 10 for 10cm, will define the range 100-109 cm as one (as 100)
+DETAIL_LEVEL = 10
+
 dev = test.find_first_device_or_exit()
 
 cfg = rs.config()
 cfg.enable_stream(rs.stream.depth, rs.format.z16, 30)
-cfg.enable_stream(rs.stream.color, rs.format.rgb8, 30)
+if DEBUG_MODE:
+    cfg.enable_stream(rs.stream.color, rs.format.bgr8, 30)
 
 
 def display_image(img):
@@ -34,11 +41,17 @@ def display_image(img):
         if k == 27:  # Esc key to stop
             cv2.destroyAllWindows()
             break
-        elif k == -1:  # normally -1 returned,so don't print it
+        elif k == -1:  # normally -1 returned
             pass
 
 
 def frames_to_image(depth, color, save, display):
+    """
+    This function gets depth and color frames, transforms them to an image (numpy array)
+    and then save and/or display
+
+    If color frame is given, it will also concatenate it with the depth frame before doing the given action
+    """
     import numpy as np
     import cv2
 
@@ -60,7 +73,7 @@ def frames_to_image(depth, color, save, display):
 
     if save:
         file_name = "output_stream.png"
-        print("Saved image in", os.getcwd() + "\\" + file_name)
+        log.i("Saved image in", os.getcwd() + "\\" + file_name)
         cv2.imwrite(file_name, img)
     if display:
         display_image(img)
@@ -83,6 +96,22 @@ def get_frames(config, laser_enabled):
     return depth, color
 
 
+def round_to_units(num):
+    """
+    Input: Distance of a certain point, in meters
+    Output: Distance according to the given detail level, in cm
+    """
+    in_cm = round(num, 2) * 100  # convert to cm
+    return math.floor(in_cm / DETAIL_LEVEL) * DETAIL_LEVEL  # rounds the distance according to the given unit
+
+
+def sort_dict(my_dict):
+    my_keys = list(my_dict.keys())
+    my_keys.sort()
+    sorted_dict = {i: my_dict[i] for i in my_keys}
+    return sorted_dict
+
+
 def get_distances(depth):
     MAX_METERS = 10  # max distance that can be detected, in meters
     dists = {}
@@ -92,23 +121,24 @@ def get_distances(depth):
             dist = depth.get_distance(x, y)
             if dist >= MAX_METERS:  # out of bounds, assuming it is a junk value
                 continue
-            # we only distinguish between two pixels if they are different more than 10cm apart
-            # we can get a more accurate measure if we round it less, say round(dist,2)*100 to get 1cm difference noted
-            dist = int(round(dist, 1) * 10)
+
+            dist = round_to_units(dist)  # round according to DETAIL_LEVEL
+
             if dists.get(dist) is not None:
                 dists[dist] += 1
             else:
                 dists[dist] = 1
             total += 1
-    # print(dists)
+
+    dists = sort_dict(dists)  # for debug convenience
+    log.d("Distances detected in frame are:", dists)
     return dists, total
 
 
 def is_depth_meaningful(config, laser_enabled=True, save_image=False, show_image=False):
     """
-    Checks if the camera is showing a frame with a meaningful depth
-    the higher the detail level is, the more it is sensitive to distance
-    ex: 1 for a difference of 1 meter, 100 for a diff of 1cm
+    Checks if the camera is showing a frame with a meaningful depth.
+    DETAIL_LEVEL is setting how close distances need to be, to be considered the same
 
     returns true if frame shows meaningful depth
     """
@@ -116,38 +146,45 @@ def is_depth_meaningful(config, laser_enabled=True, save_image=False, show_image
     depth, color = get_frames(config, laser_enabled)
 
     if not depth:
-        print("Error getting depth frame")
+        log.f("Error getting depth frame")
         return False
-    if not color:
-        print("Error getting color frame")
+    if DEBUG_MODE and not color:
+        log.d("Error getting color frame")
 
     dists, total = get_distances(depth)
 
     # save or display image (only possible through manual debugging)
     if save_image or show_image:
-        frames_to_image(depth, color,save_image,show_image)
+        frames_to_image(depth, color, save_image, show_image)
 
+    # Goes over the distances found, and checks if any distance is the same on more than 90% of the pixels
     meaningful_depth = True
     for key in dists:
         if dists[key] > total*0.9:
             meaningful_depth = False
             break
-    return meaningful_depth
+    num_blank_pixels = dists[0]
+    return meaningful_depth, num_blank_pixels
 
 
 ################################################################################################
+
 test.start("Testing depth frame - laser ON -", dev.get_info(rs.camera_info.name))
-# change file_name or show_image to save or display the image, accordingly
-# make sure to have the packages listed if changed
-res = is_depth_meaningful(cfg, True, False, False)
+res, laser_black_pixels = is_depth_meaningful(cfg, laser_enabled=True, save_image=DEBUG_MODE, show_image=DEBUG_MODE)
 test.check(res is True)
 test.finish()
 
 ################################################################################################
 
 test.start("Testing depth frame - laser OFF -", dev.get_info(rs.camera_info.name))
-res = is_depth_meaningful(cfg, False, False, False)
+res, no_laser_black_pixels = is_depth_meaningful(cfg, laser_enabled=False, save_image=DEBUG_MODE, show_image=DEBUG_MODE)
 test.check(res is True)
+test.finish()
+
+################################################################################################
+
+test.start("Testing less black pixels with the laser on")
+test.check(no_laser_black_pixels > laser_black_pixels)
 test.finish()
 
 ################################################################################################
