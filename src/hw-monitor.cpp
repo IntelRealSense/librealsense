ed9393a250db9774fb22f8e544b6b6b350e916cb
Eran <librealsense.eran@gmail.com>
2023-11-05 12:38:09 +0200

    move pack() to hw-monitor

--- src/hw-monitor.cpp
+++ src/hw-monitor.cpp
@@ -7,6 +7,12 @@
 #include <sstream>
 
 
+static inline uint32_t pack( uint8_t c0, uint8_t c1, uint8_t c2, uint8_t c3 )
+{
+    return ( c0 << 24 ) | ( c1 << 16 ) | ( c2 << 8 ) | c3;
+}
+
+
 namespace librealsense
 {
 
--- src/types.h
+++ src/types.h
@@ -262,10 +262,6 @@ namespace librealsense {
 
     inline bool operator == (const rs2_intrinsics & a, const rs2_intrinsics & b) { return std::memcmp(&a, &b, sizeof(a)) == 0; }
 
-    inline uint32_t pack(uint8_t c0, uint8_t c1, uint8_t c2, uint8_t c3)
-    {
-        return (c0 << 24) | (c1 << 16) | (c2 << 8) | c3;
-    }
 
     bool file_exists(const char* filename);
 
