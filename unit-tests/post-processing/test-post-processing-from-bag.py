51561387126a3bfb33d16fd1cddd7968e2a33687
noacoohen <noa.cohen@intel.com>
2023-11-26 18:55:13 +0200

    test makeover - use generic functions, save only bytearray of the frames data and its profile, do the alignment of the frameset inside process frame, remove locks, change function and variables names

--- unit-tests/post-processing/test-post-processing-from-bag.py
+++ unit-tests/post-processing/test-post-processing-from-bag.py
@@ -6,114 +6,64 @@
 
 import pyrealsense2 as rs
 from rspy import test, repo
-import tempfile, os.path
-import threading
+import os.path
 import time
 ################################################################################################
-
-frame_processor_lock = threading.Lock()
-frames_lock = threading.Lock()
-completion_event = threading.Event()
-all_pairs = True
-composite_frames = []
-sensor_to_frames = {}
-sensor_to_framesets = {}
-sensor_to_frame = {}
+pre_aligned_frames_map ={}
+frames_data_map = {}
+playback_status = None
+sensors = []
+align_from = rs.stream(rs.stream.depth)
+align = rs.align(rs.stream.color)
 
 
-class AlignRecordBlock:
-
-    def __init__(self, align_to, align_from):
-        self._align_from = rs.stream(align_from)
-        self._align = rs.align(align_to)
-
-    def record(self, sensor_name, frame, sctx):
-        ss = sctx.sw_sensors[sensor_name]
-        ss.on_video_frame(create_sw_frame(frame, sctx.sw_stream_profiles[sensor_name][self._align_from]))
-        ss.stop()
-        ss.close()
-
-    def process(self, frame):
-        fs = frame.as_frameset()
-        return self._align.process(fs).first_or_default(self._align_from)
+def playback_callback(status):
+    global playback_status
+    playback_status = status
 
 
-def create_sw_frame(video_frame, profile):
-    new_video_frame = rs.software_video_frame()
-    new_video_frame.data = bytearray(video_frame.get_data())
-    new_video_frame.bpp = video_frame.get_bytes_per_pixel()
-    new_video_frame.stride = video_frame.get_width() * video_frame.get_bytes_per_pixel()
-    new_video_frame.timestamp = video_frame.get_timestamp()
-    new_video_frame.domain = video_frame.get_frame_timestamp_domain()
-    new_video_frame.frame_number = video_frame.get_frame_number()
-    new_video_frame.profile = profile
-    return new_video_frame
-
-
-def validate_ppf_results(result_frame, reference_frame):
-    result_profile = result_frame.get_profile().as_video_stream_profile()
-    reference_profile = reference_frame.get_profile().as_video_stream_profile()
+def validate_ppf_results(result_frame_data, reference_frame_data):
+    result_bytearray_frame_data, result_profile = result_frame_data
+    reference_bytearray_frame_data, reference_profile = reference_frame_data
 
     test.check_equal(result_profile.width(), reference_profile.width())
     test.check_equal(result_profile.height(), reference_profile.height())
-
-    v1 = bytearray(result_frame.get_data())
-    v2 = bytearray(reference_frame.get_data())
-    test.check_equal_lists(v1,v2)
+    test.check_equal_lists(result_bytearray_frame_data,reference_bytearray_frame_data)
 
 
 def process_frame(frame, frame_source):
     sensor_name = rs.sensor.from_frame(frame).get_info(rs.camera_info.name)
-    with frame_processor_lock:
-        if sensor_name in sensor_to_frames:
-            sensor_to_frames[sensor_name].append(frame)
-        else:
-            sensor_to_frames[sensor_name] = [frame]
+    if sensor_name in pre_aligned_frames_map:
+        pre_aligned_frames_map[sensor_name].append(frame)
+    else:
+        pre_aligned_frames_map[sensor_name] = [frame]
 
-        if len(sensor_to_frames[sensor_name]) == 2:
-            frameset = frame_source.allocate_composite_frame(sensor_to_frames[sensor_name])
-            frameset.keep()
-            sensor_to_framesets[sensor_name] = frameset
-            all_pairs = True
-        else:
-            all_pairs = False
+    if len(pre_aligned_frames_map[sensor_name]) == 2:
+        frameset = frame_source.allocate_composite_frame(pre_aligned_frames_map[sensor_name])
+        fs_align = align.process(frameset.as_frameset()).first_or_default(align_from)
+        fs_align_data = bytearray(fs_align.get_data())
+        fs_align_profile = fs_align.get_profile().as_video_stream_profile()
+        frames_data_map[sensor_name] = (fs_align_data, fs_align_profile)
 
 
-def check_condition(sensors):
-    with frames_lock:
-        return len(sensor_to_frame) >= len(sensors)
+def load_ref_frames_to_map(frame):
+    if len(frames_data_map) < len(sensors):
+        sensor_name = rs.sensor.from_frame(frame).get_info(rs.camera_info.name)
+        frames_data_map[sensor_name] = (bytearray(frame.get_data()), frame.get_profile().as_video_stream_profile())
 
 
-def wait_for_condition(sensors):
-    while not check_condition(sensors):
-        time.sleep(0.0001)
-    completion_event.set()
+def get_frames(callback):
+    global frames_data_map
+    frame_data_list = []
 
-
-def callback_for_get_frames(frame, sensor_to_frame, sensors):
-    with frames_lock:
-        if len(sensor_to_frame) < len(sensors):
-            frame.keep()
-            sensor_name = rs.sensor.from_frame(frame).get_info(rs.camera_info.name)
-            sensor_to_frame[sensor_name] = frame
-
-
-def get_frames(sensors):
-    frames = []
     for s in sensors:
         s.open(s.get_stream_profiles())
 
     for s in sensors:
-        s.start(lambda frame: callback_for_get_frames(frame, sensor_to_frame, sensors))
+        s.start(callback)
 
-    timeout = 30
-    start_time = time.time()
-
-    condition_thread = threading.Thread(target=wait_for_condition(sensors))
-    condition_thread.start()
-    condition_thread.join(timeout)
-
-    test.check(time.time() - start_time < timeout)
+    while playback_status != rs.playback_status.stopped:
+        time.sleep(0.25)
 
     for s in sensors:
         s.stop()
@@ -121,77 +71,56 @@ def get_frames(sensors):
     for s in sensors:
         s.close()
 
-    for sf in sensor_to_frame:
-        frames.append(sensor_to_frame[sf])
+    for sf in frames_data_map:
+        frame_data_list.append(frames_data_map[sf])
 
-    return frames
-
-
-def get_composite_frames(sensors):
-    frame_processor = rs.processing_block(process_frame)
-    for sensor in sensors:
-        sensor.open(sensor.get_stream_profiles())
-
-    for sensor in sensors:
-        sensor.start(lambda frame: (frame.keep(), frame_processor.invoke(frame)))
-
-    while True:
-        with frame_processor_lock:
-            if len(sensor_to_framesets) >= len(sensors) and all_pairs is True:
-                break
-            time.sleep(0.0001)
-
-    for sf in sensor_to_framesets:
-        composite_frames.append(sensor_to_framesets[sf])
-
-    for sensor in sensors:
-        sensor.stop()
-
-    for sensor in sensors:
-        sensor.close()
-
-    return composite_frames
+    return frame_data_list
 
 
-def compare_processed_frames_vs_recorded_frames(record_block, file):
-    filename = os.path.join(repo.build, 'unit-tests', 'recordings', 'all_combinations_depth_color.bag')
-
+def playback_file(file, callback):
+    global playback_status
+    playback_status = None
+    filename = os.path.join(repo.build, 'unit-tests', 'recordings', file)
     ctx = rs.context()
+
     dev = ctx.load_device(filename)
     dev.set_real_time(False)
+    dev.set_status_changed_callback(playback_callback)
 
+    global sensors
     sensors = dev.query_sensors()
-    frames = get_composite_frames(sensors)
+    global frames_data_map
+    frames_data_map = {}
 
-    ref_dev = ctx.load_device(os.path.join(repo.build, 'unit-tests', 'recordings', file))
-    ref_dev.set_real_time(False)
+    frames_data_list = get_frames(callback)
 
-    ref_sensors = ref_dev.query_sensors()
-    ref_frames = get_frames(ref_sensors)
+    return frames_data_list
 
-    test.check_equal(len(ref_frames), len(frames))
 
-    for i in range(len(frames)):
-        fs_res = record_block.process(frames[i])
-        validate_ppf_results(fs_res, ref_frames[i])
+def compare_processed_frames_vs_recorded_frames(file):
+    # we need processing_block in order to have frame_source.
+    # frame_source is used to composite frames (by calling allocate_composite_frames function).
+    frame_processor = rs.processing_block(process_frame)
 
+    aligned_frames_data_list = playback_file('all_combinations_depth_color.bag', lambda frame: (frame_processor.invoke(frame)))
+    ref_frame_data_list = playback_file(file, lambda frame: load_ref_frames_to_map(frame))
+    test.check_equal(len(aligned_frames_data_list),len(ref_frame_data_list))
+
+    for i in range(len(aligned_frames_data_list)):
+        validate_ppf_results(aligned_frames_data_list[i], ref_frame_data_list[i])
 
 ################################################################################################
 with test.closure("Test align depth to color from recording"):
-    record_block = AlignRecordBlock(rs.stream.color, rs.stream.depth)
-    compare_processed_frames_vs_recorded_frames(record_block, "[aligned_2c]_all_combinations_depth_color.bag")
+    align_from = rs.stream(rs.stream.depth)
+    align = rs.align(rs.stream.color)
 
+    compare_processed_frames_vs_recorded_frames("[aligned_2c]_all_combinations_depth_color.bag")
 ################################################################################################
 with test.closure("Test align color to depth from recording"):
+    align_from = rs.stream(rs.stream.color)
+    align = rs.align(rs.stream.depth)
+    pre_aligned_frames_map = {}
 
-    composite_frames = []
-    sensor_to_frames = {}
-    sensor_to_framesets = {}
-    sensor_to_frame = {}
-    all_pairs = True
-
-    record_block = AlignRecordBlock(rs.stream.depth, rs.stream.color)
-    compare_processed_frames_vs_recorded_frames(record_block, "[aligned_2d]_all_combinations_depth_color.bag")
-
+    compare_processed_frames_vs_recorded_frames("[aligned_2d]_all_combinations_depth_color.bag")
 ################################################################################################
 test.print_results_and_exit()
