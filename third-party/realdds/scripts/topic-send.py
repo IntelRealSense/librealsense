09b2d5b4db0c3c20f8e2e53cccaa581e8ea23c87
Eran <librealsense.eran@gmail.com>
2023-11-27 10:22:37 +0200

    update scripts, tools

--- third-party/realdds/scripts/topic-send.py
+++ third-party/realdds/scripts/topic-send.py
@@ -21,7 +21,7 @@ def domain_arg(x):
     if t <= 0 or t > 232:
         raise ArgumentError( f'--domain should be [0-232]' )
     return t
-args.add_argument( '--domain', metavar='<0-232>', type=domain_arg, default=0, help='DDS domain to use (default=0)' )
+args.add_argument( '--domain', metavar='<0-232>', type=domain_arg, default=-1, help='DDS domain to use (default=0)' )
 args = args.parse_args()
 
 
@@ -44,7 +44,7 @@ dds.debug( args.debug )
 settings = {}
 
 participant = dds.participant()
-participant.init( args.domain, 'topic-send', settings )
+participant.init( dds.load_rs_settings( settings ), args.domain )
 
 message = args.message
 
