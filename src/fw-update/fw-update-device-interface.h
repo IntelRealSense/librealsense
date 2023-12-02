2e312a6b9014e59f9a6724d996d25bbc8bb1d8e9
Eran <librealsense.eran@gmail.com>
2023-12-02 09:10:38 +0200

    move extract_firmware_version_string() into ds::, d400-private

--- src/fw-update/fw-update-device-interface.h
+++ src/fw-update/fw-update-device-interface.h
@@ -14,20 +14,6 @@ namespace librealsense
     {
     public:
         virtual bool check_fw_compatibility(const std::vector<uint8_t>& image) const = 0;
-        static std::string extract_firmware_version_string(const std::vector<uint8_t>& fw_image)
-        {
-            auto version_offset = offsetof(platform::dfu_header, bcdDevice);
-            if (fw_image.size() < (version_offset + sizeof(size_t)))
-                throw std::runtime_error("Firmware binary image might be corrupted - size is only: " + std::to_string( fw_image.size() ));
-
-            auto version = fw_image.data() + version_offset;
-            uint8_t major = *(version + 3);
-            uint8_t minor = *(version + 2);
-            uint8_t patch = *(version + 1);
-            uint8_t build = *(version);
-
-            return std::to_string(major) + "." + std::to_string(minor) + "." + std::to_string(patch) + "." + std::to_string(build);
-        }
     };
 
     class updatable : public firmware_check_interface
