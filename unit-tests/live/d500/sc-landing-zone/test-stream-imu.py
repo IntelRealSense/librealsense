0024f72c7fb86db639afc83aee4aa71f1877605e
Avia Avraham <145359432+AviaAv@users.noreply.github.com>
2023-12-13 10:44:34 +0200

    comments fix

--- unit-tests/live/d500/sc-landing-zone/test-stream-imu.py
+++ unit-tests/live/d500/sc-landing-zone/test-stream-imu.py
@@ -2,7 +2,6 @@
 # Copyright(c) 2023 Intel Corporation. All Rights Reserved.
 
 # test:device D585S
-# test not running until HSD is solved: https://hsdes.intel.com/appstore/article/#/13010957807
 import pyrealsense2 as rs
 from rspy import test, log
 import time
@@ -23,7 +22,8 @@ def check_imu_streaming(stream, fps):
     pipe.stop()
 
 
-time.sleep(5)  # due to a WIP bug, we have to wait after enumeration for a few secs before Accel stream
+# due to a WIP bug, we have to wait after enumeration for a few secs before Accel stream, see RSDEV-1158/RSDEV-1227
+time.sleep(5)
 ################# Checking accel streaming with fps 100/200 ##################
 stream = rs.stream.accel
 
