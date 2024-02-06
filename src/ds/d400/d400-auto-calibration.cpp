fcc2d0516b30a9c36e45727aeb318d851b3f0350
Tamir <tamir.yurovskiy@intel.com>
2024-02-06 11:31:11 +0200

    Coverity -  Nesting level does not match indentation - 3413362

--- src/ds/d400/d400-auto-calibration.cpp
+++ src/ds/d400/d400-auto-calibration.cpp
@@ -457,9 +457,12 @@ namespace librealsense
                 if (progress_callback)
                 {
                     if (host_assistance != host_assistance_type::no_assistance)
-                        if (count < 20) progress_callback->on_update_progress(static_cast<float>(80 + count++));
+                    {
+                        if (count < 20)
+                            progress_callback->on_update_progress(static_cast<float>(80 + count++));
                         else
-                            progress_callback->on_update_progress(count++ * (2.f * static_cast<int>(speed))); //curently this number does not reflect the actual progress
+                            progress_callback->on_update_progress(count++ * (2.f * static_cast<int>(speed))); //currently this number does not reflect the actual progress
+                    }
                 }
             }, false);
             // Handle errors from firmware
