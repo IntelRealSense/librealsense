3a62d16c6c207c6f27d0e34ead54a308a4a416a7
Remi Bettan <remibettan@gmail.com>
2024-02-13 15:16:23 +0200

    enabling d457 in test

--- unit-tests/live/frames/test-pipeline-start-stop.py
+++ unit-tests/live/frames/test-pipeline-start-stop.py
@@ -1,8 +1,7 @@
 # License: Apache 2.0. See LICENSE file in root directory.
 # Copyright(c) 2023 Intel Corporation. All Rights Reserved.
 
-# Currently, we exclude D457 as it's failing
-# test:device each(D400*) !D457
+# test:device each(D400*)
 
 import pyrealsense2 as rs
 from rspy.stopwatch import Stopwatch
