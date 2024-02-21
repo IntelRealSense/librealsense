e5fbb08eacc1a7838ba249ab0d2395df49f3f208
Eran <librealsense.eran@gmail.com>
2024-02-21 19:33:12 +0200

    add option::get_value_type()

--- src/core/option-interface.h
+++ src/core/option-interface.h
@@ -1,10 +1,11 @@
 // License: Apache 2.0. See LICENSE file in root directory.
-// Copyright(c) 2023 Intel Corporation. All Rights Reserved.
+// Copyright(c) 2024 Intel Corporation. All Rights Reserved.
 #pragma once
 
 #include "extension.h"
-
 #include <src/basics.h>
+#include <librealsense2/h/rs_option.h>
+#include <rsutils/json-fwd.h>
 
 
 namespace librealsense {
@@ -23,6 +24,14 @@ class LRS_EXTENSION_API option : public recordable< option >
 public:
     virtual void set( float value ) = 0;
     virtual float query() const = 0;
+
+    // Return the value currently assigned to the option
+    // By default, this is the result of query()
+    virtual rsutils::json get_value() const noexcept;
+
+    // Return the type of the option value
+    virtual rs2_option_type get_value_type() const noexcept;
+
     virtual option_range get_range() const = 0;
     virtual bool is_enabled() const = 0;
     virtual bool is_read_only() const { return false; }
