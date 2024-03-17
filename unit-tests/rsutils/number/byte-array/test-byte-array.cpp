6917758aa6ff5d46880f7bb0ba21856066791a6d
SamerKhshiboun <samer.khshiboun@intel.com>
2024-03-17 21:12:27 +0200

    add byte manipulation functions

---
+++ unit-tests/rsutils/number/byte-array/test-byte-array.cpp
@@ -0,0 +1,21 @@
+// License: Apache 2.0. See LICENSE file in root directory.
+// Copyright(c) 2023 Intel Corporation. All Rights Reserved.
+
+//#cmake:dependencies rsutils
+
+#include <unit-tests/test.h>
+#include <rsutils/number/byte-manipulation.h>
+#include <ostream>
+
+using byte = uint8_t;
+using bytearray = std::vector< byte >;
+
+
+namespace {
+
+
+TEST_CASE( "test1" )
+{
+}
+
+}
