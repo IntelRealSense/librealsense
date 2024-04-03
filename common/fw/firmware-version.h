894a3bdfda64f970ebca3683108122ede76259e6
Eran <librealsense.eran@gmail.com>
2024-04-03 09:43:08 +0300

    bump FW to 5.16.0.0

--- common/fw/firmware-version.h
+++ common/fw/firmware-version.h
@@ -1,7 +1,7 @@
 /* License: Apache 2.0. See LICENSE file in root directory.
-   Copyright(c) 2017 Intel Corporation. All Rights Reserved. */
+   Copyright(c) 2017-24 Intel Corporation. All Rights Reserved. */
 
 #pragma once
 
-#define D4XX_RECOMMENDED_FIRMWARE_VERSION "5.15.1.0"
+#define D4XX_RECOMMENDED_FIRMWARE_VERSION "5.16.0.0"
 
