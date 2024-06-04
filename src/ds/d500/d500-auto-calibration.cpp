4242dfedb02b84b2338901cfb125d31d9e26fe78
Remi Bettan <remibettan@gmail.com>
2024-06-04 11:38:07 +0300

    implementing set_calibration_config method

--- src/ds/d500/d500-auto-calibration.cpp
+++ src/ds/d500/d500-auto-calibration.cpp
@@ -329,6 +329,29 @@ namespace librealsense
 
     void d500_auto_calibrated::set_calibration_config(const rs2_calibration_config& calib_config)
     {
+        // calculate CRC
+        uint32_t computed_crc32 = rsutils::number::calc_crc32(reinterpret_cast<const uint8_t*>(&calib_config), 
+            sizeof(rs2_calibration_config));
+
+        // prepare vector of data to be sent (header + sp)
+        rs2_calibration_config_with_header calib_config_with_header;
+        uint16_t version = ((uint16_t)0x01 << 8) | 0x01;  // major=0x01, minor=0x01 --> ver = major.minor
+        uint32_t calib_version = 0;  // ignoring this field, as requested by sw architect
+        calib_config_with_header.header = { version, static_cast<uint16_t>(ds::d500_calibration_table_id::calib_cfg_id),
+            sizeof(rs2_calibration_config), calib_version, computed_crc32 };
+        calib_config_with_header.payload = calib_config;
+        auto data_as_ptr = reinterpret_cast<const uint8_t*>(&calib_config_with_header);
+
+        // prepare command
+        command cmd(ds::SET_HKR_CONFIG_TABLE,
+            static_cast<int>(ds::d500_calib_location::d500_calib_flash_memory),
+            static_cast<int>(ds::d500_calibration_table_id::calib_cfg_id),
+            static_cast<int>(ds::d500_calib_type::d500_calib_dynamic));
+        cmd.data.insert(cmd.data.end(), data_as_ptr, data_as_ptr + sizeof(rs2_calibration_config_with_header));
+        cmd.require_response = false;
+
+        // send command 
+        _hw_monitor->send(cmd);
     }
 
     void d500_auto_calibrated::set_hw_monitor_for_auto_calib(std::shared_ptr<hw_monitor> hwm)
