0dd5280c136c4c2be65a1053cc0e9f089ab87299
ohadmeir <ohad1.meir@intel.com>
2024-06-20 15:23:01 +0300

    Remove D421 from bundled FW checks

--- common/device-model.cpp
+++ common/device-model.cpp
@@ -266,7 +266,8 @@ namespace rs2
 
             auto dev_name = get_device_name(dev);
 
-            if( dev.is<update_device>() || is_upgradeable( fw, recommended_fw_ver) )
+            // TODO - D421. Don't suggest to update FW as it doesn't support D421. Revert after 5.17 release with supporting FW
+            if( ( dev.is<update_device>() || is_upgradeable( fw, recommended_fw_ver) ) && pid != "0B5D" ) // 0x0B5D is D421 PID
             {
                 std::stringstream msg;
 
