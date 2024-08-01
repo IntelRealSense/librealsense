03d1666fa6333a03a132305f9a00d667dce37657
Remi Bettan <remibettan@gmail.com>
2024-08-01 14:03:45 +0300

    typo

--- src/ds/d500/d500-private.h
+++ src/ds/d500/d500-private.h
@@ -27,32 +27,32 @@ namespace librealsense
 
         // d500 Devices supported by the current version
         static const std::set<std::uint16_t> rs500_sku_pid = {
-            D555_PID
+            D555_PID,
             ds::D585_PID,
             ds::D585S_PID
         };
 
         static const std::set<std::uint16_t> d500_multi_sensors_pid = {
-            D555_PID
+            D555_PID,
             ds::D585_PID,
             ds::D585S_PID
         };
 
         static const std::set<std::uint16_t> d500_hid_sensors_pid = {
-            D555_PID
+            D555_PID,
             ds::D585_PID,
             ds::D585S_PID
         };
 
         static const std::set<std::uint16_t> d500_hid_bmi_085_pid = {
-            D555_PID
+            D555_PID,
             ds::D585_PID,
             ds::D585S_PID
         };
 
         static const std::map< std::uint16_t, std::string > rs500_sku_names = {
             { ds::D555_PID,          "Intel RealSense D555" },
-            { ds::D555_RECOVERY_PID, "Intel RealSense D555 Recovery" }
+            { ds::D555_RECOVERY_PID, "Intel RealSense D555 Recovery" },
             { ds::D585_PID,             "Intel RealSense D585" },
             { ds::D585S_PID,            "Intel RealSense D585S" },
             { ds::D585S_RECOVERY_PID,   "Intel RealSense D585S Recovery"}
