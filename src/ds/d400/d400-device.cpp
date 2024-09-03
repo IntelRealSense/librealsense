ec5c7fcb9f30a78f77e7c0e05924e55bbd4ab7fc
Remi Bettan <remibettan@gmail.com>
2024-09-03 16:49:46 +0300

    metadata field used for API RS2_FRAME_METADATA_SENSOR_TIMESTAMP for mipi updated as in usb

--- src/ds/d400/d400-device.cpp
+++ src/ds/d400/d400-device.cpp
@@ -1083,14 +1083,10 @@ namespace librealsense
         auto md_prop_offset = offsetof(metadata_mipi_depth_raw, depth_mode);
 
         depth_sensor.register_metadata(RS2_FRAME_METADATA_SENSOR_TIMESTAMP,
-                                       make_attribute_parser(&md_mipi_depth_mode::hw_timestamp,
-                                                             md_mipi_depth_control_attributes::hw_timestamp_attribute,
-                                                             md_prop_offset));
-
-        depth_sensor.register_metadata(RS2_FRAME_METADATA_SENSOR_TIMESTAMP,
-                                       make_attribute_parser(&md_mipi_depth_mode::optical_timestamp,
-                                                             md_mipi_depth_control_attributes::optical_timestamp_attribute,
-                                                             md_prop_offset));
+                                       make_rs400_sensor_ts_parser(make_uvc_header_parser(&platform::uvc_header::timestamp),
+                                                                   make_attribute_parser(&md_mipi_depth_mode::optical_timestamp,
+                                                                                         md_mipi_depth_control_attributes::optical_timestamp_attribute,
+                                                                                         md_prop_offset)));
 
         depth_sensor.register_metadata(RS2_FRAME_METADATA_ACTUAL_EXPOSURE,
                                        make_attribute_parser(&md_mipi_depth_mode::exposure_time,
