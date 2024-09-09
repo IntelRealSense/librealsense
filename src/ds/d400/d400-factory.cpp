701d7a7ac412bc6f1b1f1b1fe483f6e67afe89a0
noacoohen <noa.cohen@intel.com>
2024-09-09 16:44:51 +0300

    fix PR comments

--- src/ds/d400/d400-factory.cpp
+++ src/ds/d400/d400-factory.cpp
@@ -654,25 +654,26 @@ namespace librealsense
         {
             d400_device::hardware_reset();
             //limitation: the user must hold the context from which the device was created
+            //creating fake notification to trigger invoke_devices_changed_callbacks, causing connection and
             auto dev_info = this->get_device_info();
             auto non_const_device_info = std::const_pointer_cast< librealsense::device_info >( dev_info );
             std::vector< std::shared_ptr< device_info > > devices{ non_const_device_info };
-            auto ctx =  std::weak_ptr<context>(get_context());
-            //creating fake notification to trigger invoke_devices_changed_callbacks, causing connection and disconnection of mipi device
+            auto ctx = std::weak_ptr< context >( get_context() );
             std::thread fake_notification(
-                [  ctx , devices ]() {
+                [ctx, devs = std::move( devices )]()
+                {
                     try
                     {
                         if( auto strong = ctx.lock() )
                         {
-                            strong->invoke_devices_changed_callbacks( devices, {} );
+                            strong->invoke_devices_changed_callbacks( devs, {} );
                             // MIPI devices do not re-enumerate so we need to give them some time to restart
                             std::this_thread::sleep_for( std::chrono::milliseconds( 3000 ) );
                         }
                         if( auto strong = ctx.lock() )
-                            strong->invoke_devices_changed_callbacks( {}, devices );
+                            strong->invoke_devices_changed_callbacks( {}, devs );
                     }
-                    catch( const std::exception & e ) 
+                    catch( const std::exception & e )
                     {
                         std::cerr << e.what() << std::endl;
                         return;
