436572061765fd5b2d110b3b184cb993f5dbbb5c
Joanne Nguyen <jnnguyen@iastate.edu>
2024-10-18 11:09:35 -0500

    added Exported or NotExported flags to receivers for Android 12+

--- wrappers/android/librealsense/src/main/java/com/intel/realsense/librealsense/Enumerator.java
+++ wrappers/android/librealsense/src/main/java/com/intel/realsense/librealsense/Enumerator.java
@@ -5,6 +5,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.hardware.usb.UsbManager;
+import android.os.Build;
 import android.os.Handler;
 import android.os.HandlerThread;
 import android.os.Looper;
@@ -64,9 +65,26 @@ class Enumerator {
         mListener = listener;
         mContext = context;
 
-        context.registerReceiver(mBroadcastReceiver, new IntentFilter(UsbUtilities.ACTION_USB_PERMISSION));
-        context.registerReceiver(mBroadcastReceiver, new IntentFilter(UsbManager.ACTION_USB_DEVICE_ATTACHED));
-        context.registerReceiver(mBroadcastReceiver, new IntentFilter(UsbManager.ACTION_USB_DEVICE_DETACHED));
+        // Extra flag to counteract the extra requirements to pass an exported or not-exported value
+        // when broadcasting for Android 12 and up.
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+            context.registerReceiver(mBroadcastReceiver,
+                    new IntentFilter(UsbUtilities.ACTION_USB_PERMISSION),
+                    Context.RECEIVER_EXPORTED);
+
+            context.registerReceiver(mBroadcastReceiver,
+                    new IntentFilter(UsbManager.ACTION_USB_DEVICE_ATTACHED),
+                    Context.RECEIVER_EXPORTED);
+
+            context.registerReceiver(mBroadcastReceiver,
+                    new IntentFilter(UsbManager.ACTION_USB_DEVICE_DETACHED),
+                    Context.RECEIVER_EXPORTED);
+        } else {
+            // For pre-Android 12 versions, use the standard registerReceiver without the export flag
+            context.registerReceiver(mBroadcastReceiver, new IntentFilter(UsbUtilities.ACTION_USB_PERMISSION));
+            context.registerReceiver(mBroadcastReceiver, new IntentFilter(UsbManager.ACTION_USB_DEVICE_ATTACHED));
+            context.registerReceiver(mBroadcastReceiver, new IntentFilter(UsbManager.ACTION_USB_DEVICE_DETACHED));
+        }
 
         onDeviceAttach(context);
     }
