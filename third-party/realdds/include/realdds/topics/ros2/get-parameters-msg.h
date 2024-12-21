d64edd523b2c00975cbb2ce6010eed74725bfb39
Eran <librealsense.eran@gmail.com>
2024-12-21 22:27:24 +0200

    added realdds -msg code for paramerers

---
+++ third-party/realdds/include/realdds/topics/ros2/get-parameters-msg.h
@@ -0,0 +1,114 @@
+// License: Apache 2.0. See LICENSE file in root directory.
+// Copyright(c) 2024 Intel Corporation. All Rights Reserved.
+#pragma once
+
+#include <realdds/dds-defines.h>
+
+#include <realdds/topics/ros2/GetParameters.h>
+#include <realdds/topics/ros2/ParameterValue.h>
+
+#include <string>
+#include <memory>
+#include <vector>
+
+
+namespace rcl_interfaces {
+namespace srv {
+class GetParameters_RequestPubSubType;
+class GetParameters_ResponsePubSubType;
+}  // namespace srv
+}  // namespace rcl_interfaces
+
+
+namespace realdds {
+
+
+class dds_participant;
+class dds_topic;
+class dds_topic_reader;
+class dds_topic_writer;
+
+
+namespace topics {
+namespace ros2 {
+
+
+class get_parameters_request_msg
+{
+    rcl_interfaces::srv::GetParameters_Request _raw;
+
+public:
+    using type = rcl_interfaces::srv::GetParameters_RequestPubSubType;
+
+    void clear() { _raw.names().clear(); }
+    std::vector< std::string > const & names() const { return _raw.names(); }
+    void add( std::string const & name ) { _raw.names().push_back( name ); }
+
+    bool is_valid() const { return ! names().empty(); }
+    void invalidate() { clear(); }
+
+
+    static std::shared_ptr< dds_topic > create_topic( std::shared_ptr< dds_participant > const & participant,
+                                                      char const * topic_name );
+    static std::shared_ptr< dds_topic > create_topic( std::shared_ptr< dds_participant > const & participant,
+                                                      std::string const & topic_name )
+    {
+        return create_topic( participant, topic_name.c_str() );
+    }
+
+    // This helper method will take the next sample from a reader. 
+    // 
+    // Returns true if successful. Make sure you still check is_valid() in case the sample info isn't!
+    // Returns false if no more data is available.
+    // Will throw if an unexpected error occurs.
+    //
+    //Note - copies the data.
+    //TODO - add an API for a function that loans the data and enables the user to free it later.
+    static bool take_next( dds_topic_reader &,
+                           get_parameters_request_msg * output,
+                           dds_sample * optional_sample = nullptr );
+};
+
+
+class get_parameters_response_msg
+{
+    rcl_interfaces::srv::GetParameters_Response _raw;
+
+public:
+    using type = rcl_interfaces::srv::GetParameters_ResponsePubSubType;
+
+    using value_type = rcl_interfaces::msg::ParameterValue;
+
+    std::vector< value_type > const & values() const { return _raw.values(); }
+    void add( value_type const & value ) { _raw.values().push_back( value ); }
+    bool is_valid() const { return ! values().empty(); }
+    void invalidate() { _raw.values().clear(); }
+
+    // Returns some unique (to the writer) identifier for the sample that was sent, or 0 if unsuccessful
+    dds_sequence_number write_to( dds_topic_writer & );
+
+    static std::shared_ptr< dds_topic > create_topic( std::shared_ptr< dds_participant > const & participant,
+                                                      char const * topic_name );
+    static std::shared_ptr< dds_topic > create_topic( std::shared_ptr< dds_participant > const & participant,
+                                                      std::string const & topic_name )
+    {
+        return create_topic( participant, topic_name.c_str() );
+    }
+
+    // This helper method will take the next sample from a reader. 
+    // 
+    // Returns true if successful. Make sure you still check is_valid() in case the sample info isn't!
+    // Returns false if no more data is available.
+    // Will throw if an unexpected error occurs.
+    //
+    //Note - copies the data.
+    //TODO - add an API for a function that loans the data and enables the user to free it later.
+    static bool take_next( dds_topic_reader &,
+                           get_parameters_response_msg * output,
+                           dds_sample * optional_sample = nullptr );
+};
+
+
+}  // namespace ros2
+}  // namespace topics
+}  // namespace realdds
---
+++ third-party/realdds/src/topics/ros2-describe-parameters-response-msg.cpp
@@ -0,0 +1,78 @@
+// License: Apache 2.0. See LICENSE file in root directory.
+// Copyright(c) 2024 Intel Corporation. All Rights Reserved.
+
+#include <realdds/topics/ros2/describe-parameters-msg.h>
+#include <realdds/topics/ros2/DescribeParameters.h>
+#include <realdds/topics/ros2/DescribeParametersPubSubTypes.h>
+
+#include <realdds/dds-topic.h>
+#include <realdds/dds-topic-reader.h>
+#include <realdds/dds-topic-writer.h>
+#include <realdds/dds-utilities.h>
+
+#include <fastdds/dds/subscriber/DataReader.hpp>
+#include <fastdds/dds/publisher/DataWriter.hpp>
+#include <fastdds/dds/topic/Topic.hpp>
+
+
+namespace realdds {
+namespace topics {
+namespace ros2 {
+
+
+/*static*/ std::shared_ptr< dds_topic >
+describe_parameters_response_msg::create_topic( std::shared_ptr< dds_participant > const & participant, char const * topic_name )
+{
+    return std::make_shared< dds_topic >( participant,
+                                          eprosima::fastdds::dds::TypeSupport( new describe_parameters_response_msg::type ),
+                                          topic_name );
+}
+
+
+/*static*/ bool
+describe_parameters_response_msg::take_next( dds_topic_reader & reader, describe_parameters_response_msg * output, dds_sample * sample )
+{
+    describe_parameters_response_msg output_;
+    if( ! output )
+        output = &output_;  // use the local copy if the user hasn't provided their own
+    dds_sample sample_;
+    if( ! sample )
+        sample = &sample_;  // use the local copy if the user hasn't provided their own
+    auto status = reader->take_next_sample( &output->_raw, sample );
+    if( status == ReturnCode_t::RETCODE_OK )
+    {
+        // Only samples for which valid_data is true should be accessed
+        // valid_data indicates that the instance is still ALIVE and the `take` return an
+        // updated sample
+        if( ! sample->valid_data )
+            output->invalidate();
+
+        return true;
+    }
+    if ( status == ReturnCode_t::RETCODE_NO_DATA )
+    {
+        // This is an expected return code and is not an error
+        return false;
+    }
+    DDS_API_CALL_THROW( "describe_parameters_response_msg::take_next", status );
+}
+
+
+dds_sequence_number describe_parameters_response_msg::write_to( dds_topic_writer & writer )
+{
+    eprosima::fastrtps::rtps::WriteParams params;
+    bool success = DDS_API_CALL( writer.get()->write( &_raw, params ) );
+    if( ! success )
+    {
+        LOG_ERROR( "Error writing message" );
+        return 0;
+    }
+    // The params will contain, after the write, the sequence number (incremented automatically) for the sample that was
+    // sent. The source_timestamp is always INVALID for some reason.
+    return params.sample_identity().sequence_number().to64long();
+}
+
+
+}  // namespace ros2
+}  // namespace topics
+}  // namespace realdds
