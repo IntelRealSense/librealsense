2e8b9ad9035c5c9074395cccbc8873ac6a2742f8
Arun-Prasad-V <arunprasadv777@gmail.com>
2024-12-31 11:11:37 +0530

    apply WA only for D457

--- src/ds/d400/d400-device.cpp
+++ src/ds/d400/d400-device.cpp
@@ -569,7 +569,7 @@ namespace librealsense
         }
         set_hw_monitor_for_auto_calib(_hw_monitor);
 
-        _ds_device_common = std::make_shared<ds_device_common>(this, _hw_monitor);
+        _ds_device_common = std::make_shared<ds_device_common>(this, _hw_monitor, (_pid == ds::RS457_PID)? true : false);
 
         // Define Left-to-Right extrinsics calculation (lazy)
         // Reference CS - Right-handed; positive [X,Y,Z] point to [Left,Up,Forward] accordingly.
