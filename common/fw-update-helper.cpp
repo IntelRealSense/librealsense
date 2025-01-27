bd0ca26cc2a017e70204976ab6548a4b8e31164c
gwen2018 <guoping.wen@intel.com>
2025-01-27 14:13:59 -0800

    fix fw update in viewer

--- common/fw-update-helper.cpp
+++ common/fw-update-helper.cpp
@@ -191,7 +191,8 @@ namespace rs2
         // if device is D457, and fw is signed - using mipi specific procedure
         if (_is_signed
                 && (!strcmp(_dev.get_info(RS2_CAMERA_INFO_PRODUCT_ID), "ABCD")
-                 || !strcmp(_dev.get_info(RS2_CAMERA_INFO_PRODUCT_ID), "BBCD"))
+                 || !strcmp(_dev.get_info(RS2_CAMERA_INFO_PRODUCT_ID), "BBCD")
+                 || !strcmp(_dev.get_info(RS2_CAMERA_INFO_PRODUCT_ID), "ABCE"))
                 )
         {
             process_mipi();
