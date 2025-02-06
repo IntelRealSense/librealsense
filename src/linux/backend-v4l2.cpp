96f5e6734d57131dd16982792184ba47ca2bc033
gwen2018 <guoping.wen@intel.com>
2025-02-05 20:12:30 -0800

    improve depth node detection

--- src/linux/backend-v4l2.cpp
+++ src/linux/backend-v4l2.cpp
@@ -833,6 +833,30 @@ namespace librealsense
             return false;
         }
 
+        bool v4l_uvc_device::is_device_depth_node(const std::string& dev_name)
+        {
+            bool is_depth = false;
+
+            // first search video node links, for example, video-rs-depth-0
+            std::smatch match;
+            static std::regex video_dev_rs("video-rs-");
+            static std::regex video_dev_depth("video-rs-depth-\\d+$");
+            if(std::regex_search(dev_name, match, video_dev_rs))
+            {
+                if (std::regex_search(dev_name, match, video_dev_depth))
+                    is_depth = true;
+                else
+                    is_depth = false;
+            }
+            // then search video nodes to find the depth node
+            else if (is_format_supported_on_node(dev_name, "Z16 "))
+                is_depth = true;
+            else
+                is_depth = false;
+
+            return is_depth;
+        }
+
         uint16_t v4l_uvc_device::get_mipi_device_pid(const std::string& dev_name)
         {
             // GVD product ID
@@ -842,7 +866,7 @@ namespace librealsense
             const uint8_t GVD_PID_D457      = 0x12;
 
             // device PID
-	    uint16_t device_pid = 0;
+            uint16_t device_pid = 0;
 
             int fd = open(dev_name.c_str(), O_RDWR);
             if (fd < 0)
@@ -887,7 +911,7 @@ namespace librealsense
             }
 
             ::close(fd);
-	    
+
             return device_pid;
         }
 
@@ -937,14 +961,14 @@ namespace librealsense
             get_mipi_device_info(dev_name, bus_info, card);
 
             // find device PID from depth video node
-	    static uint16_t device_pid = 0;
-	    try
+            static uint16_t device_pid = 0;
+            try
             {
-                if (is_format_supported_on_node(dev_name, "Z16 "))
+                if (is_device_depth_node(dev_name))
                 {
                     device_pid = get_mipi_device_pid(dev_name);
                 }
-	    }
+            }
             catch(const std::exception & e)
             {
                 LOG_WARNING("MIPI device product id detection issue, device will be skipped: " << e.what());
@@ -968,7 +992,6 @@ namespace librealsense
                 LOG_WARNING("Unresolved Video4Linux device pattern: " << name << ", device is skipped");
                 throw linux_backend_exception("Unresolved Video4Linux device, device is skipped");
             }
-
 
             //  D457 exposes (assuming first_video_index = 0):
             // - video0 for Depth and video1 for Depth's md.
