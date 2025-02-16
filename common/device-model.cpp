5eb4be9e6aeeac2f7bb7664776cf5cfb9d981672
Remi Bettan <remibettan@gmail.com>
2025-02-16 09:24:12 +0200

    comment added

--- common/device-model.cpp
+++ common/device-model.cpp
@@ -3110,6 +3110,7 @@ namespace rs2
                         "If >0.25 we recommend applying the new calibration.\n"
                         "\"White wall\" mode should only be used when pointing at a flat white wall with projector on");
 
+                // Focal Length Calibration is disabled for D421, since this process has not been optimized for this device
                 std::string pid = dev.get_info(RS2_CAMERA_INFO_PRODUCT_ID);
                 if (pid != "1155" && ImGui::Selectable("Focal Length Calibration"))
                 {
