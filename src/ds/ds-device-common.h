e5fea7d35c7d2ed31ecfe3f8838e4ccd4cb4f695
Nir Azkiel <nir.azkiel@intel.com>
2025-03-27 15:21:39 +0200

    rename is_gmsl to is_mipi

--- src/ds/ds-device-common.h
+++ src/ds/ds-device-common.h
@@ -27,10 +27,10 @@ namespace librealsense
     class ds_device_common
     {
     public:
-        ds_device_common(device* ds_device, std::shared_ptr<hw_monitor> hwm, bool is_gmsl = false) :
+        ds_device_common(device* ds_device, std::shared_ptr<hw_monitor> hwm, bool is_mipi = false) :
             _owner(ds_device),
             _hw_monitor(hwm),
-            _is_gmsl(is_gmsl),
+            _is_mipi(is_mipi),
             _is_locked(true)
         {}
 
@@ -48,7 +48,7 @@ namespace librealsense
         device* _owner;
         std::shared_ptr<hw_monitor> _hw_monitor;
         bool _is_locked;
-        bool _is_gmsl;
+        bool _is_mipi;
     };
 
     
