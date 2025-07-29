88ae9858753dad14c6f4aa58535c9fe9946efda6
Avia Avraham <145359432+AviaAv@users.noreply.github.com>
2025-07-29 12:20:06 +0300

    increase tolerance, specify color resolution VGA
    added sleep, enable global ts if needed

--- unit-tests/live/metadata/test-timestamps.py
+++ unit-tests/live/metadata/test-timestamps.py
@@ -1,26 +1,38 @@
 # License: Apache 2.0. See LICENSE file in root directory.
-# Copyright(c) 2025 Intel Corporation. All Rights Reserved.
+# Copyright(c) 2025 RealSense, Inc. All Rights Reserved.
 
 # test:device each(D400*)
 
 import pyrealsense2 as rs
 from rspy import test, log
+import time
 
-# This test is checking that timestamps of depth and infrared frames are consistent
+# This test is checking that timestamps of depth, infrared and color frames are consistent
 
-# Tolerance for gap between depth and color
+# Tolerance for gaps between frames
 GLOBAL_TS_TOLERANCE = 1  # in ms
-FRAME_TS_TOLERANCE = 20  # in microseconds
+FRAME_TS_TOLERANCE = 1000  # in microseconds - 1 ms
 with test.closure("Test Timestamps Consistency"):
     device, ctx = test.find_first_device_or_exit()
     cfg = rs.config()
     cfg.enable_stream(rs.stream.depth)
     cfg.enable_stream(rs.stream.infrared, 1)
     cfg.enable_stream(rs.stream.infrared, 2)
-    cfg.enable_stream(rs.stream.color)
+    cfg.enable_stream(rs.stream.color, 640, 480, rs.format.bgr8, 30)  # setting VGA since it fail with HD resolution
+
+    depth_sensor = device.first_depth_sensor()
+    color_sensor = device.first_color_sensor()
+
+    for sensor in [depth_sensor, color_sensor]:  # Enable global timestamp in case it is disabled
+        if sensor.supports(rs.option.global_time_enabled):
+            if not sensor.get_option(rs.option.global_time_enabled):
+                sensor.set_option(rs.option.global_time_enabled, 1)
+        else:
+            log.f(f"Sensor {sensor.name} does not support global time option")
 
     pipe = rs.pipeline(ctx)
     pipe.start(cfg)
+    time.sleep(2)
 
     try:
         for _ in range(50):
@@ -30,7 +42,7 @@ with test.closure("Test Timestamps Consistency"):
             ir2_frame = frames.get_infrared_frame(2)
             color_frame = frames.get_color_frame()
 
-            if not (depth_frame and ir1_frame and ir2_frame):
+            if not (depth_frame and ir1_frame and ir2_frame and color_frame):
                 log.e("One or more frames are missing")
                 continue
 
@@ -44,8 +56,8 @@ with test.closure("Test Timestamps Consistency"):
             log.d(f"Color Global TS: {color_ts}")
 
             # Check global timestamps
-            test.check(depth_ts == ir1_ts)
-            test.check(depth_ts == ir2_ts)
+            test.check_approx_abs(depth_ts, ir1_ts, GLOBAL_TS_TOLERANCE)
+            test.check_approx_abs(depth_ts, ir2_ts, GLOBAL_TS_TOLERANCE)
             test.check_approx_abs(depth_ts, color_ts, GLOBAL_TS_TOLERANCE)
 
             # Frame metadata timestamps
@@ -62,8 +74,8 @@ with test.closure("Test Timestamps Consistency"):
                 log.d(f"Color Frame TS: {color_frame_ts}")
 
                 # Check frame timestamps
-                test.check(depth_frame_ts == ir1_frame_ts)
-                test.check(depth_frame_ts == ir2_frame_ts)
+                test.check_approx_abs(depth_frame_ts, ir1_frame_ts, FRAME_TS_TOLERANCE)
+                test.check_approx_abs(depth_frame_ts, ir2_frame_ts, FRAME_TS_TOLERANCE)
                 test.check_approx_abs(depth_frame_ts, color_frame_ts, FRAME_TS_TOLERANCE)
             else:
                 log.d("One or more frames do not support frame timestamp metadata")
